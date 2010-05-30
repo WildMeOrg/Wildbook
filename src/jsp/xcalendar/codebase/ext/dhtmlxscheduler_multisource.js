5b19176db096947166008523069d49ca156b7f99
holmbergius <holmbergius@gmail.com>
2010-05-30 13:41:51 -0400

    Adding for the new calendar system

---
+++ src/jsp/xcalendar/codebase/ext/dhtmlxscheduler_multisource.js
@@ -0,0 +1,1 @@
+(function(){function B(D){var C=function(){};C.prototype=D;return C}var A=scheduler._load;scheduler._load=function(C,F){C=C||this._load_url;if(typeof C=="object"){var E=B(this._loaded);for(var D=0;D<C.length;D++){this._loaded=new E();A.call(this,C[D],F)}}else{A.apply(this,arguments)}}})();
