943147f4ed5c2b1551b78d322e1fff18bdb0fab0
holmbergius <holmbergius@gmail.com>
2011-09-26 18:05:21 -0400

    moving commonConfiguration out of the bundles/en folder

--- src/main/java/org/ecocean/CommonConfiguration.java
+++ src/main/java/org/ecocean/CommonConfiguration.java
@@ -33,7 +33,7 @@ public class CommonConfiguration {
     //set up the file input stream
     if (props.size() == 0) {
       try {
-        props.load(CommonConfiguration.class.getResourceAsStream("/bundles/en/commonConfiguration.properties"));
+        props.load(CommonConfiguration.class.getResourceAsStream("/bundles/commonConfiguration.properties"));
       } catch (IOException ioe) {
         ioe.printStackTrace();
       }
@@ -43,7 +43,7 @@ public class CommonConfiguration {
   public static boolean refresh() {
     try {
       props = null;
-      props.load(CommonConfiguration.class.getResourceAsStream("/bundles/en/commonConfiguration.properties"));
+      props.load(CommonConfiguration.class.getResourceAsStream("/bundles/commonConfiguration.properties"));
     } catch (IOException ioe) {
       ioe.printStackTrace();
       return false;
