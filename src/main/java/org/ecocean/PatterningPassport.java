dc7dc14fa92b192ebbe66b2d0073eba650802ca8
webmaster <webmaster@whaleshark.org>
2012-07-03 01:28:46 -0400

    Adding some stack tracing

--- src/main/java/org/ecocean/PatterningPassport.java
+++ src/main/java/org/ecocean/PatterningPassport.java
@@ -13,7 +13,7 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
- 
+
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -27,24 +27,24 @@ import org.xml.sax.InputSource;
 
 public class PatterningPassport implements java.io.Serializable {
   private static final long serialVersionUID = -5200145396524958700L;
-  
+
   // Serialized variables
   private long timestampOfLastUpdate; // timestamp of the last update
   private String encounterUrl; // encounter URL
   private String mediaUrl; // media URL
   private String patterningPassportXmlUrl; // patterningPassport URL
-  
+
   // Local vars
   private String encounterId; // encounter ID
   private String mediaId; // media ID
   private File webappsDir; // webapps directory
   private String passportDataXml; // the custom XML for the patterning passport
- 
+
   static String NODE_NAME_ID = "idData";
-  
+
   public PatterningPassport () {
   }
-  
+
   /*
   public PatterningPassport (String passportDataXml, String encounterUrl, String mediaUrl, long timestampOfLastUpdate, String patterningPassportXmlUrl) {
     this.passportDataXml = passportDataXml;
@@ -55,13 +55,13 @@ public class PatterningPassport implements java.io.Serializable {
     this.patterningPassportXmlUrl = patterningPassportXmlUrl;
   }
   */
-  
+
   private void updateTimeStamp () {
     Date date= new java.util.Date();
     Timestamp ts = new Timestamp(date.getTime());
     this.timestampOfLastUpdate = ts.getTime();
   }
-  
+
   private Boolean makeNewPassportXml () {
     // -- FROM: http://www.mkyong.com/java/how-to-create-xml-file-in-java-dom/
     try {
@@ -76,8 +76,8 @@ public class PatterningPassport implements java.io.Serializable {
       // File locations
       File shepherdDataDir = new File(webappsDir, CommonConfiguration.getDataDirectoryName());
 
-      // Universal passport fields for the XML 
-      String encounterUrlString = "foo/" + this.encounterId; // TODO -- generate URL 
+      // Universal passport fields for the XML
+      String encounterUrlString = "foo/" + this.encounterId; // TODO -- generate URL
       String mediaSourceUrlString = "foo/" + this.mediaId; // TODO -- generate URL
       String passportXmlUrlString = "foo"; // TODO -- generate URL
       String encounterDir = this.webappsDir + "/shepherd_data_dir/encounters/" + this.encounterId;
@@ -85,28 +85,28 @@ public class PatterningPassport implements java.io.Serializable {
 
       Element metaElement = doc.createElement(NODE_NAME_ID);
       rootElement.appendChild(metaElement);
-      
+
       // . encounter
       Element encounterId = doc.createElement("encounterId");
       encounterId.appendChild(doc.createTextNode(this.encounterId));
       metaElement.appendChild(encounterId);
-      
+
       Element encounterUrl = doc.createElement("encounterUrl");
-      encounterUrl.appendChild(doc.createTextNode(encounterUrlString)); 
+      encounterUrl.appendChild(doc.createTextNode(encounterUrlString));
       metaElement.appendChild(encounterUrl);
 
       // . media source (photo)
       Element mediaId = doc.createElement("mediaId");
       mediaId.appendChild(doc.createTextNode(this.mediaId));
       metaElement.appendChild(mediaId);
-      
+
       Element mediaSourceUrl = doc.createElement("mediaSourceUrl");
-      mediaSourceUrl.appendChild(doc.createTextNode(mediaSourceUrlString)); 
+      mediaSourceUrl.appendChild(doc.createTextNode(mediaSourceUrlString));
       metaElement.appendChild(mediaSourceUrl);
 
       // . track back to this XML data's file
       Element passportXmlUrl = doc.createElement("passportXmlUrl");
-      passportXmlUrl.appendChild(doc.createTextNode(passportXmlUrlString)); 
+      passportXmlUrl.appendChild(doc.createTextNode(passportXmlUrlString));
       metaElement.appendChild(passportXmlUrl);
 
       // . passport data
@@ -115,14 +115,14 @@ public class PatterningPassport implements java.io.Serializable {
         try {
           Document passportDataDoc = docBuilder.parse( new InputSource(new StringReader(this.passportDataXml)));
           passportElement = passportDataDoc.getDocumentElement();
-          
-          // scrape out the "meta" section 
+
+          // scrape out the "meta" section
           if (passportElement.hasChildNodes() == Boolean.TRUE) {
             for(Node childNode = passportElement.getFirstChild();
                 childNode != null;){
               Node nextChild = childNode.getNextSibling();
               // do comparison/etc.
-              if (nextChild.getNodeName() == NODE_NAME_ID) {
+              if ((nextChild!=null)&&(nextChild.getNodeName()!=null)&&(nextChild.getNodeName() == NODE_NAME_ID)) {
                 passportElement.removeChild(nextChild); // remove this node if it is ID data
               }
 
@@ -130,22 +130,23 @@ public class PatterningPassport implements java.io.Serializable {
               childNode = nextChild;
             }
           }
-          
+
           Element imported = (Element)doc.importNode(passportElement, Boolean.TRUE); // need to import to get node from different doc
           doc.getDocumentElement().appendChild(imported);
         } catch (Exception e) {
-          System.out.println("-----\nFAILURE! PatterningPassport data not saved. Exception: " + e.getLocalizedMessage());
+			e.printStackTrace();
+          System.out.println("-----\nFAILURE! PatterningPassport data not saved.");
           return Boolean.FALSE;
         }
       }
-      
+
       // write the content into xml file
       TransformerFactory transformerFactory = TransformerFactory.newInstance();
       Transformer transformer = transformerFactory.newTransformer();
       DOMSource source = new DOMSource(doc);
       StreamResult result = new StreamResult(new File(xmlFilePathString));
       transformer.transform(source, result);
-      
+
       System.out.println("-----\nSUCCESS! PatterningPassport saved at " + xmlFilePathString);
       return Boolean.TRUE;
     } catch (ParserConfigurationException pce) {
@@ -155,14 +156,14 @@ public class PatterningPassport implements java.io.Serializable {
     }
     return Boolean.FALSE;
   }
-  
+
   /**
    * @return the timestampOfLastUpdate
    */
   public long getTimestampOfLastUpdate() {
     return timestampOfLastUpdate;
   }
- 
+
   /**
    * @return the passportDataXml
    */
@@ -259,5 +260,5 @@ public class PatterningPassport implements java.io.Serializable {
   public void setMediaId(String mediaId) {
     this.mediaId = mediaId;
   }
-  
+
 }
