a1a366325a00402b1979ba46bb010284ac0a7f95
webmaster <webmaster@whaleshark.org>
2012-08-01 02:13:17 -0400

    more UI refinements

--- src/main/java/org/ecocean/servlet/EncounterAddMantaPattern.java
+++ src/main/java/org/ecocean/servlet/EncounterAddMantaPattern.java
@@ -76,7 +76,10 @@ public class EncounterAddMantaPattern extends HttpServlet {
     
     StringBuffer resultComment=new StringBuffer();
     
+    String dataCollectionEventID="";
+    
     if((request.getParameter("action")!=null)&&(request.getParameter("action").equals("imageremove"))){action="imageremove";}
+    else if((request.getParameter("action")!=null)&&(request.getParameter("action").equals("rescan"))){action="rescan";}
 
     try {
       if(action.equals("imageremove")){
@@ -87,11 +90,11 @@ public class EncounterAddMantaPattern extends HttpServlet {
         try {
 
           Encounter enc=myShepherd.getEncounter(encounterNumber);
-          List<SinglePhotoVideo> myphots=enc.getSinglePhotoVideo();
-          int myPhotsSize=myphots.size();
+          //List<SinglePhotoVideo> myphots=enc.getSinglePhotoVideo();
+          //int myPhotsSize=myphots.size();
           File thisEncounterDir = new File(encountersDir, request.getParameter("number"));
-          for(int t=0;t<myPhotsSize;t++){
-            SinglePhotoVideo spv=myphots.get(t);
+          //for(int t=0;t<myPhotsSize;t++){
+            SinglePhotoVideo spv=myShepherd.getSinglePhotoVideo(request.getParameter("dataCollectionEventID"));
             String spvName=spv.getFilename().replaceAll(".jpg", "_CR.jpg").replaceAll(".JPG","_CR.JPG");
 
             File spvCRFile=new File(thisEncounterDir,spvName);
@@ -99,7 +102,7 @@ public class EncounterAddMantaPattern extends HttpServlet {
               //test comment
               File jpegVersion = new File(thisEncounterDir, spvName);
               File enhancedVersion = new File(thisEncounterDir, spvName.replaceAll("_CR", "_EH"));
-              File matchFile= new File(thisEncounterDir, "matchOutput.xhtml");
+              File matchFile= new File(thisEncounterDir, (spv.getDataCollectionEventID()+"_matchOutput.xhtml"));
               File FEATFile = new File(thisEncounterDir, spv.getFilename().replaceAll(".jpg", ".FEAT").replaceAll(".JPG", ".FEAT").replaceAll(".jpeg", ".FEAT").replaceAll(".JPEG", ".FEAT"));
               File FTFile = new File(thisEncounterDir, spvName.replaceAll("_CR", "_FT"));
            if (jpegVersion.exists() && (!jpegVersion.getName().equals(spv.getFilename()))) {
@@ -120,20 +123,48 @@ public class EncounterAddMantaPattern extends HttpServlet {
               jpegVersion=null;
               enhancedVersion=null;
             }
-          }
+          //}
           
-          
-          
-          
-          
-          
+        } 
+        catch (SecurityException thisE) {
+          thisE.printStackTrace();
+          System.out.println("Error attempting to delete the old JPEG version of a submitted manta data image!!!!");
+          removedJPEG = false;
+          resultComment.append("I hit a security error trying to delete the old feature image. Please check your file system permissions.");
+        }
+      }
+      //test comment
+      else if(action.equals("rescan")){
+        //eliminate the previous JPG version of this file if it existed                         //eliminate the previous JPG if it existed
+        encounterNumber = request.getParameter("number");
+        
+        
+        try {
+
+          Encounter enc=myShepherd.getEncounter(encounterNumber);
+          List<SinglePhotoVideo> myphots=enc.getSinglePhotoVideo();
+          int myPhotsSize=myphots.size();
+          File thisEncounterDir = new File(encountersDir, request.getParameter("number"));
+ 
+            SinglePhotoVideo spv=myShepherd.getSinglePhotoVideo(request.getParameter("dataCollectionEventID"));
+            dataCollectionEventID=spv.getDataCollectionEventID();
+
+
+              //test comment
+              File jpegVersion = new File(thisEncounterDir, spv.getFilename());
 
 
           
+          ProcessBuilder pb2 = new ProcessBuilder("/usr/bin/mmatch", encountersDir.getAbsolutePath(), jpegVersion.getAbsolutePath(),"0","0","2","1","-o",(spv.getDataCollectionEventID()+"_matchOutput.xhtml"),"-s",(encountersDir.getAbsolutePath()+"/"),"http://www.mantamatcher.org/shepherd_data_dir/encounters/","http://www.mantamatcher.org/encounters/encounter.jsp?number=");
+          pb2.directory(thisEncounterDir);
           
           
+          resultComment.append(("<br />/usr/bin/mmatch "+ encountersDir.getAbsolutePath()+" "+ jpegVersion.getAbsolutePath()+" 0"+" 0"+" 2"+" 1"+" -o"+(" "+spv.getDataCollectionEventID()+"_matchOutput.xhtml")+" -s "+(encountersDir.getAbsolutePath()+"/")+" http://www.mantamatcher.org/shepherd_data_dir/encounters/"+" http://www.mantamatcher.org/encounters/encounter.jsp?number=")+"<br /><br />");
+          System.out.println(("/usr/bin/mmatch "+ encountersDir.getAbsolutePath()+" "+ jpegVersion.getAbsolutePath()+" 0"+" 0"+" 2"+" 1"+" -o"+(" "+spv.getDataCollectionEventID()+"_matchOutput.xhtml")+" -s "+(encountersDir.getAbsolutePath()+"/")+" http://www.mantamatcher.org/shepherd_data_dir/encounters/"+" http://www.mantamatcher.org/encounters/encounter.jsp?number="));
           
-
+          String ls_str2;
+           //Process ls_proc2 = Runtime.getRuntime().exec(execString2); 
+          pb2.start();
         } 
         catch (SecurityException thisE) {
           thisE.printStackTrace();
@@ -158,7 +189,7 @@ public class EncounterAddMantaPattern extends HttpServlet {
             //determine which variable to assign the param to
             if (name.equals("number")) {
               encounterNumber = value;
-              System.out.println("Setting encounterNumber to: "+encounterNumber);
+              //System.out.println("Setting encounterNumber to: "+encounterNumber);
             } 
             
             //let's detect our filename
@@ -168,6 +199,7 @@ public class EncounterAddMantaPattern extends HttpServlet {
               //Encounter enc=myShepherd.getEncounter(encounterNumber);
               
               SinglePhotoVideo spv=myShepherd.getSinglePhotoVideo(value);
+              dataCollectionEventID=spv.getDataCollectionEventID();
               fileName=spv.getFilename().replaceAll(".jpg", "_CR.jpg").replaceAll(".JPG", "_CR.JPG").replaceAll(".jpeg", "_CR.jpeg").replaceAll(".JPEG", "_CR.JPEG");
             }
 
@@ -216,15 +248,7 @@ public class EncounterAddMantaPattern extends HttpServlet {
               
 
               
-              //OK, if the file has uploaded successfully, then it's time to get a process from the 
-              //command line to execute it.
-              //String[] execArray=new String[5];
-              //execArray[0]="/usr/bin/mmprocess";
-              //execArray[1]=write2me.getAbsolutePath().replaceAll("_CR", "");
-              //execArray[2]="4";
-              //execArray[3]="1";
-              //execArray[4]="2";
-              
+
               ProcessBuilder pb = new ProcessBuilder("/usr/bin/mmprocess", write2me.getAbsolutePath().replaceAll("_CR", ""), "4","1","2");
                   
               System.out.println("I am trying to execute the command:<br/>"+"/usr/bin/mmprocess "+ write2me.getAbsolutePath().replaceAll("_CR", "")+ " 4"+" 1"+" 2"+"<br />");
@@ -264,12 +288,12 @@ public class EncounterAddMantaPattern extends HttpServlet {
                 
                 //String execString2="/usr/bin/mmatch "+encountersDir.getAbsolutePath()+" "+winQuote(write2me.getAbsolutePath().replaceAll("_CR", ""))+" 0 0 1 0 -o "+thisEncounterDir.getAbsolutePath()+"/matchOutput.xhtml";
                 
-                ProcessBuilder pb2 = new ProcessBuilder("/usr/bin/mmatch", encountersDir.getAbsolutePath(), write2me.getAbsolutePath().replaceAll("_CR", ""),"0","0","1","0","-o","matchOutput.xhtml");
+                ProcessBuilder pb2 = new ProcessBuilder("/usr/bin/mmatch", encountersDir.getAbsolutePath(), write2me.getAbsolutePath().replaceAll("_CR", ""),"0","0","2","1","-o",(dataCollectionEventID+"_matchOutput.xhtml"),"-s",(encountersDir.getAbsolutePath()+"/"),"http://www.mantamatcher.org/shepherd_data_dir/encounters/","http://www.mantamatcher.org/encounters/encounter.jsp?number=");
                 pb2.directory(thisEncounterDir);
                 
                 
-                resultComment.append("<br /><br />I am trying to execute the matching command:<br/>"+pb2.toString()+"<br />");
-                System.out.println("<br /><br />I am trying to execute the matching command:<br/>"+"/usr/bin/mmatch "+ encountersDir.getAbsolutePath()+" "+write2me.getAbsolutePath().replaceAll("_CR", "")+" 0"+" 0"+" 1"+" 0"+" -o" + " matchOutput.xhtml"+"<br />");
+                resultComment.append(("<br />/usr/bin/mmatch "+ encountersDir.getAbsolutePath()+" "+ write2me.getAbsolutePath().replaceAll("_CR", "")+" 0"+" 0"+" 2"+" 1"+" -o"+" "+(dataCollectionEventID+"_"+"matchOutput.xhtml")+" -s "+(encountersDir.getAbsolutePath()+"/")+" http://www.mantamatcher.org/shepherd_data_dir/encounters/"+" http://www.mantamatcher.org/encounters/encounter.jsp?number=")+"<br /><br />");
+                System.out.println(("/usr/bin/mmatch "+ encountersDir.getAbsolutePath()+" "+ write2me.getAbsolutePath().replaceAll("_CR", "")+" 0"+" 0"+" 2"+" 1"+" -o"+(dataCollectionEventID+"_"+"matchOutput.xhtml")+" -s "+(encountersDir.getAbsolutePath()+"/")+" http://www.mantamatcher.org/shepherd_data_dir/encounters/"+" http://www.mantamatcher.org/encounters/encounter.jsp?number="));
                 
                 String ls_str2;
                  //Process ls_proc2 = Runtime.getRuntime().exec(execString2); 
@@ -331,11 +355,18 @@ public class EncounterAddMantaPattern extends HttpServlet {
           
           if(action.equals("imageadd")){
             
-            String resultsURL = ("/" + CommonConfiguration.getDataDirectoryName() + "/encounters/"+encounterNumber+"/matchOutput.xhtml");
+            String resultsURL = ("/" + CommonConfiguration.getDataDirectoryName() + "/encounters/"+encounterNumber+"/"+dataCollectionEventID+"_matchOutput.xhtml");
             response.sendRedirect(resultsURL);
             
             //out.println("<strong>Confirmed:</strong> I have successfully uploaded your mantamatcher data image file.");
             
+          }
+          else if(action.equals("rescan")){
+            
+            String resultsURL = ("/" + CommonConfiguration.getDataDirectoryName() + "/encounters/"+encounterNumber+"/"+dataCollectionEventID+"_matchOutput.xhtml");
+            response.sendRedirect(resultsURL);
+            
+
           }
           else {
             out.println(ServletUtilities.getHeader(request));
--- src/main/webapp/encounters/mantaMatcherAlgorithm.jsp
+++ src/main/webapp/encounters/mantaMatcherAlgorithm.jsp
@@ -23,117 +23,110 @@
 Shepherd myShepherd = new Shepherd();
 try {
 
-//get the encounter number
-String encNum = request.getParameter("encounterNumber");
+	//get the encounter number
+	String encNum = request.getParameter("encounterNumber");
 	
-//set up the JDO pieces and Shepherd
+	//set up the JDO pieces and Shepherd
 
-myShepherd.beginDBTransaction();
-Encounter enc=myShepherd.getEncounter(encNum);
-boolean hasPhotos=false;
-if((enc.getSinglePhotoVideo()!=null)&&(enc.getSinglePhotoVideo().size()>0)){hasPhotos=true;}
+	myShepherd.beginDBTransaction();
+	Encounter enc=myShepherd.getEncounter(encNum);
+	boolean hasPhotos=false;
+	if((enc.getSinglePhotoVideo()!=null)&&(enc.getSinglePhotoVideo().size()>0)){hasPhotos=true;}
 
-//let's set up references to our file system components
-String rootWebappPath = getServletContext().getRealPath("/");
-File webappsDir = new File(rootWebappPath).getParentFile();
-File shepherdDataDir = new File(webappsDir, CommonConfiguration.getDataDirectoryName());
-File encountersDir=new File(shepherdDataDir.getAbsolutePath()+"/encounters");
-File thisEncounterDir = new File(encountersDir, encNum);
+	//let's set up references to our file system components
+	String rootWebappPath = getServletContext().getRealPath("/");
+	File webappsDir = new File(rootWebappPath).getParentFile();
+	File shepherdDataDir = new File(webappsDir, CommonConfiguration.getDataDirectoryName());
+	File encountersDir=new File(shepherdDataDir.getAbsolutePath()+"/encounters");
+	File thisEncounterDir = new File(encountersDir, encNum);
 
-
-
-%>
-<p><strong>Matching Algorithm (under development)</strong></p>
-<br />
-<p style="background-color:#f0f0f0;"><em>Patterning images</em></p>
-<%
-//test comment
-if(hasPhotos){
-	File matchOutput=new File(thisEncounterDir, "matchOutput.xhtml");
-	File processedImage=new File("/foo/bar");
-	File enhancedImage=new File("/foo/bar");
-	File FTFile=new File("/foo/bar");
-
-	
-	
-	boolean hasProcessedImage=false;
-	String matchingImageName="";
-	List<SinglePhotoVideo> myphots=enc.getSinglePhotoVideo();
-	int myPhotsSize=myphots.size();
-	for(int t=0;t<myPhotsSize;t++){
-		SinglePhotoVideo spv=myphots.get(t);
-		String spvName=spv.getFilename().replaceAll(".jpg", "_CR.jpg").replaceAll(".JPG","_CR.JPG");
-
-		File spvCRFile=new File(thisEncounterDir,spvName);
-		if(spvCRFile.exists()){
-			hasProcessedImage=true;
-			matchingImageName=spvCRFile.getName();
-			processedImage=spvCRFile;
-			enhancedImage=new File(thisEncounterDir,spvCRFile.getName().replaceAll("_CR", "_EH"));
-			FTFile=new File(thisEncounterDir,spvCRFile.getName().replaceAll("_CR", "_FT"));
-		}
-	}
-	
-	if(!hasProcessedImage){
-
-%>
-<p>No candidate region image was found.</p>
-<%
-}
-else{
-%>
-<p>A candidate region image was found.<br />
-<img width="300px" height="*" src="/<%=shepherdDataDir.getName() %>/encounters/<%=encNum %>/<%=matchingImageName%>"/></p>
-
-<%	
-	if(!enhancedImage.exists()){
 	%>
-		<p>No enhanced image was found.</p>
-	<%
-	}
-	else{
-		%>
-		<p>An enhanced image was found.<br />
-			<img width="300px" height="*" src="/<%=shepherdDataDir.getName() %>/encounters/<%=encNum %>/<%=enhancedImage.getName()%>"/></p>
+	<p><strong>Matching Algorithm</strong></p>
+
+<%
+	//test comment
+	if(hasPhotos){
 		
-		<%
-		if(!FTFile.exists()){
-		%>
-			<p>No feature file was found.</p>	
-		<%
-		}
-		else{
-			%>
-			<p>An extracted feature image was found.<br />
-			<img width="300px" height="*" src="/<%=shepherdDataDir.getName() %>/encounters/<%=encNum %>/<%=FTFile.getName()%>"/></p>
-		
-			<%
-			if((request.isUserInRole("admin"))||(request.isUserInRole("imageProcessor"))){
-				if(!matchOutput.exists()){
-					%>
-					<p>No match results file was found.</p>
-					<%
-				}
-				else{
-					%>
-					<br />
-					<p style="background-color:#f0f0f0;"><em>Inspect the algorithm results</em></p>
-					<p>A match results file was found: <a href="/<%=shepherdDataDir.getName() %>/encounters/<%=encNum %>/<%=matchOutput.getName()%>">Click here.</a></p>
+		File processedImage=new File("/foo/bar");
+		File enhancedImage=new File("/foo/bar");
+		File FTFile=new File("/foo/bar");
+	
+		boolean hasProcessedImage=false;
+		String matchingImageName="";
+		List<SinglePhotoVideo> myphots=enc.getSinglePhotoVideo();
+		int myPhotsSize=myphots.size();
+		for(int t=0;t<myPhotsSize;t++){
+			SinglePhotoVideo spv=myphots.get(t);
+			String spvName=spv.getFilename().replaceAll(".jpg", "_CR.jpg").replaceAll(".JPG","_CR.JPG");
+			File matchOutput=new File(thisEncounterDir, (spv.getDataCollectionEventID()+"_matchOutput.xhtml"));
+			File spvCRFile=new File(thisEncounterDir,spvName);
+			if(spvCRFile.exists()){
+				hasProcessedImage=true;
+				matchingImageName=spvCRFile.getName();
+				processedImage=spvCRFile;
+				enhancedImage=new File(thisEncounterDir,spvCRFile.getName().replaceAll("_CR", "_EH"));
+				FTFile=new File(thisEncounterDir,spvCRFile.getName().replaceAll("_CR", "_FT"));
+				if(FTFile.exists() && ((request.isUserInRole("admin"))||(request.isUserInRole("imageProcessor")))){
+					
+				%>
+				
 			
+					<p style="background-color:#f0f0f0;"><em>Extracted Feature Image for Image <%=(t+1) %>.</em></p>
+						<p><img width="300px" height="*" src="/<%=shepherdDataDir.getName() %>/encounters/<%=encNum %>/<%=FTFile.getName()%>"/></p>
+							<p><em>Remove the processed, cropped manta patterning image.</em></p>
+		               		<p>
+		               			<form action="../EncounterAddMantaPattern" method="post" name="EncounterAddMantaPattern">
+		               				<input name="action" type="hidden" value="imageremove" id="action" />
+		            				<input name="number" type="hidden" value="<%=encNum%>" id="number" />
+									<input name="dataCollectionEventID" type="hidden" value="<%=spv.getDataCollectionEventID() %>" id="dataCollectionEventID" />
+									
+		            				<p><input name="addtlFile" type="submit" id="addtlFile" value="Remove the file" /></p>
+		        				</form>
+		        		 	</p>
 					<%
-				}
+					//if(){
+						if(!matchOutput.exists()){
+						%>
+							<p>No match results file was found.</p>
+						<%
+						}
+						else{
+						%>
+							<p><em>Inspect the algorithm results</em></p>
+							<p>A match results file was found: <a href="/<%=shepherdDataDir.getName() %>/encounters/<%=encNum %>/<%=matchOutput.getName()%>">Click here.</a></p>
+							
+							<p><em>Rescan the manta patterning image.</em></p>
+		               		<p>
+		               			<form action="../EncounterAddMantaPattern" method="post" name="EncounterAddMantaPattern">
+		               				<input name="action" type="hidden" value="rescan" id="action" />
+		            				<input name="number" type="hidden" value="<%=encNum%>" id="number" />
+									<input name="dataCollectionEventID" type="hidden" value="<%=spv.getDataCollectionEventID() %>" id="dataCollectionEventID" />
+									
+		            				<p><input name="addtlFile" type="submit" id="addtlFile" value="Rescan" /></p>
+		        				</form>
+		        		 	</p>   
+
+		    <%
+						
+						}
+					//}
+				}//if ftfile exists
+				} 
+						
+
+
 			}
-		}
+
+			
 		
+	
+	
 
-	}
-
-}
 if((request.isUserInRole("admin"))||(request.isUserInRole("imageProcessor"))){
 %>
 
 <br />
-<p style="background-color:#f0f0f0;"><em>Upload or replace the processed, cropped manta patterning image.</em></p>
+<p style="background-color:#f0f0f0;"><em>Upload or replace a processed, cropped manta patterning image.</em></p>
       <p><form action="../EncounterAddMantaPattern" method="post"
             enctype="multipart/form-data" name="EncounterAddMantaPattern"><input
         name="action" type="hidden" value="imageadd" id="action" />
@@ -149,11 +142,12 @@ if((request.isUserInRole("admin"))||(request.isUserInRole("imageProcessor"))){
         	<input name="photoNumber" type="hidden" value="<%=enc.getImages().get(0).getDataCollectionEventID()%>" id="photoNumber" />
         	<%
         }
+
         else{
         	//in this case, we need to ask the user to tell us which photo is the source image
         %>
         
-        <p><select name="photoNumber">
+        <p>Image to upload processed image for: <select name="photoNumber">
           <%
             for (int rmi = 0; rmi < enc.getSinglePhotoVideo().size(); rmi++) {
           %>
@@ -172,23 +166,8 @@ if((request.isUserInRole("admin"))||(request.isUserInRole("imageProcessor"))){
                   value="Upload" /></p>
      </form></p>
      <%
-     if(processedImage.exists()){
-     %>
-     <p><em>Remove the processed, cropped manta patterning image.</em></p>
-           <p>
-           <form action="../EncounterAddMantaPattern" method="post"
-            name="EncounterAddMantaPattern"><input
-        name="action" type="hidden" value="imageremove" id="action" />
-        <input name="number" type="hidden" value="<%=encNum%>" id="number" />
-
-        <p><input name="addtlFile" type="submit" id="addtlFile"
-                  value="Remove the file" /></p>
-     </form></p>
-<%
-     }
-}
-
-} //end if
+	}
+} 
 else{
 	%>
 	<p>No photos found for encounter.</p>
