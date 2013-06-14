9c364c3ef8fdde9fd80cdc66b7adfb6ffb681563
gwinstanley <stan@marinemegafauna.org>
2013-06-14 19:15:44 +0100

    Added utility class for determining MantaMatcher algorithm accessory files.

---
+++ src/main/java/org/ecocean/util/MantaMatcherUtilities.java
@@ -0,0 +1,112 @@
+/*
+ * The Shepherd Project - A Mark-Recapture Framework
+ * Copyright (C) 2011 Jason Holmberg
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.ecocean.util;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.ecocean.SinglePhotoVideo;
+
+/**
+ *
+ * @author Giles Winstanley
+ */
+public class MantaMatcherUtilities {
+  /** SLF4J logger instance for writing log entries. */
+//  private static final Logger log = LoggerFactory.getLogger(MantaMatcherUtilities.class);
+
+  private MantaMatcherUtilities() {}
+
+  /**
+   * Converts the specified file to a map of files, keyed by string for easy
+   * referencing, comprising six elements in order:
+   * <ul>
+   * <li>Original file (key: O).</li>
+   * <li>File representing MantaMatcher &quot;candidate region&quot; photo (key: CR).</li>
+   * <li>File representing MantaMatcher enhanced photo (key: EH).</li>
+   * <li>File representing MantaMatcher feature photo (key: FT).</li>
+   * <li>File representing MantaMatcher feature file (key: FEAT).</li>
+   * <li>File representing MantaMatcher output XHTML file (key: XHTML).</li>
+   * </ul>
+   * All files are assumed to be in the same folder, and no checking is
+   * performed to see if they exist.
+   * @param spv {@code SinglePhotoVideo} instance denoting base reference image
+   */
+  public static Map<String, File> getMatcherFilesMap(SinglePhotoVideo spv) {
+    if (spv == null)
+      throw new NullPointerException("Invalid file specified: null");
+    Map<String, File> map = getMatcherFilesMap(spv.getFile());
+    map.put("XHTML", new File(spv.getFile().getParentFile(), spv.getDataCollectionEventID() + "_matchOutput.xhtml"));
+    return map;
+  }
+
+  /**
+   * Converts the specified file to a map of files, keyed by string for easy
+   * referencing, comprising five elements in order:
+   * <ul>
+   * <li>Original file (key: O).</li>
+   * <li>File representing MantaMatcher &quot;candidate region&quot; photo (key: CR).</li>
+   * <li>File representing MantaMatcher enhanced photo (key: EH).</li>
+   * <li>File representing MantaMatcher feature photo (key: FT).</li>
+   * <li>File representing MantaMatcher feature file (key: FEAT).</li>
+   * </ul>
+   * All files are assumed to be in the same folder, and no checking is
+   * performed to see if they exist.
+   * The functionality is centralized here to reduce naming errors/conflicts.
+   * @param f base image file from which to reference other algorithm files
+   */
+  public static Map<String, File> getMatcherFilesMap(File f) {
+    if (f == null)
+      throw new NullPointerException("Invalid file specified: null");
+    String name = f.getName();
+    String regFormat = MediaUtilities.REGEX_SUFFIX_FOR_WEB_IMAGES;
+    if (!name.matches("^.+\\." + regFormat))
+      throw new IllegalArgumentException("Invalid file type specified");
+    String regex = "\\." + regFormat;
+    File pf = f.getParentFile();
+    File cr = new File(pf, name.replaceFirst(regex, "_CR.$1"));
+    File eh = new File(pf, name.replaceFirst(regex, "_EH.$1"));
+    File ft = new File(pf, name.replaceFirst(regex, "_FT.$1"));
+    File feat = new File(pf, name.replaceFirst(regex, ".FEAT"));
+    
+    Map<String, File> map = new HashMap<String, File>(6);
+    map.put("O", f);
+    map.put("CR", cr);
+    map.put("EH", eh);
+    map.put("FT", ft);
+    map.put("FEAT", feat);
+    return map;
+  }
+
+  /**
+   * Checks whether the MantaMatcher algorithm files exist for the specified
+   * base file (does not check for XHTML results file).
+   * @param f base image file from which to reference other algorithm files
+   */
+  public static boolean checkMatcherFilesExist(File f) {
+    Map<String, File> mmFiles = getMatcherFilesMap(f);
+    return mmFiles.get("O").exists() &&
+            mmFiles.get("CR").exists() &&
+            mmFiles.get("EH").exists() &&
+            mmFiles.get("FT").exists() &&
+            mmFiles.get("FEAT").exists();
+  }
+}
