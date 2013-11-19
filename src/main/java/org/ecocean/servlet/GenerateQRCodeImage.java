ad1f0392ea6537ccb947bbcceeff3e0e403a427d
webmaster <webmaster@whaleshark.org>
2013-11-19 01:33:36 -0500

    Add QR code generation

---
+++ src/main/java/org/ecocean/servlet/GenerateQRCodeImage.java
@@ -0,0 +1,49 @@
+package org.ecocean.servlet;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+ 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import net.glxn.qrgen.QRCode;
+import net.glxn.qrgen.image.ImageType;
+ 
+public class GenerateQRCodeImage extends HttpServlet {
+    
+  public void init(ServletConfig config) throws ServletException {
+    super.init(config);
+  }
+
+
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    doPost(request, response);
+  }
+  
+  public void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+ 
+        String myURL = "http://www.whaleshark.org/individuals.jsp?number="+request.getParameter("number");
+ 
+        ByteArrayOutputStream out = QRCode.from(myURL).withSize(250, 250).to(
+                ImageType.PNG).stream();
+         
+        response.setContentType("image/png");
+        response.setContentLength(out.size());
+         
+        OutputStream outStream = response.getOutputStream();
+ 
+        outStream.write(out.toByteArray());
+ 
+        outStream.flush();
+        outStream.close();
+        outStream=null;
+    }
+}
