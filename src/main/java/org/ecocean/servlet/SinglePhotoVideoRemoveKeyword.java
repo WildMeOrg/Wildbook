010089b90119600a908d58d2dde2c0b6dff70c98
RichardJune <richjune@aol.com>
2014-09-03 17:29:39 -0400

    FIX: Remove "See all sharks text"

--- src/main/java/org/ecocean/servlet/SinglePhotoVideoRemoveKeyword.java
+++ src/main/java/org/ecocean/servlet/SinglePhotoVideoRemoveKeyword.java
@@ -98,7 +98,7 @@ public class SinglePhotoVideoRemoveKeyword extends HttpServlet {
               out.println("<p><a href=\"encounters/searchResults.jsp?state="+stateName+"\">View all "+stateName+" encounters</a></font></p>");   
             }
           }
-          out.println("<p><a href=\"individualSearchResults.jsp\">View all sharks</a></font></p>");
+          out.println("<p><a href=\"individualSearchResults.jsp\">View all individuals</a></font></p>");
 
           out.println(ServletUtilities.getFooter(context));
         }
@@ -117,7 +117,7 @@ public class SinglePhotoVideoRemoveKeyword extends HttpServlet {
             out.println("<p><a href=\"encounters/searchResults.jsp?state="+stateName+"\">View all "+stateName+" encounters</a></font></p>");   
           }
         }
-        out.println("<p><a href=\"individualSearchResults.jsp\">View all sharks</a></font></p>");
+        out.println("<p><a href=\"individualSearchResults.jsp\">View all individuals</a></font></p>");
         out.println(ServletUtilities.getFooter(context));
       }
 
