c2c470979418e3a8f7a67d8438512e3620ef85f5
gwinstanley <stan@marinemegafauna.org>
2014-09-08 20:41:52 -0500

    Bug fix to clear MMA-compatible flag when all CR images removed from encounter.

--- src/main/java/org/ecocean/servlet/EncounterAddMantaPattern.java
+++ src/main/java/org/ecocean/servlet/EncounterAddMantaPattern.java
@@ -125,6 +125,19 @@ public class EncounterAddMantaPattern extends HttpServlet {
             mmFiles.get("CSV-REGIONAL").delete();
           }
           mmFiles = null;
+
+          // Check for any remaining CR images, and if none, clear MMA-compatible flag.
+          boolean hasCR = false;
+          for (SinglePhotoVideo mySPV : enc.getSinglePhotoVideo()) {
+            Map<String, File> mmaFiles = MantaMatcherUtilities.getMatcherFilesMap(spv);
+            hasCR = hasCR & mmaFiles.get("CR").exists();
+            if (hasCR)
+              break;
+          }
+          if (!hasCR) {
+            enc.setMmaCompatible(false);
+            myShepherd.commitDBTransaction();
+          }
         } 
         catch (SecurityException thisE) {
           thisE.printStackTrace();
