5f013418d18135918acf42ee7b97bf107f47dd72
Jon Van Oast <jonv@scribble.com>
2014-11-21 00:46:59 +0000

    clean up / tweaks

--- src/main/webapp/javascript/pageableTable.js
+++ src/main/webapp/javascript/pageableTable.js
@@ -1,58 +1,4 @@
 
-/*
-
-$(document).ready(function() {
-	wildbook.init(function() {
-		tableInit();
-		fetchData();
-	});
-});
-
-*/
-
-
-var testColumns = {
-	catalogNumber: { label: 'Number' },
-	//verbatimLocality: { label: 'Location' },
-	//dataTypes: { label: 'Data types', val: dataTypes },
-	individualID: { label: 'Individual' },
-	sex: { label: 'Sex', val: cleanValue },
-};
-
-
-
-function start() {
-	wildbook.init(function() {
-		TEST();
-	});
-}
-
-var encs;
-var foo;
-function TEST() {
-	foo = new pageableTable({
-		columns: testColumns,
-		tableElement: $('#results-table'),
-		sliderElement: $('#results-slider'),
-	});
-
-	foo.tableInit();
-
-	encs = new wildbook.Collection.Encounters();
-	encs.on('add', function(o) {
-		foo.tableAddRow(o);
-foo.tableAddRow(o); foo.tableAddRow(o); foo.tableAddRow(o); foo.tableAddRow(o);
-//tableAddRow(o); tableAddRow(o);tableAddRow(o); tableAddRow(o);
-	});
-	encs.fetch({
-		//fields: { individualID: 'newMatch' },
-		success: function() {
-			foo.tableShow();
-		}
-	});
-
-}
-
 
 function pageableTable(opts) {
 	var me = this;
@@ -110,7 +56,9 @@ console.log(delta);
 			search += ' ' + val;
 			td += '<td>' + val + '</td>';
 		});
-		this.tableBody.append('<tr data-search="' + search + '" id="n' + i + '"><td>' + i + '</td>' + td + '</tr>');
+		var tr = $('<tr data-search="' + search + '" id="n' + i + '"><td>' + i + '</td>' + td + '</tr>');
+		this.tableBody.append(tr);
+		return tr;
 	};
 
 
@@ -162,7 +110,7 @@ console.log(delta);
 				ends++;
 				continue;
 			}
-			this.tableBody.children()[i].className = 'pageableTable-visible';
+			$(this.tableBody.children()[i]).addClass('pageableTable-visible');
 		}
 	};
 
@@ -180,58 +128,13 @@ console.log(delta);
 	this.tableSearch = function(s) {
 		var regex = new RegExp(s, 'i');
 		this.tableBody.find('tr').removeClass('pageableTable-nof').each(function(i, el) {
-			if (!regex.test(el.getAttribute('data-search'))) el.className = 'pageableTable-nof';
+			if (!regex.test(el.getAttribute('data-search'))) $(el).addClass('pageableTable-nof');
 		});
 		this.pageTable(0);
 	};
 
 
 }
-
-/*
-var xcolumns = {
-	catalogNumber: { label: 'Number' },
-	verbatimLocality: { label: 'Location' },
-	dataTypes: { label: 'Data types', val: dataTypes },
-	sex: { label: 'Sex', val: cleanValue },
-};
-
-var columns = {
-	catalogNumber: { label: 'Number' },
-	//verbatimLocality: { label: 'Location' },
-	//dataTypes: { label: 'Data types', val: dataTypes },
-	individualID: { label: 'Individual' },
-	sex: { label: 'Sex', val: cleanValue },
-};
-
-var encs = false;
-
-var pstart = 0;
-var perPage = 20;
-
-
-var countTotal = -1;
-var countProgress = -1;
-
-
-var lastSliderStart = -1;
-var hasSlider = false;
-function initSlider() {
-	if (countTotal - perPage < 1) return;
-	hasSlider = true;
-	$('#slider').slider({
-		orientation: 'vertical',
-		value: 100,
-		slide: function(a, b) {
-			var start = Math.floor((100 - b.value) / 100 * (countTotal - perPage) + 0.5);
-			if (start == lastSliderStart) return;
-			lastSliderStart = start;
-			console.log(start);
-			pageTable(start, perPage);
-		}
-	});
-}
-*/
 
 /*
 var subPending = 0;
@@ -273,87 +176,6 @@ console.log('subPending: %d (%s %%)', subPending, percent);
 */
 
 
-var tableBody = false;
-
-var lastSearch = false;
-function tableShow() {
-	$('#progress').hide();
-	$('#test-table').tablesorter().show();
-	$('#test-table').bind('sortEnd', function() {
-		pstart = 0;
-		if (hasSlider) $('#slider').slider('option', 'value', 100);
-		pageTable(0);
-	});
-	pageTable(0, perPage);
-	$(document).on('keydown', function(ev) {
-		var m = { 38: -1, 40: 1 };
-		if (!m[ev.which]) return;
-		pageNudge(m[ev.which]);
-		ev.preventDefault();
-	});
-
-	$('#search').on('keyup', function() {
-		var val = this.value;
-		if (val == lastSearch) return;
-		lastSearch = val;
-		if (val == '') {
-			$('#table-body tr.nof').removeClass('nof');
-			pageTable(0, perPage);
-			return;
-		}
-		tableSearch(val);
-	});
-}
-
-
-var rowCount = 0;
-function tableAddRow(obj) {
-	var i = rowCount++;
-	var td = '';
-	var search = '';
-	_.each(columns, function(cstruct, c) {
-		var val = (cstruct.val ? cstruct.val(obj, c) : obj.get(c));
-		search += ' ' + val;
-		td += '<td>' + val + '</td>';
-	});
-	tableBody.append('<tr data-search="' + search + '" id="n' + i + '"><td>' + i + '</td>' + td + '</tr>');
-}
-
-//http://dev.wildme.org/mm/api/jdoql?select%20m%20from%20org.ecocean.Encounter%20where%20catalogNumber==%27073b9487-67ec-4705-a0fe-3fabc6525659%27%20&&%20measurements.contains(m)
-
-
-function pageTable(start, howMany) {
-	pstart = start;
-	$('.visible').removeClass('visible');
-	var ends = start + howMany;
-	for (var i = start ; i < ends ; i++) {
-		if (!$('#table-body').children()[i]) continue;
-		if ($($('#table-body').children()[i]).hasClass('nof')) {
-			ends++;
-			continue;
-		}
-		$('#table-body').children()[i].className = 'visible';
-	}
-}
-
-
-function pageNudge(n) {
-	pstart += n;
-	if (pstart < 0) pstart = 0;
-	if (pstart > (countTotal - perPage)) pstart = countTotal - perPage;
-	pageTable(pstart, perPage);
-
-	if (hasSlider) $('#slider').slider('option', 'value', 100 - (pstart / (countTotal - perPage)) * 100);
-}
-
-
-function tableSearch(s) {
-	var regex = new RegExp(s, 'i');
-	$('#table-body tr').removeClass('nof').each(function(i, el) {
-		if (!regex.test(el.getAttribute('data-search'))) el.className = 'nof';
-	});
-	pageTable(0, perPage);
-}
 
 
 function cleanValue(obj, fieldName) {
@@ -372,33 +194,3 @@ function dataTypes(obj, fieldName) {
 	});
 	return dt.join(', ');
 }
-
-/*
-</script>
-
-</head>
-<body>
-
-<div id="progress">loading<div id="#progress-bar"></div></div>
-
-<div style="position: relative;">
-<table id="test-table" class="tablesorter">
-</table>
-<div id="slider"></div>
-</div>
-
-
-<input id="search" placeholder="filter / search" />
-
-
-
-<!--
-<input type="button" value="^^^" onClick="pageDown();" style="float:left;" />
-<input type="button" value="vvv" onClick="pageUp();" style="float:right;" />
--->
-
-
-</body>
-</html>
-*/
-
