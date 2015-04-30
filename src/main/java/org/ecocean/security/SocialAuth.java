8e74d32c705c568cf6ec3a929e5bb3ae95d33b10
Jon Van Oast <jon@apparentinc.com>
2015-04-30 16:33:29 -0700

    gamma of federated auth

---
+++ src/main/java/org/ecocean/security/SocialAuth.java
@@ -0,0 +1,23 @@
+/* 
+
+https://github.com/pac4j/pac4j/wiki/Authenticate-with-Facebook,-Twitter-or-Google,-with-the-pac4j-library,-in-5-minutes
+
+*/
+
+package org.ecocean.security;
+
+import org.pac4j.core.client.*;
+import org.pac4j.oauth.client.*;
+
+
+public class SocialAuth {
+/*
+	public Clients build(Object env) {
+		final FacebookClient facebookClient = new FacebookClient("363791400412043", "719b2c0b21cc5e53bdc9086a283dc589");
+		final Google2Client googleClient = new Google2Client("", "");
+		return new Clients("http://localhost:8080/callback", facebookClient, googleClient);
+	}
+*/
+
+}
+
