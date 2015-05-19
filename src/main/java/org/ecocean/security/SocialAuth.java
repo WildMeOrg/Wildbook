9a89b3a64c45e87fcbd7362918e1b64fce73e3f7
Jon Van Oast <jon@apparentinc.com>
2015-05-19 11:28:58 -0700

    flickr helper method

--- src/main/java/org/ecocean/security/SocialAuth.java
+++ src/main/java/org/ecocean/security/SocialAuth.java
@@ -12,8 +12,18 @@ import org.pac4j.core.client.*;
 import org.pac4j.oauth.client.*;
 import org.ecocean.ShepherdProperties;
 
+//for flickr
+import org.scribe.builder.*;
+import org.scribe.builder.api.*;
+import org.scribe.model.*;
+import org.scribe.oauth.*;
+
+import java.util.*;
+
 
 public class SocialAuth {
+
+    public static final String FLICKR_URL = "https://api.flickr.com/services/rest/";
 
     //public SocialAuth() {}
 
@@ -32,5 +42,18 @@ public class SocialAuth {
         }
         return new FacebookClient(props.getProperty("facebookAppId"), props.getProperty("facebookSecret"));
     }
+
+
+
+    public static OAuthService getFlickrOauth(String context, String callbackUrl) throws Exception {
+        Properties props = authProps(context);
+        if ((props == null) || (props.getProperty("flickrKey") == null) || (props.getProperty("flickrSecret") == null)) {
+            //throw new Exception("facebookAppId or facebookSecret not set in socialAuth.properties");
+            return null;
+        }
+        return new ServiceBuilder().provider(FlickrApi.class).apiKey(props.getProperty("flickrKey")).apiSecret(props.getProperty("flickrSecret")).callback(callbackUrl).build();
+    }
+
+
 }
 
