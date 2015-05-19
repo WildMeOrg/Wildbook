459ee0e2083560cd06c5b9eafc17cd0df5fc49ed
Jon Van Oast <jon@apparentinc.com>
2015-05-19 11:31:36 -0700

    flickr [dis]connect functionality; left in a bunch of cruft that could possibly be used with yahoo.... commented out

--- src/main/java/org/ecocean/servlet/SocialConnect.java
+++ src/main/java/org/ecocean/servlet/SocialConnect.java
@@ -35,9 +35,15 @@ import org.ecocean.User;
 import org.pac4j.core.context.J2EContext;
 import org.pac4j.core.context.WebContext;
 import org.pac4j.oauth.client.FacebookClient;
+import org.pac4j.oauth.client.YahooClient;
 import org.pac4j.oauth.credentials.OAuthCredentials;
 import org.pac4j.oauth.profile.facebook.FacebookProfile;
+import org.pac4j.oauth.profile.yahoo.YahooProfile;
 
+import org.scribe.builder.*;
+import org.scribe.builder.api.*;
+import org.scribe.model.*;
+import org.scribe.oauth.*;
 
 
 
@@ -148,6 +154,157 @@ System.out.println("*** trying redirect?");
                 }
                 return;
             }
+
+
+        } else if ("flickr".equals(socialType)) {
+            String overif = request.getParameter("oauth_verifier");
+            String otoken = request.getParameter("oauth_token");
+
+            OAuthService service = null;
+            String callbackUrl = "http://" + CommonConfiguration.getURLLocation(request) + "/SocialConnect?type=flickr";
+            if (request.getParameter("disconnect") != null) callbackUrl += "&disconnect=1";
+            try {
+                service = SocialAuth.getFlickrOauth(context, callbackUrl);
+            } catch (Exception ex) {
+                System.out.println("SocialAuth.getFlickrOauth() threw exception " + ex.toString());
+            }
+            //WebContext ctx = new J2EContext(request, response);
+            //String callbackUrl = "http://localhost.wildme.org/a/SocialConnect?type=facebook";
+/*
+            String callbackUrl = "http://" + CommonConfiguration.getURLLocation(request) + "/SocialConnect?type=flickr";
+            if (request.getParameter("disconnect") != null) callbackUrl += "&disconnect=1";
+            yclient.setCallbackUrl(callbackUrl);
+*/
+
+            if (overif == null) {
+                Token requestToken = service.getRequestToken();
+                session.setAttribute("requestToken", requestToken);
+       System.out.println("==============================================requestToken = " + requestToken);
+                String authorizationUrl = service.getAuthorizationUrl(requestToken) + "&perms=read";
+System.out.println(authorizationUrl);
+
+//http://localhost.wildme.org/a/SocialConnect?type=xxflickr&oauth_token=72157652805581432-a5b8f3598c13e2a6&oauth_verifier=d3325223f923442e
+                response.sendRedirect(authorizationUrl);
+                return;
+
+            } else {
+System.out.println("verifier -> " + overif);
+                Token requestToken = (Token)session.getAttribute("requestToken");
+                Verifier verifier = new Verifier(overif);
+                Token accessToken = service.getAccessToken(requestToken, verifier);
+       System.out.println("==============================================requestToken = " + requestToken);
+       System.out.println("=- - - - - - - - - - - - - -==================accessToken = " + accessToken);
+System.out.println("-----------------------------------------otoken= " + otoken);
+       System.out.println("verifier = " + verifier);
+
+                OAuthRequest oRequest = new OAuthRequest(Verb.GET, SocialAuth.FLICKR_URL);
+                oRequest.addQuerystringParameter("method", "flickr.test.login");
+                service.signRequest(accessToken, oRequest);
+                Response oResponse = oRequest.send();
+System.out.println("GOT RESPONSE!!!!!!!!!!!!!!!!!!!!!!!!!!");
+System.out.println(oResponse.getBody());
+
+                String fusername = null;   //should we use <user id="XXXXXXXXX"> instead?  TODO
+                int i = oResponse.getBody().indexOf("<username>");
+                if (i > -1) {
+                    fusername = oResponse.getBody().substring(i + 10);
+                    i = fusername.indexOf("</username>");
+                    if (i > -1) fusername = fusername.substring(0, i);
+                }
+                User fuser = myShepherd.getUserBySocialId("flickr", fusername);
+   System.out.println("fusername = " + fusername + " -> user = " + fuser);
+if (fuser != null) System.out.println("user = " + user.getUsername() + "; fuser = " + fuser.getUsername());
+                if ((fuser != null) && (fuser.getUsername().equals(user.getUsername())) && (request.getParameter("disconnect") != null)) {
+                    fuser.unsetSocial("flickr");
+                    session.setAttribute("message", "disconnected from flickr");
+                    response.sendRedirect("myAccount.jsp");
+                    return;
+
+                } else if (fuser != null) {
+                    session.setAttribute("error", "looks like this account is already connected to an account");
+                    response.sendRedirect("myAccount.jsp");
+                    return;
+
+                } else {  //lets do this
+                    user.setSocial("flickr", fusername);
+                    //myShepherd.getPM().makePersistent(user);
+                    session.setAttribute("message", "connected to flickr");
+                    response.sendRedirect("myAccount.jsp");
+                    return;
+                }
+
+            }
+
+///
+/*
+    Verifier verifier = new Verifier(in.nextLine());
+    System.out.println();
+
+    // Trade the Request Token and Verfier for the Access Token
+    System.out.println("Trading the Request Token for an Access Token...");
+    Token accessToken = service.getAccessToken(requestToken, verifier);
+    System.out.println("Got the Access Token!");
+    System.out.println("(if your curious it looks like this: " + accessToken + " )");
+    System.out.println("(you can get the username, full name, and nsid by parsing the rawResponse: " + accessToken.getRawResponse() + ")");
+    System.out.println();
+
+    // Now let's go and ask for a protected resource!
+    System.out.println("Now we're going to access a protected resource...");
+    OAuthRequest request = new OAuthRequest(Verb.GET, FLICKR_URL);
+    request.addQuerystringParameter("method", "flickr.test.login");
+    service.signRequest(accessToken, request);
+    Response response = request.send();
+    System.out.println("Got it! Lets see what we found...");
+    System.out.println();
+    System.out.println(response.getBody());
+*/
+
+
+
+/*
+            OAuthCredentials credentials = null;
+            try {
+                credentials = yclient.getCredentials(ctx);
+            } catch (Exception ex) {
+                System.out.println("caught exception on yahoo credentials: " + ex.toString());
+            }
+
+            if (credentials != null) {
+                YahooProfile yahooProfile = yclient.getUserProfile(credentials, ctx);
+                User yuser = myShepherd.getUserBySocialId("yahoo", yahooProfile.getId());
+                System.out.println("getId() = " + yahooProfile.getId() + " -> user = " + yuser);
+if (yuser != null) System.out.println("user = " + user.getUsername() + "; yuser = " + yuser.getUsername());
+                if ((yuser != null) && (yuser.getUsername().equals(user.getUsername())) && (request.getParameter("disconnect") != null)) {
+                    yuser.unsetSocial("yahoo");
+                    //myShepherd.getPM().makePersistent(user);
+                    session.setAttribute("message", "disconnected from flickr");
+                    response.sendRedirect("myAccount.jsp");
+                    return;
+
+                } else if (yuser != null) {
+                    session.setAttribute("error", "looks like this account is already connected to an account");
+                    response.sendRedirect("myAccount.jsp");
+                    return;
+
+                } else {  //lets do this
+                    user.setSocial("flickr", yahooProfile.getId());
+                    //myShepherd.getPM().makePersistent(user);
+                    session.setAttribute("message", "connected to flickr");
+                    response.sendRedirect("myAccount.jsp");
+                    return;
+                }
+            } else {
+
+System.out.println("*** trying redirect?");
+                try {
+                    yclient.redirect(ctx, false, false);
+                } catch (Exception ex) {
+                    System.out.println("caught exception on yahoo processing: " + ex.toString());
+                }
+                return;
+            }
+
+*/
 
 
         } else {
