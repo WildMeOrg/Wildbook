4dc6882dff27ed6962d5a6bcdebec71c5aa2bdfa
RichardJune <richjune@aol.com>
2015-05-29 01:59:08 -0400

    More social merging

--- src/main/java/org/ecocean/servlet/EncounterForm.java
+++ src/main/java/org/ecocean/servlet/EncounterForm.java
@@ -619,7 +619,7 @@ got regular field (measurement(heightsamplingProtocol))=(samplingProtocol0)
 
       List<Measurement> measurements = getMeasurements(fv, encID, context);
       for (Measurement measurement : measurements) {
-        enc.addMeasurement(measurement);
+        enc.setMeasurement(measurement,myShepherd);
       }
 
 
