15b0e331328c2d38f636869e0a120d5ce944f296
Jon Van Oast <jon@apparentinc.com>
2015-06-03 16:42:25 -0700

    new wildbook.socialApiKey(svc) for getting api key(s) for hello.js

--- src/main/webapp/javascript/pages/submit.js
+++ src/main/webapp/javascript/pages/submit.js
@@ -19,10 +19,9 @@ $(function() {
     //
     //Initiate hello.js
     /* hello.init({ facebook: {'wildme.org': '363791400412043'}}, { */ // Can base your keys off urls if the service allows/requires
-    hello.init({facebook: wildbookGlobals.social.facebookAppId,
-    /*             twitter: "UTEfL90bUGqXcsERcFbJRU4Ng", */
-                google: "195771644717-2am21965cpsueu7u49f6dgnnmqg7nmm1.apps.googleusercontent.com",
-                flickr: "d8de31bc9e774909bdcd77d0c3f7c6e2"}, {
+    hello.init({facebook: wildbook.socialApiKey('facebook'),
+                google: wildbook.socialApiKey('google'),
+                flickr: wildbook.socialApiKey('flickr'),
        scope: "files, photos"/* ,
        redirect_uri : "../redirect.html" */
     });
