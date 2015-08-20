5130bbaa31ec6ad91722cfd70f7b4558ca918d8a
RichardJune <richjune@aol.com>
2015-08-20 15:30:23 -0400

    Working! Snapshot after first tuning and optimization.

--- src/main/java/org/ecocean/neural/TrainStdDev.java
+++ src/main/java/org/ecocean/neural/TrainStdDev.java
@@ -80,8 +80,11 @@ public class TrainStdDev extends HttpServlet {
   //create text file so we can also use this training data in the Neuroph UI
     BufferedWriter writer = null;
     
-    double stdDev=0.05;
-    double bestStdDev=stdDev;
+    double intersectStdDev=0.05;
+    
+    
+    //results array order: intersection, FastDTW, I3S, Proportion
+    double[] bestStdDev=new double[5];
     double maxPointsDifference=0;
     
     
@@ -94,8 +97,8 @@ public class TrainStdDev extends HttpServlet {
         
         
         
-        File trainingFile = new File(shepherdDataDir.getAbsolutePath()+"/network_stddev.txt");
-        writer = new BufferedWriter(new FileWriter(trainingFile));
+        //File trainingFile = new File(shepherdDataDir.getAbsolutePath()+"/network_stddev.txt");
+        //writer = new BufferedWriter(new FileWriter(trainingFile));
         
         StringBuffer writeMe=new StringBuffer();
         
@@ -106,112 +109,170 @@ public class TrainStdDev extends HttpServlet {
         SummaryStatistics proportionStats=TrainNetwork.getProportionStats(request);
         SummaryStatistics i3sStats=TrainNetwork.getI3SStats(request);
        
-        
-        
-        // add training data to training set (logical OR function)
-     
-       while(stdDev<=1){
-         
-         int numMatches=0;
-         int numNonMatches=0;
-        
-        //start the process again 
-        double totalMatchScores=0;
-        double totalFalseMatchScores=0;
-        
+        i3sStats.clear();
         
         Vector encounters=myShepherd.getAllEncountersNoFilterAsVector();
         int numEncs=encounters.size();
-        for(int i=0;i<(numEncs-1);i++){
-          for(int j=(i+1);j<numEncs;j++){
-            
-            Encounter enc1=(Encounter)encounters.get(i);
-            Encounter enc2=(Encounter)encounters.get(j);
-            //make sure both have spots!
-            if(((enc1.getSpots()!=null)&&(enc1.getSpots().size()>0)&&(enc1.getRightSpots()!=null))&&((enc1.getRightSpots().size()>0))&&((enc2.getSpots()!=null)&&(enc2.getSpots().size()>0)&&(enc2.getRightSpots()!=null)&&((enc2.getRightSpots().size()>0)))){
-              try{
-                System.out.println("Learning: "+enc1.getCatalogNumber()+" and "+enc2.getCatalogNumber());
+        
+        int numCombos=(numEncs*(numEncs-1))/2;
+        double[][] resultsArray=new double[numCombos][5];
+        int resultsCounter=0;
+        
+       
                 
-                //if both have spots, then we need to compare them
-             
-                //first, are they the same animal?
-                //default is 1==no
-                double output=1;
-                if((enc1.getIndividualID()!=null)&&(!enc1.getIndividualID().toLowerCase().equals("unassigned"))){
-                  if((enc2.getIndividualID()!=null)&&(!enc2.getIndividualID().toLowerCase().equals("unassigned"))){
-                    //train a match
-                    if(enc1.getIndividualID().equals(enc2.getIndividualID())){output=0;}
+                for(int i=0;i<(numEncs-1);i++){
+                  for(int j=(i+1);j<numEncs;j++){
+                    
+                    Encounter enc1=(Encounter)encounters.get(i);
+                    Encounter enc2=(Encounter)encounters.get(j);
+                    //make sure both have spots!
+                    if(((enc1.getSpots()!=null)&&(enc1.getSpots().size()>0)&&(enc1.getRightSpots()!=null))&&((enc1.getRightSpots().size()>0))&&((enc2.getSpots()!=null)&&(enc2.getSpots().size()>0)&&(enc2.getRightSpots()!=null)&&((enc2.getRightSpots().size()>0)))){
+                      try{
+                        System.out.println("Learning: "+enc1.getCatalogNumber()+" and "+enc2.getCatalogNumber());
+                        
+                        //if both have spots, then we need to compare them
+                     
+                        //first, are they the same animal?
+                        //default is 1==no
+                        double output=1;
+                        if((enc1.getIndividualID()!=null)&&(!enc1.getIndividualID().toLowerCase().equals("unassigned"))){
+                          if((enc2.getIndividualID()!=null)&&(!enc2.getIndividualID().toLowerCase().equals("unassigned"))){
+                            //train a match
+                            if(enc1.getIndividualID().equals(enc2.getIndividualID())){output=0;}
+                          }
+                          
+                        }
+                        resultsArray[resultsCounter][0]=output;
+                        
+                        
+                        EncounterLite el1=new EncounterLite(enc1);
+                        EncounterLite el2=new EncounterLite(enc2);
+                        
+                        //HolmbergIntersection
+                        Double numIntersections=EncounterLite.getHolmbergIntersectionScore(el1, el2,intersectionProportion);
+                        int totInter=-1;
+                        if(numIntersections!=null){totInter=numIntersections.intValue();}
+                       resultsArray[resultsCounter][1]=totInter;
+                        
+                        //FastDTW
+                        TimeWarpInfo twi=EncounterLite.fastDTW(el1, el2, 30);
+                        
+                        java.lang.Double distance = new java.lang.Double(-1);
+                        if(twi!=null){
+                          WarpPath wp=twi.getPath();
+                            String myPath=wp.toString();
+                          distance=new java.lang.Double(twi.getDistance());
+                        }   
+                        resultsArray[resultsCounter][2]=distance;
+                        
+                        //I3S
+                        I3SMatchObject newDScore=EncounterLite.improvedI3SScan(el1, el2);
+                        double i3sScore=-1;
+                        if(newDScore!=null){i3sScore=newDScore.getI3SMatchValue();}
+                        resultsArray[resultsCounter][3]=i3sScore;
+                        
+                        //Proportion metric
+                        Double proportion=EncounterLite.getFlukeProportion(el1,el2);
+                        resultsArray[resultsCounter][4]=proportion;
+                        
+                       
+                        
+                        
+                      
+                    }
+                    catch(Exception e){
+                      e.printStackTrace();
+                    }
+        
+                      
+                      
+                    }
+                    
+                    resultsCounter++;
                   }
+                  
                   
                 }
                 
-                
-                EncounterLite el1=new EncounterLite(enc1);
-                EncounterLite el2=new EncounterLite(enc2);
-                
-                //HolmbergIntersection
-                Double numIntersections=EncounterLite.getHolmbergIntersectionScore(el1, el2,intersectionProportion);
-                int totInter=-1;
-                if(numIntersections!=null){totInter=numIntersections.intValue();}
-               
-                
-                //FastDTW
-                TimeWarpInfo twi=EncounterLite.fastDTW(el1, el2, 30);
-                
-                java.lang.Double distance = new java.lang.Double(-1);
-                if(twi!=null){
-                  WarpPath wp=twi.getPath();
-                    String myPath=wp.toString();
-                  distance=new java.lang.Double(twi.getDistance());
-                }   
-                
-                //I3S
-                I3SMatchObject newDScore=EncounterLite.improvedI3SScan(el1, el2);
-                double i3sScore=-1;
-                if(newDScore!=null){i3sScore=newDScore.getI3SMatchValue();}
-                
-                //Proportion metric
-                Double proportion=EncounterLite.getFlukeProportion(el1,el2);
-                
-                double thisScore=TrainNetwork.getOverallFlukeMatchScore(request, totInter, distance.doubleValue(), i3sScore, new Double(proportion), stdDev, intersectionStats,dtwStats,i3sStats, proportionStats);
-                  
-                  if(output==0){numMatches++;totalMatchScores+=thisScore;}
-                  else{numNonMatches++;totalFalseMatchScores+=thisScore;}
-                  
-                
-                
-              
-            }
-            catch(Exception e){
-              e.printStackTrace();
-            }
 
-              
-              
-            }
-            
-          }
-          
-          
-        }
-        
-        totalMatchScores=totalMatchScores/numMatches;
-        totalFalseMatchScores=totalFalseMatchScores/numNonMatches;
          
-         if((totalMatchScores-totalFalseMatchScores)>maxPointsDifference){
-           maxPointsDifference=totalMatchScores-totalFalseMatchScores;
-           bestStdDev=stdDev;
-           
-           System.out.println("\n\n\nBest std dev so far: "+bestStdDev+" with a difference of "+maxPointsDifference+"\n\n\n");
-         }
+                 
+                 while(intersectStdDev<=1){
+                   System.out.println("Checking: "+intersectStdDev);
+                   double dtwStdDev=0.01;
+                   
+                   while(dtwStdDev<=1){
+                     System.out.println("Checking: "+intersectStdDev+" "+dtwStdDev);
+                     double i3sStdDev=0.01;
+                     
+                     while(i3sStdDev<=1){
+                       System.out.println("Checking: "+intersectStdDev+" "+dtwStdDev+" "+i3sStdDev);
+                       double proportionStdDev=0.01;
+                       
+                         
+                         while(proportionStdDev<=1){   
+                           System.out.println("Checking: "+intersectStdDev+" "+dtwStdDev+" "+i3sStdDev+" "+proportionStdDev);
+                           
+                           int numMatches=0;
+                           int numNonMatches=0;
+                           
+                           double totalMatchScores=0;
+                           double totalFalseMatchScores=0;
+                           
+                           
+                           for(int i=0;i<resultsCounter;i++){
+                             
+                             double output=resultsArray[i][0];
+                             double intersectScore=resultsArray[i][1];
+                             double dtwScore=resultsArray[i][2];
+                             double i3sScore=resultsArray[i][3];
+                             double proportionScore=resultsArray[i][4];
+                             
+                               double thisScore=TrainNetwork.getOverallFlukeMatchScore(request, intersectScore, dtwScore, i3sScore, proportionScore, intersectionStats,dtwStats,i3sStats, proportionStats,intersectStdDev,dtwStdDev,i3sStdDev,proportionStdDev);
+                               
+                              
+                              //start the process again 
+                            
+                               
+                               
+                               if(output<1){numMatches++;totalMatchScores+=thisScore;}
+                               else{numNonMatches++;totalFalseMatchScores+=thisScore;}
+                               
+                               
+                             
+                       }
+                       
+                           totalMatchScores=totalMatchScores/numMatches;
+                           totalFalseMatchScores=totalFalseMatchScores/numNonMatches;
+                            
+                            if((totalMatchScores-totalFalseMatchScores)>maxPointsDifference){
+                              maxPointsDifference=totalMatchScores-totalFalseMatchScores;
+                              bestStdDev[0]=intersectStdDev;
+                              bestStdDev[1]= dtwStdDev;
+                              bestStdDev[2]= i3sStdDev;
+                              bestStdDev[3]= proportionStdDev;
+                              
+                              System.out.println("\n\n\n\n\nBest std dev so far: "+bestStdDev.toString()+" with a difference of "+maxPointsDifference+"\n\n\n\n\n");
+                            }
+                             
+                             
+                 
+                 
+                 
+                 proportionStdDev=proportionStdDev+0.05;
+               } //end intersect std dev iterating for loop
+           i3sStdDev=i3sStdDev+0.05;
+          } //end intersect std dev iterating for loop
+            dtwStdDev=dtwStdDev+0.05;
+        } //end dtw std dev iterating for loop
         
-        stdDev=stdDev+0.05;
-      } //end std dev iterating for loop
+        intersectStdDev=intersectStdDev+0.05;
+      } //end intersect std dev iterating for loop
       
       
        
         //write out the training set
-        writer.write(writeMe.toString());
+        //writer.write(writeMe.toString());
         
         
   
@@ -238,7 +299,13 @@ public class TrainStdDev extends HttpServlet {
         myShepherd.commitDBTransaction();
         myShepherd.closeDBTransaction();
         out.println(ServletUtilities.getHeader(request));
-        out.println("<strong>Success!</strong> I have successfully trained the network and detected and optimum std dev. of: "+bestStdDev);
+        
+        
+        out.println("<strong>Success!</strong><br />");
+         out.println(" I have successfully trained the network and detected and optimum std dev. for Holmberg Intersectionof: "+bestStdDev[0]);
+        out.println("<br /><strong>Success!</strong> I have successfully trained the network and detected and optimum std dev. for FastDTW of: "+bestStdDev[1]);
+        out.println("<br /><strong>Success!</strong> I have successfully trained the network and detected and optimum std dev. for I3S of: "+bestStdDev[2]);
+        out.println("<br /><strong>Success!</strong> I have successfully trained the network and detected and optimum std dev. for Proportional Natching of: "+bestStdDev[3]);
 
         out.println("<p><a href=\"http://" + CommonConfiguration.getURLLocation(request) + "/adoptions/adoption.jsp\">Return to the Adoption Create/Edit page.</a></p>\n");
         out.println(ServletUtilities.getFooter(context));
