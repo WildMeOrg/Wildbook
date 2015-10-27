4b6fd20cd7005accac967914d34216bc39a2682f
gwinstanley <stan@marinemegafauna.org>
2015-10-27 12:06:27 +1000

    Updated SpamChecker to use externally-defined resources.

--- src/main/java/org/ecocean/SpamChecker.java
+++ src/main/java/org/ecocean/SpamChecker.java
@@ -1,11 +1,63 @@
 package org.ecocean;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
+ * Utility class to check for spam content.
+ *
  * @author Giles Winstanley
  */
 public final class SpamChecker {
+  /** SLF4J logger instance for writing log entries. */
+  private static final Logger log = LoggerFactory.getLogger(SpamChecker.class);
   /** Enumeration of possible spam detection outcomes. */
   public enum Result { SPAM, POSSIBLE_SPAM, NOT_SPAM };
+  /** List of regex patterns which if matched denote definite spam. */
+  private List<Pattern> spamPatterns;
+  /** List of regex patterns which if matched denote possible spam. */
+  private List<Pattern> possibleSpamPatterns;
+
+  public SpamChecker() throws IOException {
+    this.spamPatterns = loadPatterns("/SpamChecker_spam.txt");
+    this.possibleSpamPatterns = loadPatterns("/SpamChecker_possibleSpam.txt");
+  }
+
+  /**
+   * Loads words-matching patterns from a text file resource.
+   * The specified resource should contain each entry on a separate line, with case-insensitive substring matches
+   * entered as plain text, and regular-expression matches (for entire text) delimited by forward slashes
+   * (e.g. &quot;/.&#42;([Ff]oo|[Bb]ar)&#42;/&quot;).
+   * @param rName resource name to be found on classpath
+   * @return list of {@code Pattern} instances for word matching
+   * @throws IOException
+   */
+  private static List<Pattern> loadPatterns(String rName) throws IOException {
+    List<Pattern> list = new ArrayList<>();
+    try(InputStream in = SpamChecker.class.getResourceAsStream(rName);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+      String s = null;
+      while ((s = br.readLine()) != null) {
+        list.add(Pattern.compile(s));
+        // Check for regex-match if text is slash-delimited (e.g. "/regex/")
+        Matcher m = Pattern.compile("^/(.+)/$").matcher(s);
+        if (m.matches()) {
+          list.add(Pattern.compile(m.group(1)));
+        }
+        // Otherwise add regex for plain substring match (mapped to regex: ".*text.*").
+        else {
+          list.add(Pattern.compile(".*" + s + ".*", Pattern.CASE_INSENSITIVE));
+        }
+      }
+    }
+    return list;
+  }
 
   /**
    * Checks if the specified {@code Encounter} is a spam submission, based on the text content submitted.
@@ -14,7 +66,7 @@ public final class SpamChecker {
    * @param enc encounter to check
    * @return true if spam detected, false otherwise
    */
-  public static Result isSpam(Encounter enc) {
+  public Result isSpam(Encounter enc) {
     // Checks for definite spam.
     if (containsDefiniteSpam(enc.getSubmitterName()) || containsDefiniteSpam(enc.getSubmitterPhone()))
       return Result.SPAM;
@@ -40,14 +92,14 @@ public final class SpamChecker {
    * @param text text to check
    * @return true if spam words detected, false otherwise
    */
-  public static boolean containsDefiniteSpam(String text) {
+  public boolean containsDefiniteSpam(String text) {
     if (text == null)
       return false;
-    String s = text.toLowerCase();
-    if (s.contains("porn") || s.contains("href"))
-      return true;
-//    if (s.contains("[url]") || s.contains("url="))
-//      return true;
+    for (Pattern p : spamPatterns) {
+      if (p.matcher(text).matches()) {
+        return true;
+      }
+    }
     return false;
   }
 
@@ -56,12 +108,14 @@ public final class SpamChecker {
    * @param text text to check
    * @return true if possible spam words detected, false otherwise
    */
-  public static boolean containsPossibleSpam(String text) {
+  public boolean containsPossibleSpam(String text) {
     if (text == null)
       return false;
-    String s = text.toLowerCase();
-    if (text.matches("^(?i).*https?://.*$"))
-      return true;
+    for (Pattern p : possibleSpamPatterns) {
+      if (p.matcher(text).matches()) {
+        return true;
+      }
+    }
     return false;
   }
 }
