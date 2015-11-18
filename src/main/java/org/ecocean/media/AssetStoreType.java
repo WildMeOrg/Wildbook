9dba39cf71a02a03c350fe12a101afa13cefd850
Jon Van Oast <jon@apparentinc.com>
2015-11-17 17:12:47 -0800

    lets add some S3

--- src/main/java/org/ecocean/media/AssetStoreType.java
+++ src/main/java/org/ecocean/media/AssetStoreType.java
@@ -22,6 +22,7 @@ package org.ecocean.media;
  * AssetStoreType indicates the type of an AssetStore.
  */
 public enum AssetStoreType {
-    LOCAL
+    LOCAL,
+    S3
     // TODO Azure, YouTube, SCP...
 }
