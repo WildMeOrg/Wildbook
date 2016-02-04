41bcc0db2894cd0eea853027544ff17c2e7027cb
Jon Van Oast <sito.org@gmail.com>
2016-02-04 18:23:25 +0000

    untest - full set!

--- src/main/java/org/ecocean/neural/TrainNetwork.java
+++ src/main/java/org/ecocean/neural/TrainNetwork.java
@@ -492,21 +492,21 @@ public class TrainNetwork {
           ArrayList<WildbookInstance> list=new ArrayList<WildbookInstance>();
           
           
-int testStart = 40;
-int testLimit = 5;
+int testStart = 580;
+int testLimit = 400;
           
           //kick off IBEIS for each Encounter
           //RESTORE ME
-          //for(int i=0;i<(numEncs-1);i++){
-          for(int i= testStart ;i< testStart+testLimit;i++){
+          for(int i=0;i<(numEncs-1);i++){
+          //for(int i= testStart ;i< testStart+testLimit;i++){
             ArrayList<Encounter> qencs=new ArrayList<Encounter>();
             Encounter myEnc=(Encounter)encounters.get(i);
             qencs.add(myEnc);
             ArrayList<Encounter> tencs=new ArrayList<Encounter>();
             
 
-            //for(int j=(i+1);j<numEncs;j++){
-            for(int j=(i+1);j<(i+1+testLimit);j++){
+            for(int j=(i+1);j<numEncs;j++){
+            //for(int j=(i+1);j<(i+1+testLimit);j++){
               tencs.add((Encounter)encounters.get(j));
             } 
             
@@ -537,10 +537,11 @@ System.out.println(i + ") beginIdentify (taskID=" + taskID + ") ================
           
           
           
-          for(int i= testStart ;i< testStart+testLimit;i++){
+          //for(int i= testStart ;i< testStart+testLimit;i++){
           //RESTORE ME
-          //for(int i=0;i<(numEncs-1);i++){
-            for(int j=(i+1);j<(i+1+testLimit);j++){
+          for(int i=0;i<(numEncs-1);i++){
+            //for(int j=(i+1);j<(i+1+testLimit);j++){
+            for(int j=(i+1);j<numEncs;j++){
               
               EncounterLite enc1=new EncounterLite((Encounter)encounters.get(i));
               EncounterLite enc2=new EncounterLite((Encounter)encounters.get(j));
