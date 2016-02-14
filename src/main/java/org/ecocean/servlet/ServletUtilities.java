0277b17c0d554bc206549b0c6ef25062de4cc847
RichardJune <richjune@aol.com>
2016-02-14 01:37:48 -0500

    LOCAL: restore locationID-based security

--- src/main/java/org/ecocean/servlet/ServletUtilities.java
+++ src/main/java/org/ecocean/servlet/ServletUtilities.java
@@ -364,55 +364,62 @@ public class ServletUtilities {
 
   public static boolean isUserAuthorizedForEncounter(Encounter enc, HttpServletRequest request) {
     boolean isOwner = false;
-    if (request.isUserInRole("admin")) {
+    //if (request.isUserInRole("admin")) {
       if (request.getUserPrincipal()!=null) {
-        isOwner = true;
-      } 
-      else if (request.isUserInRole(enc.getLocationCode())) {
-        isOwner = true;
-      } 
-      else if ((((enc.getSubmitterID() != null) && (request.getRemoteUser() != null) && (enc.getSubmitterID().equals(request.getRemoteUser()))))) {
-        isOwner = true;
+        if (request.isUserInRole("admin")) {
+          isOwner = true;
+        }
+        else if (request.isUserInRole(enc.getLocationCode())) {
+          isOwner = true;
+        } 
+        else if ((((enc.getSubmitterID() != null) && (request.getRemoteUser() != null) && (enc.getSubmitterID().equals(request.getRemoteUser()))))) {
+          isOwner = true;
+        }
       }
       return isOwner;
-  }
-    return isOwner;
+  //}
+   // return isOwner;
 }
 
   public static boolean isUserAuthorizedForIndividual(MarkedIndividual sharky, HttpServletRequest request) {
-    if (request.isUserInRole("admin")) {
+    //if (request.isUserInRole("admin")) {
       if (request.getUserPrincipal()!=null) {
-        return true;
-      }
-  
-      Vector encounters = sharky.getEncounters();
-      int numEncs = encounters.size();
-      for (int y = 0; y < numEncs; y++) {
-        Encounter enc = (Encounter) encounters.get(y);
-        if (request.isUserInRole(enc.getLocationCode())) {
+          //return true;
+        
+        if (request.isUserInRole("admin")) {
           return true;
         }
+    
+        Vector encounters = sharky.getEncounters();
+        int numEncs = encounters.size();
+        for (int y = 0; y < numEncs; y++) {
+          Encounter enc = (Encounter) encounters.get(y);
+          if (request.isUserInRole(enc.getLocationCode())) {
+            return true;
+          }
+        }
       }
-    } 
+    //} 
     return false;
   }
   
   //occurrence
   public static boolean isUserAuthorizedForOccurrence(Occurrence sharky, HttpServletRequest request) {
-    if (request.isUserInRole("admin")) {
+    
       if (request.getUserPrincipal()!=null) {
-        return true;
+        
+          if (request.isUserInRole("admin")) {  return true;  } 
+          ArrayList<Encounter> encounters = sharky.getEncounters();
+          int numEncs = encounters.size();
+          for (int y = 0; y < numEncs; y++) {
+            Encounter enc = (Encounter) encounters.get(y);
+            if (request.isUserInRole(enc.getLocationCode())) {
+              return true;
+            }
+          }
       }
-  
-      ArrayList<Encounter> encounters = sharky.getEncounters();
-      int numEncs = encounters.size();
-      for (int y = 0; y < numEncs; y++) {
-        Encounter enc = (Encounter) encounters.get(y);
-        if (request.isUserInRole(enc.getLocationCode())) {
-          return true;
-        }
-      }
-    } 
+      
+
     return false;
   }
   //occurrence
