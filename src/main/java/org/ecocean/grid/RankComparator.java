2892058677457d6b59ae8bd4f46199ac97570ffd
RichardJune <richjune@aol.com>
2016-02-25 21:04:47 -0500

    Simplifying RankCOmparator to better honor contract

--- src/main/java/org/ecocean/grid/RankComparator.java
+++ src/main/java/org/ecocean/grid/RankComparator.java
@@ -18,14 +18,11 @@ public class RankComparator implements Comparator {
       MatchObject a1=((WildbookInstance)e1).getMatchObject();
       MatchObject b1=((WildbookInstance)e2).getMatchObject();
       
-
+      double a1_adjustedValue = 0;
+      double b1_adjustedValue = 0;
       
-    //intersection
-      if((algorithm.equals("intersection"))||(algorithm.equals("swale"))){   
-        
-        double a1_adjustedValue = 0;
-        double b1_adjustedValue = 0;
-        
+     
+        //positive scoring algorithms
         if(algorithm.equals("intersection")){
           a1_adjustedValue=a1.getIntersectionCount();
           b1_adjustedValue=b1.getIntersectionCount();
@@ -35,54 +32,40 @@ public class RankComparator implements Comparator {
           b1_adjustedValue=b1.getSwaleValue();
         }
         
-        if (a1_adjustedValue > b1_adjustedValue) {
-          return -1;
-        } 
-        else if (a1_adjustedValue == b1_adjustedValue) {
-          return 0;
-        } 
-        else {
-          return 1;
-        }
-      }
-      else if((algorithm.equals("euclidean"))||(algorithm.equals("i3s"))||(algorithm.equals("MSM"))||(algorithm.equals("fastDTW"))||(algorithm.equals("proportion"))){
-        double a1_adjustedValue = 0;
-        double b1_adjustedValue = 0;
         
+        //cross thread the negative scoring alforithms
         if(algorithm.equals("euclidean")){
-          a1_adjustedValue=a1.getEuclideanDistanceValue().doubleValue();
-          b1_adjustedValue=b1.getEuclideanDistanceValue().doubleValue();
+          a1_adjustedValue=b1.getEuclideanDistanceValue().doubleValue();
+          b1_adjustedValue=a1.getEuclideanDistanceValue().doubleValue();
         }
         else if(algorithm.equals("i3s")){
-          a1_adjustedValue=a1.getI3SMatchValue();
-          b1_adjustedValue=b1.getI3SMatchValue();
+          a1_adjustedValue=b1.getI3SMatchValue();
+          b1_adjustedValue=a1.getI3SMatchValue();
         }
         else if(algorithm.equals("MSM")){
-          a1_adjustedValue=a1.getMSMValue().doubleValue();
-          b1_adjustedValue=b1.getMSMValue().doubleValue();
+          a1_adjustedValue=b1.getMSMValue().doubleValue();
+          b1_adjustedValue=a1.getMSMValue().doubleValue();
         }
         else if(algorithm.equals("fastDTW")){
-          a1_adjustedValue=a1.getLeftFastDTWResult().doubleValue();
-          b1_adjustedValue=b1.getLeftFastDTWResult().doubleValue();
+          a1_adjustedValue=b1.getLeftFastDTWResult().doubleValue();
+          b1_adjustedValue=a1.getLeftFastDTWResult().doubleValue();
         }
         else if(algorithm.equals("proportion")){
-          a1_adjustedValue=a1.getProportionValue().doubleValue();
-          b1_adjustedValue=b1.getProportionValue().doubleValue();
+          a1_adjustedValue=b1.getProportionValue().doubleValue();
+          b1_adjustedValue=a1.getProportionValue().doubleValue();
         }
         
+
         if (a1_adjustedValue > b1_adjustedValue) {
-          return 1;
+          return -1;
         } 
         else if (a1_adjustedValue < b1_adjustedValue) {
-          return -1;
+          return 1;
         } 
         else {
           return 0;
         }
-      }
       
-      
-      return 0;
       
     }
 }
