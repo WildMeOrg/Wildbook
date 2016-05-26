722c802031bacad38412ba3c1ec5e37f57cb835e
Jon Van Oast <jon@apparentinc.com>
2016-05-26 00:51:55 -0700

    now include IAStatus

--- src/main/java/org/ecocean/servlet/MediaAssetContext.java
+++ src/main/java/org/ecocean/servlet/MediaAssetContext.java
@@ -91,7 +91,7 @@ public class MediaAssetContext extends HttpServlet {
 
 
     JSONObject anns = new JSONObject();
-    ArrayList<Annotation> annotations = mAsset.findAnnotations(myShepherd);
+    ArrayList<Annotation> annotations = mAsset.getAnnotations();
     for(Annotation ann : annotations) {
       anns.put(ann.getId(),ann.sanitizeJson(request));
     }
@@ -107,6 +107,7 @@ public class MediaAssetContext extends HttpServlet {
     }
     res.put("Occurrences", occs);
 
+    res.put("IAStatus", Util.toggleJSONObject(mAsset.getIAStatus(myShepherd)));
     out.println(res);
 
   } catch (JSONException jsoe) {
