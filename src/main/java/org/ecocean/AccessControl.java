33c228b0563ed2f36c17a19bb8c056f1dc8de534
Jon Van Oast <jon@apparentinc.com>
2016-06-02 15:44:15 -0700

    pondering Anonymous

--- src/main/java/org/ecocean/AccessControl.java
+++ src/main/java/org/ecocean/AccessControl.java
@@ -1,3 +1,7 @@
+/*
+   note: in currenct incarnation, "anonymous" has username == null
+   this should be assigned for "anonymous-owned" objects (as opposed to a null AccessControl value
+*/
 package org.ecocean;
 
 import org.json.JSONObject;
@@ -24,5 +28,9 @@ public class AccessControl implements java.io.Serializable {
 
     public AccessControl(final HttpServletRequest request) {
         this.username = ((request.getUserPrincipal() == null) ? null : request.getUserPrincipal().getName());
+    }
+
+    public boolean isAnonymous() {
+        return (username == null);
     }
 }
