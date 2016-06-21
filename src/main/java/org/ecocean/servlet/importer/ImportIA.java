60a9f1035af9c8e96826a65b3191bf1ad71cf9ee
drewblount <me@drew.computer>
2016-06-21 14:16:12 +0300

    Skeleton of ImportIA servlet

---
+++ src/main/java/org/ecocean/servlet/importer/ImportIA.java
@@ -0,0 +1,42 @@
+package org.ecocean.servlet.importer;
+
+import org.ecocean.*;
+import org.ecocean.servlet.*;
+import org.ecocean.identity.*;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.*;
+
+public class ImportIA extends HttpServlet {
+
+  public void init(ServletConfig config) throws ServletException {
+    super.init(config);
+  }
+
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    doPost(request, response);
+  }
+
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    String context="context0";
+    context=ServletUtilities.getContext(request);
+    Shepherd myShepherd = new Shepherd(context);
+
+    System.out.println("\n\nStarting ImportIA servlet...");
+
+    //setup data dir
+    String rootWebappPath = getServletContext().getRealPath("/");
+    File webappsDir = new File(rootWebappPath).getParentFile();
+    File shepherdDataDir = new File(webappsDir, CommonConfiguration.getDataDirectoryName(context));
+    if(!shepherdDataDir.exists()){shepherdDataDir.mkdirs();}
+    File tempSubdir = new File(webappsDir, "temp");
+    if(!tempSubdir.exists()){tempSubdir.mkdirs();}
+    System.out.println("\n\n     Finished directory creation...");
+
+  }
+}
