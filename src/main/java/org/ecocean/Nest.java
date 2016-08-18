012e4eaea448eb3611d37bbc50fe04affa9b29c5
drewblount <me@drew.computer>
2016-08-18 16:37:34 -0700

    adds constructor and methods for loading a DataSheets that are defined in config files

--- src/main/java/org/ecocean/Nest.java
+++ src/main/java/org/ecocean/Nest.java
@@ -3,12 +3,16 @@ package org.ecocean;
 
 import java.util.List;
 import java.util.ArrayList;
+
+import java.io.IOException;
+
 import org.ecocean.datacollection.*;
+import org.ecocean.Util;
 
 public class Nest implements java.io.Serializable {
 
   private String id;
-  private List<DataSheet> dataSheets;
+  private List<DataSheet> dataSheets = new ArrayList<DataSheet>();
 
   private String locationID;
   private String locationNote;
@@ -19,17 +23,31 @@ public class Nest implements java.io.Serializable {
    * empty constructor required by the JDO Enhancer
    */
   public Nest() {
-    this.dataSheets = new ArrayList<DataSheet>();
   }
 
   public Nest(String id) {
-    this.dataSheets = new ArrayList<DataSheet>();
     this.id = id;
+  }
+
+  public Nest(DataSheet sheet) {
+    this.id = Util.generateUUID();
+    this.record(sheet);
+  }
+
+  // the goal here is to offer a quick hook to a "null nest",
+  // that is a nest that has a datasheet with fields but no values.
+  public static Nest nestWithConfigDataSheet(String context) throws IOException {
+    return new Nest(DataSheet.fromCommonConfig("nest", context));
   }
 
   public void record(DataSheet datasheet) {
     this.dataSheets.add(datasheet);
   }
+
+  public void addConfigDataSheet(String context) throws IOException {
+    this.record(DataSheet.fromCommonConfig("nest", context));
+  }
+
 
   public List<DataSheet> getDataSheets() {
     return dataSheets;
