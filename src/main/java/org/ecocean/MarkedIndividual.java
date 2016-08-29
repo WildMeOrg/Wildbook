0f93827d6e15f90c21f30c0cc07ad1a6099879a1
drewblount <me@drew.computer>
2016-08-29 10:37:30 -0700

    adds photographer to the JSON returned by getExemplarImages()

--- src/main/java/org/ecocean/MarkedIndividual.java
+++ src/main/java/org/ecocean/MarkedIndividual.java
@@ -98,7 +98,7 @@ public class MarkedIndividual implements java.io.Serializable {
   private Vector interestedResearchers = new Vector();
 
   private String dateTimeCreated;
-  
+
   private String dateTimeLatestSighting;
 
   //FOR FAST QUERY PURPOSES ONLY - DO NOT MANUALLY SET
@@ -227,7 +227,7 @@ public class MarkedIndividual implements java.io.Serializable {
 		this.dateFirstIdentified = d;
 		return d;
 	}
-	
+
 	 public String refreshDateLastestSighting() {
 	    Encounter[] sorted = this.getDateSortedEncounters(true);
 	    if (sorted.length < 1) return null;
@@ -1151,7 +1151,7 @@ public class MarkedIndividual implements java.io.Serializable {
     }
     return "";
   }
-  
+
   public String getDateLatestSighting() {
     if (dateTimeLatestSighting != null) {
       return dateTimeLatestSighting;
@@ -1162,7 +1162,7 @@ public class MarkedIndividual implements java.io.Serializable {
   public void setDateTimeCreated(String time) {
     dateTimeCreated = time;
   }
-  
+
   public void setDateTimeLatestSighting(String time) {
     dateTimeLatestSighting = time;
   }
@@ -1374,7 +1374,7 @@ public class MarkedIndividual implements java.io.Serializable {
       }
     maxYearsBetweenResightings=maxYears;
     }
-  
+
 
 
   public String sidesSightedInPeriod(int m_startYear, int m_startMonth, int m_startDay, int m_endYear, int m_endMonth, int m_endDay, String locCode) {
@@ -1849,6 +1849,8 @@ public Float getMinDistanceBetweenTwoMarkedIndividuals(MarkedIndividual otherInd
     for (Encounter enc : this.getDateSortedEncounters()) {
       if((enc.getDynamicPropertyValue("PublicView")==null)||(enc.getDynamicPropertyValue("PublicView").equals("Yes"))){
         ArrayList<Annotation> anns = enc.getAnnotations();
+        String photographerName = enc.getPhotographerName();
+
         if ((anns == null) || (anns.size() < 1)) {
           continue;
         }
@@ -1858,24 +1860,27 @@ public Float getMinDistanceBetweenTwoMarkedIndividuals(MarkedIndividual otherInd
           if (ma != null) {
             //JSONObject j = new JSONObject();
             JSONObject j = ma.sanitizeJson(req, new JSONObject());
-            
-            
-            
+
+            if ((j!=null) && (photographerName!=null) && (!photographerName.equals(""))) {
+              j.put("photographer",photographerName);
+            }
+
+
             if ((j!=null)&&(ma.getMimeTypeMajor()!=null)&&(ma.getMimeTypeMajor().equals("image"))) {
-              
-              
+
+
               //ok, we have a viable candidate
-              
+
               //put ProfilePhotos at the beginning
               if(ma.hasKeyword("ProfilePhoto")){al.add(0, j);}
               //otherwise, just add it to the bottom of the stack
               else{
                 al.add(j);
               }
-              
+
             }
-            
-            
+
+
           }
         }
     }
@@ -1883,16 +1888,16 @@ public Float getMinDistanceBetweenTwoMarkedIndividuals(MarkedIndividual otherInd
     return al;
 
   }
-  
+
   public org.datanucleus.api.rest.orgjson.JSONObject getExemplarImage(HttpServletRequest req) throws JSONException {
-    
+
     ArrayList<org.datanucleus.api.rest.orgjson.JSONObject> al=getExemplarImages(req);
     if(al.size()>0){return al.get(0);}
     return new JSONObject();
-    
+
 
   }
-  
+
 
   // WARNING! THIS IS ONLY CORRECT IF ITS LOGIC CORRESPONDS TO getExemplarImage
   public String getExemplarPhotographer() {
