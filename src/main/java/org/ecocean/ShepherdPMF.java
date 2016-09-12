41e4a0cf78ff53bcb631fe8a1440ac00b6121a31
WildMeOrg <jason@wildme.org>
2016-09-11 22:49:32 -0400

    Initialize hashmap

--- src/main/java/org/ecocean/ShepherdPMF.java
+++ src/main/java/org/ecocean/ShepherdPMF.java
@@ -154,14 +154,21 @@ public class ShepherdPMF {
   }
   
   public static void removeShepherdState(String shepherdID){
+    if(shepherds==null) shepherds=new ConcurrentHashMap<String, String>();
+
     shepherds.remove(shepherdID);
   }
   
   public static String getShepherdState(String shepherdID){
+    if(shepherds==null) shepherds=new ConcurrentHashMap<String, String>();
+
     return shepherds.get(shepherdID);
   }
   
-  public static ConcurrentHashMap<String,String> getAllShepherdStates(){return shepherds;}
+  public static ConcurrentHashMap<String,String> getAllShepherdStates(){
+    if(shepherds==null) shepherds=new ConcurrentHashMap<String, String>();
+    return shepherds;
+  }
   
 
 }
