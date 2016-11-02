db8e63d8c148b511e61c3f2813379a066937aed5
Jon Van Oast <jon@apparentinc.com>
2016-11-02 16:26:53 -0700

    LOCAL: grab "guest matching" from master

--- src/main/java/org/ecocean/media/MediaAsset.java
+++ src/main/java/org/ecocean/media/MediaAsset.java
@@ -658,12 +658,15 @@ public class MediaAsset implements java.io.Serializable {
         if (bestType == null) bestType = "master";
         //note, this next line means bestType may get bumped *up* for anon user.... so we should TODO some logic in there if ever needed
         if (AccessControl.simpleUserString(request) == null) bestType = "mid";
+        if (store instanceof URLAssetStore) bestType = "original";  //this is cuz it is assumed to be a "public" url
 //System.out.println(" = = = = bestSafeAsset() wanting bestType=" + bestType);
 
-        //if we are a child asset, we need to find our parent then find best from there!  (unless we are the best)
+        //gotta consider that we are the best!
+        if (this.hasLabel("_" + bestType)) return this;
+
+        //if we are a child asset, we need to find our parent then find best from there!
         MediaAsset top = this;  //assume we are the parent-est
         if (parentId != null) {
-            if (this.hasLabel("_" + bestType)) return this;
             top = MediaAssetFactory.load(parentId, myShepherd);
             if (top == null) throw new RuntimeException("bestSafeAsset() failed to find parent on " + this);
         }
@@ -896,6 +899,19 @@ System.out.println("   ....  ??? do we have a " + t);
 
     public MediaAsset updateChild(String type) throws IOException {
         return updateChild(type, null);
+    }
+
+    public ArrayList<MediaAsset> detachChildren(Shepherd myShepherd, String type) throws IOException {
+        if (store == null) throw new IOException("store is null on " + this);
+        ArrayList<MediaAsset> disposable = this.findChildrenByLabel(myShepherd, "_" + type);
+        if ((disposable != null) && (disposable.size() > 0)) {
+            for (MediaAsset ma : disposable) {
+                ma.setParentId(null);
+                ma.addDerivationMethod("detachedFrom", this.getId());
+                System.out.println("INFO: detached child " + ma + " from " + this);
+            }
+        }
+        return disposable;
     }
 
     public ArrayList<MediaAsset> findChildren(Shepherd myShepherd) {
