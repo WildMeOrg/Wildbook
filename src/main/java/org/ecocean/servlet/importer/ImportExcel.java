ee3b3fc4d970d8f3a0a2c633582a3a4d34bc044b
Colin Kingen <colinwkingen@gmail.com>
2017-03-01 10:17:29 -0800

    attach annotations and media assets to encounters

--- src/main/java/org/ecocean/servlet/importer/ImportExcel.java
+++ src/main/java/org/ecocean/servlet/importer/ImportExcel.java
@@ -20,8 +20,6 @@ import org.ecocean.servlet.*;
 import org.ecocean.media.*;
 import javax.jdo.*;
 import java.lang.StringBuffer;
-import java.util.Vector;
-import java.util.Iterator;
 import java.lang.NumberFormatException;
 //import org.apache.poi.hssf.usermodel.*;
 //import org.apache.poi.poifs.filesystem.NPOIFSFileSystem;
@@ -61,88 +59,101 @@ public class ImportExcel extends HttpServlet {
     context = ServletUtilities.getContext(request);
     baseURL = getBaseURL(request); 
     
-    out.println("Importer usage in browser: https://yourhost/ImportExcel?exceldir=yourexceldirectory&imgdir=yourimagedirectory&commit='trueorfalse' ");
-    out.println("The default directories are /opt/excel_imports and /opt/image_imports. commit=false to test data parsing, true to actually save.");
+    out.println("Importer usage in browser: https://yourhost/ImportExcel?commit='trueorfalse' ");
+    out.println("The default directories are /opt/excel_imports/ and /opt/image_imports/. commit=false to test data parsing, true to actually save.");
     // set up a Shepherd
     // He will make sure that the default asset store is created if Wildbook hasn't made one for whatever reason.
     Shepherd startShepherd=null;
     startShepherd=new Shepherd(context);
     startShepherd.setAction("ImportExcel.class");
-    
     if (!CommonConfiguration.isWildbookInitialized(startShepherd)) {
       System.out.println("WARNING: Wildbook not initialized. Starting Wildbook");    
       StartupWildbook.initializeWildbook(request, startShepherd);
+      startShepherd.commitDBTransaction();
     }
+    startShepherd.closeDBTransaction();
     
     boolean committing = (request.getParameter("commit")!=null && !request.getParameter("commit").toLowerCase().equals("false")); //false by default
     
-    String exceldir = "/opt/excel_imports";
-    if (request.getParameter("exceldir") != null) exceldir = request.getParameter("exceldir");
-    File[] excelFileList = getFiles(exceldir);
-    boolean excelFound = excelFileList.length > 0;
-    for (File file : excelFileList) {
-      out.println("++ Processing Excel File: "+file.getName()+" at "+ file.getAbsolutePath());
-      processExcel(file, response, committing);
-    }
-    
+    Hashtable<Integer,MediaAsset> assetIds = new Hashtable<Integer,MediaAsset>(); 
     String imagedir = "/opt/image_imports/";
     if (request.getParameter("imagedir") != null) imagedir = request.getParameter("imagedir");
     File[] imageFileList = getFiles(imagedir);
     boolean imagesFound = imageFileList.length > 0;
+    Hashtable<Integer,MediaAsset> assetHash = new Hashtable<Integer,MediaAsset>();
     for (File file : imageFileList) {
       out.println("++ Processing Image File: "+file.getName()+" at "+ file.getAbsolutePath());
-      int[] arrIds = processImage(file, response, committing, imagedir);
-      System.out.println("====== "+Arrays.toString(arrIds)+"  AnnotationID, AssetID ======");
+      assetHash = processImage(file, response, committing, imagedir);
+      if (assetHash != null) {
+        assetIds.putAll(assetHash);  
+      }
+    }       
+    String assetString = StringUtils.join(assetIds.keySet(), " ");
+    System.out.println("All saved values in assetIds : " + assetString);
+    
+    String exceldir = "/opt/excel_imports/";
+    if (request.getParameter("exceldir") != null) exceldir = request.getParameter("exceldir");
+    boolean excelFound = false;
+    File[] excelFileList = null;
+    try {
+      excelFileList = getFiles(exceldir);
+      excelFound = excelFileList.length > 0;
+      for (File file : excelFileList) {
+        out.println("++ Processing Excel File: "+file.getName()+" at "+ file.getAbsolutePath());
+        processExcel(file, response, committing, assetIds);
+      }      
+    } catch (Exception e) {
+      e.printStackTrace();
+      out.println("!!!! Exception While Grabbing File List from "+exceldir+" !!!!");
     }
     
     out.println("Excel File(s) found = "+String.valueOf(excelFound)+" at "+excelFileList[0].getAbsolutePath());
-    //out.println("Image File(s) found = "+String.valueOf(imagesFound)+" at "+imageFileList[0].getAbsolutePath());
+    out.println("Image File(s) found = "+String.valueOf(imagesFound)+" at "+imageFileList[0].getAbsolutePath());
     
     //Create Workbook instance holding reference to .xls file 
     // XSSF is used for .xlxs files
     // NPOIFSFileSystem fs = new NPOIFSFileSystem(dataFile);  
     // HSSFWorkbook wb = new HSSFWorkbook(fs.getRoot(), true);
 
-    // CHECK if local media store exist by calling static function in startupwildbook
-    // get the code from index.jsp ~line 30-38
     // persist images in asset store, then associate with encounter
     // be careful of committing one and not the other, if you do it will contain a reference to 
     // that object in RAM, but not the persisted one if at all. 
+    out.close();
   }
   
-  public int[] processImage(File imageFile, HttpServletResponse response, boolean committing, String imagedir) throws IOException {
+  public Hashtable<Integer,MediaAsset> processImage(File imageFile, HttpServletResponse response, boolean committing, String imagedir) throws IOException {
     
     Shepherd myShepherd = new Shepherd(context);
-    String rootDir = getServletContext().getRealPath("/");
+    // String rootDir = getServletContext().getRealPath("/");
     String assetStorePath="/data/wildbook_data_dir";
     String assetStoreURL= baseURL + "/wildbook_data_dir";
     LocalAssetStore assetStore = null;
+    MediaAsset ma = null;
+    myShepherd.beginDBTransaction();
     try {
-      if (committing) myShepherd.beginDBTransaction();
-      assetStore = new LocalAssetStore("Turtle-Conservancy-Asset-Store", new File(assetStorePath).toPath(), assetStoreURL, true);
-      if (committing) myShepherd.getPM().makePersistent(assetStore);
-      if (committing) myShepherd.commitDBTransaction();
-      out.println("++++ Success creating asset store at "+assetStoreURL+" ++++");
+      if (committing) {
+        assetStore = new LocalAssetStore("Turtle-Conservancy-Asset-Store", new File(assetStorePath).toPath(), assetStoreURL, true);
+        myShepherd.getPM().makePersistent(assetStore);
+        myShepherd.commitDBTransaction();
+      }
     } catch (Exception e) {
-      out.println("++++ Failed to create and persist local asset store at "+assetStoreURL+" ++++");
+      out.println("!!!! Failed to create and persist local asset store at "+assetStoreURL+" !!!!");
       e.printStackTrace();      
     }
         
     String photoFileName = null;
     String photoNumber = null;
-    int assetId = 0;
     boolean isValid = false;
     try {
-      if (committing) myShepherd.beginDBTransaction();
       photoFileName = imageFile.getName();
       photoNumber = photoFileName.substring(4,7);
       char x = 'x';
       if (photoNumber.substring(2).charAt(0) == x) {
         isValid = false;
-        myShepherd.closeDBTransaction();
         out.println("Image rejected. Image filename contains invalid characters for asset ID.");
       } else {
         isValid = true;
+        out.println("++ Image Valid ++");
       }
       char z = '0';
       if (photoNumber.charAt(0) == z) {
@@ -157,69 +168,69 @@ public class ImportExcel extends HttpServlet {
     if (isValid == true) {
       File photo = new File(imagedir, photoFileName);
       JSONObject params = assetStore.createParameters(photo);
-      MediaAsset ma = null;
       if (committing && isValid == true) {
         try {
+          out.println("++++ Creating Media Asset ++++");
+          myShepherd.beginDBTransaction();
           ma = new MediaAsset(assetStore, params);
-          out.println("++++ Created a media asset with params: "+ma.getParameters().toString()+" ++++");
+          ma.addDerivationMethod("createEncounter", System.currentTimeMillis());
+          ma.addLabel("_original");
+          ma.copyIn(photo);
+          ma.updateMetadata();
+          ma.updateStandardChildren(myShepherd);
+          ma.generateUUIDFromId();
         } catch (Exception e) {
           out.println("!!!! Error creating media asset for image number "+photoNumber+" !!!!");
           e.printStackTrace();
         }
       }
       
-      if (committing) {
+      if (committing && isValid == true) {
         try {
-          ma.copyIn(photo);
+          out.println("++++ Persisting Media Asset ++++");
           myShepherd.getPM().makePersistent(ma);
-          ma.updateStandardChildren(myShepherd);
-          ma.updateMinimalMetadata();
+          myShepherd.commitDBTransaction();
+          myShepherd.beginDBTransaction();
         } catch (Exception e) {
+          myShepherd.rollbackDBTransaction();
           out.println("!!!! Error persisting media asset !!!!");
           e.printStackTrace(); 
         }
       }
       
       Annotation ann = new Annotation("Psammobates geometricus", ma);
-      if (committing) {
+      if (committing && isValid == true) {
         try {
           myShepherd.storeNewAnnotation(ann);
+          myShepherd.getPM().makePersistent(ann);
           myShepherd.commitDBTransaction();
+          myShepherd.closeDBTransaction();
         } catch (Exception e) {
           out.println("!!!! Error storing new annotation for media asset !!!!");
           e.printStackTrace(); 
         }
       }
-      assetId = ma.getId();
-      // Hashmap --> annotations to id of enc
-      // Then add the map to the enc when creating it
-      // enc.addAnnotations
-      
-      // MAYBE change this to return the asset, and store it in an array in main.
-      // Do the same with encounters, then we can try and associate only stuff from this batch.
-      Encounter match = null;
-      Iterator<Encounter> allEncs = myShepherd.getAllEncounters();
-      while (allEncs.hasNext() == true) {
-        Encounter enc = allEncs.next();
-        if (enc.getDynamicPropertyValue("imageNum") == photoNumber) {
-          // Associate after commiting
-        }
-      }    
+      // enc.addAnnotations  
     }
-    
     // You are trying to create media assets for each image file. Each image has a photo number
     // at the 4-6 character of it's file name.
     // Each encounter should have a photo number as well. Create the media assets, then create the encounters.
     // Then associate each encounter with the appropriate media assets. 
     // SAVE ASSET --> GET NUMBER --> SEARCH ENCOUNTER NUMBERS -->>> SAVE PAIR 
-    
-    // THE END
+    Hashtable<Integer,MediaAsset> hashValues = new Hashtable<Integer, MediaAsset>();
+    if (committing && isValid == true) {
+      System.out.println("Photonumber : " + Integer.valueOf(photoNumber));
+      hashValues.put(Integer.valueOf(photoNumber), ma);
+    }
     isValid = false;
-    myShepherd.closeDBTransaction();
-    return new int[]{assetId,Integer.parseInt(photoNumber)};
+    if (ma != null && photoNumber != null) {
+      return hashValues; 
+    } else {
+      return null;
+    }
   }
   
-  public void processExcel(File dataFile, HttpServletResponse response, boolean committing) throws IOException {  
+  public void processExcel(File dataFile, HttpServletResponse response, boolean committing, Hashtable<Integer, MediaAsset> assetIds) throws IOException {  
     
     Shepherd myShepherd = new Shepherd(context);
     
@@ -246,61 +257,76 @@ public class ImportExcel extends HttpServlet {
 
     int rows = sheet.getPhysicalNumberOfRows();; // No of rows
     int cols = sheet.getRow(0).getPhysicalNumberOfCells(); // No of columns
-    // int tmp = 0;
     out.println("Num Cols = "+cols);
     out.println("committing = "+committing);
 
     int printPeriod = 25;
-
-    if (committing) myShepherd.beginDBTransaction();
     out.println("+++++ LOOPING THROUGH FILE +++++");
+    Integer encId = null;
+    boolean isValid = true;
     for (int i=1; i<rows; i++) {
       try {
         if (committing) myShepherd.beginDBTransaction();
         row = sheet.getRow(i);
-        
-        Encounter enc = parseEncounter(row);
-        String indID = enc.getIndividualID();
-        MarkedIndividual ind = null;
-        boolean needToAddEncToInd = false;
-        if (indID!=null) {
-          ind = myShepherd.getMarkedIndividualQuiet(indID);
-          if (ind==null) {
-            ind = new MarkedIndividual(indID,enc);
-          } else {
-            needToAddEncToInd = true;
+        if (getInteger(row, 7) != null) {
+          encId= getInteger(row, 7);          
+        }
+        out.println("---- CURRENT ID: "+encId+" ----");
+        if (assetIds.get(encId) == null) {
+          isValid = false;
+          out.println("!!! ID Not Found in Asset List !!!");
+        }
+        Encounter enc = null;
+        if (committing && isValid == true) {
+          enc = parseEncounter(row, myShepherd);
+          String indID = enc.getIndividualID();
+          MarkedIndividual ind = null;
+          boolean needToAddEncToInd = false;
+          if (indID!=null) {
+            ind = myShepherd.getMarkedIndividualQuiet(indID);
+            if (ind==null) {
+              ind = new MarkedIndividual(indID,enc);
+            } else {
+              needToAddEncToInd = true;
+            }
+          }   
+          try {
+            out.println("Adding media asset : "+String.valueOf(encId));
+            enc.setState("approved");
+            if (committing && isValid == true) myShepherd.storeNewEncounter(enc, Util.generateUUID());
+            MediaAsset ma = myShepherd.getMediaAsset(String.valueOf(encId));
+            
+            enc.addMediaAsset(ma);
+          } catch (Exception e) {
+            e.printStackTrace();
+            out.println("!!! Failed to Store New Encounter !!!");
+          }
+          
+          if (needToAddEncToInd) ind.addEncounter(enc, context);
+          if (committing && indID!=null && isValid == true && !myShepherd.isMarkedIndividual(indID)) myShepherd.storeNewMarkedIndividual(ind);
+          if (committing && isValid == true) myShepherd.commitDBTransaction();
+          // New Close it.
+          if (i%printPeriod==0) {
+            out.println("Parsed row ("+i+"), containing Enc "+enc.getEncounterNumber()
+            +" with Latitude "+enc.getDecimalLatitude()
+            +" and Longitude "+enc.getDecimalLongitude()
+            +", dateInMillis "+enc.getDateInMilliseconds()
+            +", individualID "+enc.getIndividualID()
+            +", sex "+enc.getSex()
+            +", living status "+enc.getLivingStatus()
+            +", identification notes "+enc.getIdentificationRemarks()
+                );
           }
         }
-        try {
-          enc.setState("approved");
-          if (committing) myShepherd.storeNewEncounter(enc, Util.generateUUID());
-        } catch (Exception e) {
-          e.printStackTrace();
-          out.println("!!! Failed to Store New Encounter !!!");
-        }
-        if (needToAddEncToInd) ind.addEncounter(enc, context);
-        if (committing && indID!=null  && !myShepherd.isMarkedIndividual(indID)) myShepherd.storeNewMarkedIndividual(ind);
-        if (committing) myShepherd.commitDBTransaction();
-        if (i%printPeriod==0) {
-          out.println("Parsed row ("+i+"), containing Enc "+enc.getEncounterNumber()
-          +" with Latitude "+enc.getDecimalLatitude()
-          +" and Longitude "+enc.getDecimalLongitude()
-          +" with country "+enc.getCountry()
-          +", dateInMillis "+enc.getDateInMilliseconds()
-          +", individualID "+enc.getIndividualID()
-          +", sex "+enc.getSex()
-          +", living status "+enc.getLivingStatus()
-          +", identification notes "+enc.getIdentificationRemarks()
-          );
-        }
-      }
-      catch (Exception e) {
+      } catch (Exception e) {
         fs.close();
         out.println("!!! Encountered an error while Iterating through rows !!!");
         e.printStackTrace(out);
         myShepherd.rollbackDBTransaction();
       }
+      isValid = true;
     }
+    if (committing && isValid == true) myShepherd.closeDBTransaction();
     fs.close();
     wb.close();
   }
@@ -333,13 +359,26 @@ public class ImportExcel extends HttpServlet {
     return scheme + name + port + path;
   }
   
-  public Encounter parseEncounter(XSSFRow row) {
-    // The individual ID should be the picture number, which should be the same as the ross number.
-    // Ross number is primary ID, but of it doesn't include photo discard. If no Ross number.
-    String indID = stripAccents(getString(row, 13)); 
+  public Encounter parseEncounter(XSSFRow row, Shepherd myShepherd) {
+    // The encounter ID should be the ross number, which should be the same as the image number.
+    // Ross number is primary ID, but of it doesn't match an image number discard.
     Encounter enc = new Encounter();
+    Integer encNum = getInteger(row, 7);
+    String encNumString = String.valueOf(encNum);
+    String indID = null;
+    if (getString(row, 12) != null) {
+      indID = getString(row, 12);
+      enc.setIndividualID(indID);
+    }
     
-    enc.setIndividualID(indID);
+    out.println("Processing encounter : "+encNumString);
+    
+    String vickiNum = getString(row, 6);
+    
+    enc.setEncounterNumber(encNumString);
+    if (vickiNum != null) {
+      enc.setAlternateID(vickiNum);
+    }
    
     enc.setDecimalLatitude(getDouble(row,4));
     enc.setDecimalLongitude(getDouble(row,5));
@@ -349,6 +388,14 @@ public class ImportExcel extends HttpServlet {
     
     Date rossDate = getDate(row,8);
     Date vickiDate = getDate(row,2); 
+    
+    Double weightD = getDouble(row, 10);
+    Double lengthD = getDouble(row, 11);
+    
+    Measurement weight = new Measurement(encNumString,"Weight", weightD, "Gram", "directly measured");
+    Measurement length = new Measurement(encNumString,"Length", lengthD, "Millimeter", "directly measured");
+    enc.setMeasurement(weight, myShepherd);
+    enc.setMeasurement(length, myShepherd);
 
     Date encDate = resolveDate(rossDate, vickiDate);
     DateTime dt = new DateTime(encDate);
@@ -359,22 +406,11 @@ public class ImportExcel extends HttpServlet {
     enc.setLivingStatus(getLiving(notes));
     enc.setIdentificationRemarks(notes);
  
-    parseDynProp(enc, "rossNum", row, 7);
-    parseDynProp(enc, "vickiNum", row, 6);
     parseDynProp(enc, "encounterTime", row, 3);
-    parseDynProp(enc, "imageNum", row, 13);
-    
-    // Weight Commmon configuration.labels.properties <-- define measurments in language folder
-    // Measurement object  
-    
     // Constructor for encounter takes annotation list - maybe useful
-    
     // setAnnotations takes array list
-    
     enc.setDWCDateAdded();
     enc.setDWCDateLastModified();
-    
-    // Probably always going to be Psammobates geometricus.
     enc.setSubmitterID("Bulk Import");
     enc.setGenus("Psammobates geometricus");
     enc.setSpecificEpithet("geometricus");
@@ -393,13 +429,11 @@ public class ImportExcel extends HttpServlet {
     }
   }
   
-
   private void parseDynProp(Encounter enc, String name, XSSFRow row, int i) {
     String val = getString(row, i);
     if (val == null) return;
     enc.setDynamicProperty(name, val);
   }
-
 
   // following 'get' functions swallow errors
   public Integer getInteger(XSSFRow row, int i) {
