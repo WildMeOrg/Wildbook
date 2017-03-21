041524b91f4384ce5c004bb87d05ad7c5955ba86
Colin Kingen <colinwkingen@gmail.com>
2017-03-21 13:23:50 -0700

    creating importer for bass based on humpback model

--- src/main/java/org/ecocean/servlet/importer/ImportBass.java
+++ src/main/java/org/ecocean/servlet/importer/ImportBass.java
@@ -23,41 +23,45 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 public class ImportBass extends HttpServlet {
-  static PrintWriter out;
-  static String context; 
-  private static ArrayList<String> unmatchedFiles = new ArrayList<String>();
-  
+  private static PrintWriter out;
+  private static String context; 
+  // Values for Reference catalog are the alternates.
+  private static int idColumn = 1;
+  private static ArrayList<String> unmatchedFiles = new ArrayList<String>(); 
   
   public void init(ServletConfig config) throws ServletException {
     super.init(config);
   }
-  
+
   public void doGet(HttpServletRequest request,  HttpServletResponse response) throws ServletException,  IOException {
     doPost(request,  response);
   }
   
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,  IOException {
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,  IOException { 
     out = response.getWriter();
     context = ServletUtilities.getContext(request);
-   
-    Shepherd myShepherd = null;
-    myShepherd = new Shepherd(context);
-    if (!CommonConfiguration.isWildbookInitialized(myShepherd)) {
-      myShepherd.beginDBTransaction();
+    
+    out.println("Importer usage in browser: https://yourhost/HumpbackImporter?commit='trueorfalse' ");
+    out.println("The default directories are /opt/excel_imports/ and /opt/image_imports/. commit=false to test data parsing, true to actually save.");
+    
+    Shepherd initShepherd=null;
+    initShepherd=new Shepherd(context);
+    initShepherd.beginDBTransaction();
+    initShepherd.setAction("ImportHumpback.class");
+    if (!CommonConfiguration.isWildbookInitialized(initShepherd)) {
       System.out.println("WARNING: Wildbook not initialized. Starting Wildbook");    
-      StartupWildbook.initializeWildbook(request, myShepherd);
-      myShepherd.commitDBTransaction();
+      StartupWildbook.initializeWildbook(request, initShepherd);
     }
+    initShepherd.commitDBTransaction();
+    initShepherd.closeDBTransaction();
+    
     
     boolean committing = (request.getParameter("commit")!=null && !request.getParameter("commit").toLowerCase().equals("false"));
     
-    myShepherd.beginDBTransaction();
-    AssetStore assetStore = AssetStore.getDefault(myShepherd);
-    myShepherd.commitDBTransaction();
-    
-    String excelDir = "/opt/bass_excel/";
-    if (request.getParameter("exceldir") != null) excelDir = request.getParameter("exceldir");
+    String exceldir = "/opt/excel_bass/";
+    if (request.getParameter("exceldir") != null) exceldir = request.getParameter("exceldir");
     File[] excelFileList = null;
     try {
       excelFileList = getFiles(exceldir);
@@ -68,18 +72,18 @@ public class ImportBass extends HttpServlet {
         }              
       } catch (Exception e) {
         e.printStackTrace();
-        out.println("Choked on the next file.");
+        out.println(" !!!! Choked on the file, Processing Excel Failed. !!!! ");
       }
     } catch (Exception e) {
       e.printStackTrace();
       out.println("!!!! Exception While Grabbing File List from "+exceldir+" !!!!");
     }
     unmatchedFilenames("", "get");
-    out.close();  
-    myShepherd.closeDBTransaction();
+    out.close();
   }
   
-  public void processExcel(File dataFile, HttpServletResponse response, HttpServletRequest request, boolean committing) throws IOException {
+  
+  public void processExcel(File dataFile, HttpServletResponse response, HttpServletRequest request, boolean committing) throws IOException { 
     FileInputStream fs = new FileInputStream(dataFile);
     XSSFWorkbook wb = new XSSFWorkbook(fs);
     XSSFSheet sheet = wb.getSheetAt(0);
@@ -101,9 +105,251 @@ public class ImportBass extends HttpServlet {
     out.println("Num Columns = "+cols);
     out.println("Committing ? = "+String.valueOf(committing));
     
+    out.println("++++ PROCESSING EXCEL FILE, NOM NOM ++++");
+    Shepherd myShepherd = null;
+    myShepherd = new Shepherd(context);
+    Encounter enc = null;
+    myShepherd.beginDBTransaction();
+    AssetStore assetStore = AssetStore.getDefault(myShepherd);
+    myShepherd.commitDBTransaction();
     
+    for (int i=1; i<rows; i++) {     
+      
+      row = sheet.getRow(i);
+      String imageFile = getString(row, 0);
+      
+      enc = parseEncounter(row, myShepherd);  
+      
+      
+      ArrayList<Keyword> keywords = generateKeywords(row, dataFile, myShepherd);    
+      attachAsset(enc, imageFile, request, myShepherd, assetStore, keywords);
+    }
+    
+    
+    
+    // Lets just pull out the encounter/individual association to try to get this gnarly thread pile-up fixed.
+    // This is totally a hack. Associating in the main loop overloaded postgres connections.
+    Iterator<Encounter> allEncs = myShepherd.getAllEncounters();
+    Encounter encToAssociate = null;
+    while (allEncs.hasNext()) {
+      encToAssociate = allEncs.next();
+      if (encToAssociate.getIndividualID() != null && !encToAssociate.getIndividualID().equals("0")) {
+        findOrCreateIndy(encToAssociate.getIndividualID(), encToAssociate, myShepherd);   
+
+      }
+    }
+    myShepherd.closeDBTransaction();
+    wb.close();
   }
   
+  public ArrayList<Keyword> generateKeywords(XSSFRow row, File dataFile, Shepherd myShepherd) {
+    
+    ArrayList<Keyword> keys = new ArrayList<Keyword>(); 
+    
+    String sideString = getStringOrIntString(row, 9);
+    if (sideString != null) {
+      sideString = sideString.toLowerCase() + " side picture";
+    }
+    String mediaType = getStringOrIntString(row, 10);
+    if (mediaType != null) {
+      mediaType = "Media Type : "+mediaType.toLowerCase();
+    }
+    String downloaded = getStringOrIntString(row, 12);
+    if (downloaded != null) {
+      downloaded = downloaded.toLowerCase();
+    }
+    
+    myShepherd.beginDBTransaction();
+    Keyword dl = myShepherd.getKeyword(downloaded);
+    myShepherd.commitDBTransaction();
+    myShepherd.beginDBTransaction();
+    Keyword side = myShepherd.getKeyword(sideString);
+    myShepherd.commitDBTransaction();
+    myShepherd.beginDBTransaction();
+    Keyword med = myShepherd.getKeyword(mediaType);
+    myShepherd.commitDBTransaction();
+    
+    if (myShepherd.getKeyword(mediaType) == null && mediaType != null) {
+      med = new Keyword(mediaType);
+      myShepherd.beginDBTransaction();
+      myShepherd.getPM().makePersistent(med);
+      myShepherd.commitDBTransaction();
+    }
+    if (med != null) {
+      keys.add(med);
+    }
+    
+    if (myShepherd.getKeyword(downloaded) == null && downloaded != null) {
+      dl = new Keyword(dataFile.getName()); 
+      myShepherd.beginDBTransaction();
+      myShepherd.getPM().makePersistent(dl);
+      myShepherd.commitDBTransaction();
+    }
+    if (dl != null) {
+      keys.add(dl);
+    }
+    
+    if (myShepherd.getKeyword(sideString) == null && side != null) {
+      side = new Keyword(sideString);
+      myShepherd.beginDBTransaction();
+      myShepherd.getPM().makePersistent(side);
+      myShepherd.commitDBTransaction();          
+    }      
+    
+    if (side != null) {
+      keys.add(side);
+    }  
+    return keys;
+  }
+  
+  public void attachAsset(Encounter enc, String imageName, HttpServletRequest request, Shepherd myShepherd, AssetStore assetStore, ArrayList<Keyword> keywords ) {
+    MediaAsset  ma = null;
+    File photo = null;
+    
+    JSONObject params = new JSONObject();
+    String imagedir = "/opt/image_bass/";
+    if (request.getParameter("imagedir") != null) imagedir = request.getParameter("imagedir");
+    File[] imageFileList = getFiles(imagedir);
+    
+    boolean match = false;
+    
+    for (File imageFile : imageFileList) {  
+      if (imageFile.getName().equals(imageName)) {
+        match = true;
+        out.println("\n MATCH! Image Filename : "+imageFile.getName()+" = Image I'm looking for : "+imageName);
+        try {
+          photo = new File(imagedir, imageName);
+          params = assetStore.createParameters(photo);
+          ma = new MediaAsset(assetStore, params);         
+        } catch (Exception e) {
+          out.println("!!!! Error Trying to Create Media Asset!!!!");
+          e.printStackTrace();
+        }
+        try {
+          out.println("++++ Persisting Media Asset ++++");
+          myShepherd.beginDBTransaction();
+          myShepherd.getPM().makePersistent(ma);
+          myShepherd.commitDBTransaction();
+        } catch (Exception e) {
+          out.println("!!!! Could not Persist Media Asset !!!!");
+          e.printStackTrace();
+        }
+        try {
+          ma.addDerivationMethod("Bass Importer", System.currentTimeMillis());
+          ma.addLabel("_original");
+          ma.copyIn(photo);
+          //ma.setAccessControl(request);
+          ma.updateMetadata();
+          myShepherd.beginDBTransaction();
+          ma.updateStandardChildren(myShepherd);
+          myShepherd.commitDBTransaction();
+        } catch (Exception e) {
+          out.println("!!!! Error Trying to Save Media Asset Properties !!!!");
+          e.printStackTrace(out);
+        }
+        
+        try {
+          ma.setKeywords(keywords);
+        } catch (Exception e) {
+          out.println("!!!! Error Adding Keywords to Encounter !!!!");
+          e.printStackTrace();
+        }
+        
+        try {
+          out.println("Media Asset Parameters : "+ma.getParametersAsString()+" !!!!");
+          myShepherd.beginDBTransaction();
+          enc.addMediaAsset(ma);
+          myShepherd.commitDBTransaction();
+          out.println("Encounter As String : "+enc.toString()+" !!!!");
+          out.println("++++ Adding Media Asset ++++");
+        } catch (Exception e) {
+          out.println("!!!! Error Adding Media Asset to Encounter !!!!");
+          e.printStackTrace();
+        }       
+      }
+    } 
+    if (match == false) {
+      unmatchedFilenames(imageName, "store");
+    } else {      
+      match = false;
+    }
+  }
+  
+  public void unmatchedFilenames(String name, String flag) {
+    if (flag.equals("store")) {
+      unmatchedFiles.add(name);
+    }
+    if (flag.equals("get")) {
+      for (String file : unmatchedFiles) {
+        out.println("Unmatched File : "+file);
+      }
+      out.println("Total of "+Integer.toString(unmatchedFiles.size())+" Files Not Matched.");
+    } 
+  }
+  
+  public Encounter parseEncounter(XSSFRow row, Shepherd myShepherd) {  
+    String indyId = null;
+    
+    try {
+      indyId = getStringOrIntString(row, idColumn);      
+    } catch (Exception e) {
+      e.printStackTrace();
+      out.println("Error Getting Indy Id!");
+    }
+    
+    Encounter enc = new Encounter();
+    enc.setCatalogNumber(Util.generateUUID());
+    enc.setGenus("Stereolepis");
+    enc.setSpecificEpithet("gigas");
+    enc.setState("approved");
+    enc.setDWCDateAdded();   
+    enc.setDWCDateLastModified();
+    enc.setSubmitterID("Bulk Import");
+    if (indyId != null ) {
+      enc.setIndividualID(indyId); 
+    }
+    
+    myShepherd.beginDBTransaction();
+    myShepherd.getPM().makePersistent(enc);
+    myShepherd.commitDBTransaction();
+    
+    out.println("Here's the ID for this Individual : "+indyId);
+    return enc;
+  }
+  
+  public void findOrCreateIndy(String indyId, Encounter enc, Shepherd myShepherd) {
+    MarkedIndividual mi = null;
+    
+    try {
+      if (indyId != null) {
+        myShepherd.beginDBTransaction();
+        mi = myShepherd.getMarkedIndividualQuiet(indyId);      
+        myShepherd.commitDBTransaction();        
+      } 
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    
+    if (mi == null && !indyId.equals("0")) {
+      out.println("No Individual with ID : "+indyId+" exists. Creating.");
+      mi = new MarkedIndividual(indyId, enc);
+      myShepherd.beginDBTransaction();
+      myShepherd.storeNewMarkedIndividual(mi);
+      myShepherd.commitDBTransaction();    
+    }
+    if (mi != null && !indyId.equals("0")) {
+      try {
+        myShepherd.beginDBTransaction();
+        mi.addEncounter(enc, context);
+        myShepherd.commitDBTransaction();      
+      } catch (Exception e) {
+        e.printStackTrace(out);
+        out.println("Choked while saving Indy to Enc");
+      }      
+    }
+    
+  }
   
   public File[] getFiles(String path) {
     File[] arr;
@@ -160,15 +406,4 @@ public class ImportBass extends HttpServlet {
     return null;
   }
   
-  public void unmatchedFilenames(String name, String flag) {
-    if (flag.equals("store")) {
-      unmatchedFiles.add(name);
-    }
-    if (flag.equals("get")) {
-      for (String file : unmatchedFiles) {
-        out.println("Unmatched File : "+file);
-      }
-      out.println("Total of "+Integer.toString(unmatchedFiles.size())+" Files Not Matched.");
-    } 
-  }
 }
