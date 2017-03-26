d97c52b161d3d4eaba5d2c42eb91fa80916ec330
drewblount <me@drew.computer>
2017-03-25 17:20:31 -0700

    adds two string methods

--- src/main/java/org/ecocean/Util.java
+++ src/main/java/org/ecocean/Util.java
@@ -515,6 +515,11 @@ public class Util {
       return "("+fieldName+".indexOf('"+containsThis+"') != -1)";
     }
 
+    public static String jdoStringContainsConstraint(String fieldName, String containsThis, boolean toLowerCase) {
+      if (!toLowerCase) return jdoStringContainsConstraint(fieldName, containsThis);
+      return "("+fieldName+".toLowerCase().indexOf('"+containsThis.toLowerCase()+"') != -1)";
+    }
+
     public static String undoUrlEncoding(String str) {
       return str.replaceAll("%20", " ").trim();
     }
@@ -535,5 +540,7 @@ public class Util {
       return obj.toString();
     }
 
-
+    public static boolean stringExists(String str) {
+      return (str!=null && !str.equals(""));
+    }
 }
