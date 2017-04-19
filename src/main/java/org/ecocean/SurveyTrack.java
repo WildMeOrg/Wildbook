3775f761ca077e85071d2a4abf768fe558b84443
Colin Kingen <colinwkingen@gmail.com>
2017-04-19 16:59:46 -0700

    add distance measurement and some other getter setter type things to surveyTrack

--- src/main/java/org/ecocean/SurveyTrack.java
+++ src/main/java/org/ecocean/SurveyTrack.java
@@ -14,6 +14,8 @@ public class SurveyTrack implements java.io.Serializable{
    * 
    */
   private static final long serialVersionUID = -8570163271211244522L;
+  
+  private ArrayList<Occurrence> occurrences;
 
   private String surveyTrackID;
   private String parentSurveyID;
@@ -21,9 +23,15 @@ public class SurveyTrack implements java.io.Serializable{
   private String vesselID;
   private String locationID;
   
-  private long distance;
+  // Line transect, ect.
+  private String type;
+  
+  private Measurement distance;
   
   private Path path;
+  
+  private String dateTimeCreated;
+  private String dateTimeModified;
   
   public SurveyTrack(){};
   
@@ -39,9 +47,122 @@ public class SurveyTrack implements java.io.Serializable{
     }
   }
   
+  public String getDateTimeCreated() {
+    if (dateTimeCreated != null) {
+      return dateTimeCreated;
+    }
+    return "";
+  }
+
+  public void setDateTimeCreated(String time) {
+    dateTimeCreated = time;
+  }
+
+  public void setDateTimeCreated() {
+        dateTimeCreated = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date());
+  }
+  
+  public String getDWCDateLastModified() {
+    return dateTimeModified;
+  }
+
+  public void setDWCDateLastModified(String lastModified) {
+    dateTimeModified = lastModified;
+  }
+
+  public void setDWCDateLastModified() {
+    dateTimeModified = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date());
+  }
+  
   public String getID(){
+    if (surveyTrackID != null && !surveyTrackID.equals("")) {
+      
+    }
     return surveyTrackID;
   }
   
+  public void setID(String id) {
+    if (id != null && !id.equals("")) {
+      surveyTrackID = id;
+    }
+  }
+  
+  public ArrayList<Occurrence> getAllOccurrences() {
+    if (!occurrences.isEmpty()) {
+     return occurrences; 
+    } else {
+      return null;
+    }
+  }
+  
+  public Occurrence getOccurenceByID(String id) {
+    for (int i=0; i<occurrences.size(); i++) {
+      Occurrence thisOcc = occurrences.get(i);
+      if (thisOcc.getOccurrenceID().equals(id)) {
+        return thisOcc;
+      }
+    }
+    return null;
+  }
+  
+  public void addOccurence(Occurrence occ) {
+    if (occ != null) {
+      occurrences.add(occ);
+      setDWCDateLastModified();
+    }
+  }
+  
+  public void addMultipleOccurences(ArrayList<Occurrence> occArray) {
+    if (occArray.size() >= 1) {
+      for (int i=0; i<occArray.size(); i++) {
+        occurrences.add(occArray.get(i));
+      }
+      setDWCDateLastModified();
+    }
+  }
+  
+  public Measurement getDistance() {
+    if (distance != null) {
+      return distance;
+    }
+    return null;
+  }
+  
+  public void setDistance(Measurement dist) {
+    if (dist.getUnits() != null) {
+      distance = dist;
+      setDWCDateLastModified();
+    }
+  }
+  
+  public void setSurveyType(String typ) {
+    if (typ != null && !type.equals("")) {
+      type = typ;
+      setDWCDateLastModified();
+    }
+  }
+  
+  public String getSurveyType() {
+    if (type != null && !type.equals("")) {
+      return type;
+    } else {
+      return null;
+    }
+  }
+  
+  public void setLocationID(String loc) {
+    if (loc != null && !type.equals("")) {
+      locationID = loc;
+      setDWCDateLastModified();
+    }
+  }
+  
+  public String getLocationID() {
+    if (locationID != null && !locationID.equals("")) {
+      return locationID;
+    } else {
+      return null;
+    }
+  }
   
 }
