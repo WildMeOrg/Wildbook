678207663b9e2e48003901e3fe3283072b98fcfe
drewblount <me@drew.computer>
2017-04-24 18:28:46 -0700

    occurrence search backend

--- src/main/java/org/ecocean/OccurrenceQueryResult.java
+++ src/main/java/org/ecocean/OccurrenceQueryResult.java
@@ -28,6 +28,7 @@ public class OccurrenceQueryResult {
   private String queryPrettyPrint;
 
   public OccurrenceQueryResult(Vector<Occurrence> result, String jdoqlRepresentation, String queryPrettyPrint) {
+    System.out.println("New OccurrenceQueryResult called. (jdoql,prettyPrint)= ("+jdoqlRepresentation+" ,"+queryPrettyPrint+")");
     this.result = result;
     this.jdoqlRepresentation = jdoqlRepresentation;
     this.queryPrettyPrint = queryPrettyPrint;
