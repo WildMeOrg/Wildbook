65962629f32a2d685ba825da666789170858a8fb
WildMeOrg <services@wildme.org>
2017-06-21 19:00:25 -0400

    Merge fix

--- src/main/java/org/ecocean/YouTube.java
+++ src/main/java/org/ecocean/YouTube.java
@@ -23,7 +23,7 @@ import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.services.youtube.YouTube;
+//import com.google.api.services.youtube.YouTube;
 //import com.google.api.services.samples.youtube.cmdline.Auth;
 import com.google.api.services.youtube.model.*;
 //import com.google.api.services.youtube.YouTube;
