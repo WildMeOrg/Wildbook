c3299b93781497d64ccd3cb66390813e1981a2a0
WildMeOrg <services@wildme.org>
2017-06-23 11:10:42 -0400

    Remove hardcoded dependency on sacanTask size

--- src/main/java/org/ecocean/grid/GridManager.java
+++ src/main/java/org/ecocean/grid/GridManager.java
@@ -58,7 +58,7 @@ public class GridManager {
   public int maxGroupSize = 100;
   public int numCompletedWorkItems = 0;
   
-  public ConcurrentHashMap<String,Integer> scanTaskSizes=new ConcurrentHashMap<String, Integer>();
+  //public ConcurrentHashMap<String,Integer> scanTaskSizes=new ConcurrentHashMap<String, Integer>();
 
   //Modified Groth algorithm parameters
   private String epsilon = "0.01";
@@ -638,11 +638,11 @@ public class GridManager {
   }
   */
   
-  public void addScanTaskSize(String scanTaskID, int size){
-    scanTaskSizes.put(scanTaskID, new Integer(size));
-  }
+  //public void addScanTaskSize(String scanTaskID, int size){
+  //  scanTaskSizes.put(scanTaskID, new Integer(size));
+  //}
   
-  public Integer getScanTaskSize(String scanTaskID){return scanTaskSizes.get(scanTaskID);}
+  //public Integer getScanTaskSize(String scanTaskID){return scanTaskSizes.get(scanTaskID);}
   
   public static ConcurrentHashMap<String,EncounterLite> getMatchGraph(){return matchGraph;}
   public static void addMatchGraphEntry(String elID,EncounterLite el){
