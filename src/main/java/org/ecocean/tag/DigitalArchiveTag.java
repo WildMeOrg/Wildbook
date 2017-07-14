988a38568cd380694a8af69b92a80769e2995011
Colin Kingen <colinwkingen@gmail.com>
2017-07-14 13:17:58 -0700

    extend abstract tag to DigitalArchiveTag

---
+++ src/main/java/org/ecocean/tag/DigitalArchiveTag.java
@@ -0,0 +1,34 @@
+package org.ecocean.tag;
+
+public class DigitalArchiveTag extends AbstractTag {
+
+  private static final long serialVersionUID = 1L;
+  private String dTagID;
+  private String serialNumber;
+  
+  public DigitalArchiveTag() {
+    
+  }
+  
+  public DigitalArchiveTag(String dt, String serialNumber, String argosPttNumber) {
+    this.dTagID = dt;
+    this.serialNumber = serialNumber;
+  }
+
+  public String getDTagID() {
+    return dTagID;
+  }
+
+  public void setDTagID(String dt) {
+    this.dTagID = dt;
+  }
+
+  public String getSerialNumber() {
+    return serialNumber;
+  }
+
+  public void setSerialNumber(String serialNumber) {
+    this.serialNumber = serialNumber;
+  }
+  
+}
