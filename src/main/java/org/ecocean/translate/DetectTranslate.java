c90ed8108dc85d2b520e72289116a838932d2d2c
Chris Wainner <chris.jwainner@gmail.com>
2017-07-24 14:44:32 -0700

    Implement detect translate methods from twitterBotParseLocation branch

--- src/main/java/org/ecocean/translate/DetectTranslate.java
+++ src/main/java/org/ecocean/translate/DetectTranslate.java
@@ -10,30 +10,50 @@ import com.google.cloud.translate.Translate.TranslateOption;
 
 public class DetectTranslate {
   
+  public static String translateToEnglish(String text, String context){
+    String apiKey= CommonConfiguration.getProperty("translate_key", context);
+    Translate translate = TranslateOptions.newBuilder().setApiKey(apiKey).build().getService();
+    Translation translation = translate.translate(text,
+    TranslateOption.targetLanguage("en"));
+    System.out.println(translation.getTranslatedText());
+    text=translation.getTranslatedText();
+    return text;
+  }
+
+  public static String detectLanguage(String text, String context){
+    String apiKey= CommonConfiguration.getProperty("translate_key", context);
+    Translate translate = TranslateOptions.newBuilder().setApiKey(apiKey).build().getService();
+    Detection detection = translate.detect(text);
+    String detectedLanguage = detection.getLanguage();
+    return detectedLanguage;
+  }
+
+  //Legacy Methods from Stella
+
   public static String translate(String ytRemarks, String context){
     String apiKey= CommonConfiguration.getProperty("translate_key", context);
     Translate translate = TranslateOptions.newBuilder().setApiKey(apiKey).build().getService();
 
-    
+
 //    TranslateOptions.newBuilder().setApiKey(apiKey);
-    
-//    Translate translate = TranslateOptions.getDefaultInstance().getService();             
+
+//    Translate translate = TranslateOptions.getDefaultInstance().getService();
     Translation translation = translate.translate(ytRemarks,
     TranslateOption.targetLanguage("en"));
-    System.out.println(translation.getTranslatedText());  
-    ytRemarks=translation.getTranslatedText();  
-      
+    System.out.println(translation.getTranslatedText());
+    ytRemarks=translation.getTranslatedText();
+
     return ytRemarks;
-    
+
   }
   public static String detect(String ytRemarks, String context){
     String apiKey= CommonConfiguration.getProperty("translate_key", context);
     Translate translate = TranslateOptions.newBuilder().setApiKey(apiKey).build().getService();
-    
+
     Detection detection = translate.detect(ytRemarks);
     String detectedLanguage = detection.getLanguage();
-    return detectedLanguage; 
-    
+    return detectedLanguage;
+
   }
 
 }
