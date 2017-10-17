3e7324088fc2b41b80df92dd91df7d6c81921d35
Colin Kingen <colinwkingen@gmail.com>
2017-10-17 11:56:12 -0700

    import all csv and send to correct file specific methods

--- src/main/java/org/ecocean/servlet/importer/ImportLegacyBentoCsv.java
+++ src/main/java/org/ecocean/servlet/importer/ImportLegacyBentoCsv.java
@@ -2,7 +2,7 @@ package org.ecocean.servlet.importer;
 
 import org.json.JSONObject;
 
-// import au.com.bytecode.opencsv.*;
+import com.opencsv.*;
 import java.io.*;
 import java.util.*;
 import java.util.Map.Entry;
@@ -27,6 +27,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 public class ImportLegacyBentoCsv extends HttpServlet {
   /**
@@ -54,22 +55,66 @@ public class ImportLegacyBentoCsv extends HttpServlet {
     myShepherd.setAction("ImportLegacyBentoCsv.class");
     
     out.println("Grabbing all CSV files... ");
-    String dir = "/opt/dukeImport/DUML Files for Colin-NEW/Raw Data Files/tables_170303/";
-    
     // Grab each CSV file, make switches similar to access import.
     // Stub a method for each one. 
-    // Find the DUML equivalent -or- start with surveys. 
-    //CSVReader perser = new CSVReader();
+    // Find the DUML equivalent -or- start with surveys.     
+    String dir = "/opt/dukeImport/DUML Files for Colin-NEW/Raw Data Files/tables_170303/";
+    CSVReader effortCSV = grabReader(dir, "efforttable_final.csv");
+    CSVReader biopsyCSV = grabReader(dir, "biopsytable_final.csv");
+    CSVReader followsCSV = grabReader(dir, "followstable_final.csv");
+    CSVReader sightingsCSV = grabReader(dir, "sightingstable_final.csv");
+    CSVReader surveyLogCSV = grabReader(dir, "surveylogtable_final.csv");
+    CSVReader tagCSV = grabReader(dir, "tagtable_final.csv");
     
-    String name = null;
-    File effortFile = grabCSVFile(name);
+    if (true) {
+      processEffort(myShepherd, effortCSV);
+    }
+    if (true) {
+      processBiopsy(myShepherd, biopsyCSV);
+    }
+    if (true) {
+      processFollows(myShepherd, followsCSV);
+    }
+    if (true) {
+      processSightings(myShepherd, sightingsCSV);
+    }
+    if (true) {
+      processSurveyLog(myShepherd, surveyLogCSV);
+    }
+    if (true) {
+      processTags(myShepherd, tagCSV);
+    }
     
   }
-
+ 
+  private CSVReader grabReader(String dir, String filename) {
+    CSVReader reader = null;
+    try {
+      reader = new CSVReader(new FileReader(dir+filename));
+    } catch (FileNotFoundException e) {
+      System.out.println("Failed to retrieve CSV file at "+dir+filename);
+      e.printStackTrace();
+    }
+    return reader;
+  }
   
-  private File grabCSVFile(String name) {
-    File file = new File(name);
-    return file;
+  private void processEffort(Shepherd myShepherd, CSVReader effortCSV) {
+    
+  }
+  private void processBiopsy(Shepherd myShepherd, CSVReader biopsyCSV) {
+    
+  }
+  private void processFollows(Shepherd myShepherd, CSVReader followsCSV) {
+    
+  }
+  private void processSightings(Shepherd myShepherd, CSVReader sightingsCSV) {
+    
+  }
+  private void processSurveyLog(Shepherd myShepherd, CSVReader surveyLogCSV) {
+    
+  }
+  private void processTags(Shepherd myShepherd, CSVReader tagCSV) {
+    
   }
   
 }
