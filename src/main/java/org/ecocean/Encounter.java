d7dcca12505d4af267e553a101c8c5baa6a956c2
Drew Blount <me@drew.computer>
2017-11-08 12:07:53 -0800

    first round merging Encounter LightRestServlet

--- src/main/java/org/ecocean/Encounter.java
+++ src/main/java/org/ecocean/Encounter.java
@@ -1453,7 +1453,10 @@ System.out.println("did not find MediaAsset for params=" + sp + "; creating one?
   public void setDWCImageURL(String link) {
     dwcImageURL = link;
   }
-
+  // lmao, we have this capitalization of the getter for reflexivity purposes
+  public String getModified() {
+    return modified;
+  }
   public String getDWCDateLastModified() {
     return modified;
   }
@@ -1468,6 +1471,12 @@ System.out.println("did not find MediaAsset for params=" + sp + "; creating one?
   public String getDWCDateAdded() {
     return dwcDateAdded;
   }
+
+  // lmao, we have this capitalization of the getter for reflexivity purposes
+  public String getDwcDateAdded() {
+    return dwcDateAdded;
+  }
+
 
   public Long getDWCDateAddedLong(){
     return dwcDateAddedLong;
