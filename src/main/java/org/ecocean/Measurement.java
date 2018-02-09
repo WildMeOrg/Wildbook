0f9d1840e2982e4c29a32fb1823c10a3a436296a
Jon Van Oast <jon@wildme.org>
2018-02-08 17:16:57 -0800

    .toString() plz

--- src/main/java/org/ecocean/Measurement.java
+++ src/main/java/org/ecocean/Measurement.java
@@ -1,5 +1,7 @@
 package org.ecocean;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 public class Measurement extends DataCollectionEvent {
 
   private static final long serialVersionUID = -7934850478287322048L;
@@ -31,4 +33,11 @@ public class Measurement extends DataCollectionEvent {
     return units;
   }
 
+    public String toString() {
+        return new ToStringBuilder(this)
+            .append("type", getType())
+            .append("value", value)
+            .append("units", units)
+            .toString();
+    }
 }
