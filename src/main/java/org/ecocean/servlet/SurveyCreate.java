d04936ddcea5d1c018e80a463cef4e3874e65fd1
Colin W Kingen <colinwkingen@gmail.com>
2018-02-12 15:41:51 -0800

    add submit occ to selected ST, add security to survey creation/modification in web.xml

--- src/main/java/org/ecocean/servlet/SurveyCreate.java
+++ src/main/java/org/ecocean/servlet/SurveyCreate.java
@@ -134,9 +134,22 @@ public class SurveyCreate extends HttpServlet {
             st = createSurveyTrack(request, sv);
             myShepherd.commitDBTransaction();
             st.setParentSurveyID(sv.getID());
+            String vessel = null;
+            if (request.getParameter("vessel")!=null) {
+              vessel = request.getParameter("vessel");
+              st.setVesselID(vessel);
+            }
+            String locationID = null;
+            if (request.getParameter("locationID")!=null) {
+              locationID = request.getParameter("locationID");
+              st.setLocationID(locationID);
+            }
+            String type = null;
+            if (request.getParameter("type")!=null) {
+              type = request.getParameter("type");
+              st.setType(type);
+            }
             sv.addSurveyTrack(st);
-            System.out.println("Created Survey Track : "+st.getID());
-            System.out.println("Did the survey get it? "+sv.getAllSurveyTracks().toString());
           } catch (Exception e) {
             myShepherd.rollbackDBTransaction();
             e.printStackTrace();
@@ -151,7 +164,6 @@ public class SurveyCreate extends HttpServlet {
             myShepherd.rollbackDBTransaction();
             System.out.println("Failed to persist a Path for this survey.");
             e.printStackTrace();
-            
           }
         }
         
