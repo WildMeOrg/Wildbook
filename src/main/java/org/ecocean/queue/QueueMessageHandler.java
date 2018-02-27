ebec42cf756049a644b4a88224b8b96d3e17435e
Jon Van Oast <jon@wildme.org>
2018-02-27 15:20:52 -0800

    Queue/* cleanup and shutdown for RabbitMQ

--- src/main/java/org/ecocean/queue/QueueMessageHandler.java
+++ src/main/java/org/ecocean/queue/QueueMessageHandler.java
@@ -1,17 +1,7 @@
 package org.ecocean.queue;
 
-//public class QueueMessageHandler implements Runnable {
 public class QueueMessageHandler {
-/*
-    private String message;
-    public void setMessage(String msg) {
-        this.message = msg;
-    }
-    public void run() {
-        System.out.println("WARNING: default QueueMessageHandler.run() should be overridden!  [message=" + this.message + "]");
-    }
-*/
-    public boolean handler(String msg) throws java.io.IOException {
+    public boolean handler(String msg) throws java.io.IOException {  //true will ACK message, false will not
         System.out.println("WARNING: default QueueMessageHandler.handler() should be overridden!  [message=" + msg + "]");
         return true;
     }
