778c3a6bc0aeacda6c4a01ff05eee9e6e36ff3f1
Drew Blount <me@drew.computer>
2018-03-09 19:21:07 -0800

    adds fields to encounter light string fields

--- src/main/java/org/ecocean/servlet/LightRestServlet.java
+++ src/main/java/org/ecocean/servlet/LightRestServlet.java
@@ -81,7 +81,7 @@ public class LightRestServlet extends HttpServlet
 {
     private static final long serialVersionUID = -4445182084242929362L;
 
-    private static final String[] Encounter_Light_Str_Fields = {"catalogNumber","individualID","sex","otherCatalogNumbers","verbatimLocality","locationID","submitterName","submitterProject","genus","specificEpithet", "dwcDateAdded","modified"};
+    private static final String[] Encounter_Light_Str_Fields = {"catalogNumber","individualID","sex","otherCatalogNumbers","verbatimLocality","locationID","submitterName","submitterID","submitterProject","genus","specificEpithet", "dwcDateAdded","modified", "occurrenceID"};
     private static final String[] Encounter_Light_Int_Fields = {"year","month","day"};
 
     public static final NucleusLogger LOGGER_REST = NucleusLogger.getLoggerInstance("DataNucleus.REST");
