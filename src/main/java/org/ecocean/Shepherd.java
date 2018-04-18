3c3b27f1d229492bad2e900f24ce64e85b418e54
Drew Blount <me@drew.computer>
2018-04-18 10:52:26 -0700

    fixes errors introduced in merge

--- src/main/java/org/ecocean/Shepherd.java
+++ src/main/java/org/ecocean/Shepherd.java
@@ -2712,6 +2712,10 @@ public class Shepherd {
     //pmf=null;
   }
 
+  public boolean isDBTransactionActive() {
+    return (pm!=null && !pm.isClosed() && pm.currentTransaction().isActive());
+  }
+
   /**
    * Commits (makes permanent) any changes made to an open database
    */
