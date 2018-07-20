744534ddc26b2b7c527906b7b6d670fbec8ece84
Drew <drew@wildme.org>
2018-07-20 14:11:28 -0700

    adds methods for occurrence search and categorical, properties-defined searches

--- src/main/java/org/ecocean/OccurrenceQueryProcessor.java
+++ src/main/java/org/ecocean/OccurrenceQueryProcessor.java
@@ -3,6 +3,7 @@ package org.ecocean;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Vector;
 import java.io.*;
@@ -20,10 +21,11 @@ public class OccurrenceQueryProcessor extends QueryProcessor {
 
   private static final String BASE_FILTER = "SELECT FROM org.ecocean.Occurrence WHERE \"OCCURRENCEID\" != null && ";
 
-  public static final String[] SIMPLE_STRING_FIELDS = new String[]{"fieldStudySite", "fieldSurveyCode", "sightingPlatform", "groupComposition", "groupBehavior", "humanActivityNearby","initialCue","seaState","observer","comments"};
+  public static final String[] SIMPLE_STRING_FIELDS = new String[]{"fieldStudySite", "fieldSurveyCode", "sightingPlatform", "humanActivityNearby","seaState","observer","comments","occurrenceID"};
 
   public static final String[] CATEGORICAL_STRING_FIELDS = new String[]{"submitterID"};
 
+  public static final String[] CATEGORY_DEFINED_STRING_FIELDS = new String[]{"groupBehavior","groupComposition","initialCue"};
 
   public static String queryStringBuilder(HttpServletRequest request, StringBuffer prettyPrint, Map<String, Object> paramMap){
 
@@ -64,6 +66,14 @@ public class OccurrenceQueryProcessor extends QueryProcessor {
     for (int i = 1;i<=numObs;i++) {
       jdoqlVariableDeclaration = QueryProcessor.updateJdoqlVariableDeclaration(jdoqlVariableDeclaration, "org.ecocean.Observation observation" + i);      
     }
+
+    //Taxonomies
+    List<String> scientificNames = getScientificNames(request);
+    int numTaxonomies = scientificNames.size();
+    if (numTaxonomies>0) {
+      filter = filterTaxonomies(filter, request, prettyPrint, scientificNames);
+      jdoqlVariableDeclaration = addTaxonomyVars(jdoqlVariableDeclaration, numTaxonomies);
+    }
     
     // make sure no trailing ampersands
     filter = QueryProcessor.removeTrailingAmpersands(filter);
@@ -71,6 +81,51 @@ public class OccurrenceQueryProcessor extends QueryProcessor {
     filter += parameterDeclaration;
     System.out.println("OccurrenceQueryProcessor filter: "+filter);
     return filter;
+  }
+
+  public static int getNumTaxonomies(HttpServletRequest request) {
+    int numTaxonomies = 0;
+    while(request.getParameter("taxonomy"+numTaxonomies)!=null) {
+      numTaxonomies++;
+    }
+    return numTaxonomies;
+  }
+  public static List<String> getScientificNames(HttpServletRequest request) {
+    List<String> names = new ArrayList<String>();
+    int numTaxonomies = 0;
+    String currentName = request.getParameter("taxonomy0");
+    while(Util.stringExists(currentName)) {
+      names.add(Util.basicSanitize(currentName));
+      numTaxonomies++;
+      currentName = request.getParameter("taxonomy"+numTaxonomies);
+    }
+    return names;
+
+  }
+
+  private static String filterTaxonomies(String filter, HttpServletRequest request, StringBuffer prettyPrint, List<String> scientificNames) {
+    int numTaxonomies = scientificNames.size();
+    StringBuilder taxQuery = new StringBuilder();
+    for (int i=0;i<numTaxonomies;i++) {
+      String thisTitle = "taxonomy"+i;
+      String taxName = scientificNames.get(i);
+      if (!Util.stringExists(taxName)) continue;
+      if (i>0) {
+        taxQuery.append(" && "); // OR??
+        prettyPrint.append(" and ");
+      }
+      taxQuery.append("(taxonomies.contains("+thisTitle+") && "+thisTitle+".scientificName == \""+taxName+"\")");
+      prettyPrint.append("taxonomy "+i+" equals");
+    }
+    prettyPrint.append(".</br>");
+    filter = filterWithCondition(filter, taxQuery.toString()); // is this ok??? should fix w jdo subfilter method
+    return filter;
+  }
+  private static String addTaxonomyVars(String jdoqlVariableDeclaration, int numTaxonomies) {
+    for (int i = 0; i < numTaxonomies; i++) {
+      QueryProcessor.updateJdoqlVariableDeclaration(jdoqlVariableDeclaration, "org.ecocean.Taxonomy taxonomy" + i);
+    }
+    return jdoqlVariableDeclaration;
   }
 
   public static OccurrenceQueryResult processQuery(Shepherd myShepherd, HttpServletRequest request, String order){
