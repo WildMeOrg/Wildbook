2154f0869643ee188d9b514223bf3f69a7b0d840
Jon Van Oast <jon@wildme.org>
2018-08-06 16:34:44 -0700

    kinda more plugin architecture for ia.js

--- src/main/webapp/javascript/ia.IBEIS.js
+++ src/main/webapp/javascript/ia.IBEIS.js
@@ -1,5 +1,18 @@
-IA.IBEIS = {
+IA.plugins.push({
+    code: 'IBEIS',
+    name: 'Wildbook-IA (aka IBEIS)',
     getResult: function(task) {
         return '<div><b>getResult:</b> ' + JSON.stringify(task) + '</div>';
+    },
+
+    getGeneralType: function(task) {
+    },
+
+    //can assume task.parameters is set
+    isMyTask: function(task) {
+        for (var key in task.parameters) {
+            if (key.startsWith('ibeis.')) return 'IBEIS';
+        }
+        return false;
     }
-};
+});
