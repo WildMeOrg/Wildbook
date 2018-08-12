3013cfbe07133cc6770c28f36cf6b85d3cfdd295
Jason Holmberg <holmbergius@gmail.com>
2018-08-12 13:35:07 -0700

    UserConversion6

--- src/main/java/org/ecocean/Shepherd.java
+++ src/main/java/org/ecocean/Shepherd.java
@@ -2112,6 +2112,15 @@ public class Shepherd {
     return null;
   }
   
+  public List<User> getUsersWithEmailAddresses(){
+    String filter="SELECT FROM org.ecocean.User WHERE emailAddress != null";
+    Query query=getPM().newQuery(filter);
+    Collection c = (Collection) (query.execute());
+    ArrayList<User> users=new ArrayList<User>(c);
+    query.closeAll();
+    return users;
+  }
+  
   public User getUserByAffiliation(String affil){
     String filter="SELECT FROM org.ecocean.User WHERE affiliation == \""+affil+"\"";
     Query query=getPM().newQuery(filter);
@@ -3207,7 +3216,7 @@ public class Shepherd {
   public List<User> getAllUsers(String ordering) {
     Collection c;
     ArrayList<User> list = new ArrayList<User>();
-    System.out.println("Shepherd.getAllUsers() called in context "+getContext());
+    //System.out.println("Shepherd.getAllUsers() called in context "+getContext());
     Extent userClass = pm.getExtent(User.class, true);
     Query users = pm.newQuery(userClass);
     if(ordering!=null) {
@@ -3232,7 +3241,7 @@ public class Shepherd {
 
   public String getAllUserEmailAddressesForLocationID(String locationID, String context){
     String addresses="";
-    List<User> users = getAllUsers();
+    List<User> users = getUsersWithEmailAddresses();
     int numUsers=users.size();
     for(int i=0;i<numUsers;i++){
       User user=users.get(i);
