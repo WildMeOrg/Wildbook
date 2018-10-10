9aa905879c7fbc478dcb888fb96f9819ca81f856
Jon Van Oast <jon@wildme.org>
2018-10-10 10:21:41 +0000

    fix(?) task persistence

--- src/main/java/org/ecocean/servlet/EncounterForm.java
+++ src/main/java/org/ecocean/servlet/EncounterForm.java
@@ -1015,6 +1015,7 @@ System.out.println("depth --> " + fv.get("depth").toString());
                 //*after* persisting this madness, then lets kick MediaAssets to IA for whatever fate awaits them
                 //  note: we dont send Annotations here, as they are always(forever?) trivial annotations, so pretty disposable
                 Task task = org.ecocean.ia.IA.intakeMediaAssets(myShepherd, enc.getMedia());  //TODO are they *really* persisted for another thread (queue)
+                myShepherd.storeNewTask(task);
                 Logger log = LoggerFactory.getLogger(EncounterForm.class);
                 log.info("New encounter submission: <a href=\""+request.getScheme()+"://" + CommonConfiguration.getURLLocation(request) + "/encounters/encounter.jsp?number=" + encID+"\">"+encID+"</a>");
 System.out.println("ENCOUNTER SAVED???? newnum=" + newnum);
