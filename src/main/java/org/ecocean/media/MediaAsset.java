f49419095a6926def973974ec3cddb98b3647b1a
Drew Blount <me@drew.computer>
2018-12-12 10:34:17 +0400

    MediaAsset toString now contains keywords, along with Util method forjoining a list of strings

--- src/main/java/org/ecocean/media/MediaAsset.java
+++ src/main/java/org/ecocean/media/MediaAsset.java
@@ -185,6 +185,9 @@ public class MediaAsset implements java.io.Serializable {
     public String getAcmId() {
         return this.acmId;
     }
+    public boolean hasAcmId() {
+        return (null!=this.acmId);
+    }
 
     private URL getUrl(final AssetStore store, final Path path) {
         if (store == null) {
@@ -1004,11 +1007,14 @@ public class MediaAsset implements java.io.Serializable {
 
 
     public String toString() {
+        List<String> kwNames = getKeywordNames();
+        String kwString = (kwNames==null) ? "None" : Util.joinStrings(kwNames);
         return new ToStringBuilder(this)
                 .append("id", id)
                 .append("parent", parentId)
                 .append("labels", ((labels == null) ? "" : labels.toString()))
                 .append("store", store.toString())
+                .append("keywords", kwString)
                 .toString();
     }
 
@@ -1133,6 +1139,13 @@ System.out.println(">> updateStandardChildren(): type = " + type);
     }
     public ArrayList<Keyword> getKeywords() {
         return keywords;
+    }
+    public List<String> getKeywordNames() {
+        List<String> names = new ArrayList<String>();
+        for (Keyword kw: getKeywords()) {
+            names.add(kw.getReadableName());
+        }
+        return names;
     }
     public boolean hasKeywords(){
         return (keywords!=null && (keywords.size()>0));
