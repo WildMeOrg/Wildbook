74dafd1f8e5f778db023f5b82c1b46ecbbac45a4
Jon Van Oast <jon@wildme.org>
2019-01-02 15:33:03 -0800

    a weak enc.getTaxonomy() to act as stand-in for actual getter ... #someday

--- src/main/java/org/ecocean/Encounter.java
+++ src/main/java/org/ecocean/Encounter.java
@@ -2300,6 +2300,14 @@ the decimal one (Double) .. half tempted to break out a class for this: lat/lon/
       return Util.taxonomyString(getGenus(), getSpecificEpithet());
   }
 
+    //hacky (as generates new Taxonomy -- with random uuid) but still should work for tax1.equals(tax2);
+    // TODO FIXME this should be superceded by the getter for Taxonomy property in the future....
+    public Taxonomy getTaxonomy() {
+        String sciname = this.getTaxonomyString();
+        if (sciname == null) return null;
+        return new Taxonomy(sciname);
+    }
+
     //right now this updates .genus and .specificEpithet ... but in some glorious future we will just store Taxonomy!
     //  note that "null" cases will leave *current values untouched* (does not reset them)
     public void setTaxonomy(Taxonomy tax) {
