4d11105b521bb1c1a63943598ddbe3bb1883a558
Jason Holmberg <holmbergius@gmail.com>
2019-01-04 15:53:45 -0800

    LightRestServlet: Prevent unauthenticated pulls of full object graphs.
    # Conflicts:
    #	src/main/webapp/WEB-INF/web.xml

--- src/main/java/org/ecocean/servlet/LightRestServlet.java
+++ src/main/java/org/ecocean/servlet/LightRestServlet.java
@@ -227,19 +227,19 @@ public class LightRestServlet extends HttpServlet
         try
         {
             String token = getNextTokenAfterSlash(req);
-            if (token.equalsIgnoreCase("query") || token.equalsIgnoreCase("jdoql"))
+            if ((token.equalsIgnoreCase("query") || token.equalsIgnoreCase("jdoql"))&&(req.getRemoteUser()!=null))
             {
                 // GET "/query?the_query_details" or GET "/jdoql?the_query_details" where "the_query_details" is "SELECT FROM ... WHERE ... ORDER BY ..."
                 String queryString = URLDecoder.decode(req.getQueryString(), "UTF-8");
                 PersistenceManager pm = pmf.getPersistenceManager();
                 String servletID=Util.generateUUID();
-                ShepherdPMF.setShepherdState("RestServlet.class"+"_"+servletID, "new");
+                ShepherdPMF.setShepherdState("LightRestServlet.class"+"_"+servletID, "new");
                 
                 
                 try
                 {
                     pm.currentTransaction().begin();
-                    ShepherdPMF.setShepherdState("RestServlet.class"+"_"+servletID, "begin");
+                    ShepherdPMF.setShepherdState("LightRestServlet.class"+"_"+servletID, "begin");
                     
 
                     Query query = pm.newQuery("JDOQL", queryString);
@@ -266,7 +266,7 @@ public class LightRestServlet extends HttpServlet
                     resp.setHeader("Content-Type", "application/json");
                     resp.setStatus(200);
                     pm.currentTransaction().commit();
-                    ShepherdPMF.setShepherdState("RestServlet.class"+"_"+servletID, "commit");
+                    ShepherdPMF.setShepherdState("LightRestServlet.class"+"_"+servletID, "commit");
                     
                 }
                 finally
@@ -274,17 +274,18 @@ public class LightRestServlet extends HttpServlet
                     if (pm.currentTransaction().isActive())
                     {
                         pm.currentTransaction().rollback();
-                        ShepherdPMF.setShepherdState("RestServlet.class"+"_"+servletID, "rollback");
+                        ShepherdPMF.setShepherdState("LightRestServlet.class"+"_"+servletID, "rollback");
                         
                     }
                     pm.close();
                     //ShepherdPMF.setShepherdState("RestServlet.class"+"_"+servletID, "close");
-                    ShepherdPMF.removeShepherdState("RestServlet.class"+"_"+servletID);
+                    ShepherdPMF.removeShepherdState("LightRestServlet.class"+"_"+servletID);
                     
                     
                 }
                 return;
             }
+            /*
             else if (token.equalsIgnoreCase("jpql"))
             {
                 // GET "/jpql?the_query_details" where "the_query_details" is "SELECT ... FROM ... WHERE ... ORDER BY ..."
@@ -328,11 +329,12 @@ public class LightRestServlet extends HttpServlet
                 }
                 return;
             }
+            */
             else
             {
                 // GET "/{candidateclass}..."
                 String className = token;
-                ClassLoaderResolver clr = nucCtx.getClassLoaderResolver(RestServlet.class.getClassLoader());
+                ClassLoaderResolver clr = nucCtx.getClassLoaderResolver(LightRestServlet.class.getClassLoader());
                 AbstractClassMetaData cmd = nucCtx.getMetaDataManager().getMetaDataForEntityName(className);
                 try
                 {
@@ -348,12 +350,15 @@ public class LightRestServlet extends HttpServlet
                     resp.getWriter().write(error.toString());
                     resp.setStatus(404);
                     resp.setHeader("Content-Type", "application/json");
+                    ShepherdPMF.removeShepherdState("LightRestServlet.class");
                     return;
                 }
 
                 Object id = getId(req);
+                //don't let an unauthenticated user do a full grab of all instances of an object type 
                 if (id == null)
                 {
+                  if(req.getRemoteUser()!=null){
                     // Find objects by type or by query
                     try
                     {
@@ -390,6 +395,7 @@ public class LightRestServlet extends HttpServlet
                                 pm.currentTransaction().rollback();
                             }
                             pm.close();
+                            ShepherdPMF.removeShepherdState("LightRestServlet.class");
                         }
                         return;
                     }
@@ -400,6 +406,7 @@ public class LightRestServlet extends HttpServlet
                         resp.getWriter().write(error.toString());
                         resp.setStatus(400);
                         resp.setHeader("Content-Type", "application/json");
+                        ShepherdPMF.removeShepherdState("LightRestServlet.class");
                         return;
                     }
                     catch (NucleusException ex)
@@ -409,6 +416,7 @@ public class LightRestServlet extends HttpServlet
                         resp.getWriter().write(error.toString());
                         resp.setStatus(404);
                         resp.setHeader("Content-Type", "application/json");
+                        ShepherdPMF.removeShepherdState("LightRestServlet.class");
                         return;
                     }
                     catch (RuntimeException ex)
@@ -419,8 +427,19 @@ public class LightRestServlet extends HttpServlet
                         resp.getWriter().write(error.toString());
                         resp.setStatus(404);
                         resp.setHeader("Content-Type", "application/json");
+                        ShepherdPMF.removeShepherdState("LightRestServlet.class");
                         return;
                     }
+                  }
+                  else{
+                    JSONObject error = new JSONObject();
+                    error.put("exception", "You have to log in to GET a full class list of objects.");
+                    resp.getWriter().write(error.toString());
+                    resp.setStatus(400);
+                    resp.setHeader("Content-Type", "application/json");
+                    ShepherdPMF.removeShepherdState("LightRestServlet.class");
+                    return;
+                  }
                 }
 
                 // GET "/{candidateclass}/id" - Find object by id
@@ -440,13 +459,13 @@ public class LightRestServlet extends HttpServlet
                     //resp.getWriter().write(jsonobj.toString());
                     resp.setHeader("Content-Type","application/json");
                     //pm.currentTransaction().commit();
-                    return;
+                    
                 }
                 catch (NucleusObjectNotFoundException ex)
                 {
                     resp.setContentLength(0);
                     resp.setStatus(404);
-                    return;
+                   
                 }
                 catch (NucleusException ex)
                 {
@@ -455,7 +474,7 @@ public class LightRestServlet extends HttpServlet
                     resp.getWriter().write(error.toString());
                     resp.setStatus(404);
                     resp.setHeader("Content-Type", "application/json");
-                    return;
+                    
                 }
                 finally
                 {
@@ -464,6 +483,8 @@ public class LightRestServlet extends HttpServlet
                         pm.currentTransaction().rollback();
                     }
                     pm.close();
+                    ShepherdPMF.removeShepherdState("LightRestServlet.class");
+                    return;
                 }
             }
         }
@@ -476,6 +497,7 @@ public class LightRestServlet extends HttpServlet
                 resp.getWriter().write(error.toString());
                 resp.setStatus(404);
                 resp.setHeader("Content-Type", "application/json");
+                
             }
             catch (JSONException e1)
             {
@@ -1002,7 +1024,9 @@ System.out.println("??? TRY COMPRESS ??");
         private void getPMF(HttpServletRequest req){
             String context="context0";
             context=ServletUtilities.getContext(req);
+            ShepherdPMF.setShepherdState("LightRestServlet.class", "new");
             pmf=ShepherdPMF.getPMF(context);
+            
             this.nucCtx = ((JDOPersistenceManagerFactory)pmf).getNucleusContext();
             thisRequest = req;
         }
