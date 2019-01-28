2efeb5e3f610fd9dfb30e319cfcfd6680099ae16
Jon Van Oast <jon@wildme.org>
2019-01-28 23:21:52 +0000

    cleanup

--- config/markedindividual_uuid_update1_whaleshark.sql
+++ config/markedindividual_uuid_update1_whaleshark.sql
@@ -14,52 +14,20 @@ ALTER TABLE "MARKEDINDIVIDUAL_ENCOUNTERS" DROP CONSTRAINT "SHARK_ENCOUNTERS_FK1"
 ALTER TABLE "MARKEDINDIVIDUAL_ENCOUNTERS" ADD CONSTRAINT "SHARK_ENCOUNTERS_FK1" FOREIGN KEY ("INDIVIDUALID_OID") REFERENCES "MARKEDINDIVIDUAL"("INDIVIDUALID") ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
 END;
 
--- h/t  https://gist.github.com/scaryguy/6269293
-
--- we cant straight up drop the primary key constraint on MARKEDINDIVIDUAL due to foreign key constraints...
-
---ALTER TABLE "MARKEDINDIVIDUAL_ENCOUNTERS" DROP CONSTRAINT "SHARK_ENCOUNTERS_pkey";
---ALTER TABLE "MARKEDINDIVIDUAL_ENCOUNTERS" DROP CONSTRAINT "SHARK_ENCOUNTERS_FK1";
-
 -- this table seems to only appear on some (ancient?) wildbooks... i think the functionality is gone, so we dont
 --  do any other cleanup of id-mapping etc here.  dont worry if you get an error about this
 ALTER TABLE "MARKEDINDIVIDUAL_UNIDENTIFIABLEENCOUNTERS" DROP CONSTRAINT "SHARK_LOGENCOUNTERS_FK1";
 
 
+-- whoa, lets assign random uuids to MarkedIndividual!  welcome to the future.
+--    (this will also bubble over to MARKEDINDIVIDUAL_ENCOUNTERS too)
 UPDATE "MARKEDINDIVIDUAL" SET "INDIVIDUALID" = uuid_generate_v4();
--- ALTER TABLE "MARKEDINDIVIDUAL_ENCOUNTERS" ADD COLUMN "ID_OID" VARCHAR(36);
--- CREATE INDEX "MARKEDINDIVIDUAL_ENCOUNTERS_ID_idx" ON "MARKEDINDIVIDUAL_ENCOUNTERS" ("ID_OID");
-
--- now this updates the join table to have the new ID_OID populated based on corresponding MARKEDINDIVIDUAL
--- h/t  https://stackoverflow.com/a/2766766
---UPDATE "MARKEDINDIVIDUAL_ENCOUNTERS" AS b SET "ID_OID" = a."ID" FROM "MARKEDINDIVIDUAL" AS a WHERE a."INDIVIDUALID" = b."INDIVIDUALID_OID";
-
--- remove old primary key, and add new
---ALTER TABLE "MARKEDINDIVIDUAL" DROP CONSTRAINT "MARKEDINDIVIDUAL_pkey";
---ALTER TABLE "MARKEDINDIVIDUAL" ADD PRIMARY KEY ("ID");
-
--- now add in the new primary key on join table, based on ID & IDX
---ALTER TABLE "MARKEDINDIVIDUAL_ENCOUNTERS" ADD PRIMARY KEY ("ID_OID", "IDX");
-
--- with this populated from above, we can now build a foreign key constraint
---ALTER TABLE "MARKEDINDIVIDUAL_ENCOUNTERS" ADD CONSTRAINT "MARKEDINDIVIDUAL_ENCOUNTERS_FK3" FOREIGN KEY ("ID_OID") REFERENCES "MARKEDINDIVIDUAL"("ID") DEFERRABLE INITIALLY DEFERRED;
-
-
--- drop not null constraint on this column in join table since it will be empty going forward
---  (technically we sh/could drop it altogether)
---ALTER TABLE "MARKEDINDIVIDUAL_ENCOUNTERS" ALTER COLUMN "INDIVIDUALID_OID" DROP NOT NULL;
 
 
 -- now lets update other places that need to know our new ids
 -- NOTE: these fields should probably (or are) be deprecated, in favor of propery obj reference
 UPDATE "ENCOUNTER" AS b SET "INDIVIDUALID" = a."INDIVIDUALID" FROM "MARKEDINDIVIDUAL" AS a WHERE a."LEGACYINDIVIDUALID" = b."INDIVIDUALID";
 UPDATE "ADOPTION" AS b SET "INDIVIDUAL" = a."INDIVIDUALID" FROM "MARKEDINDIVIDUAL" AS a WHERE a."LEGACYINDIVIDUALID" = b."INDIVIDUAL";
-
-
--- this one "would be nice", except fails if (when) there is a bunk indiv id on an encounter.  add after manual cleanup
--- ALTER TABLE "ENCOUNTER" ADD CONSTRAINT "ENCOUNTER_INDIVIDUALID_FK_NEW" FOREIGN KEY ("INDIVIDUALID") REFERENCES "MARKEDINDIVIDUAL"("ID") DEFERRABLE INITIALLY DEFERRED;
-
-
 
 --- TODO migrate the old INDIVIDUALID and ALTERNATEID(s) TO NAMES (MultiValue) !!!!!!!!!!!!!!  FIXME
 ---  note: this is now done via appadmin/migrateMarkedIndividualNames.jsp
