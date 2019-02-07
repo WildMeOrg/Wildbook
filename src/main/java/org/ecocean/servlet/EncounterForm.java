1662caac78be572b9133350be84a9095aeaa3721
Colin W Kingen <colinwkingen@gmail.com>
2019-02-07 11:00:01 -0800

    Validate JPEG in EncounterForm, MediaAssetCreate

--- src/main/java/org/ecocean/servlet/EncounterForm.java
+++ src/main/java/org/ecocean/servlet/EncounterForm.java
@@ -1203,6 +1203,7 @@ System.out.println("ENCOUNTER SAVED???? newnum=" + newnum);
       try{
         ma.addLabel("_original");
         ma.copyIn(tmpFile);
+        ma.validateSourceImage();
         ma.updateMetadata();
         newAnnotations.add(new Annotation(Util.taxonomyString(genus, specificEpithet), ma));
       }
@@ -1240,6 +1241,7 @@ System.out.println("ENCOUNTER SAVED???? newnum=" + newnum);
       try{
         ma.addLabel("_original");
         ma.copyIn(tmpFile);
+        ma.validateSourceImage();
         ma.updateMetadata();
         newAnnotations.add(new Annotation(Util.taxonomyString(genus, specificEpithet), ma));
         System.out.println("Added new annotation for: "+item.getName());
