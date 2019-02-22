bba5eb95658229a9eeb1074df9bf69750585798c
Jon Van Oast <jon@wildme.org>
2019-02-21 16:58:37 -0800

    reshuffle how IAQueryCache functions, blah

--- src/main/java/org/ecocean/identity/IAQueryCache.java
+++ src/main/java/org/ecocean/identity/IAQueryCache.java
@@ -1,24 +1,96 @@
 package org.ecocean.identity;
 import org.ecocean.Annotation;
+import org.ecocean.Encounter;
+import org.ecocean.Shepherd;
 import org.ecocean.cache.QueryCache;
+import org.ecocean.cache.CachedQuery;
 import org.ecocean.cache.QueryCacheFactory;
+import org.json.JSONObject;
+import org.json.JSONArray;
 
 
-public class IAQueryCache extends QueryCache {
-    private static String namePrefix = "IBEISIA.";
-    public synchronized static QueryCache get(String context) {
+public class IAQueryCache {
+    private static String NAME_PREFIX = "IBEISIA.";
+    private static String NAME_TARGETANNOTATIONS = "targetAnnotations";
+
+    public static CachedQuery setTargetAnnotationsCache(String context, Annotation ann, JSONObject jobj) {
+        String qname = generateQueryName(context, ann, NAME_TARGETANNOTATIONS);
+        if (qname == null) {
+            System.out.println("WARNING: IAQueryCache.setTargetAnnotationsCache() could not find query name for " + ann);
+            return null;
+        }
         QueryCache qc = QueryCacheFactory.getQueryCache(context);
-        return (IAQueryCache)qc;
+        CachedQuery q = qc.getQueryByName(qname);
+        if (q == null) {
+            Shepherd myShepherd = new Shepherd(context);
+            myShepherd.beginDBTransaction();
+            q = new CachedQuery(qname, jobj, true, myShepherd);
+            qc.addCachedQuery(q);
+            myShepherd.commitDBTransaction();
+            System.out.println("INFO: IAQueryCache.setTargetAnnotationsCache(" + qname + ") created new CachedQuery id=" + q.getUUID());
+        } else {
+            q.setJSONSerializedQueryResult(jobj, true);
+            System.out.println("INFO: IAQueryCache.setTargetAnnotationsCache(" + qname + ") updated CachedQuery id=" + q.getUUID());
+        }
+        return q;
     }
 
+    public static JSONObject getTargetAnnotationsCache(String context, Annotation ann) {
+        String qname = generateQueryName(context, ann, NAME_TARGETANNOTATIONS);
+        if (qname == null) {
+            System.out.println("WARNING: IAQueryCache.getTargetAnnotationsCache() could not find query name for " + ann);
+            return null;
+        }
+        QueryCache qc = QueryCacheFactory.getQueryCache(context);
+        CachedQuery q = qc.getQueryByName(qname);
+        if (q == null) {
+            System.out.println("INFO: IAQueryCache.getTargetAnnotationsCache(" + qname + ") has no such CachedQuery, sorry!");
+            return null;
+        }
+        return q.getJSONSerializedQueryResult();
+    }
 
+    //NOTE: this assumes ann as been "approved" for sending
+    public static JSONObject addTargetAnnotation(String context, Annotation ann) {
+        JSONObject current = getTargetAnnotationsCache(context, ann);
+        if (current == null) {
+            System.out.println("WARNING: IAQueryCache.addTargetAnnotation() has no current cache whence to add " + ann);
+            return null;
+        }
+        JSONArray tIdArr = current.optJSONArray("database_annot_uuid_list");
+        JSONArray tNameArr = current.optJSONArray("database_annot_name_list");
+        if ((tIdArr == null) && (tNameArr == null)) {
+            System.out.println("WARNING: IAQueryCache.addTargetAnnotation() found empty current arrays!  weird.");
+            return null;
+        }
+        Shepherd myShepherd = new Shepherd(context);
+        myShepherd.beginDBTransaction();
+        String indivId = ann.findIndividualId(myShepherd);
+        myShepherd.rollbackDBTransaction();
+        //cheap fix to handle name-conflict potential: we dont add an annot which is already on list in cache!
+        // TODO ... do this!  :)  :(
+        if (indivId == null) {
+            tNameArr.put(IBEISIA.IA_UNKNOWN_NAME);
+        } else {
+            tNameArr.put(indivId);
+        }
+        tIdArr.put(IBEISIA.toFancyUUID(ann.getAcmId()));
+        current.put("database_annot_uuid_list", tIdArr);
+        current.put("database_annot_name_list", tNameArr);
+        setTargetAnnotationsCache(context, ann, current);
+        return current;
+    }
 
-    private String generateQueryName(Annotation ann) {
+    private static String generateQueryName(String context, Annotation ann, String type) {
         if (ann == null) return null;
-        //FIXME wtf, should we use enc.taxonomyString() i guess?
-        String val = ann.getIAClass();
-        if (val == null) val = "UNKNOWN";
-        return namePrefix + val;
+        Shepherd myShepherd = new Shepherd(context);
+        myShepherd.beginDBTransaction();
+        Encounter enc = ann.findEncounter(myShepherd);
+        myShepherd.rollbackDBTransaction();
+        if (enc == null) return null;
+        String val = enc.getTaxonomyString();
+        if (val == null) return null;
+        return NAME_PREFIX + val.replaceAll(" ", "_");
     }
 }
 
