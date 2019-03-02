45edcc48e333364cbcfcf7cecc5e6e8ebb9b431e
Colin W Kingen <colinwkingen@gmail.com>
2019-03-01 16:24:20 -0800

    Check on ID class before encounter form auto send

--- src/main/java/org/ecocean/identity/IBEISIA.java
+++ src/main/java/org/ecocean/identity/IBEISIA.java
@@ -1458,7 +1458,9 @@ System.out.println("     ---> " + annIds);
                         String aid = annIds.optString(i, null);
                         if (aid == null) continue;
                         Annotation ann = ((Annotation) (myShepherd2.getPM().getObjectById(myShepherd2.getPM().newObjectIdInstance(Annotation.class, aid), true)));
-                        if (ann != null) needIdentifying.add(ann);
+                        if (ann != null&&IBEISIA.validForIdentification(ann, context)) {
+                            needIdentifying.add(ann);
+                        }
                     }
                 }
             }
@@ -1472,6 +1474,7 @@ System.out.println("     ---> " + annIds);
                     myShepherd2.storeNewTask(task);
                 }
             } else {
+                System.out.println("[INFO]: No annotations were suitable for identification. Check resulting identification class(es).");
                 myShepherd2.rollbackDBTransaction();
             }
             myShepherd2.closeDBTransaction();
