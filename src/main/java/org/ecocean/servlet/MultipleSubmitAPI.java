bab6316d16f39209d3c709f2863bf6f1bf523730
Colin W Kingen <colinwkingen@gmail.com>
2019-04-19 15:26:16 -0700

    MultipleSubmit 11: complete basic pipeline, all encs/assets saved with data

--- src/main/java/org/ecocean/servlet/MultipleSubmitAPI.java
+++ src/main/java/org/ecocean/servlet/MultipleSubmitAPI.java
@@ -2,10 +2,14 @@ package org.ecocean.servlet;
 
 
 import org.ecocean.CommonConfiguration;
+import org.ecocean.Util;
 import org.ecocean.Encounter;
 import org.ecocean.Shepherd;
 import org.ecocean.servlet.ServletUtilities;
 import org.ecocean.media.AssetStore;
+import org.ecocean.media.MediaAsset;
+import org.ecocean.media.MediaAssetFactory;
+import org.ecocean.Annotation;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -15,6 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
@@ -27,6 +32,10 @@ import com.google.gson.*;
 
 import org.json.JSONObject;
 import org.json.JSONArray;
+
+import org.joda.time.format.*;
+import org.joda.time.*;
+
 
 @MultipartConfig
 public class MultipleSubmitAPI extends HttpServlet {
@@ -87,7 +96,6 @@ public class MultipleSubmitAPI extends HttpServlet {
         response.setContentType("application/json");
         JSONObject rtn = new JSONObject();
         rtn.put("success", "false");
-        ///System.out.println("Recaptcha checked? : "+request.getParameter("recaptcha-checked"));
         System.out.println("PART NAMES:");
 
         String jsonStr = request.getParameter("json-data");
@@ -104,55 +112,103 @@ public class MultipleSubmitAPI extends HttpServlet {
 
         Collection<Part> partCol = request.getParts();
         ArrayList<Part> params = new ArrayList<>(partCol);
+
         for (Part param : params) {
             String filename = "";
             if (param.getSubmittedFileName()!=null) filename = param.getSubmittedFileName();
             System.out.println("Param- Name:"+param.getName()+" Type: "+param.getContentType()+"  Filename: "+filename);
         }
 
-        //Part rePart = request.getPart("recaptcha-checked");
-        //String reString = rePart.getSubmittedFileName();
-        //System.out.println("Filename reString: "+reString);
-
         if ("true".equals(json.get("recaptcha-checked").getAsString())) {
             try {
+                JsonObject encImgLists = json.getAsJsonObject("enc-image-lists");
+                Shepherd myShepherd = new Shepherd(context);
+                AssetStore astore = AssetStore.getDefault(myShepherd);
                 for (int i=0;i<numEncs;i++) {
-                    // create encounter. 
-                    //retrieve information about images for that encounter
-                    // make assets 
-                    //associate assets
-                    // anns? 
-
-                    Shepherd myShepherd = new Shepherd(context);
-                    AssetStore astore = AssetStore.getDefault(myShepherd);
-                    rtn = postEncountersAndAssets(rtn, context, request);
+                    System.out.println("Making anns for enc "+i);
+                    
+                    try {
+                        Encounter enc = createEncounter(json, i);
+                        myShepherd.storeNewEncounter(enc);
+                        System.out.println("Created Encounter id="+enc.getID());
+                        JsonArray arr = encImgLists.getAsJsonArray(String.valueOf(i));
+                        for (int j=0;j<arr.size();j++) {
+                            String keyNum = arr.get(j).getAsString();
+                            Part encPart = request.getPart("image-file-"+keyNum);
+                            System.out.println("Got part for ann num="+j);
+                            Annotation ann = makeMediaAssetFromPart(encPart, astore, enc, myShepherd);
+                            enc.addAnnotation(ann);
+                            System.out.println("Created Annotation id="+ann.getId());
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Exception creating Encounter and assets!");
+                        e.printStackTrace();
+                    }
                     rtn.put("success", "true");
                 }
             } catch (Exception e) {
                 e.printStackTrace();
+            } finally {
+                // send the created assets to intakeAnnotations? 
             }
         }
         out.println(rtn);
         out.close();
     }
 
-    private JSONObject makeMediaAssetFromPart(Part part, String encId, AssetStore astore) {
-        // Need to get an asset store
-        JSONObject rtn = new JSONObject();
-        return rtn;
+    // Similar to the methods in EncounterForm
+    private Annotation makeMediaAssetFromPart(Part part, AssetStore astore, Encounter enc, Shepherd myShepherd) {
+        JSONObject sParams = astore.createParameters(new File(enc.subdir()+File.separator+part.getSubmittedFileName()));
+        sParams.put("key", Util.hashDirectories(enc.getID())+"/"+part.getName());
+        MediaAsset ma = new MediaAsset(astore, sParams);
+        File uploadFile = ma.localPath().toFile();
+        File uploadDir = uploadFile.getParentFile();
+        if (!uploadDir.exists()) uploadDir.mkdirs();
+        try {
+            System.out.println("Gonna try ma.localPath().toString() --> "+ma.localPath().toString());
+            part.write(ma.localPath().toString());
+        } catch (Exception e) { 
+            e.printStackTrace();
+        }
+        if (uploadFile.exists()) {
+            try {
+                ma.addLabel("_original");
+                ma.copyIn(uploadFile);
+                ma.validateSourceImage();
+
+                ma.updateMetadata();
+            } catch (IOException ioe) {
+                System.out.println("SEVERE: IOException copying image file "+part.getSubmittedFileName()+" to MediaAsset with id="+ma.getId());
+                ioe.printStackTrace();
+            }
+            // oh fuuukk... we need to handle species.
+            MediaAssetFactory.save(ma, myShepherd);
+            System.out.println("Trying to updateStandardChildren...");
+            ma.updateStandardChildren(myShepherd);
+            ma.updateMinimalMetadata();
+            Annotation ann = new Annotation("",ma);
+            myShepherd.storeNewAnnotation(ann); 
+            return ann;
+        }
+        return null;
     }
 
-    private JSONObject postEncountersAndAssets(JSONObject rtn, String context, HttpServletRequest request) {
-
-        //get number encounters
-
-        //get number images 
-
-        //create encounter 1 and metadata, prior (?) to making asset 
-
-        //repeat until done
-
-        return rtn;
+    private Encounter createEncounter(JsonObject json, int encIdx) {
+        String locStr = json.get("enc-data-"+encIdx).getAsJsonObject().get("location").getAsString();
+        String dateStr = json.get("enc-data-"+encIdx).getAsJsonObject().get("date").getAsString();
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("MM/dd/yyyy");
+        DateTime dt = null;
+        if (hasVal(dateStr)) {
+            dt = formatter.parseDateTime(dateStr);
+        }
+        Encounter enc = new Encounter(dt, locStr);
+        String comments = json.get("enc-data-"+encIdx).getAsJsonObject().get("comments").getAsString();
+        if (hasVal(comments)) {
+            enc.setComments(comments);
+        }
+        System.out.println("This locationId for new encounter: "+locStr);
+        System.out.println("The locationId FROM the encounter: "+enc.getLocationID());
+        return enc;
     }
 
     private boolean hasVal(String str) {
