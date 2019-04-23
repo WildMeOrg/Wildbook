e952a0afee3934a15ef2868d73906fe376b569ae
Jon Van Oast <jon@wildme.org>
2019-04-23 20:50:15 +0000

    get non-occurrence encounters in OBISSeamap

--- src/main/java/org/ecocean/opendata/OBISSeamap.java
+++ src/main/java/org/ecocean/opendata/OBISSeamap.java
@@ -37,31 +37,43 @@ public class OBISSeamap extends Share {
     public void generate() throws IOException {
         String outPath = getProperty("outputFile", null);
         if (outPath == null) throw new IllegalArgumentException("must have 'outputFile' set in properties file");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(outPath));
+
         Shepherd myShepherd = new Shepherd(context);
         myShepherd.setAction(this.typeCode() + ".generate");
         myShepherd.beginDBTransaction();
+
         // here we want to export all Occurrences (and their Encounters), and then Occurrence-less Encounters as well
         String jdoql = "SELECT FROM org.ecocean.Occurrence";
         Query query = myShepherd.getPM().newQuery(jdoql);
         Collection c = (Collection) (query.execute());
         List<Occurrence> occs = new ArrayList<Occurrence>(c);
         query.closeAll();
-        myShepherd.rollbackDBTransaction();
-        BufferedWriter writer = new BufferedWriter(new FileWriter(outPath));
         for (Occurrence occ : occs) {
             if (!isShareable(occ)) continue;
             String row = tabRow(occ);
-            if (row != null) writer.write(row);
+            if (row != null) writer.write(row + "\n");
         }
 
-//select count(*) from "ENCOUNTER" left join "OCCURRENCE_ENCOUNTERS" on ("ENCOUNTER"."CATALOGNUMBER" = "OCCURRENCE_ENCOUNTERS"."CATALOGNUMBER_EID") where "OCCURRENCE_ENCOUNTERS"."OCCURRENCEID_OID" is null;
-        
+        // cant figure out how to do this via jdoql.  :/
+        String sql = "SELECT * FROM \"ENCOUNTER\" LEFT JOIN \"OCCURRENCE_ENCOUNTERS\" ON (\"ENCOUNTER\".\"CATALOGNUMBER\" = \"OCCURRENCE_ENCOUNTERS\".\"CATALOGNUMBER_EID\") WHERE \"OCCURRENCE_ENCOUNTERS\".\"OCCURRENCEID_OID\" IS NULL";
+        query = myShepherd.getPM().newQuery("javax.jdo.query.SQL", sql);
+        query.setClass(Encounter.class);
+        c = (Collection) (query.execute());
+        List<Encounter> encs = new ArrayList<Encounter>(c);
+        query.closeAll();
+        for (Encounter enc : encs) {
+            if (!isShareable(enc)) continue;
+            String row = tabRow(enc);
+            if (row != null) writer.write(row + "\n");
+        }
+
         writer.close();
+        myShepherd.rollbackDBTransaction();
         log(outPath + " written by generate()");
     }
 
     public boolean isShareable(Object obj) {
-System.out.println("isShareable-OBJ: " + obj);
         if (obj == null) return false;
         if (obj instanceof Encounter) return isShareable((Encounter)obj);
         if (obj instanceof Occurrence) return isShareable((Occurrence)obj);
@@ -72,7 +84,6 @@ System.out.println("isShareable-OBJ: " + obj);
     public boolean isShareable(Encounter enc) {
         if (enc == null) return false;
         if (getShareAll()) return true;
-System.out.println("isShareable-ENC: " + enc);
         User cu = getCollaborationUser();
         if ((cu != null) && Util.stringExists(cu.getUsername()))
             return Collaboration.canUserAccessEncounter(enc, context, cu.getUsername()); 
@@ -82,7 +93,6 @@ System.out.println("isShareable-ENC: " + enc);
     public boolean isShareable(Occurrence occ) {
         if (occ == null) return false;
         if (getShareAll()) return true;
-System.out.println("isShareable-OCC: " + occ);
         if ((occ.getEncounters() == null) || (occ.getEncounters().size() < 1)) return false;
         for (Encounter enc : occ.getEncounters()) {
             if (!isShareable(enc)) return false;
