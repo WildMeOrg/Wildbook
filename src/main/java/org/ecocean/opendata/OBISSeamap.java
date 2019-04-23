008be610557bbe3d87ddf52ec55317c4f60dbe68
Jon Van Oast <jon@wildme.org>
2019-04-23 20:20:11 +0000

    more elaborate OBIS

--- src/main/java/org/ecocean/opendata/OBISSeamap.java
+++ src/main/java/org/ecocean/opendata/OBISSeamap.java
@@ -1,10 +1,25 @@
 package org.ecocean.opendata;
 
+import org.ecocean.Shepherd;
+import org.ecocean.Encounter;
+import org.ecocean.Occurrence;
+import org.ecocean.Taxonomy;
+import org.ecocean.User;
+import org.ecocean.Util;
+import org.ecocean.security.Collaboration;
+import javax.jdo.Query;
+import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class OBISSeamap extends Share {
 
-    OBISSeamap(final String context) {
+    public OBISSeamap(final String context) {
         super(context);
+        this.init();
     }
 
     public void init() {
@@ -12,14 +27,95 @@ public class OBISSeamap extends Share {
             log("not enabled; exiting init()");
             return;
         }
+        //do these once to get into cache
+        getCollaborationUser();
+        getShareAll();
+        //TODO support organizationId (when Organization makes it to master!)
     }
 
 
-    public void generate() {
+    public void generate() throws IOException {
+        String outPath = getProperty("outputFile", null);
+        if (outPath == null) throw new IllegalArgumentException("must have 'outputFile' set in properties file");
+        Shepherd myShepherd = new Shepherd(context);
+        myShepherd.setAction(this.typeCode() + ".generate");
+        myShepherd.beginDBTransaction();
+        // here we want to export all Occurrences (and their Encounters), and then Occurrence-less Encounters as well
+        String jdoql = "SELECT FROM org.ecocean.Occurrence";
+        Query query = myShepherd.getPM().newQuery(jdoql);
+        Collection c = (Collection) (query.execute());
+        List<Occurrence> occs = new ArrayList<Occurrence>(c);
+        query.closeAll();
+        myShepherd.rollbackDBTransaction();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(outPath));
+        for (Occurrence occ : occs) {
+            if (!isShareable(occ)) continue;
+            String row = tabRow(occ);
+            if (row != null) writer.write(row);
+        }
+
+//select count(*) from "ENCOUNTER" left join "OCCURRENCE_ENCOUNTERS" on ("ENCOUNTER"."CATALOGNUMBER" = "OCCURRENCE_ENCOUNTERS"."CATALOGNUMBER_EID") where "OCCURRENCE_ENCOUNTERS"."OCCURRENCEID_OID" is null;
+        
+        writer.close();
+        log(outPath + " written by generate()");
     }
 
     public boolean isShareable(Object obj) {
+System.out.println("isShareable-OBJ: " + obj);
+        if (obj == null) return false;
+        if (obj instanceof Encounter) return isShareable((Encounter)obj);
+        if (obj instanceof Occurrence) return isShareable((Occurrence)obj);
         return false;
+    }
+
+
+    public boolean isShareable(Encounter enc) {
+        if (enc == null) return false;
+        if (getShareAll()) return true;
+System.out.println("isShareable-ENC: " + enc);
+        User cu = getCollaborationUser();
+        if ((cu != null) && Util.stringExists(cu.getUsername()))
+            return Collaboration.canUserAccessEncounter(enc, context, cu.getUsername()); 
+        return false;
+    }
+
+    public boolean isShareable(Occurrence occ) {
+        if (occ == null) return false;
+        if (getShareAll()) return true;
+System.out.println("isShareable-OCC: " + occ);
+        if ((occ.getEncounters() == null) || (occ.getEncounters().size() < 1)) return false;
+        for (Encounter enc : occ.getEncounters()) {
+            if (!isShareable(enc)) return false;
+        }
+        return true;
+    }
+
+
+    //these are the row (record) for tab-delim output; assuming OBISSeamap flat-file Darwin Core
+    //  NOTE: these do not include trailing newline
+    public String tabRow(Occurrence occ) {
+        if (occ == null) return null;
+        List<String> fields = new ArrayList<String>();
+        fields.add(getGUID(occ.getOccurrenceID()));
+        Taxonomy tx = occ.getTaxonomy();
+        if ((tx == null) || (tx.getScientificName() == null)) {
+            log("cannot share " + occ + " due to invalid Taxonomy!");
+            return null;
+        }
+        fields.add(tx.getScientificName());
+        return String.join("\t", fields);
+    }
+
+    public String tabRow(Encounter enc) {
+        if (enc == null) return null;
+        List<String> fields = new ArrayList<String>();
+        fields.add(getGUID(enc.getCatalogNumber()));
+        if (!Util.stringExists(enc.getTaxonomyString())) {
+            log("cannot share " + enc + " due to invalid Taxonomy!");
+            return null;
+        }
+        fields.add(enc.getTaxonomyString());
+        return String.join("\t", fields);
     }
 
 }
--- src/main/java/org/ecocean/opendata/Share.java
+++ src/main/java/org/ecocean/opendata/Share.java
@@ -9,6 +9,7 @@ import org.ecocean.CommonConfiguration;
 import java.util.Properties;
 import org.ecocean.ShepherdProperties;
 import org.ecocean.Util;
+import org.ecocean.User;
 /*
 import org.ecocean.Annotation;
 import org.ecocean.Taxonomy;
@@ -29,6 +30,11 @@ import java.io.PrintWriter;
 public abstract class Share {
     private static final String PROP_FILE = "opendata.properties";
     private static final String LICENSE_CC_BY_NC = "CC-BY-NC";
+    private static String wbGUID = null;  //cache for getGUID()
+
+    //we cache these (via init()) so that we dont have to read all the time (e.g. in isShareable())
+    private User collaborationUser = null;
+    private Boolean shareAll = null;
 
     protected String context = null;
 
@@ -49,7 +55,7 @@ public abstract class Share {
 
 
     //does the work of making the thing and "sending" it (as applicable)
-    public abstract void generate();
+    public abstract void generate() throws java.io.IOException;
 
     //can this item be shared in this capacity?
     public abstract boolean isShareable(Object obj);
@@ -72,6 +78,37 @@ public abstract class Share {
         } catch (Exception ex) {
             return null;
         }
+    }
+
+    public User getCollaborationUser() {
+        if (collaborationUser != null) return collaborationUser;  //yes, it will check each time if (intentionally) null/unset. :/
+        String uid = getProperty("collaborationUser", null);
+        if (uid == null) return null;
+        Shepherd myShepherd = new Shepherd(context);
+        collaborationUser = myShepherd.getUserByUUID(uid);
+        return collaborationUser;
+    }
+
+    public boolean getShareAll() {
+        if (shareAll != null) return shareAll;
+        shareAll = Util.booleanNotFalse(getProperty("shareAll", "false"));
+        return shareAll;
+    }
+
+    //wildbook universal GUID, cached for convenience
+    public String getGUID() {
+        if (wbGUID != null) return wbGUID;
+        Shepherd myShepherd = new Shepherd(context);
+        myShepherd.beginDBTransaction();
+        wbGUID = CommonConfiguration.getGUID(myShepherd);
+        myShepherd.commitDBTransaction();
+        return wbGUID;
+    }
+
+    //"long" version which is combination of this wildbook uuid + object id (of your choice)
+    public String getGUID(String id) {
+        if (id == null) return null;
+        return getGUID() + ":" + id;
     }
 
     public void log(String msg) {
