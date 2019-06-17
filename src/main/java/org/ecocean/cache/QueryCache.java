46b849282fafc2114312972cf2f5ea0a437bfa24
Jason Holmberg <holmbergius@gmail.com>
2019-06-17 13:40:19 -0700

    Cache: lay down base architecture

---
+++ src/main/java/org/ecocean/cache/QueryCache.java
@@ -0,0 +1,45 @@
+package org.ecocean.cache;
+
+
+import java.util.List;
+import java.util.Map;
+import org.ecocean.cache.StoredQuery;
+import org.ecocean.Shepherd;
+
+//A singleton responsible for storing, retrieving, and eventually executing queries that are desired for caching. It will have all necessary getters and setters for dealing with the cache.
+public class QueryCache {
+  
+  private Map<String,CachedQuery> cachedQueries;
+
+  public QueryCache(){}
+  
+  
+  public CachedQuery getQueryByName(String name, String context){
+    if(cachedQueries==null)loadQueries(context);
+    return cachedQueries.get(name);
+  }
+  
+  
+  public Map<String,CachedQuery> cachedQueries(){return cachedQueries;}
+  
+  public void loadQueries(String context){
+    Shepherd myShepherd=new Shepherd(context);
+    myShepherd.beginDBTransaction();
+    
+    List<StoredQuery> queries=myShepherd.getAllStoredQueries();
+    int numQueries=queries.size();
+    for(int i=0;i<numQueries;i++){
+      StoredQuery sq=queries.get(i);
+      cachedQueries.put(sq.getName(), sq.getCachedQueryCopy());
+    }
+    
+    myShepherd.rollbackDBTransaction();
+    myShepherd.closeDBTransaction();
+  }
+  
+  
+  
+
+
+  
+}
---
+++ src/main/java/org/ecocean/cache/QueryCacheFactory.java
@@ -0,0 +1,26 @@
+package org.ecocean.cache;
+
+import org.ecocean.cache.QueryCache;
+
+public class QueryCacheFactory {
+
+  private static QueryCache qc;
+
+  public synchronized static QueryCache getQueryCache(String context) {
+
+    try {
+      if (qc == null) {
+
+        qc = new QueryCache();
+
+      }
+      qc.loadQueries(context);
+      return qc;
+    } catch (Exception jdo) {
+      jdo.printStackTrace();
+      System.out.println("I couldn't instantiate a QueryCache.");
+      return null;
+    }
+  }
+
+}
