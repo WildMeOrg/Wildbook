56ee3214165b659f07333b23b7135c67ff67e21f
Drew Blount <me@drew.computer>
2019-07-09 14:31:50 +0400

    Fixes a breaking bug---where t.h. did this come from???---where encounters without a location ID got a NPE on the encounter page

--- src/main/java/org/ecocean/servlet/ServletUtilities.java
+++ src/main/java/org/ecocean/servlet/ServletUtilities.java
@@ -383,7 +383,7 @@ public class ServletUtilities {
       if (request.isUserInRole("admin")) {
         isOwner = true;
       }
-      else if (request.isUserInRole(enc.getLocationCode())) {
+      else if (enc.getLocationCode()!=null && request.isUserInRole(enc.getLocationCode())) {
         isOwner = true;
       }
       else if ((((enc.getSubmitterID() != null) && (request.getRemoteUser() != null) && (enc.getSubmitterID().equals(request.getRemoteUser()))))) {
