912e2e044b5f1126a8532f5531fa135e7b50e037
Julian Donovan <donovan21@up.edu>
2019-11-07 20:24:16 -0800

    Added several tests

--- src/main/webapp/javascript/tests/graphAbstract_test.js
+++ src/main/webapp/javascript/tests/graphAbstract_test.js
@@ -7,6 +7,92 @@ QUnit.begin(() => {
     ga = new GraphAbstract(individualID);
 });
 
+let terracedTestEventHooks = {
+    'before': () => {
+	$('#test').append('<div id="testA" class="active"></div>' +
+			  '<div id="testB" style="display: none"></div>');
+    },
+    'after': () => $('#test').empty()
+};
+QUnit.module('showTable()', terracedTestEventHooks, () => {
+    QUnit.test('Display table', t => {
+	ga.showTable('#testA', '#testB');
+	t.ok($('#testA').is(':hidden'));
+	t.ok(!$('#testA').hasClass('active'));
+	t.ok($('#testB').is(':visible'));
+	t.ok($('#testB').hasClass('active'));
+    });
+});
+
+//setupGraph() - Wrapper function, not tested
+
+QUnit.module('addSvg()', {'after': () => $('#test').empty() }, () => {
+    QUnit.test('Append SVG', t => {
+	ga.addSvg('#test');
+	t.equal($('.container').attr('width'), ga.width);
+	t.equal($('.container').attr('height'), ga.height);
+	t.ok($('.container').has('g'))
+    });
+});
+
+//TODO - addLegend(), first this function needs to be implemented
+
+QUnit.module('addTooltip()', {'after': () => $('#test').empty() }, () => {
+    QUnit.test('Append tooltip', t => {
+	ga.addTooltip('#test');
+	t.ok(ga.tooltip);
+	t.equal($('.tooltip').css('opacity'), 0);
+    });
+});
+
+//TODO - Needs work
+/*QUnit.module('drawNodeOutlines()', terracedTestEventHooks, () => {
+    QUnit.test('Create Node', t => {
+	ga.drawNodeOutlines("#test");
+	
+    });
+    });*/
+
+QUnit.module('colorGender()', () => {
+    function validateColor(test, color, colorRef) {
+	test.strictEqual(color, colorRef);
+    }
+
+    QUnit.test('Male nodes', t => {
+	let d = {'data': {'gender': 'male'}};
+	let color = ga.colorGender(d);
+	validateColor(t, color, ga.maleColor);
+    });
+
+    QUnit.test('Female nodes', t => {
+	let d = {'data': {'gender': 'female'}};
+	let color = ga.colorGender(d);
+	validateColor(t, color, ga.femaleColor);
+    });
+
+    QUnit.test('Default gender nodes', t => {
+	let d = {'data': {}};
+	let color = ga.colorGender(d);
+	validateColor(t, color, ga.defGenderColor);
+    });
+});
+
+//TODO - drawNodeSymbols()
+
+//TODO - addNodeText()
+/*let mockedNode;
+QUnit.module('addNodeText()', {'beforeEach': mockedNode = d3.select('body')}, () => {
+    QUnit.test('Add hidden text', t => {
+	let data = [{'data': {'name': 'a'}}];
+	let mockedDataNode = mockedNode.data(data).enter();
+	let isHidden = true;
+	ga.addNodeText(mockedDataNode, isHidden);
+	t.ok($('body.text'));
+	console.log($('body.text'));
+	t.ok($('body.text').css('fill-opacity') === 0); 
+    });
+});*/
+
 QUnit.module('wheelDelta()', {'before': () => d3.event = {'deltaY': 0, 'deltaMode': 1}}, () => {
     QUnit.test('Zoom unchanged', t => {
 	d3.event.deltaY = 0;
@@ -26,6 +112,16 @@ QUnit.module('wheelDelta()', {'before': () => d3.event = {'deltaY': 0, 'deltaMod
 	t.ok(delta > 0); 
     });
 });
+
+QUnit.module('incompleteInfoMessage()', {'after': () => $('#test').empty() }, () => {
+    QUnit.test('Display message', t => {
+	ga.incompleteInfoMessage('#test');
+	t.equal($('#incompleteInfoMsg').text(), 'There are currently no known relationships ' +
+		'for this Marked Individual');
+    });
+});
+
+//TODO - calcNodeSize()
 
 QUnit.module('calcNodeSize()', {'beforeEach': () => ga.radius = null}, () => {
     function checkRadius(test, ga) {
@@ -58,14 +154,9 @@ QUnit.module('calcNodeSize()', {'beforeEach': () => ga.radius = null}, () => {
     });
 });
 
-//TODO - Test
-/*QUnit.module('addTooltip()', () => {
-    QUnit.test('Valid selector', t => {
-	ga.addTooltip("body");
-	t.ok(ga.tooltip);
-	t.equal($('.tooltip').css('opacity'), 0);
-    });
-});*/
+//TODO - getLinkColor()
+
+//TODO - getSizeScalar()
 
 //TODO: Add functionality
 /*QUnit.module('handleMouseOver()', () => {
@@ -74,57 +165,4 @@ QUnit.module('calcNodeSize()', {'beforeEach': () => ga.radius = null}, () => {
     });
 });*/
 
-/*QUnit.module('colorCollapsed()', () => {
-    function validateColor(test, color, colorRef) {
-	test.strictEqual(color, colorRef);
-    }
-    
-    QUnit.test('Non-collapsed children nodes', t => {
-	let d = {'_children': true};
-	let color = ga.colorCollapsed(d);
-	validateColor(t, color, ga.collapsedNodeColor);
-    });
-
-    QUnit.test('Collapsed children nodes', t => {
-	let d = {};
-	let color = ga.colorCollapsed(d);
-	validateColor(t, color, ga.defNodeColor);
-    });
-});*/
-
-QUnit.module('colorGender()', () => {
-    function validateColor(test, color, colorRef) {
-	test.strictEqual(color, colorRef);
-    }
-
-    QUnit.test('Male nodes', t => {
-	let d = {'data': {'gender': 'male'}};
-	let color = ga.colorGender(d);
-	validateColor(t, color, ga.maleColor);
-    });
-
-    QUnit.test('Female nodes', t => {
-	let d = {'data': {'gender': 'female'}};
-	let color = ga.colorGender(d);
-	validateColor(t, color, ga.femaleColor);
-    });
-
-    QUnit.test('Default gender nodes', t => {
-	let d = {'data': {}};
-	let color = ga.colorGender(d);
-	validateColor(t, color, ga.defGenderColor);
-    });
-});
-
-let mockedNode;
-QUnit.module('addNodeText()', {'beforeEach': mockedNode = d3.select("body")}, () => {
-    QUnit.test('Add hidden text', t => {
-	let data = [{'data': {'name': 'a'}}];
-	let mockedDataNode = mockedNode.data(data).enter();
-	let isHidden = true;
-	ga.addNodeText(mockedDataNode, isHidden);
-	t.ok($('body.text'));
-	console.log($('body.text'));
-	t.ok($('body.text').css('fill-opacity') === 0); 
-    });
-});
+//TODO - handleMouseOut()
