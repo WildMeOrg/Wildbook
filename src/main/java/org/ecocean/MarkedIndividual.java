37de9af74f92d67865156bc49e6982338b65e223
Colin W Kingen <colinwkingen@gmail.com>
2019-11-26 16:09:33 -0800

    fix indy ID npe trimming whitespace

--- src/main/java/org/ecocean/MarkedIndividual.java
+++ src/main/java/org/ecocean/MarkedIndividual.java
@@ -2432,10 +2432,6 @@ public Float getMinDistanceBetweenTwoMarkedIndividuals(MarkedIndividual otherInd
     // to find an *exact match* on a name, you can use:   regex = "(^|.*;)NAME(;.*|$)";
     // NOTE: this is case-insentitive, and as such it squashes the regex as well, sorry!
     public static List<MarkedIndividual> findByNames(Shepherd myShepherd, String regex, String genus, String specificEpithet) {
-      
-        // im not mad im just disappointed       
-        genus = genus.trim();
-        specificEpithet = specificEpithet.trim();
 
         int idLimit = 2000;  //this is cuz we get a stack overflow if we have too many.  :(  so kinda have to fail when we have too many
         System.out.println("findByNames regex: "+regex);
@@ -2452,6 +2448,8 @@ public Float getMinDistanceBetweenTwoMarkedIndividuals(MarkedIndividual otherInd
         //System.out.println("findByNames: "+genus+" "+specificEpithet);
         String taxonomyStringFilter="";
         if((genus!=null)&&(specificEpithet!=null)) {
+          genus = genus.trim();
+          specificEpithet = specificEpithet.trim();
           taxonomyStringFilter=" && enc.genus == '"+genus+"' && enc.specificEpithet == '"+specificEpithet+"' VARIABLES org.ecocean.Encounter enc";
         }
         String jdoql = "SELECT FROM org.ecocean.MarkedIndividual WHERE (names.id == " + String.join(" || names.id == ", nameIds)+")"+taxonomyStringFilter;
