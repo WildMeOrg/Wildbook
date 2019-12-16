4ed0cd4136ee9f0e32796e5ebd4e82779fd831a8
Jon Van Oast <jon@wildme.org>
2019-12-16 11:34:11 -0800

    LOCAL: Decision alpha

---
+++ src/main/java/org/ecocean/Decision.java
@@ -0,0 +1,73 @@
+package org.ecocean;
+
+import org.json.JSONObject;
+//import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class Decision {
+
+    private int id;
+    private User user;
+    private Encounter encounter;
+    private long timestamp;
+    private String property;
+    private String value;
+
+    public Decision() {
+        this.timestamp = System.currentTimeMillis();
+    }
+    public Decision(User user, Encounter enc, String property, JSONObject value) {
+        this.timestamp = System.currentTimeMillis();
+        this.user = user;
+        this.encounter = enc;
+        this.property = property;
+        if (value != null) this.value = value.toString();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User u) {
+        user = u;
+    }
+
+    public Encounter getEncounter() {
+        return encounter;
+    }
+    public void setEncounter(Encounter enc) {
+        encounter = enc;
+    }
+
+    public String getProperty() {
+        return property;
+    }
+    public void setProperty(String prop) {
+        property = prop;
+    }
+
+    public JSONObject getValue() {
+        return Util.stringToJSONObject(value);
+    }
+    public void setValue(JSONObject val) {
+        if (val == null) {
+            value = null;
+        } else {
+            value = val.toString();
+        }
+    }
+
+
+
+/*
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append(indexname)
+                .append(readableName)
+                .toString();
+    }
+*/
+
+}
---
+++ src/main/java/org/ecocean/servlet/DecisionStore.java
@@ -0,0 +1,78 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.ecocean.Shepherd;
+import org.ecocean.Decision;
+import org.ecocean.User;
+import org.ecocean.Util;
+import org.ecocean.Encounter;
+import org.ecocean.servlet.ServletUtilities;
+import org.ecocean.AccessControl;
+import org.json.JSONObject;
+
+
+public class DecisionStore extends HttpServlet {
+    @Override
+    public void init(final ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+
+    @Override
+    public void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    public void doPost(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+        String context = ServletUtilities.getContext(request);
+        Shepherd myShepherd = new Shepherd(context);
+        myShepherd.beginDBTransaction();
+        JSONObject jsonIn = ServletUtilities.jsonFromHttpServletRequest(request);
+        PrintWriter out = response.getWriter();
+
+        User user = AccessControl.getUser(request, myShepherd);
+        if (user == null) {
+            response.sendError(401, "access denied");
+            response.setContentType("text/plain");
+            out.println("access denied");
+            return;
+        }
+
+        JSONObject rtn = new JSONObject("{\"success\": false}");
+        String encId = jsonIn.optString("encId", "_FAIL_");
+        String prop = jsonIn.optString("property", null);
+        JSONObject value = jsonIn.optJSONObject("value");
+        Encounter enc = myShepherd.getEncounter(encId);
+        //TODO we could make this check owner of Encounter(s) etc etc
+        if (enc == null) {
+            rtn.put("error", "invalid encId passed");
+        } else if (prop == null) {
+            //TODO could check property names?
+            rtn.put("error", "invalid property passed");
+        } else if (value == null) {
+            rtn.put("error", "invalid value passed");
+        } else {  //good to go?
+            Decision dec = new Decision(user, enc, prop, value);
+            myShepherd.getPM().makePersistent(dec);
+            rtn.put("success", true);
+            rtn.put("decisionId", dec.getId());
+        }
+
+        if (rtn.optBoolean("success", false)) {
+            myShepherd.commitDBTransaction();
+        } else {
+            myShepherd.rollbackDBTransaction();
+        }
+        response.setContentType("text/json");
+        out.println(rtn);
+        out.close();
+    }
+}
