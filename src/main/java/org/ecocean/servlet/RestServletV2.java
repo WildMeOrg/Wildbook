aacd7394d8d77101ee76ac4e2551c9e0af90e156
Jon Van Oast <jon@wildme.org>
2020-03-11 16:32:01 -0700

    verrrrry baby steps into v2 of api

---
+++ src/main/java/org/ecocean/servlet/RestServletV2.java
@@ -0,0 +1,82 @@
+package org.ecocean.servlet;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+import org.ecocean.Util;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+
+public class RestServletV2 extends HttpServlet {
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    public void doOptions(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletUtilities.doOptions(request, response);
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        handleRequest(request, response, Util.stringToJSONObject(request.getParameter("content")));
+    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        JSONObject payload = new JSONObject();
+        try {
+            payload = ServletUtilities.jsonFromHttpServletRequest(request);
+        } catch (Exception ex) {
+            _log("failed to parse json payload from request: " + ex.toString());
+        }
+        handleRequest(request, response, payload);
+    }
+
+    private void handleRequest(HttpServletRequest request, HttpServletResponse response, JSONObject payload) throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin", "*");  //allow us stuff from localhost
+        String context = ServletUtilities.getContext(request);
+        String instanceId = Util.generateUUID();
+
+        if (payload == null) payload = new JSONObject();
+        payload.put("_queryString", request.getQueryString());
+        boolean debug = (payload.optBoolean("_debug", false) || ((request.getQueryString() != null) && request.getQueryString().matches(".*_debug.*")));
+
+        if (debug) _log(instanceId, "payload: " + payload.toString());
+        JSONObject rtn = new JSONObject();
+        rtn.put("success", false);
+/*
+        Shepherd myShepherd = new Shepherd(context);
+        myShepherd.setAction("RestServletV2.handleContent");
+        myShepherd.commitDBTransaction();
+        myShepherd.closeDBTransaction();
+*/
+
+
+        if (debug) {
+            _log(instanceId, "rtn: " + rtn.toString());
+            JSONObject jbug = new JSONObject();
+            jbug.put("payload", payload);
+            jbug.put("instanceId", instanceId);
+            jbug.put("timestamp", System.currentTimeMillis());
+            jbug.put("remoteHost", ServletUtilities.getRemoteHost(request));
+            rtn.put("_debug", jbug);
+        }
+        response.setContentType("application/javascript");
+        PrintWriter out = response.getWriter();
+        out.println(rtn.toString());
+        out.close();
+    }
+
+    private void _log(String msg) {
+        _log("-", msg);
+    }
+    private void _log(String id, String msg) {
+        System.out.println("[RestServletV2:" + id + "] " + msg);
+    }
+
+}
+  
+  
