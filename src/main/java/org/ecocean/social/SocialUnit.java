45f0e62fd4c0b865a05ccb76c26fb7d46ffd9c53
Colin Kingen <colinwkingen@gmail.com>
2020-03-13 15:17:27 -0700

    add membership list to SocialUnit and utility methods

--- src/main/java/org/ecocean/social/SocialUnit.java
+++ src/main/java/org/ecocean/social/SocialUnit.java
@@ -1,6 +1,8 @@
 package org.ecocean.social;
 
 import org.ecocean.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class SocialUnit implements java.io.Serializable {
@@ -11,6 +13,8 @@ public class SocialUnit implements java.io.Serializable {
   private static final long serialVersionUID = 3996955430559204532L;
 
   private String socialUnitName;
+
+  private List<Membership> members = new ArrayList<>();
   
   //default constructor for JDO
   public SocialUnit(){}
@@ -19,14 +23,46 @@ public class SocialUnit implements java.io.Serializable {
     this.socialUnitName=name;
   }
   
-
   //this is a convenience method to get the MarkedIndividuals associated with this community via its Relationship objects
-  public List<MarkedIndividual> getMarkedIndividuals(Shepherd myShepherd){
-      return myShepherd.getAllMarkedIndividualsInCommunity(socialUnitName);
+  public List<MarkedIndividual> getMarkedIndividuals(){
+      //return myShepherd.getAllMarkedIndividualsInCommunity(socialUnitName);
+      List<MarkedIndividual> mis = new ArrayList<>();
+      for (Membership member : members) {
+        mis.add(member.getMarkedIndividual());
+      }
+      return mis;
+  }
+
+  public List<MarkedIndividual> getMarkedIndividuals(Shepherd myShepherd) {
+    return getMarkedIndividuals();
+  }
+
+  public List<Membership> getAllMembers() {
+    return members;
+  }
+
+  public void addMember(Membership mem) {
+    if (!members.contains(mem)) {
+      members.add(mem);
+    }
+  }
+
+  public boolean hasMarkedIndividualAsMember(MarkedIndividual mi) {
+    return getMarkedIndividuals().contains(mi);
+  }
+
+  public Membership getMembershipForMarkedIndividual(MarkedIndividual mi) {
+    for (Membership member : members) {
+      if (member.getMarkedIndividual().getId().equals(mi.getId())) {
+        return member;
+      }
+    }
+    return null;
   }
   
   public String getSocialUnitName(){return socialUnitName;}
   public void setSocialUnitName(String name){socialUnitName=name;}
   
+
   
 }
