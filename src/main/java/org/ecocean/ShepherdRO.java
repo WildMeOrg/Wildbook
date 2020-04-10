8304ef7c027ee3b5618e0373ca0ec084f1360fb7
Jon Van Oast <jon@wildme.org>
2020-04-10 07:51:08 +0000

    oh datanucleus

--- src/main/java/org/ecocean/ShepherdRO.java
+++ src/main/java/org/ecocean/ShepherdRO.java
@@ -2,6 +2,11 @@
     this is a Shepherd-compatible object, which only can be used for reading from db.
     it will use (when available) "jdoconfigReadOnly.properties" to access a separate
     db instance.  this is very handy for (read-only) secondary postgres replication, for example.
+
+    NOTE!   this mess needs to be addressed/fixed before this can function.  this can be done with a custom tweak to the datanucleus-rdbms package
+    https://github.com/datanucleus/datanucleus-rdbms/issues/336
+    *  see:  config/datanucleus-bug-for-ShepherdRO.diff
+
 */
 package org.ecocean;
 
