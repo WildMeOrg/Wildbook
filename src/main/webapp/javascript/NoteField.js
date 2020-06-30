778412d10de2083dd7fb04b970e64a9a641c9881
Jon Van Oast <jon@wildme.org>
2020-06-30 12:11:28 -0700

    code updates

--- src/main/webapp/javascript/NoteField.js
+++ src/main/webapp/javascript/NoteField.js
@@ -121,56 +121,59 @@ wildbook.NoteField = {
 		$("#imgHeight").val('');
 		$("#imgVspace").val('');
 		$("#imgHspace").val('');
-		$("#imAlign").val('');
-		$imgid = this.id = "img_" + Math.round(e.timeStamp)
-		var elem = this;
 		
 		$("#imgWidth").change(function(){
 			var imWidth = $("#imgWidth").val();
 			if(imWidth > 99)
-				$("#" + $imgid).attr('width',imWidth);
+				imgElem.attr('width',imWidth);
 			else
 				alert("Width Must be greater than 100")
 		})
 		$("#imgHeight").change(function(){
 			var imgHeight = $("#imgHeight").val();
 			if(imgHeight > 99)
-				$("#" + $imgid).attr('height',imgHeight);
+				imgElem.attr('height',imgHeight);
 			else
 				alert("Height Must be greater than 100")
 		})
 		$("#imgVspace").change(function(){
 			var imgVspace = $("#imgVspace").val();
 			if(imgVspace > 1)
-				$("#" + $imgid).attr('vspace',imgVspace);
+				imgElem.attr('vspace',imgVspace);
 		})
 		$("#imgHspace").change(function(){
 			var imgHspace = $("#imgHspace").val();
 			if(imgHspace > 1)
-				$("#" + $imgid).attr('hspace',imgHspace);
+				imgElem.attr('hspace',imgHspace);
 		})
 		$("#imAlign").change(function(){
 			var imAlign = $("#imAlign").val();
-			$("#" + $imgid).attr('align', imAlign);
+			if(imAlign == 'middle'){
+				imgElem.attr('class','center')
+			}
+			else{
+				imgElem.attr('align',imAlign);
+				imgElem.attr('class','')
+			}
+			
 		})
 		$("#imgSetBtn").click(function(){
 			$('#addImgAttr').hide();
 		})
 	})
-	$(".ql-editor").on("click","a", function (e){
+	$(".ql-editor").on("click", "a", function (e){
 		e.preventDefault();
 		$('#addLinkAttr').css( 'position', 'absolute' );
 		$('#addLinkAttr').css( 'top', e.pageY + 60);
 		$('#addLinkAttr').css( 'left', e.pageX );
 		$('#addLinkAttr').css( 'z-index','1');
 		$('#addLinkAttr').show();
-		$id = this.id = "link_" + Math.round(e.timeStamp)
-		var linkElem =this;
 		
+		var linkElem = $(this);
 		$("#linkAttr").val('')
-		$("#linkAttr").bind('click',function(e){
+		$("#linkAttr").change(function(){
 			var t = $("#linkAttr").val();
-			$("#" + $id).attr('target',t)
+			linkElem.attr('target',t);
 		})
 		$("#linkSetBtn").click(function(){
 			$('#addLinkAttr').hide();
@@ -199,7 +202,7 @@ wildbook.NoteField = {
 		}
 	})
 
-	$('body').append("<div id='addImgAttr'>	<table><tr><td><input type='number' placeholder='width' id='imgWidth' min='100' style='width:60px;height:30px' /></td><td><input type='number' placeholder='height' id='imgHeight' style='width:60px;height:30px' /></td></tr><tr><td><input type='number' placeholder='hspace' id='imgHspace' style='width:60px;height:30px' /></td><td><input type='number' placeholder='vspace' id='imgVspace' style='width:60px;height:30px' /></td></tr><tr><td><select id='imAlign' style='width:60px;height:30px'><option value='unset'>Unset</option><option value='left'>Left</option><option value='right'>Right</option><option value='top'>Top</option><option value='bottom'>Bottom</option><option value='middle'>Middle</option></select></td><td><input type='button' value='Close' id='imgSetBtn' style='width:60px;height:30px' /></td></tr></table></div>");
+	$('body').append("<div id='addImgAttr'>	<table><tr><td><input type='number' placeholder='width' id='imgWidth' min='100' style='width:60px;height:30px' /></td><td><input type='number' placeholder='height' id='imgHeight' style='width:60px;height:30px' /></td><td><input type='number' placeholder='hspace' id='imgHspace' style='width:60px;height:30px' /></td><td><input type='number' placeholder='vspace' id='imgVspace' style='width:60px;height:30px' /></td><td><select id='imAlign' style='width:60px;height:30px'><option value='unset'>Unset</option><option value='left'>Left</option><option value='right'>Right</option><option value='top'>Top</option><option value='bottom'>Bottom</option><option value='middle'>Middle</option></select></td><td><input type='button' value='Close' id='imgSetBtn' style='width:60px;height:30px' /></td></tr></table></div>");
 	$('#addImgAttr').hide();
 	$('body').append("<div id='addLinkAttr'><table><tr><td><select id='linkAttr' style='width:60px;height:30px'><option value=''>Default</option><option value='_blank'>Blank</option><option value='_self'>Self</option></select></td><td><input type='button' value='Close' id='linkSetBtn' style='width:60px;height:30px' /></td></tr></table></div>");
 	$('#addLinkAttr').hide();
