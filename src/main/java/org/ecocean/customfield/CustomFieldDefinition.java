da384d05fd34fe05fbc9aeb7e0c4ea2182bbd099
Jon Van Oast <jon@wildme.org>
2020-07-30 22:43:22 -0700

    create cfd from json, plus misc

--- src/main/java/org/ecocean/customfield/CustomFieldDefinition.java
+++ src/main/java/org/ecocean/customfield/CustomFieldDefinition.java
@@ -1,26 +1,11 @@
 package org.ecocean.customfield;
 
 import org.ecocean.Util;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-/*
-import org.ecocean.User;
-import org.ecocean.Organization;
-
-import java.util.Set;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Collection;
+import org.ecocean.Shepherd;
+import org.ecocean.DataDefinition;
 import org.json.JSONObject;
-import org.json.JSONArray;
-import javax.servlet.http.HttpServletRequest;
-import java.lang.reflect.Method;
-import java.lang.reflect.Field;
-import java.lang.reflect.Constructor;
-
-*/
+import java.util.List;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 public class CustomFieldDefinition implements java.io.Serializable {
     private String id = null;
@@ -32,14 +17,18 @@ public class CustomFieldDefinition implements java.io.Serializable {
     public CustomFieldDefinition() {
         id = Util.generateUUID();
     }
-    public CustomFieldDefinition(String className, String type, String name) {
+    public CustomFieldDefinition(String className, String type, String name) throws CustomFieldException {
         this();
+        if (!validClassName(className)) throw new CustomFieldException("CustomFieldDefinition() invalid className in constructor");
+        if (!DataDefinition.isValidType(type)) throw new CustomFieldException("CustomFieldDefinition() invalid type in constructor");
         this.className = className;
         this.name = name;
         this.type = type;
     }
-    public CustomFieldDefinition(String className, String type, String name, boolean mult) {
+    public CustomFieldDefinition(String className, String type, String name, boolean mult) throws CustomFieldException {
         this();
+        if (!validClassName(className)) throw new CustomFieldException("CustomFieldDefinition() invalid className in constructor");
+        if (!DataDefinition.isValidType(type)) throw new CustomFieldException("CustomFieldDefinition() invalid type in constructor");
         this.className = className;
         this.name = name;
         this.type = type;
@@ -55,13 +44,15 @@ public class CustomFieldDefinition implements java.io.Serializable {
     public String getType() {
         return type;
     }
-    public void setType(String t) {
+    public void setType(String t) throws CustomFieldException {
+        if (!DataDefinition.isValidType(t)) throw new CustomFieldException("CustomFieldDefinition.setType() passed invalid type");
         type = t;
     }
     public String getClassName() {
         return className;
     }
-    public void setClassName(String c) {
+    public void setClassName(String c) throws CustomFieldException {
+        if (!validClassName(c)) throw new CustomFieldException("CustomFieldDefinition.setClassName() passed invalid className");
         className = c;
     }
     public String getName() {
@@ -77,6 +68,31 @@ public class CustomFieldDefinition implements java.io.Serializable {
         multiple = m;
     }
 
+    public static boolean validClassName(String cn) {
+        if (cn == null) return false;
+        return true;  //FIXME
+    }
+
+    public static CustomFieldDefinition updateCustomFieldDefinition(Shepherd myShepherd, JSONObject defn) throws CustomFieldException {
+        CustomFieldDefinition cfd = fromJSONObject(defn);
+        String id = defn.optString("id", null);
+        //FIXME???: modifying existing cfd is... not easy. we should check if it has been *used at all first* i guess???  and if it is... gulp?
+        if (Util.isUUID(id)) {  //lets see if we are updating....
+            List<CustomFieldDefinition> all = myShepherd.getAllCustomFieldDefinitions();
+            if (!Util.collectionIsEmptyOrNull(all)) for (CustomFieldDefinition c : all) {
+                if (id.equals(c.getId())) throw new CustomFieldException("modification of existing CustomFieldDefinition currently not supported");
+            }
+            cfd.setId(id);  //if we made it this far, is a new cfd but wants to set the id
+        }
+        myShepherd.getPM().makePersistent(cfd);
+        return cfd;
+    }
+    //note: this ignores 'id' passed in!  (it will set own)
+    public static CustomFieldDefinition fromJSONObject(JSONObject defn) throws CustomFieldException {
+        if (defn == null) throw new CustomFieldException("fromJSONObject() passed null");
+        return new CustomFieldDefinition(defn.optString("className", null), defn.optString("type", null), defn.optString("name", null), defn.optBoolean("multiple", false));
+    }
+
     public boolean equals(final Object d2) {
         if (d2 == null) return false;
         if (!(d2 instanceof CustomFieldDefinition)) return false;
@@ -87,6 +103,16 @@ public class CustomFieldDefinition implements java.io.Serializable {
     public int hashCode() {  //we need this along with equals() for collections methods (contains etc) to work!!
         if (id == null) return Util.generateUUID().hashCode();  //random(ish) so we dont get two users with no uuid equals! :/
         return id.hashCode();
+    }
+
+    public JSONObject toJSONObject() {
+        JSONObject j = new JSONObject();
+        j.put("id", id);
+        j.put("name", name);
+        j.put("className", className);
+        j.put("type", type);
+        j.put("multiple", multiple);
+        return j;
     }
 
     public String toString() {
---
+++ src/main/java/org/ecocean/customfield/CustomFieldException.java
@@ -0,0 +1,9 @@
+package org.ecocean.customfield;
+
+public class CustomFieldException extends Exception {
+    public CustomFieldException(String message) {
+        super(message);
+    }
+}
+
+
