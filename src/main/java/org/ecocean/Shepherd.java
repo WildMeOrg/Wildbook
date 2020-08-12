5040df6a899f70974b5db600c3d5602065c8d7ac
Jason Holmberg <holmbergius@gmail.com>
2020-08-12 12:40:55 -0700

    HUSH ubiquitous println

--- src/main/java/org/ecocean/Shepherd.java
+++ src/main/java/org/ecocean/Shepherd.java
@@ -1052,7 +1052,7 @@ public class Shepherd {
     try {
       username = request.getUserPrincipal().toString();
     } catch (Exception e) {
-      System.out.println("Shepherd.getUsername(HttpServletRequest) called with no user logged in");
+      //System.out.println("Shepherd.getUsername(HttpServletRequest) called with no user logged in");
     }
     return username;
   }
