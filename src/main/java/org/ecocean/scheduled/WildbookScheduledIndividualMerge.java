b665fc22e5dd4db17c12f223235ff807012fe2e1
Colin Kingen <colinwkingen@gmail.com>
2020-08-26 16:45:35 -0700

    don't slog the request everywhere, toss secondaryIndividual

--- src/main/java/org/ecocean/scheduled/WildbookScheduledIndividualMerge.java
+++ src/main/java/org/ecocean/scheduled/WildbookScheduledIndividualMerge.java
@@ -1,22 +1,26 @@
 package org.ecocean.scheduled;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.ecocean.MarkedIndividual;
 import org.ecocean.Shepherd;
 
 public class WildbookScheduledIndividualMerge extends WildbookScheduledTask {
 
+    private static final long serialVersionUID = 1L;
+
     MarkedIndividual primaryIndividual;
     MarkedIndividual secondaryIndividual;
 
     // maybe hold users here to inform, or just the initiator?
+    String initiatorName;
 
-    public WildbookScheduledIndividualMerge(MarkedIndividual primaryIndividual, MarkedIndividual secondaryIndividual, long executionTime) {
-        this.scheduledTaskType = "IndividualMerge";
+    public WildbookScheduledIndividualMerge() {} //empty for jdo
+
+    public WildbookScheduledIndividualMerge(MarkedIndividual primaryIndividual, MarkedIndividual secondaryIndividual, long executionTime, String initiatorName) {
+        this.scheduledTaskType = "WildbookScheduledIndividualMerge";
         this.taskScheduledExecutionTimeLong = executionTime;
         this.primaryIndividual = primaryIndividual;
         this.secondaryIndividual = secondaryIndividual;
+        this.initiatorName = initiatorName;
     }
 
     @Override
@@ -24,20 +28,15 @@ public class WildbookScheduledIndividualMerge extends WildbookScheduledTask {
         System.out.println("[WARN]: WildbookScheduledTask.execute() was called without proper arguments for task type "+this.scheduledTaskType+". Failed.");
     }
 
-    @Override 
-    public void execute(Shepherd myShepherd) {
-        execute();
-    }
-
     @Override
-    public void execute(Shepherd myShepherd, HttpServletRequest request) {
+    public void execute(Shepherd myShepherd) {
         try {
             myShepherd.beginDBTransaction();
 
-            mergeIndividuals(primaryIndividual, secondaryIndividual, myShepherd, request);
+            mergeIndividuals(primaryIndividual, secondaryIndividual, myShepherd);
             this.setTaskComplete();
 
-            //update user notifications
+            //TODO update user notifications
 
         } catch (Exception e) {
             myShepherd.rollbackDBTransaction();
@@ -49,12 +48,17 @@ public class WildbookScheduledIndividualMerge extends WildbookScheduledTask {
         
     }
     
-    private void mergeIndividuals(MarkedIndividual primaryIndividual, MarkedIndividual secondaryIndividual, Shepherd myShepherd, HttpServletRequest request) {
+    private void mergeIndividuals(MarkedIndividual primaryIndividual, MarkedIndividual secondaryIndividual, Shepherd myShepherd) {
         if (primaryIndividual!=null&&secondaryIndividual!=null) {
             try {
-                primaryIndividual.mergeAndThrowawayIndividual(secondaryIndividual, request, myShepherd);
+                System.out.println("MergeIndividual task is within execution time. Trying to merge individuals.");
+                primaryIndividual.mergeIndividual(secondaryIndividual, initiatorName);
                 myShepherd.updateDBTransaction();
-                myShepherd.throwAwayMarkedIndividual(secondaryIndividual);
+                MarkedIndividual tempSecondaryIndividual = secondaryIndividual;
+                //avoiding a foreign key error
+                this.secondaryIndividual = null;
+                myShepherd.updateDBTransaction();
+                myShepherd.throwAwayMarkedIndividual(tempSecondaryIndividual);
             } catch (Exception e) {
                 System.out.println("[ERROR]: Could not perform automatic mergeIndividuals action with WildbookScheduledIndividualMerge.");
                 e.printStackTrace();
