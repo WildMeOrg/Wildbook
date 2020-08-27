cc42bcada695cf80f80695afead0e7adffb2c111
Colin Kingen <colinwkingen@gmail.com>
2020-08-27 14:52:12 -0700

    start saving ignored/denied states for merge participant users

--- src/main/java/org/ecocean/scheduled/WildbookScheduledIndividualMerge.java
+++ src/main/java/org/ecocean/scheduled/WildbookScheduledIndividualMerge.java
@@ -1,5 +1,10 @@
 package org.ecocean.scheduled;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.xpath.operations.Bool;
 import org.ecocean.MarkedIndividual;
 import org.ecocean.Shepherd;
 
@@ -7,11 +12,13 @@ public class WildbookScheduledIndividualMerge extends WildbookScheduledTask {
 
     private static final long serialVersionUID = 1L;
 
-    MarkedIndividual primaryIndividual;
-    MarkedIndividual secondaryIndividual;
+    private MarkedIndividual primaryIndividual;
+    private MarkedIndividual secondaryIndividual;
 
-    // maybe hold users here to inform, or just the initiator?
-    String initiatorName;
+    private String initiatorName;
+
+    // participant names, with Boolean list of denied state in position 0, ignored state in position 2.
+    private HashMap<String,List<Boolean>> participantsDeniedIgnored = new HashMap<String,List<Boolean>>();
 
     public WildbookScheduledIndividualMerge() {} //empty for jdo
 
@@ -21,6 +28,7 @@ public class WildbookScheduledIndividualMerge extends WildbookScheduledTask {
         this.primaryIndividual = primaryIndividual;
         this.secondaryIndividual = secondaryIndividual;
         this.initiatorName = initiatorName;
+        this.setParticipants();
     }
 
     @Override
@@ -35,8 +43,6 @@ public class WildbookScheduledIndividualMerge extends WildbookScheduledTask {
 
             mergeIndividuals(primaryIndividual, secondaryIndividual, myShepherd);
             this.setTaskComplete();
-
-            //TODO update user notifications
 
         } catch (Exception e) {
             myShepherd.rollbackDBTransaction();
@@ -67,6 +73,60 @@ public class WildbookScheduledIndividualMerge extends WildbookScheduledTask {
             System.out.println("[ERROR]: Could not perform automatic mergeIndividuals action with WildbookScheduledIndividualMerge due to null candidate individual.");
         }
        
+    }
+
+    // this stuff could probably be moved to another abstract class for tasks requiring user confimation or denial
+    public void setParticipants() {
+        if (primaryIndividual==null||secondaryIndividual==null) {
+            ArrayList<String> primaryIndividualUsernames = primaryIndividual.getAllAssignedUsers();
+            ArrayList<String> secondaryIndividualUsernames = secondaryIndividual.getAllAssignedUsers();
+            List<String> usernameMasterList = new ArrayList<>();
+            if (primaryIndividualUsernames!=null) {
+                usernameMasterList.addAll(primaryIndividualUsernames);
+            }
+            if (secondaryIndividualUsernames!=null) {
+                usernameMasterList.addAll(secondaryIndividualUsernames);
+            }
+            for (String username : usernameMasterList) {
+                List<Boolean> denyIgnore = new ArrayList<>(1);
+    
+                //on instantiation, no one has denied the action
+                denyIgnore.add(0, Boolean.FALSE);
+                //or ignored it
+                denyIgnore.add(1, Boolean.FALSE);
+    
+                participantsDeniedIgnored.put(username, denyIgnore);
+            }
+            System.out.println("List of Scheduled merge participants: ");
+        } else {
+            System.out.println("You cannot set participants on an IndividualMerge task that doesn't have two MarkedIndividuals.");
+        }
+    }
+
+    public void setTaskDeniedStateForUser(String username, boolean denied) {
+        List<Boolean> stateForUser = participantsDeniedIgnored.get(username);
+        if (stateForUser!=null) {
+            stateForUser.add(0, denied);
+        }
+    }
+
+    public void setTaskIgnoredStateForUser(String username, boolean ignored) {
+        List<Boolean> stateForUser = participantsDeniedIgnored.get(username);
+        if (stateForUser!=null) {
+            stateForUser.add(1, ignored);
+        }
+    }
+
+    public Boolean getTaskDeniedStateForUser(String username) {
+        List<Boolean> stateForUser = participantsDeniedIgnored.get(username);
+        if (stateForUser==null) return Boolean.FALSE;
+        return stateForUser.get(0);
+    }
+
+    public Boolean getTaskIgnoredStateForUser(String username) {
+        List<Boolean> stateForUser = participantsDeniedIgnored.get(username);
+        if (stateForUser==null) return Boolean.FALSE;
+        return stateForUser.get(1);
     }
     
 }
