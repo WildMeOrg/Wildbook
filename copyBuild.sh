349d5fd318714f8b112e62172c9f9c4c06631b3b
Flukebook Dev <info@wildme.org>
2020-09-28 22:28:38 +0000

    updates copyBuild.sh to use the *real* new deploy method

--- copyBuild.sh
+++ copyBuild.sh
@@ -2,6 +2,7 @@
 
 echo \ * copying resources...
 sudo service tomcat8 stop
-sudo cp -R target/wildbook..../* /var/lib/tomcat8/webapps/wildbook/
+sudo cp -R target/wildbook-7.0.0-EXPERIMENTAL/* /var/lib/tomcat8/webapps/wildbook/
 sudo service tomcat8 start
 echo \ * copyBuild complete.
+
