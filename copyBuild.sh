86a050a30040125512cfd59558bb2ab095b75906
Flukebook Dev <info@wildme.org>
2020-09-28 22:24:37 +0000

    updates copyBuild to use new target-copying method'

--- copyBuild.sh
+++ copyBuild.sh
@@ -2,6 +2,6 @@
 
 echo \ * copying resources...
 sudo service tomcat8 stop
-sudo cp target/wildbook-7.0.0-EXPERIMENTAL.war /var/lib/tomcat8/webapps/wildbook.war
+sudo cp -R target/wildbook..../* /var/lib/tomcat8/webapps/wildbook/
 sudo service tomcat8 start
 echo \ * copyBuild complete.
