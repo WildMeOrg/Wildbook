59534558cc596d656e8da8169f86923e6f45cf10
Colin Kingen <colinwkingen@gmail.com>
2020-11-06 15:35:14 -0800

    WB-1107 StandardImport allow string for distinguishingScar

--- src/main/java/org/ecocean/servlet/importer/StandardImport.java
+++ src/main/java/org/ecocean/servlet/importer/StandardImport.java
@@ -1092,10 +1092,8 @@ public class StandardImport extends HttpServlet {
 
       // end add to projects
 
-
-  	String scar = getIntAsString(row, "Encounter.distinguishingScar");
+  	String scar = getStringOrInt(row, "Encounter.distinguishingScar");
   	if (scar!=null) enc.setDistinguishingScar(scar);
-
 
   	// SAMPLES
     TissueSample sample = null;
