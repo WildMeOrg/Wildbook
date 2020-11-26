4695d9444185211e5a56e407c66bde5852fafdb4
Jason Holmberg <holmbergius@gmail.com>
2020-11-25 19:06:57 -0800

    FIX: remove annot from mastching tasks before delete

--- src/main/java/org/ecocean/servlet/EncounterRemoveAnnotation.java
+++ src/main/java/org/ecocean/servlet/EncounterRemoveAnnotation.java
@@ -116,13 +116,33 @@ public class EncounterRemoveAnnotation extends HttpServlet {
               //if not trivial but has no sibs, revert to trivial
                 if(!ann.isTrivial() && (ann.getSiblings()==null||ann.getSiblings().size()==0)) {
                  
+                  
+                  
+                  List<Task> iaTasks = Task.getTasksFor(ann, myShepherd);
+                  if (iaTasks!=null&&!iaTasks.isEmpty()) {
+                    for (Task iaTask : iaTasks) {
+                      iaTask.removeObject(ann);
+                      myShepherd.updateDBTransaction();
+                    }
+                  }
+                  
                   Annotation newAnnot=ann.revertToTrivial(myShepherd);
+                  
                   myShepherd.getPM().deletePersistent(ann);
                   myShepherd.updateDBTransaction();
                   res.put("revertToTrivial",true);
                 }
                 //otherwise just delete and move on
                 else if(!ann.isTrivial()) {
+                  
+                  List<Task> iaTasks = Task.getTasksFor(ann, myShepherd);
+                  if (iaTasks!=null&&!iaTasks.isEmpty()) {
+                    for (Task iaTask : iaTasks) {
+                      iaTask.removeObject(ann);
+                      myShepherd.updateDBTransaction();
+                    }
+                  }
+                  
                   enc.removeAnnotation(ann);
                   myShepherd.getPM().deletePersistent(ann);
                   myShepherd.commitDBTransaction();
