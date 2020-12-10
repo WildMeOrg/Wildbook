617a23aa6314cc642a53012dbf9ad3d5158b8ddb
dev@mantamatcher <info@wildme.org>
2020-12-10 01:37:36 +0000

    LOCAL: update copyBuildDirOnly.sh to reflect mm directory in manta branch

--- copyBuildDirOnly.sh
+++ copyBuildDirOnly.sh
@@ -3,9 +3,9 @@
 echo \ ** stopping tomcat...
 sudo service tomcat8 stop &&
 	echo \ *** removing deployed application dir from tomcat webapps directory...&&
-	sudo rm -rf /var/lib/tomcat8/webapps/wildbook &&
+	sudo rm -rf /var/lib/tomcat8/webapps/mm &&
 	echo \ **** copying new application dir to tomcat webapps directory... &&
-	sudo cp -r target/wildbook-7.0.0-EXPERIMENTAL /var/lib/tomcat8/webapps/wildbook &&
+	sudo cp -r target/wildbook-7.0.0-EXPERIMENTAL /var/lib/tomcat8/webapps/mm &&
 	echo \ ***** restarting tomcat... &&
 	sudo service tomcat8 restart &&
 	echo \ * application deployed                              
