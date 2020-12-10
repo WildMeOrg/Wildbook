ae48760aedd234a44a8edafef28e7c9548a8d0cb
Colin Kingen <colinwkingen@gmail.com>
2020-12-10 10:48:51 -0800

    WB-986 add left/right image columns + change some borrowed OBIS code

--- src/main/java/org/ecocean/servlet/export/EncounterSearchExportExcelSimple.java
+++ src/main/java/org/ecocean/servlet/export/EncounterSearchExportExcelSimple.java
@@ -117,7 +117,9 @@ public class EncounterSearchExportExcelSimple extends HttpServlet {
                     "Marked Individual",
                     "Location ID",
                     "Submitter Email Address",
-                    "Date Encounter Submitted"                    
+                    "Date Encounter Submitted",
+                    "Has Left Spot Image",
+                    "Has Right Spot Image"                    
                 };
 
                 for (int i=0;i<headerLabels.length;i++) {
@@ -128,143 +130,207 @@ public class EncounterSearchExportExcelSimple extends HttpServlet {
                 // Excel export =========================================================
                 int count = 0;
 
-                //int numResults = 0;
                 for (int i = 0; i < numMatchingEncounters; i++) {
                     Encounter enc = (Encounter) rEncounters.get(i);
                     if (hiddenData.contains(enc))
                         continue;
                     count++;
-                    //numResults++;
 
-                    // OBIS formt export
-                    Label lNumber = new Label(0, count, enc.getDWCDateLastModified());
-                    sheet.addCell(lNumber);
-                    Label lNumberx1 = new Label(1, count, CommonConfiguration.getProperty("institutionCode", context));
-                    sheet.addCell(lNumberx1);
-                    Label lNumberx2 = new Label(2, count, CommonConfiguration.getProperty("catalogCode", context));
-                    sheet.addCell(lNumberx2);
-                    Label lNumberx3 = new Label(3, count, enc.getEncounterNumber());
-                    sheet.addCell(lNumberx3);
-                    Label lNumberx4 = new Label(4, count, ("http://" + CommonConfiguration.getURLLocation(request)
+                    int colCount = 0;
+
+                    Label labelModified = new Label(colCount, count, enc.getDWCDateLastModified());
+                    sheet.addCell(labelModified);
+                    colCount++;
+
+                    Label labelIntCode = new Label(colCount, count, CommonConfiguration.getProperty("institutionCode", context));
+                    sheet.addCell(labelIntCode);
+                    colCount++;
+
+                    Label labelCatalogCode = new Label(colCount, count, CommonConfiguration.getProperty("catalogCode", context));
+                    sheet.addCell(labelCatalogCode);
+                    colCount++;
+
+                    Label labelEncId = new Label(colCount, count, enc.getEncounterNumber());
+                    sheet.addCell(labelEncId);
+                    colCount++;
+
+                    Label lNumberx4 = new Label(colCount, count, ("http://" + CommonConfiguration.getURLLocation(request)
                             + "/encounters/encounter.jsp?number=" + enc.getEncounterNumber()));
                     sheet.addCell(lNumberx4);
+                    colCount++;
 
                     if ((enc.getGenus() != null) && (enc.getSpecificEpithet() != null)) {
-                        Label lNumberx5 = new Label(5, count, (enc.getGenus() + " " + enc.getSpecificEpithet()));
+                        Label lNumberx5 = new Label(colCount, count, (enc.getGenus() + " " + enc.getSpecificEpithet()));
                         sheet.addCell(lNumberx5);
                     } else if (CommonConfiguration.getProperty("genusSpecies0", context) != null) {
-                        Label lNumberx5 = new Label(5, count,
+                        Label lNumberx5 = new Label(colCount, count,
                                 (CommonConfiguration.getProperty("genusSpecies0", context)));
                         sheet.addCell(lNumberx5);
                     }
+                    colCount++;
+                    
+                    Label labelP = new Label(colCount, count, "P");
+                    sheet.addCell(labelP);
+                    colCount++;
 
-                    Label lNumberx6 = new Label(6, count, "P");
-                    sheet.addCell(lNumberx6);
-                    Calendar toDay = Calendar.getInstance();
-                    int year = toDay.get(Calendar.YEAR);
-                    Label lNumberx7 = new Label(7, count, CommonConfiguration.getProperty("citation", context));
+                    Label lNumberx7 = new Label(colCount, count, CommonConfiguration.getProperty("citation", context));
                     sheet.addCell(lNumberx7);
-                    Label lNumberx8 = new Label(8, count, CommonConfiguration.getProperty("kingdom", context));
-                    sheet.addCell(lNumberx8);
-                    Label lNumberx9 = new Label(9, count, CommonConfiguration.getProperty("phylum", context));
-                    sheet.addCell(lNumberx9);
-                    Label lNumberx10 = new Label(10, count, CommonConfiguration.getProperty("class", context));
-                    sheet.addCell(lNumberx10);
-                    Label lNumberx11 = new Label(11, count, CommonConfiguration.getProperty("order", context));
-                    sheet.addCell(lNumberx11);
-                    Label lNumberx13 = new Label(12, count, CommonConfiguration.getProperty("family", context));
-                    sheet.addCell(lNumberx13);
+                    colCount++;
 
+                    Label lNumberx8 = new Label(colCount, count, CommonConfiguration.getProperty("kingdom", context));
+                    sheet.addCell(lNumberx8);
+                    colCount++;
+
+                    Label lNumberx9 = new Label(colCount, count, CommonConfiguration.getProperty("phylum", context));
+                    sheet.addCell(lNumberx9);
+                    colCount++;
+
+                    Label lNumberx10 = new Label(colCount, count, CommonConfiguration.getProperty("class", context));
+                    sheet.addCell(lNumberx10);
+                    colCount++;
+
+                    Label lNumberx11 = new Label(colCount, count, CommonConfiguration.getProperty("order", context));
+                    sheet.addCell(lNumberx11);
+                    colCount++;
+
+                    Label lNumberx12 = new Label(colCount, count, CommonConfiguration.getProperty("family", context));
+                    sheet.addCell(lNumberx12);
+                    colCount++;
+
+                    Label labelGenus = null;
+                    Label labelSpecies = null;
                     if ((enc.getGenus() != null) && (enc.getSpecificEpithet() != null)) {
-                        Label lNumberx14 = new Label(13, count, enc.getGenus());
-                        sheet.addCell(lNumberx14);
-                        Label lNumberx15 = new Label(14, count, enc.getSpecificEpithet());
-                        sheet.addCell(lNumberx15);
+                        labelGenus = new Label(colCount, count, enc.getGenus());
+                        sheet.addCell(labelGenus);
+
+                        labelSpecies = new Label(colCount+1, count, enc.getSpecificEpithet());
+                        sheet.addCell(labelSpecies);
+
                     } else if (CommonConfiguration.getProperty("genusSpecies0", context) != null) {
                         StringTokenizer str = new StringTokenizer(
                                 CommonConfiguration.getProperty("genusSpecies0", context), " ");
                         if (str.countTokens() > 1) {
-                            Label lNumberx14 = new Label(13, count, str.nextToken());
-                            sheet.addCell(lNumberx14);
-                            Label lNumberx15 = new Label(14, count, str.nextToken());
-                            sheet.addCell(lNumberx15);
+                            labelGenus = new Label(colCount, count, str.nextToken());
+                            sheet.addCell(labelGenus);
+
+                            labelSpecies = new Label(colCount+1, count, str.nextToken());
+                            sheet.addCell(labelSpecies);
                         }
                     }
+                    colCount+=2;
 
                     if (enc.getYear() > 0) {
-                        Label lNumberx19 = new Label(18, count, Integer.toString(enc.getYear()));
-                        sheet.addCell(lNumberx19);
+                        Label label = new Label(colCount, count, Integer.toString(enc.getYear()));
+                        sheet.addCell(label);
                     }
+                    colCount++;
+
                     if (enc.getMonth() > 0) {
-                        Label lNumberx20 = new Label(19, count, Integer.toString(enc.getMonth()));
-                        sheet.addCell(lNumberx20);
+                        Label label = new Label(colCount, count, Integer.toString(enc.getMonth()));
+                        sheet.addCell(label);
                     }
+                    colCount++;
+
                     if (enc.getDay() > 0) {
-                        Label lNumberx21 = new Label(20, count, Integer.toString(enc.getDay()));
-                        sheet.addCell(lNumberx21);
+                        Label label = new Label(colCount, count, Integer.toString(enc.getDay()));
+                        sheet.addCell(label);
                     }
+                    colCount++;
 
                     if (enc.getHour() > -1) {
-                        Label lNumberx22 = new Label(21, count, (enc.getHour() + ":" + enc.getMinutes()));
-                        sheet.addCell(lNumberx22);
+                        Label label = new Label(colCount, count, (enc.getHour() + ":" + enc.getMinutes()));
+                        sheet.addCell(label);
                     }
+                    colCount++;
 
-                    Label lNumberx23 = new Label(22, count, enc.getLocation());
-                    sheet.addCell(lNumberx23);
-                    if ((enc.getDWCDecimalLatitude() != null) && (enc.getDWCDecimalLongitude() != null)) {
-                        Label lNumberx24 = new Label(23, count, enc.getDWCDecimalLongitude());
-                        sheet.addCell(lNumberx24);
-                        Label lNumberx25 = new Label(24, count, enc.getDWCDecimalLatitude());
-                        sheet.addCell(lNumberx25);
-                    }
-                    // check for available locale coordinates
-                    // this functionality is primarily used for data export to iobis.org
-                    else if ((enc.getLocationCode() != null) && (!enc.getLocationCode().equals(""))) {
+                    Label label = new Label(colCount, count, enc.getLocation());
+                    sheet.addCell(label);
+                    colCount++;
+
+                    if (enc.getDWCDecimalLongitude() != null && enc.getDWCDecimalLatitude() != null) {
+                        Label labelLon = new Label(colCount, count, enc.getDWCDecimalLongitude());
+                        sheet.addCell(labelLon);
+                        Label labelLat  = new Label(colCount, count, enc.getDWCDecimalLatitude());
+                        sheet.addCell(labelLat);
+
+                    } else if (enc.getLocationCode() != null && !enc.getLocationCode().equals("")) {
                         try {
                             String lc = enc.getLocationCode();
                             if (props.getProperty(lc) != null) {
                                 String gps = props.getProperty(lc);
                                 StringTokenizer st = new StringTokenizer(gps, ",");
-                                Label lNumberx25 = new Label(24, count, st.nextToken());
-                                sheet.addCell(lNumberx25);
-                                Label lNumberx24 = new Label(23, count, st.nextToken());
+                                Label lNumberx24 = new Label(colCount, count, st.nextToken());
                                 sheet.addCell(lNumberx24);
+                                Label lNumberx25 = new Label(colCount+1, count, st.nextToken());
+                                sheet.addCell(lNumberx25);
                             }
                         } catch (Exception e) {
                             e.printStackTrace();
                             System.out.println("     I hit an error getting locales in searchResults.jsp.");
                         }
                     }
+                    colCount+=2;
+
                     if ((enc.getSex() != null) && (!enc.getSex().equals("unknown"))) {
-                        Label lSex = new Label(25, count, enc.getSex());
+                        Label lSex = new Label(colCount, count, enc.getSex());
                         sheet.addCell(lSex);
                     }
+                    colCount++;
+                    
                     if (enc.getComments() != null) {
-                        Label lNumberx26 = new Label(26, count,
-                                enc.getComments().replaceAll("<br>", ". ").replaceAll("\n", "").replaceAll("\r", ""));
-                        sheet.addCell(lNumberx26);
-                    }
-                    if (enc.getSizeAsDouble() != null) {
-                        Label lNumberx27 = new Label(27, count, enc.getSizeAsDouble().toString());
+                        Label lNumberx27 = new Label(colCount, count,
+                                enc.getComments().replace("<br>", ". ").replaceAll("\n", "").replaceAll("\r", ""));
                         sheet.addCell(lNumberx27);
                     }
-                    if (enc.getIndividual() != null) {
-                        Label lNumberx28 = new Label(28, count, enc.getIndividual().getDisplayName());
+                    colCount++;
+                    
+                    if (enc.getSizeAsDouble() != null) {
+                        Label lNumberx28 = new Label(colCount, count, enc.getSizeAsDouble().toString());
                         sheet.addCell(lNumberx28);
                     }
-                    if (enc.getLocationCode() != null) {
-                        Label lNumberx29 = new Label(29, count, enc.getLocationCode());
+                    colCount++;
+
+                    if (enc.getIndividual() != null) {
+                        Label lNumberx29 = new Label(colCount, count, enc.getIndividual().getDisplayName());
                         sheet.addCell(lNumberx29);
                     }
-                    if (enc.getSubmitterEmail() != null) {
-                        Label lNumberx30 = new Label(30, count, enc.getSubmitterEmail());
+                    colCount++;
+
+                    if (enc.getLocationCode() != null) {
+                        Label lNumberx30 = new Label(colCount, count, enc.getLocationCode());
                         sheet.addCell(lNumberx30);
                     }
+                    colCount++;
 
-                    if (enc.getDWCDateAdded() != null) {
-                        Label lNumberx31 = new Label(31, count, enc.getDWCDateAdded());
+                    if (enc.getSubmitterEmail() != null) {
+                        Label lNumberx31 = new Label(colCount, count, enc.getSubmitterEmail());
                         sheet.addCell(lNumberx31);
                     }
+                    colCount++;
+                    
+                    if (enc.getDWCDateAdded() != null) {
+                        Label lNumberx32 = new Label(colCount, count, enc.getDWCDateAdded());
+                        sheet.addCell(lNumberx32);
+                    }
+                    colCount++;
+
+
+                    if (enc.hasAnnotations()) {
+                        String labelBool = "false";
+                        if (enc.hasLeftSpotImage()) labelBool = "true";
+                        Label lNumberx33 = new Label(colCount, count, labelBool);
+                        sheet.addCell(lNumberx33);
+                    }
+                    colCount++;
+
+                    if (enc.hasAnnotations()) {
+                        String labelBool = "false";
+                        if (enc.hasRightSpotImage()) labelBool = "true";
+                        Label lNumberx34 = new Label(colCount, count, labelBool);
+                        sheet.addCell(lNumberx34);
+                    }
+                    colCount++;
+
 
                 } // end for loop iterating encounters
 
