aa8dc09052e9b3574a84a9700eee01430e2121c2
Ubuntu <ubuntu@unacceptablebehavior.org>
2020-12-14 05:39:22 +0000

    LOCAL minor fix to copyBuildDirOnly.sh

--- copyBuildDirOnly.sh
+++ copyBuildDirOnly.sh
@@ -3,9 +3,9 @@
 echo \ ** stopping tomcat...
 sudo service tomcat8 stop &&
 	echo \ *** removing deployed application dir from tomcat webapps directory...&&
-	sudo rm -rf /var/lib/tomcat8/webapps/giraffe&&
+	sudo rm -rf /var/lib/tomcat8/webapps/giraffe &&
 	echo \ **** copying new application dir to tomcat webapps directory... &&
-	sudo cp -r target/wildbook-7.0.0-EXPERIMENTAL /var/lib/tomcat8/webapps/giraffe&&
+	sudo cp -r target/wildbook-7.0.0-EXPERIMENTAL /var/lib/tomcat8/webapps/giraffe &&
 	echo \ ***** restarting tomcat... &&
 	sudo service tomcat8 restart &&
 	echo \ * application deployed                              
