1786546c07e508658532e8d94872b5a46be1d0bc
Mark <mark.aaron.fisher@gmail.com>
2021-01-09 21:35:24 -0800

    LOCAL copyBuildDirOnly.sh customized for /var/lib/tomcat8/webapps/ncaquariums directory

--- copyBuildDirOnly.sh
+++ copyBuildDirOnly.sh
@@ -3,11 +3,11 @@
 echo \ ** stopping tomcat...
 sudo service tomcat8 stop &&
 	echo \ *** removing deployed application dir from tomcat webapps directory...&&
-	sudo rm -rf /var/lib/tomcat8/webapps/wildbook &&
+	sudo rm -rf /var/lib/tomcat8/webapps/ncaquariums &&
 	echo \ **** copying new application dir to tomcat webapps directory... &&
-	sudo cp -r target/wildbook-7.0.0-EXPERIMENTAL /var/lib/tomcat8/webapps/wildbook &&
-	echo \ ***** changing permissions to /var/lib/tomcat8/webapps/wildbook... &&
-	sudo chown -R tomcat8:tomcat8 /var/lib/tomcat8/webapps/wildbook &&
+	sudo cp -r target/wildbook-7.0.0-EXPERIMENTAL /var/lib/tomcat8/webapps/ncaquariums &&
+	echo \ ***** changing permissions to /var/lib/tomcat8/webapps/ncaquariums... &&
+	sudo chown -R tomcat8:tomcat8 /var/lib/tomcat8/webapps/ncaquariums &&
 	echo \ ***** restarting tomcat... &&
 	sudo service tomcat8 restart &&
 	echo \ * application deployed                              
