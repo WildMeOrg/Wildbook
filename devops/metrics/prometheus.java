4af1f82cda2db5a6d1fc83fdea64a7d37a3774d5
marcial21 <45084026+marcial21@users.noreply.github.com>
2021-02-22 17:11:31 -0800

    Added files for prometheus setup.

--- devops/metrics/prometheus.java
+++ devops/metrics/prometheus.java
@@ -2,9 +2,11 @@ import io.prometheus.client.Counter;
 import io.prometheus.client.hotspot.DefaultExports;
 import io.prometheus.client.exporter.HTTPServer;
 
+//How will data be accessed here....
+
 public class metricsExample {
     //change the strings in this
-    static final Counter functionCounter = Counter.build().name("my_function_calls_total").help("Number of times my_function was called").register();
+    static final Counter functionCounter = Counter.build().name("something").help("# of times something is called? registered? seen? ").register();
 
     static void incrementCounter() 
     {
@@ -23,5 +25,21 @@ public class metricsExample {
     }
 }
 
-//how do we connect this to the wildbook endpoints?
+public class flukebook 
+{
+
+    static final Gauge species = Gauge.build()
+        .name("numSpecies").help("Number of species in Flukebook.").register();
+    static final Gauge users = Gauge.build()
+        .name("numberOfUsers").help("Number of users in Flukebook.").register();
+    static final Guage usersWLoginP = Gauge.build()
+        .name("numberOfUsersWLoginP").help("Number of users with login privileges in Flukebook").register(); 
+    static final Guage usersWOLoginP = Gauge.build()
+        .name("numberOfUsersWOLoginP").help("Number of users with out login privileges in Flukebook").register(); 
+    static final Guage usersActive = Gauge.build()
+        .name("numberofUsersActive").help("Number of active users.").register(); 
+    
+}
+
+//how do we connect this to the wildbook?
 
