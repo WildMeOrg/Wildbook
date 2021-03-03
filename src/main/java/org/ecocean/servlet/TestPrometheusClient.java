9be6a5f2834cd75f05b5502b81bb0a866a2049d0
Jason Holmberg <holmbergius@gmail.com>
2021-03-03 15:06:08 -0800

    Code example for moving data from the Wildbook database to a prometheus client counter

---
+++ src/main/java/org/ecocean/servlet/TestPrometheusClient.java
@@ -0,0 +1,122 @@
+/*
+ * The Shepherd Project - A Mark-Recapture Framework
+ * Copyright (C) 2011 Jason Holmberg
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package org.ecocean.servlet;
+
+import com.oreilly.servlet.multipart.FilePart;
+import com.oreilly.servlet.multipart.MultipartParser;
+import com.oreilly.servlet.multipart.ParamPart;
+import com.oreilly.servlet.multipart.Part;
+
+import org.ecocean.CommonConfiguration;
+import org.ecocean.Encounter;
+import org.ecocean.Shepherd;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.xml.bind.DatatypeConverter;
+
+import io.prometheus.*;
+import io.prometheus.client.Counter;
+
+/**
+ *
+ * This servlet allows the user to upload an extracted, processed patterning file that corresponds to
+ * a previously uploaded set of spots. This file is then used for visualization of the extracted pattern
+ * and visualizations of potentially matched spots.
+ * @author jholmber
+ *
+ */
+public class TestPrometheusClient extends HttpServlet {
+
+
+  public void init(ServletConfig config) throws ServletException {
+    super.init(config);
+  }
+
+
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    doPost(request, response);
+  }
+
+
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    
+    
+    //database connection setup
+    String context="context0";
+    context=ServletUtilities.getContext(request);
+    Shepherd myShepherd = new Shepherd(context);
+    myShepherd.setAction("TestPrometheusSevlet.class");
+
+
+
+    //set up for response
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+
+    //begin db connection
+      myShepherd.beginDBTransaction();
+      try {
+        
+    	//create counter with name and description  
+        final Counter encs = Counter.build()
+            .name("number_encounters").help("Number encounters").register();
+        
+        //get the data from the database
+        int numEncounters=myShepherd.getNumEncounters();
+        
+        //put the data into the database as a double 
+        encs.inc((double)numEncounters);
+        
+
+      } 
+      catch (Exception lEx) {
+    	
+    	//gracefully catch any errors  
+        lEx.printStackTrace();
+        out.println(ServletUtilities.getHeader(request));
+        out.println("<strong>Error:</strong> I was unable to upload your file.");
+        out.println(ServletUtilities.getFooter(context));
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+
+      }
+      finally {
+    	  
+    	//always close DB connections  
+        myShepherd.rollbackDBTransaction();
+        myShepherd.closeDBTransaction();
+      }
+
+    out.close();
+  }
+
+
+}
+
+
