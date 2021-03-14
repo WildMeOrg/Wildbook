d4b5312400591e05f38eb65f72c2ae62ea69c042
marcial21 <45084026+marcial21@users.noreply.github.com>
2021-03-14 13:29:55 -0700

    Adding new file for configuring endpoint.

---
+++ src/main/java/org/ecocean/servlet/MonitoringConfig.java
@@ -0,0 +1,19 @@
+
+@Configuration
+public class MonitoringConfig {
+
+    @Bean
+    SpringBootMetricsCollector springBootMetricsCollector(Collection<PublicMetrics> publicMetrics) {
+
+        SpringBootMetricsCollector springBootMetricsCollector = new SpringBootMetricsCollector(publicMetrics);
+        springBootMetricsCollector.register();
+
+        return springBootMetricsCollector;
+    }
+
+    @Bean
+    ServletRegistrationBean servletRegistrationBean() {
+        DefaultExports.initialize();
+        return new ServletRegistrationBean(new MetricsServlet(), "/prometheus");
+    }
+}
