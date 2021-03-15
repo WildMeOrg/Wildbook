0c54efcb9a6f8d0d6a6fe09929f6b87d6bbd438b
zebra generic <info@wildme.org>
2021-03-15 00:08:32 +0000

    Changes

--- src/main/java/org/ecocean/servlet/TestPrometheusClient.java
+++ src/main/java/org/ecocean/servlet/TestPrometheusClient.java
@@ -47,9 +47,9 @@ import io.prometheus.client.Counter;
 import io.prometheus.client.Gauge;
 import io.prometheus.client.CollectorRegistry; 
 import io.prometheus.client.exporter.MetricsServlet;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
+//import org.eclipse.jetty.server.Server;
+//import org.eclipse.jetty.servlet.ServletContextHandler;
+//import org.eclipse.jetty.servlet.ServletHolder;
 import java.util.*;
 
 /**
