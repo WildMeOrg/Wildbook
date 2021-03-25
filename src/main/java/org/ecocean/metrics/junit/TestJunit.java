d94db7ab05ae7f3421229394a5cc4ae2a6dcd3cd
marcial21 <45084026+marcial21@users.noreply.github.com>
2021-03-24 17:39:14 -0700

    added skeleton for unit tests.

---
+++ src/main/java/org/ecocean/metrics/junit/TestJunit.java
@@ -0,0 +1,18 @@
+package org.ecocean.metrics.junit; 
+
+import org.junit.Test; 
+import static org.junit.Assert.assertEquals;
+
+import org.ecocean.metrics.Prometheus; 
+
+public class TestJunit 
+{
+  String messsage = "Hello worl";
+  Prometheus promObject = new Prometheus(); 
+  
+  @Test
+  public void testPrintMessage()
+  {
+    assertEquals(messsage, "Hello worl"); 
+  }
+}
