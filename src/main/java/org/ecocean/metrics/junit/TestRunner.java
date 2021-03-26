082a61b53a9b24c4c8ba83f58c2b703aa2d5d63f
marcial21 <45084026+marcial21@users.noreply.github.com>
2021-03-26 16:04:53 -0700

    Trying something else

--- src/main/java/org/ecocean/metrics/junit/TestRunner.java
+++ src/main/java/org/ecocean/metrics/junit/TestRunner.java
@@ -1,20 +1,20 @@
-package org.ecocean.metrics.junit; 
+// package org.ecocean.metrics.junit; 
 
-import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
-import org.junit.runner.notification.Failure;
+// import org.junit.runner.JUnitCore;
+// import org.junit.runner.Result;
+// import org.junit.runner.notification.Failure;
 
-public class TestRunner
-{
-  public static void main(String[] args)
-  {
-    Result result = JUnitCore.runClasses(TestJunit.class);
+// public class TestRunner
+// {
+//   public static void main(String[] args)
+//   {
+//     Result result = JUnitCore.runClasses(TestJunit.class);
     
-    for(Failure failure : result.getFailures())
-    {
-      System.out.println(failure.toString()); 
-    }
+//     for(Failure failure : result.getFailures())
+//     {
+//       System.out.println(failure.toString()); 
+//     }
     
-    System.out.println(result.wasSuccessful());
-  }
-}
+//     System.out.println(result.wasSuccessful());
+//   }
+// }
