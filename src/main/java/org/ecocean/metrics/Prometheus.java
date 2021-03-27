f1d39efa1b275f48608e189cfcca527efb4f0e22
schibel21 <42845594+schibel21@users.noreply.github.com>
2021-03-26 17:56:21 -0700

    Fix 7

--- src/main/java/org/ecocean/metrics/Prometheus.java
+++ src/main/java/org/ecocean/metrics/Prometheus.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.ecocean.Shepherd;
+import org.ecocean.Encounter;
 import org.ecocean.User;
 import org.ecocean.media.MediaAsset;
 import org.ecocean.media.MediaAssetSet;
