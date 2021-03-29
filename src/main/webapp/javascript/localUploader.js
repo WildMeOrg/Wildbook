2538426780b28116bd79e81461c116c64cd16845
Mark <mark.aaron.fisher@gmail.com>
2021-03-29 16:24:59 -0700

    cherry pick WB-652 fixes

--- src/main/webapp/javascript/localUploader.js
+++ src/main/webapp/javascript/localUploader.js
@@ -100,7 +100,9 @@ console.log('%d %o', i, files[i]);
 		//flow.assignDrop(document.getElementById('dropTarget'));
 
 		flow.on('fileAdded', function(file, event){
-          file.name = file.name.replace(/[^a-zA-Z0-9\. ]/g, "");
+          if(file && file.name){
+              file.name = file.name.replace(/[^a-zA-Z0-9\. ]/g, "");
+          }
           console.log('added %o %o', file, event);
 		});
 		flow.on('fileProgress', function(file, chunk){
