f8896c38c04ae255ab0995a8fa02d0221f766eb9
schibel21 <42845594+schibel21@users.noreply.github.com>
2021-03-29 09:54:28 -0700

    Fix 2

--- src/main/java/org/ecocean/metrics/Prometheus.java
+++ src/main/java/org/ecocean/metrics/Prometheus.java
@@ -11,6 +11,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.Vector;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
