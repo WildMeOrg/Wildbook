88da8f7c30e27adb957c81d1527b68ed1a9dbf05
schibel21 <42845594+schibel21@users.noreply.github.com>
2021-04-06 16:55:05 -0700

    Fix 8

--- src/main/java/org/ecocean/metrics/Prometheus.java
+++ src/main/java/org/ecocean/metrics/Prometheus.java
@@ -141,7 +141,7 @@ public class Prometheus
             // out.println("<p> Genues Types: "+genusesNamej.get(j)+"</p>");
 
             ArrayList<Encounter> allEncSpecies = ms.getAllEncountersForSpecies(genuesNames.get(i), specieNames.get(j));
-            int totalEncsSpecies = allEncsSpecies.size();
+            int totalEncsSpecies = allEncSpecies.size();
             this.encsSpecies.inc((double)totalEncsSpecies);
             out.println("<p> Number of encounters by Species, for Species" +specieNames.get(i)+ "is: "+this.encsSpecies.get()+"</p>");
 
