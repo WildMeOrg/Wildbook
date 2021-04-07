920d4dd330d5930a0fdc24663922ce9da3e86af0
marcial21 <45084026+marcial21@users.noreply.github.com>
2021-04-06 20:41:23 -0700

    new constructor for prometheus.java

--- src/main/java/org/ecocean/metrics/junit/TestJunit.java
+++ src/main/java/org/ecocean/metrics/junit/TestJunit.java
@@ -3,16 +3,30 @@ package org.ecocean.metrics.junit;
 import org.junit.Test; 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+import java.io.PrintWriter;
+
+import org.ecocean.Shepherd;
 import org.ecocean.metrics.Prometheus; 
 
 public class TestJunit 
 {
   String messsage = "Hello worl";
-  //Prometheus promObject = new Prometheus(); 
+  Shepherd myShepherd;
+  File myFile;
+  PrintWriter pw; 
+  Prometheus promObject; 
 
   @Test
   public void testPrintMessage()
   {
     assertEquals(messsage, "Hello worl"); 
   }
+  
+  @Test
+  public void testSetNumberOfUsers()
+  {
+    assertEquals(messsage, "hi");
+  }
+  
 }
