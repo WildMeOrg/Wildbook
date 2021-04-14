3cf353f77774c6f1e743becc47439f22c50f1ade
Ubuntu <ubuntu@Staging-Flukebook.uunnmh44rgiejgi2jpqacreiph.xx.internal.cloudapp.net>
2021-04-14 18:30:41 +0000

    WB-652 limit sanitization of cells in the spreadsheet to those ending in mediaAsset\d+. Also fixes same issue in WB-1591 AND removes sleep issue in WB-1572

--- src/main/java/org/ecocean/servlet/importer/StandardImport.java
+++ src/main/java/org/ecocean/servlet/importer/StandardImport.java
@@ -50,6 +50,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map.Entry;
+import java.util.regex.Pattern;
 
 //import com.amazonaws.services.route53.model.GetGeoLocationRequest;
 
@@ -162,7 +164,6 @@ public class StandardImport extends HttpServlet {
     //Thus MUST be full path, such as: /import/NEAQ/converted/importMe.xlsx
     String filename = request.getParameter("filename");
     if(Util.stringExists(filename)){
-      filename = filename.replaceAll("[^a-zA-Z0-9\\. ]", "");
       System.out.println("Filename? = "+filename);
     }
 
@@ -1323,7 +1324,6 @@ public class StandardImport extends HttpServlet {
     }
 
     String localPath = getString(row, "Encounter.mediaAsset"+i);
-    if(Util.stringExists(localPath)) localPath = localPath.replaceAll("[^a-zA-Z0-9\\. ]", "");
 
     if (isUserUpload) {
       // user uploads currently flatten all images into a folder (TODO fix that!) so we trim extensions
@@ -2006,17 +2006,19 @@ System.out.println("use existing MA [" + fhash + "] -> " + myAssets.get(fhash));
   public String getString(final Row row, final int i) {
     System.out.println("Calling getString on row "+row.getRowNum()+" with cell "+i+" value "+String.valueOf(row.getCell(i)));
     final Cell cell = row.getCell(i);
+    String colNameOfCell = getKeyByValue(colIndexMap, cell.getColumnIndex()); //get the key of colIndexMap from it's index value.
     String str = null;
     try {
       if (cell!=null&&cell.getCellType()==Cell.CELL_TYPE_STRING) {
         System.out.println("Current cell: "+cell.toString()+" Current row: "+cell.getRowIndex()+" Current col: "+cell.getColumnIndex());
         str = cell.getStringCellValue();
-        if(Util.stringExists(str)) str = str.replaceAll("[^a-zA-Z0-9\\. ]", "");
+        if(Pattern.matches(".*mediaAsset\\d+$", colNameOfCell)){ //only column names ending with mediaAssets followed by digits should return true here and get sanitized below
+          if(Util.stringExists(str)) str = str.replaceAll("[^a-zA-Z0-9\\. ]", "");
+      }
       }
       // not ideal, but maybe get something
       if (str==null&&cell!=null) {
         str = cell.toString();
-        if(Util.stringExists(str)) str = str.replaceAll("[^a-zA-Z0-9\\. ]", "");
       }
     } catch (Exception e) {
       // it should be basically impossible to get here. this is not a challenge.
@@ -2029,6 +2031,15 @@ System.out.println("use existing MA [" + fhash + "] -> " + myAssets.get(fhash));
     }
     feedback.logParseValue(i, str, row);
     return str.trim();
+  }
+
+  public static <T, E> T getKeyByValue(Map<T, E> map, E value) {
+    for (Entry<T, E> entry : map.entrySet()) {
+      if (Objects.equals(value, entry.getValue())) {
+        return entry.getKey();
+      }
+    }
+    return null;
   }
 
   public Boolean getBooleanFromString(Row row, int i) {
