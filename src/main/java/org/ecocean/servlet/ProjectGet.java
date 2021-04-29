a414be29b052a61341c9a155d2d5cc32630b94a0
Jason Holmberg <holmbergius@gmail.com>
2021-04-29 09:57:49 -0700

    WB-1615: part 2 (remove printlns)

--- src/main/java/org/ecocean/servlet/ProjectGet.java
+++ src/main/java/org/ecocean/servlet/ProjectGet.java
@@ -55,7 +55,7 @@ public class ProjectGet extends HttpServlet {
             res.put("success",false);
             JSONObject j = ServletUtilities.jsonFromHttpServletRequest(request);
 
-            //System.out.println("Here 1");
+            
             
             boolean getEncounterMetadata = false;
             String getEncounterMetadataOpt = j.optString("getEncounterMetadata", null);
@@ -68,7 +68,7 @@ public class ProjectGet extends HttpServlet {
                 getUserIncrementalIds = true;
             }
 
-            //System.out.println("Here 2");
+            
             
             String projectUUID = null;
             String ownerId = null;
@@ -298,38 +298,38 @@ public class ProjectGet extends HttpServlet {
             }
 
             //get specific project
-            //System.out.println("Here 7");
+   
             projectIdPrefix = j.optString("projectIdPrefix", null);
             projectUUID = j.optString("projectUUID", null);
             if ((Util.stringExists(projectIdPrefix)|| Util.stringExists(projectUUID))&&!complete) {
-              //System.out.println("Here 8");
+            
                 Project project = null;
                 if (Util.stringExists(projectIdPrefix)) {
                     project = myShepherd.getProjectByProjectIdPrefix(projectIdPrefix);
-                    //System.out.println("Here 8a");
+                 
                 }
                 if (Util.stringExists(projectUUID)) {
                     project = myShepherd.getProject(projectUUID);
-                    //System.out.println("Here 8b");
+                    
                 }
 
                 JSONArray projectArr = new JSONArray();
-                //System.out.println("Here 9");
+                
                 if (project!=null) {
-                  //System.out.println("Here 9a");
+                 
                     if (getEncounterMetadata) {
-                      //System.out.println("Here 9b");
+                    
                         projectArr.put(project.asJSONObjectWithEncounterMetadata(myShepherd, request));
-                        //System.out.println("Here 9c");
+                      
                     } else {
                         projectArr.put(project.asJSONObject());
-                        //System.out.println("Here 9d");
+                       
                     }
                 }
-                //System.out.println("Here 10");
+               
                 if (getEditPermission!=null&&"true".equals(getEditPermission)) {
                     User user = myShepherd.getUser(request);
-                    //System.out.println("Here 10a");
+                   
                     res.put("userCanEdit","false");
                     if (user!=null&&(user.hasRoleByName("admin", myShepherd)||user.getId().equals(project.getOwnerId()))) {
                         res.put("userCanEdit","true");
@@ -339,9 +339,9 @@ public class ProjectGet extends HttpServlet {
                 response.setStatus(HttpServletResponse.SC_OK);
                 res.put("success",true);
                 complete = true;
-                //System.out.println("Here 11");
+                
             }
-            //System.out.println("Here 12");
+           
             out.println(res);
             out.close();
 
