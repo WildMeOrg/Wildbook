e7213a59e9d0af90c5f6aea73007fcdca8a32b15
LAPTOP-QCOBLT72\Aditya <Aditya.Chaubey@hotmail.com>
2021-07-16 00:41:16 +0530

    Adding Validate User Api

---
+++ src/main/java/org/ecocean/servlet/ValidateUser.java
@@ -0,0 +1,30 @@
+package org.ecocean.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ValidateUser extends HttpServlet {
+  
+  @Override
+  public void init(final ServletConfig config) throws ServletException {
+      super.init(config);
+  }
+
+
+  @Override
+  public void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+      doPost(request, response);
+  } 
+  
+  @Override
+  public void doPost(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+    System.out.print(request.getParameter("username"));
+  }
+    
+
+}
