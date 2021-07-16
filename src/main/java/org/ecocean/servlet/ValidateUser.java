cf919b7bfd74710248f68f4423d66dfa72191500
LAPTOP-QCOBLT72\Aditya <Aditya.Chaubey@hotmail.com>
2021-07-16 13:12:17 +0530

    test

--- src/main/java/org/ecocean/servlet/ValidateUser.java
+++ src/main/java/org/ecocean/servlet/ValidateUser.java
@@ -1,3 +1,7 @@
+//Validating user name at the first page before we got on to the demographic survey so that user 
+
+
+
 package org.ecocean.servlet;
 
 import java.io.IOException;
@@ -7,6 +11,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.ecocean.Role;
+import org.ecocean.Shepherd;
+import org.ecocean.User;
+import org.ecocean.Util;
 
 public class ValidateUser extends HttpServlet {
   
@@ -23,10 +32,29 @@ public class ValidateUser extends HttpServlet {
   
   @Override
   public void doPost(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
-    System.out.print("Testing for username");
-    System.out.print(request.getParameter("username"));
+    System.out.println("Testing for username");
+    System.out.println(request.getParameter("username"));
     
   }
-    
+  
+  public static User registerUser(Shepherd myShepherd, String username, String email, String pw1, String pw2) throws java.io.IOException {
+    if (!Util.stringExists(username)) throw new IOException("Username already exists");
+    username = username.trim();
+    if (!Util.isValidEmailAddress(email)) throw new IOException("Invalid email format");
+    if (!Util.stringExists(pw1) || !Util.stringExists(pw2) || !pw1.equals(pw2)) throw new IOException("Password invalid or do not match");
+    if (pw1.length() < 8) throw new IOException("Password is too short");
+    User exists = myShepherd.getUser(username);
+    if (exists == null) exists = myShepherd.getUserByEmailAddress(email);
+    if ((exists != null) || username.equals("admin")) throw new IOException("Invalid username/email");
+    String salt = Util.generateUUID();
+    String hashPass = ServletUtilities.hashAndSaltPassword(pw1, salt);
+    User user = new User(username, hashPass, salt);
+    user.setEmailAddress(email);
+    user.setNotes("<p data-time=\"" + System.currentTimeMillis() + "\">created via registration.</p>");
+    Role role = new Role(username, "cat_walk_volunteer");
+    role.setContext(myShepherd.getContext());
+    myShepherd.getPM().makePersistent(role);
+    return user;
+  }
 
 }
