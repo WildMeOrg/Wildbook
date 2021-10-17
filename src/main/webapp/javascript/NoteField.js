4b3ec1495717d2d139d7b0d97fd4cae21e0fe4e6
hardikdeveloper <hardikps.work@gmail.com>
2021-10-17 19:35:21 +0530

    test

--- src/main/webapp/javascript/NoteField.js
+++ src/main/webapp/javascript/NoteField.js
@@ -22,8 +22,8 @@ wildbook.NoteField = {
                 return node;
             }
         }
-        /*Quill.register({ 'formats/link': MyLink,
-        	'modules/htmlEditButton': htmlEditButton} );*/
+        Quill.register({ 'formats/link': MyLink,
+        	'modules/htmlEditButton': htmlEditButton} );
 
         wildbook.NoteField.quill[id] = new Quill('#id-' + id, {
             modules: {
@@ -44,6 +44,17 @@ wildbook.NoteField = {
 	            ['clean']      
                 ]
             },
+            htmlEditButton: {
+                debug: true, // logging, default:false
+                msg: "Edit the content in HTML format", //Custom message to display in the editor, default: Edit HTML here, when you click "OK" the quill editor's contents will be replaced
+                okText: "Ok", // Text to display in the OK button, default: Ok,
+                cancelText: "Cancel", // Text to display in the cancel button, default: Cancel
+                buttonHTML: "&lt;&gt;", // Text to display in the toolbar button, default: <>
+                buttonTitle: "Show HTML source", // Text to display as the tooltip for the toolbar button, default: Show HTML source
+                syntax: false, // Show the HTML with syntax highlighting. Requires highlightjs on window.hljs (similar to Quill itself), default: false
+                prependSelector: 'div#myelement', // a string used to select where you want to insert the overlayContainer, default: null (appends to body),
+                editorModules: {} // The default mod
+              },
             theme: 'snow'
         });
         var menus = '<span class="ql-formats">';
