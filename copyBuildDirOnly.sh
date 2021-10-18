6d567952fa699ebb7b923e51f2bac45266e6547d
Ubuntu <ubuntu@saimaaSeal2.k4fv4qajfpaubbm24zbggp1onb.fx.internal.cloudapp.net>
2021-10-18 21:58:42 +0000

    change copyBuild script to Wildbook6 for saimaa

--- copyBuildDirOnly.sh
+++ copyBuildDirOnly.sh
@@ -5,7 +5,7 @@ sudo service tomcat8 stop &&
 	echo \ *** removing deployed application dir from tomcat webapps directory...&&
 	sudo rm -rf /var/lib/tomcat8/webapps/wildbook &&
 	echo \ **** copying new application dir to tomcat webapps directory... &&
-	sudo cp -r target/wildbook-7.0.0-EXPERIMENTAL /var/lib/tomcat8/webapps/wildbook &&
+	sudo cp -r target/wildbook-6.0.0-FINAL /var/lib/tomcat8/webapps/wildbook &&
 	echo \ ***** changing permissions to /var/lib/tomcat8/webapps/wildbook... &&
 	sudo chown -R tomcat8:tomcat8 /var/lib/tomcat8/webapps/wildbook &&
 	echo \ ***** restarting tomcat... &&
