1534665c4937f418973893379410c47aabb8d7ce
rutvikmehta <rutvik106@live.com>
2021-12-03 17:19:59 +0530

    changed code to remove flag.

--- src/main/java/org/ecocean/servlet/DecisionStore.java
+++ src/main/java/org/ecocean/servlet/DecisionStore.java
@@ -120,11 +120,11 @@ public class DecisionStore extends HttpServlet {
 
     private void deleteFlag(HttpServletResponse response, PrintWriter out, Shepherd myShepherd, JSONObject rtn, Encounter enc, String value) {
         try {
-            String jdoql = "DELETE FROM DECISION WHERE ENCOUNTER_CATALOGNUMBER_OID='" + enc.getCatalogNumber() + "' AND VALUE='" + value + "' AND PROPERTY LIKE 'flag'";
+            String jdoql = "SELECT FROM DECISION WHERE ENCOUNTER_CATALOGNUMBER_OID='" + enc.getCatalogNumber() + "' AND VALUE='" + value + "' AND PROPERTY LIKE 'flag'";
             rtn.put("query", jdoql);
             rtn.put("success", true);
             Query query = myShepherd.getPM().newQuery(jdoql);
-            query.execute();
+            query.deletePersistentAll();
             query.closeAll();
 
         }catch (Exception e){
