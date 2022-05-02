b56255424ebe0c5fe22cbc898ff7a1704148aa00
holmbergius <holmbergius@gmail.com>
2022-05-02 12:11:43 -0700

    WB-1962: Use newer task inspection code

--- src/main/java/org/ecocean/servlet/GetCurrentIAInfo.java
+++ src/main/java/org/ecocean/servlet/GetCurrentIAInfo.java
@@ -115,25 +115,26 @@ public class GetCurrentIAInfo extends HttpServlet {
                ArrayList<Task> tasks=new ArrayList<Task>();
                List<Task> relatedTasks = Task.getTasksFor(ann, myShepherd);
                if(relatedTasks!=null && relatedTasks.size()>0){
-                 for(Task task:relatedTasks){  
-                   if(task.getParent()!=null && task.getParent().getChildren().size()==1){
-                        //System.out.println("I am a task with only one algorithm");
-                        if(!tasks.contains(task)){
-                            tasks.add(task);
-                        }
+                 for(Task task:relatedTasks){
+                               
+                     if(task.getParent()!=null && task.getParent().getChildren().size()==1 && task.getParameters()!=null && task.getParameters().has("ibeis.identification")){
+                                 //System.out.println("I am a task with only one algorithm");
+                                 if(!tasks.contains(task)){
+                                     tasks.add(task);
+                                  }
+                     }
+                     else if(task.getChildren()!=null && task.getChildren().size()>0 && (task.getParent()!=null && task.getParent().getChildren().size()<=1)){
+                                 //System.out.println("I am a task with child ID tasks.");
+                                 if(!tasks.contains(task)){
+                                     tasks.add(task);
+                                  }
+                     }
                    }
-                   else if(task.getChildren()!=null && (task.getParent()!=null && task.getParent().getChildren().size()<=1)){
-                        //System.out.println("I am a task with child ID tasks.");
-                        if(!tasks.contains(task)){
-                            tasks.add(task);
-                         }
-                    }
-                 }
                   Comparator<Task> byRanking = 
                       (Task tsk1, Task tsk2) -> Long.compare(tsk1.getCreatedLong(), tsk2.getCreatedLong());
                   Collections.sort(tasks, byRanking);
                   Collections.reverse(tasks); // now desc, ez
-                  annIA.put("lastTaskId", tasks.get(0).getId());
+                  if(tasks.size()>0)annIA.put("lastTaskId", tasks.get(0).getId());
               }
 
         } 
