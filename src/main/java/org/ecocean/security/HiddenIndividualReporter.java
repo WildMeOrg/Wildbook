0ab61acea7e4a5f0bc2baaad06056c4062ca58a9
holmbergius <holmbergius@gmail.com>
2023-08-25 09:51:12 -0700

    More refinement on individual permissions

--- src/main/java/org/ecocean/security/HiddenIndividualReporter.java
+++ src/main/java/org/ecocean/security/HiddenIndividualReporter.java
@@ -33,14 +33,23 @@ public class HiddenIndividualReporter extends HiddenDataReporter<MarkedIndividua
 	}
 	
 	 public Vector viewableResults(Vector tObjectsToFilter, boolean hiddenIdsToOwnersIsValid, Shepherd myShepherd) {
-	    //if (!hiddenIdsToOwnersIsValid) loadAllViewable(tObjectsToFilter, myShepherd);
+	    if (!hiddenIdsToOwnersIsValid) loadAllViewable(tObjectsToFilter, myShepherd);
 	    Vector cleanResults = new Vector();
 	    for (Object untypedObj: tObjectsToFilter) {
 	      MarkedIndividual typedObj = (MarkedIndividual) untypedObj;
 	      // if hiddenData doesn't contain the object, add it to clean results
-	      if (ServletUtilities.isUserAuthorizedForIndividual(typedObj, request)) cleanResults.add(untypedObj);
+	      if (!this.contains(typedObj)) cleanResults.add(untypedObj);
 	    }
 	    return cleanResults;
+	  }
+	 
+	  // calls 'add' on each of tObjects depending on canUserView
+	  // we only add individuals the user should NOT see
+	  public void loadAllViewable(Vector tObjects, Shepherd myShepherd) {
+	    for (Object tObject: tObjects) {
+	      MarkedIndividual indy = (MarkedIndividual) tObject;
+	      if (!ServletUtilities.isUserAuthorizedForIndividual(indy, request)) this.add(indy);
+	    }
 	  }
 	
 	
