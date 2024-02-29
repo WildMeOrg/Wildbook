7b159a2f044429ed052bfad5c5d3afeba7ed82f2
erinz2020 <erinz2020@gmail.com>
2024-02-29 17:18:36 +0000

    navBar part2

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -3,17 +3,20 @@ import Header from './components/home/Header';
 import LatestData from './components/home/LatestData';
 import PickUp from './components/home/PickUp';
 import Report from './components/home/Report';
+import Projects from './components/home/Projects';
+import Footer from './components/Footer';
 
 export default function Home() {
 
     return (
         <>
            <div className="col-12">        
-            <Header />
-            <LatestData />
-            <PickUp />
-            <Report />  
-                      
+                <Header />
+                <LatestData />
+                <PickUp />
+                <Report />  
+                <Projects />
+                {/* <Footer />                       */}
             </div>        
         </>        
     );
