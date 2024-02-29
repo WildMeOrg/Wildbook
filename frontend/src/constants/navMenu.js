a9efc5a5917c3061c585ed40c3d45d67bc3085ad
erinz2020 <erinz2020@gmail.com>
2024-02-29 17:18:36 +0000

    navBar part2

---
+++ frontend/src/constants/navMenu.js
@@ -0,0 +1,32 @@
+export default menu = [
+    {'Learn': [
+        {name: 'About Wildbook', to: '/about'},
+        {name: 'Citing Wildbook', to: '/citing.jsp'},
+        {name: 'Wildbook for Birds', to: '/about'},
+        {name: 'Wildbook for Sharks', to: '/about'},
+    ]},
+    {'Submit': [
+        {name: 'Report a Sighting', to: '/submit'},
+        {name: 'Bulk Import', to: '/submit'},
+    ]},
+    {'Individuals': [
+        {name: 'Search', to: '/individuals'},
+        {name: 'Add Individual', to: '/individuals'},
+        {name: 'Bulk Import', to: '/individuals'},
+    ]},
+    {'Sightings': [
+        {name: 'Search', to: '/sightings'},
+        {name: 'Add Sighting', to: '/sightings'},
+        {name: 'Bulk Import', to: '/sightings'},
+    ]},
+    {'Encounters': [
+        {name: 'Search', to: '/encounters'},
+        {name: 'Add Encounter', to: '/encounters'},
+        {name: 'Bulk Import', to: '/encounters'},
+    ]},
+    {'Administers': [
+        {name: 'Search', to: '/administers'},
+        {name: 'Add Administer', to: '/administers'},
+        {name: 'Bulk Import', to: '/administers'},
+    ]},
+]
