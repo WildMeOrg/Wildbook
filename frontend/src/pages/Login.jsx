a3d7182ad84a92bdff589081a619c0ef8af61760
erinz2020 <erinz2020@gmail.com>
2024-03-05 21:57:46 +0000

    call login api

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,13 +3,14 @@ import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import { useState } from 'react';
 import BrutalismButton from '../components/BrutalismButton';
 import Cookies from 'js-cookie';
-
+import { Navigate, useNavigate } from 'react-router-dom';
+import { useIntl } from 'react-intl';
 
 function LoginPage() {
 
     const [rememberMe, setRememberMe] = useState(false);
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
         console.log('Form submitted');
     
@@ -17,8 +18,31 @@ function LoginPage() {
           Cookies.set('rememberMe', 'true', { expires: 1/3 });
         } else {
           Cookies.set('rememberMe', 'true');
-        }    
-      };
+        }  
+        
+        const response = await fetch('/api/v3/login', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+              username: username,
+              password: password,
+          })
+        })
+        if (response.ok) {
+          navigate('/home');
+          console.log(response);
+          return response;
+        } else {
+          throw new Error('login failed!');
+        }
+      }
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const intl = useIntl();
 
   return (
     <Container fluid>
@@ -36,35 +60,66 @@ function LoginPage() {
                 <Form className="login-form" style={{width: '400px'}} onSubmit={handleSubmit}>
                     <h3 className="text-center mb-4">SIGN IN</h3>
                     <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control type="text" placeholder="Username" />
+                        <Form.Label>{
+                          intl.formatMessage({
+                            id: 'USERNAME',
+                          })
+                          }</Form.Label>
+                        <Form.Control type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} />
                     </Form.Group>
 
                     <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Label>{
+                          intl.formatMessage({
+                            id: 'PASSWORD',
+                          })
+                          }</Form.Label>
+                        <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
                     </Form.Group>
 
-                    <Form.Group controlId="formBasicCheckbox" className="mb-3">
+                    <Form.Group controlId="formBasicCheckbox" className="mb-3 mt-3">
                         <Row>
                             <Col xs={6} className="text-start">
-                            <Form.Check type="checkbox" label="Remember me" checked={rememberMe} onChange={(e) => setRememberMe(e.target.checked)} />
+                            <Form.Check 
+                              type="checkbox" 
+                              label={
+                                intl.formatMessage({
+                                  id: 'REMEMBER_ME',
+                                })
+                                } 
+                              checked={rememberMe} 
+                              onChange={(e) => setRememberMe(e.target.checked)} 
+                          />
                             </Col>
                             <Col xs={6} className="text-end">
-                            <a href="/forgot-password">Forgot Password?</a>
+                            <a href="/forgot-password">{
+                            intl.formatMessage({
+                              id: 'FORGOT_PASSWORD',
+                            })
+                          }</a>
                             </Col>
                         </Row>
                     </Form.Group>
 
                     <BrutalismButton onClick={handleSubmit}>
-                        Sign In
+                      {
+                        intl.formatMessage({
+                          id: 'SIGN_IN',
+                        })
+                      }
                     </BrutalismButton>              
-                    {/* <Button variant="primary" type="submit" className="w-100">
-                    Sign In
-                    </Button> */}
 
-                    <div className="text-center mt-2">
-                    New to Wildbook? <a href="/request-account">Request Account</a>
+                    <div className="text-center mt-3 d-flex justify-content-between">
+                      {
+                          intl.formatMessage({
+                            id: 'NEW_TO_WILDBOOK',
+                          })
+                          }
+                           <a href="/request-account">{
+                              intl.formatMessage({
+                                id: 'REQUEST_ACCOUNT',
+                              })}
+                           </a>
                     </div>
                 </Form>
             </div>
