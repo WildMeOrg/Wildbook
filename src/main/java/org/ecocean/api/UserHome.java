b97d21982e018dcdb32be5b35c3e477e84e870e0
Jon Van Oast <jon@wildme.org>
2024-03-07 17:21:42 -0700

    stubbed out user home api

---
+++ src/main/java/org/ecocean/api/UserHome.java
@@ -0,0 +1,70 @@
+package org.ecocean.api;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import org.ecocean.Shepherd;
+import org.ecocean.User;
+import org.ecocean.Project;
+import org.ecocean.servlet.ServletUtilities;
+
+
+
+public class UserHome extends ApiBase {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String context = ServletUtilities.getContext(request);
+        Shepherd myShepherd = new Shepherd(context);
+        myShepherd.setAction("api.UserHome");
+        myShepherd.beginDBTransaction();
+
+        JSONObject home = new JSONObject();
+        User currentUser = myShepherd.getUser(request);
+        if (currentUser == null) {
+            response.setStatus(401);
+            response.setHeader("Content-Type", "application/json");
+            response.getWriter().write("{\"success\": false}");
+            myShepherd.rollbackDBTransaction();
+            myShepherd.closeDBTransaction();
+            return;
+        }
+
+        home.put("user", currentUser.infoJSONObject(true));
+
+        JSONArray projArr = new JSONArray();
+        int pcount = 0;
+        for (Project proj : currentUser.getProjects(myShepherd)) {
+            JSONObject pj = new JSONObject();
+            pj.put("id", proj.getId());
+            projArr.put(pj);
+            pcount++;
+            if (pcount > 2) break;
+        }
+        home.put("projects", projArr);
+
+        response.setStatus(200);
+        response.setHeader("Content-Type", "application/json");
+        response.getWriter().write(home.toString());
+        myShepherd.rollbackDBTransaction();
+        myShepherd.closeDBTransaction();
+    }  	
+
+}
+
+/*
+
+    proposal: combine all things needed for landing page here
+    (some of these can be separate query when ES exists?)
+    maybe only for current user?
+
+    * "latest data": N most recent sightings
+    * latest: bulk import, individual, "matching action"
+    * projects data: see/replicate projectsList.jsp
+
+*/
--- src/main/java/org/ecocean/api/UserInfo.java
+++ src/main/java/org/ecocean/api/UserInfo.java
@@ -15,21 +15,10 @@ public class UserInfo extends ApiBase {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         JSONObject info = new JSONObject();
 
+        //results = user.infoJSONObject(true);
         response.setStatus(200);
         response.setHeader("Content-Type", "application/json");
         response.getWriter().write(info.toString());
     }  	
 
 }
-
-/*
-
-    proposal: combine all things needed for landing page here
-    (some of these can be separate query when ES exists?)
-    maybe only for current user?
-
-    * "latest data": N most recent sightings
-    * latest: bulk import, individual, "matching action"
-    * projects data: see/replicate projectsList.jsp
-
-*/
