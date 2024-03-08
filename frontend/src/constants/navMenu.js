bc2d64bcfc6891c4fd94ec5f99e56842a39c7bf0
erinz2020 <erinz2020@gmail.com>
2024-03-08 21:01:50 +0000

    logout + home api

--- frontend/src/constants/navMenu.js
+++ frontend/src/constants/navMenu.js
@@ -1,29 +1,58 @@
 const menu = [
-    {'Learn': [
-        {name: 'About Wildbook', href: '/overview.jsp'},
-        {name: 'Citing Wildbook', href: '/wildbook/citing.jsp'},
-        {name: 'How to photograph', href: '/photographing.jsp'},
-        {name: 'Learn more about wildbook', href: 'https://www.wildme.org/#/wildbook'},
-    ]},
     {'Submit': [
         {name: 'Report a Sighting', href: '/submit.jsp'},
         {name: 'Bulk Import', href: '/import/instructions.jsp'},
     ]},
-    {'Individuals': [
-        
-        {name: 'Individual Search', href: '/individualSearch.jsp'},
+    {'Learn': [
+        {name: 'About Wildbook', href: '/overview.jsp'},
+        {name: 'Contact Us', href: '/photographing.jsp'},
+        {name: 'Citing Wildbook', href: '/citing.jsp'},
+        {name: 'How to photograph', href: '/photographing.jsp'},
+        {name: 'Privacy Policy', href: '/photographing.jsp'},
+        {name: 'Terms of Use', href: '/photographing.jsp'},
+        {name: 'Learn more about wildbook', href: 'https://www.wildme.org/#/wildbook'},
     ]},
-    {'Sightings': [
-        {name: 'Individual Search', href: '/occurrenceSearch.jsp'},
+    {'My Data': [
+        {name: 'View my encounters', href: 'encounters/searchResults.jsp?username=tomcat'},
+        {name: 'View my individuals', href: '/import/instructions.jsp'},
+        {name: 'View my sightings', href: '/import/instructions.jsp'},
+        {name: 'View my bulk imports', href: '/imports.jsp'},
+        {name: 'View my projects', href: '/projects/projectList.jsp'},
     ]},
-    {'Encounters': [
-        {name: 'Search', href: '/encounters/encounterSearch.jsp'},
+    {'Search': [        
+        {name: 'Encounters', href: '/encounters/encounterSearch.jsp'},
+        {name: 'Individuals', href: '/individualSearch.jsp'},
+        {name: 'Sightings', href: '/occurrenceSearch.jsp'},
     ]},
+    {'Animals': [
+        {name: 'Individual Gallery', href: '/gallery.jsp'},
+        {name: 'Animal Calendar', href: '/import/instructions.jsp'},
+    ]},   
     {'Administers': [
-        {name: 'Search', href: '/administers'},
-        {name: 'Add Administer', href: '/administers'},
-        {name: 'Bulk Import', href: '/administers'},
-    ]},
+        {name: 'My Account', href: '/myAccount.jsp'},
+        {name: 'Manage My Accounts', href: '/myUsers.jsp'},
+        {name: 'User Management', href: '/appadmin/users.jsp?context=context0'},
+        {name: 'Library Administration', href: '/appadmin/admin.jsp'},
+        {name: 'Logs', href: '/appadmin/logs.jsp'},
+        {name: 'Photo Keywords', href: '/appadmin/kwAdmin.jsp'},
+        {name: 'Software Documentation', href: 'https://docs.wildme.org/product-docs/en/wildbook/introduction/'},
+        {name: 'Data Integrity', href: 'appadmin/dataIntegrity.jsp'},
+    ]}, 
+
+    // {'Sightings': [
+    //     {name: 'Search', href: '/occurrenceSearch.jsp'},
+    //     {name: 'View All', href: '/occurrenceSearchResults.jsp'},
+    // ]},
+
+    // {'Individuals': [        
+    //     {name: 'Gallery', href: '/gallery.jsp'},
+    //     {name: 'View All', href: '/individualSearchResults.jsp'},
+    // ]},
+    
+    // {'Encounters': [
+    //     {name: 'Search', href: '/encounters/encounterSearch.jsp'},
+    // ]},
+    
 ]
 
 
