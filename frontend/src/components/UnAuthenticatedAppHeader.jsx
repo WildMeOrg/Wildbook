939466e38a187df6c8bb5be3036fddcb3f1ec7c9
erinz2020 <erinz2020@gmail.com>
2024-03-14 18:00:40 +0000

    update polling

--- frontend/src/components/UnAuthenticatedAppHeader.jsx
+++ frontend/src/components/UnAuthenticatedAppHeader.jsx
@@ -1,25 +1,56 @@
-import React from 'react';
+import React, { useState, useContext} from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import Avatar from './Avatar';
 import '../css/dropdown.css';
 import menu from '../constants/navMenu';
 import DownIcon from './svg/DownIcon';
 import Button from 'react-bootstrap/Button';
+import NotificationButton from './navBar/NotificationButton';
 import MultiLanguageDropdown from './navBar/MultiLanguageDropdown';
+import AuthContext from '../AuthProvider';
 
-export default function UnauthenticatedAppHeader () {
+
+export default function AuthenticatedAppHeader () {
     const location = window.location;
     const navBarFilled = location.pathname === '/';
     const backgroundColor = !navBarFilled ? '#00a1b2' : 'transparent';
 
-    return (<Navbar variant="dark" expand="lg" style={{ 
-          backgroundColor: backgroundColor,
-          width: '100%', 
-          height: '43px',
-          display: 'flex', 
-          justifyContent: 'space-between',
-          overflow: 'visible',
-          fontSize: '1rem',
-          }}>
+    const isLoggedIn = useContext(AuthContext);
+    console.log('=============>>>>>>>>>>>>>>>',isLoggedIn);
+
+    const logout = async event => {
+      console.log('Logging out');
+      event.preventDefault();
+      await fetch('/api/v3/logout')
+        .then(response => {
+          if (response.status === 200) {
+            console.log('User logged out');
+            window.location.href = '/';
+          } else if (response.status === 401) {
+            console.log('User is not logged in');
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        });  
+    };
+
+    return (<Navbar variant="dark" expand="lg" 
+
+    style={{
+      backgroundColor: backgroundColor,
+      height: '43px',
+      fontSize: '1rem',
+      position: 'fixed',
+      top: 0,
+      maxWidth: '1440px',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      zIndex: '100',
+      width: '100%',
+    }}
+    >
             <Navbar.Brand href="/" style={{ marginLeft: '1rem' }}>Amphibian Wildbook</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav" style={{marginLeft: '20%'}}>
@@ -53,6 +84,7 @@ export default function UnauthenticatedAppHeader () {
                 }}
                 href={"/login"}>Login
               </Button>
+              
               <MultiLanguageDropdown />
               
             </Navbar.Collapse>
