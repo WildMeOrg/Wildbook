a4665173e7af5cbd9d6f8c3750323a5ea7971b0e
erinz2020 <erinz2020@gmail.com>
2024-03-19 20:51:58 +0000

    adjust dropdown pisition, footer version2, fake projects

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import FooterLogo from './svg/FooterLogo';
+import { FormattedMessage } from 'react-intl';
+import FooterLink from './footer/FooterLink';
 
 const Footer = () => {
   return (
@@ -10,31 +13,67 @@ const Footer = () => {
         zIndex: '100',
         width: '100%',
         backgroundColor: '#cfe2ff',
-        paddingTop: '100px',
+        paddingTop: '50px',
         paddingBottom: '50px',
-        marginTop: '100px',
+        fontSize: '1rem',
       }}>
       <Container>
         <Row className="justify-content-md-center">
+          
           <Col xs lg="2">
-            <p>Documentation</p>
-            <p>Community Forum</p>
-            <p>Terms of Use</p>
+            <FooterLink 
+              href="https://www.wildme.org/donate.html" 
+              text={<FormattedMessage id='DONATE'/>}
+            />
+            <FooterLink 
+              href="https://docs.wildme.org/product-docs/en/wildbook/getting-started-with-wildbook" 
+              text={<FormattedMessage id='DOCUMENTATION'/>}
+            />
+            <FooterLink 
+              href="/terms-of-use.jsp" 
+              text={<FormattedMessage id='TERM_OF_USE'/>}
+            />
+
           </Col>
-          <Col md="auto">
-            <p>Bulk Report</p>
-            <p>Donate</p>
-          </Col>
+          
           <Col xs lg="2">
-            <p>Gmail</p>
-            <p>Discord</p>
-            <p>Github</p>
-            <p>X (Twitter)</p>
+            <FooterLink 
+              href="https://community.wildme.org" 
+              text={<FormattedMessage id='COMMUNITY_FORUM'/>}
+            />
+              <FooterLink 
+              href="https://github.com/WildMeOrg" 
+              text={<FormattedMessage id='GITHUB'/>}
+            />
+          </Col>
+
+          <Col xs lg="1">
+            <FooterLink 
+              href="https://www.instagram.com/conservationxlabs" 
+              text={<FormattedMessage id='INSTAGRAM'/>}
+            />
+            <FooterLink 
+              href="https://www.facebook.com/ConservationXLabs" 
+              text={<FormattedMessage id='FACEBOOK'/>}
+            />
+            <FooterLink 
+              href="https://twitter.com/conservationx" 
+              text={<FormattedMessage id='TWITTER'/>}
+            />
+            <FooterLink 
+              href="https://www.linkedin.com/company/conservationxlabs/" 
+              text={<FormattedMessage id='LINKEDIN'/>}
+            />
+
           </Col>
         </Row>
-        <Row className="justify-content-md-center">
+        <Row className="justify-content-md-center" style={{
+          marginTop: '50px'
+        }}>
           <Col md="auto">
-            <p>© 2024 @ Conservation X Labs | All Rights Reserved</p>
+            <p>
+              <FormattedMessage id='FOOTER_COPYRIGHT'/>
+            </p>
           </Col>
         </Row>
       </Container>
