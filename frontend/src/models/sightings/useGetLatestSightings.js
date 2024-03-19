fd960c3cd378f3699fa0ff663b8f0ca5c137cf94
erinz2020 <erinz2020@gmail.com>
2024-03-19 20:51:03 +0000

    footer version1

---
+++ frontend/src/models/sightings/useGetLatestSightings.js
@@ -0,0 +1,25 @@
+
+
+import { get } from 'lodash-es';
+// import * as Sentry from '@sentry/react';
+import queryKeys from '../../constants/queryKeys';
+import useFetch from '../../hooks/useFetch';
+
+export default function useGetMe() {
+//   function onSuccess(response) {
+//     if (!__DEV__) {
+//       Sentry.setUser({
+//         email: get(response, ['data', 'email']),
+//         id: get(response, ['data', 'guid']),
+//         username: get(response, ['data', 'full_name']),
+//       });
+//     }
+//   }
+
+  return useFetch({
+    queryKey: queryKeys.me,
+    url: '/latestSightings',
+    // onSuccess,
+    queryOptions: { retry: false },
+  });
+}
