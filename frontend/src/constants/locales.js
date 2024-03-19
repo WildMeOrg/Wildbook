77863a5c4c7b47a6cbaac87331aff0c1031f371e
erinz2020 <erinz2020@gmail.com>
2024-03-19 20:49:37 +0000

    i18n continued

---
+++ frontend/src/constants/locales.js
@@ -0,0 +1,17 @@
+
+const locales = ['en', 'es', 'fr', 'it'];
+const localeMap = {
+    en: 'UK',
+    es: 'ES',
+    fr: 'FR',
+    it: 'IT',
+};
+
+const languageMap = {
+    en: 'English',
+    es: 'Español',
+    fr: 'Francés',
+    it: 'Italiano',
+};
+
+export { locales, localeMap, languageMap };
