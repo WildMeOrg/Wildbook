f48a477af44877e63d04c54d5272d0dbf9138dc2
erinz2020 <erinz2020@gmail.com>
2024-03-23 00:03:59 +0000

    remove space between footer links, update site title

--- frontend/src/FrontDesk.jsx
+++ frontend/src/FrontDesk.jsx
@@ -3,11 +3,9 @@ import UnauthenticatedSwitch from './UnAuthenticatedSwitch';
 import AuthenticatedSwitch from './AuthenticatedSwitch';
 import axios from 'axios';
 import AuthContext from './AuthProvider';
-
-// import CreateAdminUser from './pages/setup/CreateAdminUser';
+import ThemeProvider from './ThemeProvider';
 
 export default function FrontDesk({ adminUserInitialized = true }) {
-  // Display a loading spinner while waiting for authentication status from the server.
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [error, setError] = useState();
 
@@ -15,11 +13,9 @@ export default function FrontDesk({ adminUserInitialized = true }) {
     console.log("Polling API...");
     axios.head('/api/v3/user')
       .then(response => {
-        // console.log('Success', response);
         setIsLoggedIn(response.status === 200);
       })
       .catch(error => {
-        // console.log('Error', error);
         setError(error);
       });
   };
@@ -35,22 +31,19 @@ export default function FrontDesk({ adminUserInitialized = true }) {
     return () => clearInterval(intervalId);
   }, []);
 
-  //   if (isFetched && !adminUserInitialized) return <CreateAdminUser />;
   if (isLoggedIn) {
-  /* Disable email verification site in development mode.
-  Also explicitly checking is_email_confirmed for "false"
-  to be safer about locking users out of their accounts
-  in case the property isn't present for some reason. */
-
-  return (
-    <AuthContext.Provider value={isLoggedIn}>
-      <AuthenticatedSwitch adminUserInitialized />
-    </AuthContext.Provider>
-  );
+    return (
+      <AuthContext.Provider value={isLoggedIn}>
+        <ThemeProvider>          
+          <AuthenticatedSwitch adminUserInitialized />
+        </ThemeProvider>
+      </AuthContext.Provider>
+    );
   }
   if (error) {
     console.log('Error', error);
-    return <UnauthenticatedSwitch />};
+    return <UnauthenticatedSwitch />
+  };
 
   return (
     <h1>Loading</h1>
