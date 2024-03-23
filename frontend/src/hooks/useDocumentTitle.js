aa1bde6c3947387369de7ef0a7669fcb74abff0e
erinz2020 <erinz2020@gmail.com>
2024-03-23 00:02:50 +0000

    remove duplicate header menu, set tab icon and title, i18n, etc

--- frontend/src/hooks/useDocumentTitle.js
+++ frontend/src/hooks/useDocumentTitle.js
@@ -1,39 +1,23 @@
 import { useEffect } from 'react';
-import { useIntl } from 'react-intl';
-import { get } from 'lodash-es';
+import useGetSiteSettings from '../models/useGetSiteSettings';
 
-// import useSiteSettings from '../models/site/useSiteSettings';
+export default function useDocumentTitle () {
+  const { data: siteSettings, loading, refresh } = useGetSiteSettings();
+  document.title = siteSettings?.siteName || 'wildbook';
+  useEffect(() => {
+    console.log('siteSettings changing', siteSettings);
+    let iconURL = siteSettings?.siteFavicon;
+    let link = document.querySelector("link[rel*='icon']");
 
-export default function (message, configuration = {}) {
-  const intl = useIntl();
-  const appendSiteNameConfiguration = get(
-    configuration,
-    'appendSiteName',
-    true,
-  );
-  const translateMessage = get(
-    configuration,
-    'translateMessage',
-    true,
-  );
-  const refreshKey = get(configuration, 'refreshKey', null);
-  const messageValues = get(configuration, 'messageValues', {});
-
-//   const siteSettings = useSiteSettings();
-//   const siteSettings = null;
-//   const siteName = get(siteSettings, ['data', 'site.name', 'value']);
-//   const appendSiteName = siteName && appendSiteNameConfiguration;
-  document.title = "Wildbook"
-//   useEffect(() => {
-//     const translatedMessage = translateMessage
-//       ? intl.formatMessage({ id: message }, messageValues)
-//       : message;
-//     if (appendSiteName && siteName) {
-//       document.title = `${translatedMessage} • ${siteName}`;
-//     } else {
-//       document.title = translatedMessage;
-//     }
-//   }, [message, refreshKey, siteName, appendSiteName]);
-
+    if (link) {
+      link.href = iconURL;
+    } else {
+      link = document.createElement('link');
+      link.type = 'image/png';
+      link.rel = 'shortcut icon';
+      link.href = iconURL;
+      document.getElementsByTagName('head')[0].appendChild(link);
+    }
+  }, [siteSettings]);
 
 }
