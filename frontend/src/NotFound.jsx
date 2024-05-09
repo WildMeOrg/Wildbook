0e8becee0fbbea2bf922890e5153067450ec782a
erinz2020 <erinz2020@gmail.com>
2024-05-09 20:25:07 +0000

    use more bootstrap className

--- frontend/src/NotFound.jsx
+++ frontend/src/NotFound.jsx
@@ -3,39 +3,21 @@ import React from "react";
 export default function NotFound() {
   return (
     <div
-      style={{
-        position: "relative",
-        zIndex: "200",
-        paddingTop: "100px",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "80vh",
-      }}
+      className="position-relative d-flex justify-content-center align-items-center"
+      style={{ zIndex: 200, paddingTop: "100px", height: "80vh" }}
     >
       <img
         src="/react/images/404.png"
         alt="notFound-forest"
-        width="100%"
-        className="vh-100"
-        style={{
-          objectFit: "cover",
-          position: "absolute",
-          top: 0,
-          left: 0,
-        }}
+        className="position-absolute top-0 start-0 w-100 vh-100"
+        style={{ objectFit: "cover" }}
       />
       <img
         src="/react/images/Hedgehog.png"
         alt="notFound-hedgehog"
-        objectFit="cover"
-        style={{
-          position: "absolute",
-          top: 500,
-          left: 400,
-        }}
+        className="position-absolute"
+        style={{ objectFit: "cover", top: "500px", left: "400px" }}
       />
-
       <h1>404 - Not Found</h1>
     </div>
   );
---
+++ frontend/src/constants/footerMenu.js
@@ -0,0 +1,25 @@
+const footerLinks1 = [
+  { href: "https://www.wildme.org/donate.html", id: "FOOTER_DONATE" },
+  {
+    href: "https://docs.wildme.org/product-docs/en/wildbook/getting-started-with-wildbook",
+    id: "FOOTER_DOCUMENTATION",
+  },
+];
+const footerLinks2 = [
+  { href: "https://community.wildme.org", id: "FOOTER_COMMUNITY_FORUM" },
+  { href: "https://github.com/WildMeOrg", id: "FOOTER_GITHUB" },
+];
+const footerLinks3 = [
+  {
+    href: "https://www.instagram.com/conservationxlabs",
+    id: "FOOTER_INSTAGRAM",
+  },
+  { href: "https://www.facebook.com/ConservationXLabs", id: "FOOTER_FACEBOOK" },
+  { href: "https://twitter.com/conservationx", id: "FOOTER_TWITTER" },
+  {
+    href: "https://www.linkedin.com/company/conservationxlabs/",
+    id: "FOOTER_LINKEDIN",
+  },
+];
+
+export { footerLinks1, footerLinks2, footerLinks3 };
