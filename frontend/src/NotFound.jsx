2329d7c7b7d9284a67ac0fc625f433287d833a2c
erinz2020 <erinz2020@gmail.com>
2024-05-09 21:01:01 +0000

    will update in next branch

--- frontend/src/NotFound.jsx
+++ frontend/src/NotFound.jsx
@@ -6,7 +6,7 @@ export default function NotFound() {
       className="position-relative d-flex justify-content-center align-items-center"
       style={{ zIndex: 200, paddingTop: "100px", height: "80vh" }}
     >
-      <img
+      {/* <img
         src="/react/images/404.png"
         alt="notFound-forest"
         className="position-absolute top-0 start-0 w-100 vh-100"
@@ -17,7 +17,7 @@ export default function NotFound() {
         alt="notFound-hedgehog"
         className="position-absolute"
         style={{ objectFit: "cover", top: "500px", left: "400px" }}
-      />
+      /> */}
       <h1>404 - Not Found</h1>
     </div>
   );
