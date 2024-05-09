e9e077b415206c48f997d4b5f293504a074c6d31
Jon Van Oast <jon@wildme.org>
2024-05-09 16:15:50 -0600

    another batch

--- src/main/java/org/ecocean/datacollection/MeasurementEvent.java
+++ src/main/java/org/ecocean/datacollection/MeasurementEvent.java
@@ -1,32 +1,30 @@
 package org.ecocean.datacollection;
 
 public class MeasurementEvent extends DataCollectionEvent {
+    private static final long serialVersionUID = -7934850478287322048L;
 
-  private static final long serialVersionUID = -7934850478287322048L;
+    private Double value;
+    private String units;
 
-  private Double value;
-  private String units;
+    public MeasurementEvent() {}
 
-  public MeasurementEvent() {
-  }
+    public MeasurementEvent(String correspondingEncounterNumber, String type, Double value,
+        String units, String samplingProtocol) {
+        super(correspondingEncounterNumber, type);
+        super.setSamplingProtocol(samplingProtocol);
+        this.value = value;
+        this.units = units;
+    }
 
-  public MeasurementEvent(String correspondingEncounterNumber, String type, Double value, String units, String samplingProtocol) {
-    super(correspondingEncounterNumber, type);
-    super.setSamplingProtocol(samplingProtocol);
-    this.value = value;
-    this.units = units;
-  }
+    public Double getValue() {
+        return value;
+    }
 
-  public Double getValue() {
-    return value;
-  }
+    public void setValue(Object value) {
+        this.value = (Double)value;
+    }
 
-  public void setValue(Object value) {
-    this.value = (Double) value;
-  }
-
-  public String getUnits() {
-    return units;
-  }
-
+    public String getUnits() {
+        return units;
+    }
 }
--- src/main/java/org/ecocean/tag/AcousticTag.java
+++ src/main/java/org/ecocean/tag/AcousticTag.java
@@ -1,35 +1,32 @@
 package org.ecocean.tag;
 
 public class AcousticTag extends AbstractTag {
-  static final long serialVersionUID = -3502387833027576733L;
+    static final long serialVersionUID = -3502387833027576733L;
 
-  private String serialNumber;
-  
-  private String idNumber;
-  
-  public AcousticTag() {
-    
-  }
-  
-  public AcousticTag(String serialNumber, String idNumber) {
-    this.serialNumber = serialNumber;
-    this.idNumber = idNumber;
-  }
+    private String serialNumber;
 
-  public String getSerialNumber() {
-    return serialNumber;
-  }
+    private String idNumber;
 
-  public void setSerialNumber(String serialNumber) {
-    this.serialNumber = serialNumber;
-  }
+    public AcousticTag() {}
 
-  public String getIdNumber() {
-    return idNumber;
-  }
+    public AcousticTag(String serialNumber, String idNumber) {
+        this.serialNumber = serialNumber;
+        this.idNumber = idNumber;
+    }
 
-  public void setIdNumber(String idNumber) {
-    this.idNumber = idNumber;
-  }
+    public String getSerialNumber() {
+        return serialNumber;
+    }
 
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
 }
