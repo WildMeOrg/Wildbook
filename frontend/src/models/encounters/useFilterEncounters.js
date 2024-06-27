4b7357ea689181f87b737749717973ee3c682efc
erinz2020 <erinz2020@gmail.com>
2024-06-27 13:54:11 +0000

    update hook

--- frontend/src/models/encounters/useFilterEncounters.js
+++ frontend/src/models/encounters/useFilterEncounters.js
@@ -10,33 +10,36 @@ export default function useFilterEncounters({ queries, params = {} }) {
   const mustNotQueries = mustNots.map((f) => f.query);
 
   const compositeQuery = {
-    bool: { filter: filterQueries, must_not: mustNotQueries },
+    bool: { filter: filterQueries, must_not: mustNotQueries || [] },
   };
+
   return useFetch({
-    method: "post",
-    queryKey: getEncounterFilterQueryKey(queries, params),
-    url: "/sightings/search",
-    data: compositeQuery,
-    params: {
-      limit: 20,
-      offset: 0,
-      sort: "created",
-      reverse: false,
-      ...params,
-    },
-    dataAccessor: (result) => {
-      const resultCountString = get(result, [
-        "data",
-        "headers",
-        "x-total-count",
-      ]);
-      return {
-        resultCount: parseInt(resultCountString, 10),
-        results: get(result, ["data", "data"]),
-      };
-    },
-    queryOptions: {
-      retry: 2,
-    },
-  });
+        method: "post",
+        queryKey: getEncounterFilterQueryKey(queries, params),
+        url: "/search/encounter",
+        data: queries,
+        params: {
+        //   limit: 20,
+        //   offset: 0,
+          sort: "date",
+        //   reverse: false,
+        size:1,
+        from: 3,
+        
+        // //   ...params,
+        },
+        dataAccessor: (result) => {
+            console.log("result", result);
+        const resultCountString = get(result, ["data", "headers", "x-wildbook-total-hits"]);
+        return {
+            resultCount: parseInt(resultCountString, 10) || 0,
+            results: get(result, ["data", "data","hits"], []),
+        };
+        },
+        queryOptions: {
+        retry: 2,
+        },
+      })
+  
+  
 }
