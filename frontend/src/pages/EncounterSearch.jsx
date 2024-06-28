1b51182a19ae90489cc7c381f6acd0ab11cc3d74
erinz2020 <erinz2020@gmail.com>
2024-06-28 21:47:33 +0000

    new library for slider

--- frontend/src/pages/EncounterSearch.jsx
+++ frontend/src/pages/EncounterSearch.jsx
@@ -1,70 +1,84 @@
 import React, { useEffect, useState } from "react";
 import DataTable from "../components/DataTable";
 import useFilterEncounters from "../models/encounters/useFilterEncounters";
+import FilterPanel from "../components/FilterPanel";
+import useEncounterSearchSchemas from "../models/encounters/useEncounterSearchSchemas";
 
 export default function EncounterSearch() {
   const columns = [
     { name: "Encounter ID", selector: "id" },
-    { name: "Date", selector: "date" },
-    { name: "Media Assets", selector: "mediaAssets" },
+    { name: "Created Date", selector: "date" },
     { name: "Individual ID", selector: "individualId" },
-    { name: "Location ID", selector: "locationId" }
+    { name: "Location ID", selector: "locationId" },
+    { name: "Number Annotations", selector: "numberAnnotations" },
+    { name: "Species", selector: "taxonomy" },
   ];
+
+  const schemas = useEncounterSearchSchemas();
 
   const [page, setPage] = useState(0);
   const [perPage, setPerPage] = useState(10);
 
-  const formFilters =  {
-    "query" : {
-       "bool" : {
-          "filter" : [
- 
-            //   {
-            //     "match" : {
-            //        "taxonomy" : "Giraffa tippelskirchi"
-            //     }
-            //  },
- 
-            //  {
-            //     "match" : {
-            //        "individualId" : "924911ec-3c40-4678-ae29-1b4e38523f17"
-            //     }
-            //  },
- 
-            // {
-            //      "match" : {
-            //      "numberAnnotations" : "1"
-            //      }
-            // },
- 
-            // {
-            //      "match" : {
-            //      "id" : "a29b7e4b-e19b-4973-99e9-39a9ce12559e"
-            //      }
-            // },
- 
-            // {
-            //     "range" : {
-            //        "numberAnnotations" : {
-            //           "gte" : 1,
-            //           "lte" : 5
-            //        }
-            //     }
-            //  },
- 
-            //  {
-            //     "range": {
-            //        "date": {
-            //           "gte": "2022-01-01T00:00:00Z",
-            //           "lte": "2024-12-31T23:59:59Z"
-            //        }
-            //     }
-            //  },
-          ],
-          "must_not" : []
-       }
+  const formFilters = {
+    "query": {
+      "bool": {
+        "filter": [
+
+          //   {
+          //     "match" : {
+          //        "taxonomy" : "Giraffa tippelskirchi"
+          //     }
+          //  },
+
+          //  {
+          //     "match" : {
+          //        "individualId" : "924911ec-3c40-4678-ae29-1b4e38523f17"
+          //     }
+          //  },
+
+          // {
+          //      "match" : {
+          //      "numberAnnotations" : "1"
+          //      }
+          // },
+
+          // {
+          //      "match" : {
+          //      "id" : "a29b7e4b-e19b-4973-99e9-39a9ce12559e"
+          //      }
+          // },
+
+          // {
+          //     "range" : {
+          //        "numberAnnotations" : {
+          //           "gte" : 1,
+          //           "lte" : 5
+          //        }
+          //     }
+          //  },
+
+          //  {
+          //     "range": {
+          //        "date": {
+          //           "gte": "2022-01-01T00:00:00Z",
+          //           "lte": "2024-12-31T23:59:59Z"
+          //        }
+          //     }
+          //  },
+          {
+            geo_distance: {
+              distance: "50km",
+              locationGeoPoint: {
+                lat: 11,
+                lon: 22
+              }
+            }
+          }
+        ],
+        "must_not": []
+      }
     }
- }
+  }
 
   const { data: encounterData, loading } = useFilterEncounters({
     queries: formFilters,
@@ -82,11 +96,12 @@ export default function EncounterSearch() {
   useEffect(() => {
     console.log("Page: ", page);
     console.log("PerPage: ", perPage);
-  }, [page, perPage]);  
+  }, [page, perPage]);
 
   return (
     <div className="w-100"
     >
+      <FilterPanel schemas={schemas} />
       <DataTable
         title="Encounters"
         columnNames={columns}
