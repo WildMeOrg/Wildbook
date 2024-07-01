75f59aa65dfc04cee122e8d57c6ce81a0892038d
Jon Van Oast <jon@wildme.org>
2024-07-01 17:43:57 -0600

    getAllIAClasses() for IAJsonProperties; additions to site-settings api

--- src/main/java/org/ecocean/api/SiteSettings.java
+++ src/main/java/org/ecocean/api/SiteSettings.java
@@ -1,12 +1,15 @@
 package org.ecocean.api;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
+import org.ecocean.Annotation;
 import org.ecocean.CommonConfiguration;
+import org.ecocean.IAJsonProperties;
 import org.ecocean.LocationID;
 import org.ecocean.servlet.ServletUtilities;
 import org.ecocean.Shepherd;
@@ -14,6 +17,8 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class SiteSettings extends ApiBase {
+    public static String[] VALUES_SEX = { "unknown", "male", "female" };
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         String context = ServletUtilities.getContext(request);
@@ -35,6 +40,18 @@ public class SiteSettings extends ApiBase {
             txArr.put(txj);
         }
         settings.put("siteTaxonomies", txArr);
+
+        // apparently *nothing* actually uses the common configuration values!
+        // settings.put("sex", CommonConfiguration.getIndexedPropertyValues("sex", context));
+        settings.put("sex", VALUES_SEX);
+        settings.put("lifeStage",
+            CommonConfiguration.getIndexedPropertyValues("lifeStage", context));
+        settings.put("annotationViewpoint", Annotation.getAllValidViewpoints());
+
+        IAJsonProperties iaConfig = IAJsonProperties.iaConfig();
+        Object[] iac = iaConfig.getAllIAClasses().toArray();
+        Arrays.sort(iac);
+        settings.put("iaClass", iac);
 
         myShepherd.rollbackDBTransaction();
         myShepherd.closeDBTransaction();
