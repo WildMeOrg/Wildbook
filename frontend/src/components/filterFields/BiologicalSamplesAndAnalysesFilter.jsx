9e7009e2e3d62826362e2cd0082f00ba00c1bf3f
erinz2020 <erinz2020@gmail.com>
2024-07-03 21:48:01 +0000

    add some filters

---
+++ frontend/src/components/filterFields/BiologicalSamplesAndAnalysesFilter.jsx
@@ -0,0 +1,8 @@
+
+export default function BiologicalSamplesAndAnalysesFilter() {
+    return (
+        <div>
+        <h1>Biological Samples And Analyses Filter</h1>
+        </div>
+    );
+    }
---
+++ frontend/src/components/filterFields/IdentityFilter.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+
+export default function IdentityFilter() {
+  return (
+    <div>
+      <h1>Identity Filter</h1>
+    </div>
+  );
+}
