edb40932120a46ebae65067f075611334a72954e
erinz2020 <erinz2020@gmail.com>
2024-07-09 00:06:57 +0000

    helper component, map part2

--- frontend/src/components/filterFields/LocationFilterMap.jsx
+++ frontend/src/components/filterFields/LocationFilterMap.jsx
@@ -1,14 +1,52 @@
-import {FormattedMessage} from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 import Map from "../Map";
+import { FormGroup, FormLabel, FormControl } from 'react-bootstrap';
+import { useState } from 'react';
+import Description from '../Description';
 
 
 export default function LocationFilterMap() {
 
+    const [bounds, setBounds] = useState(null);
+
+
     return (
         <div>
-        <h1>Location Map</h1>
-        <FormattedMessage id="LOCATION_MAP_DESC"/>
-        <Map />
+            <h3><FormattedMessage id="LOCATION_MAP" /></h3>
+            <Description>
+                <FormattedMessage id="LOCATION_MAP_DESC" />
+            </Description>
+
+            <div style={{
+                margin: '12px',
+                display: 'flex',
+                flexDirection: 'row',
+            }}>
+                {["Northeast_Latitude",
+                    "Northeast_Latitude",
+                    "Northeast_Latitude",
+                    "Northeast_Latitude"].map((item, index) => {
+                        return (
+                            <FormGroup key={index} style={{
+                                marginRight: '10px',
+                            }}>
+                                <FormLabel><FormattedMessage id={item.replace(/_/g, " ")} /></FormLabel>
+                                <FormControl
+                                    type="text"
+                                    placeholder={bounds?.south}
+                                />
+                            </FormGroup>
+                        );
+                    })
+                }
+
+            </div>
+            <Map
+                bounds={bounds}
+                setBounds={setBounds}
+            />
+            <FormattedMessage id="GPS_COORDINATES" />
+
         </div>
     );
-    }
+}
