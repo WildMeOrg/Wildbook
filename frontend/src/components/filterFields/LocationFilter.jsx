8601f62fc80661721aa3190b869f9783f4aec4fa
erinz2020 <erinz2020@gmail.com>
2024-07-11 00:53:35 +0000

    a baby query

--- frontend/src/components/filterFields/LocationFilter.jsx
+++ frontend/src/components/filterFields/LocationFilter.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import LocationFilterMap from './LocationFilterMap';
 import LocationFilterText from './LocationFilterText';
 
-export default function LocationFilter() {
-    
+export default function LocationFilter({
+    onChange,
+    onClearFilter,
+}) {
     return (
         <div>
-        <LocationFilterMap />
-        <LocationFilterText />
+            <LocationFilterMap
+                onChange={onChange}
+                onClearFilter={onClearFilter}
+            />
+            <LocationFilterText
+                test="test"
+                onChange={onChange}
+                onClearFilter={onClearFilter}
+            />
         </div>
-        )
+    )
 }
