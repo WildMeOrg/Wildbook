56c047d34005f62fbda41c97a4844c0bc452f343
erinz2020 <erinz2020@gmail.com>
2024-07-11 00:51:52 +0000

    more filters

--- frontend/src/components/filterFields/BiologicalSamplesAndAnalysesFilter.jsx
+++ frontend/src/components/filterFields/BiologicalSamplesAndAnalysesFilter.jsx
@@ -1,8 +1,30 @@
 
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import Form from 'react-bootstrap/Form';
+import Description from '../Form/Description';
+import FormGroupText from '../Form/FormGroupText';
+
 export default function BiologicalSamplesAndAnalysesFilter() {
+    const label = <FormattedMessage id="FILTER_BIOLOGICAL_SAMPLE" />
     return (
         <div>
-        <h1>Biological Samples And Analyses Filter</h1>
+            <h3><FormattedMessage id="FILTER_BIOLOGICAL_SAMPLE" /></h3>
+            <Description>
+                <FormattedMessage id="FILTER_IDENTITY_DESC" />
+            </Description>
+            <Form>
+                <Form.Check
+                    type="checkbox"
+                    id="custom-checkbox"
+                    label={label}
+                // checked={isChecked}
+                // onChange={handleOnChange}
+                />
+            </Form>
+            <FormGroupText
+                label="FILTER_BIOLOGICAL_SAMPLE_ID_CONTAINS"
+            />
         </div>
     );
-    }
+}
--- frontend/src/components/filterFields/IdentityFilter.jsx
+++ frontend/src/components/filterFields/IdentityFilter.jsx
@@ -1,10 +1,47 @@
 
 import React from "react";
+import { FormattedMessage } from "react-intl";
+import Form from "react-bootstrap/Form";
+import Description from "../Form/Description";
+import FormGroupText from "../Form/FormGroupText";
 
 export default function IdentityFilter() {
+  const sightedTimes = <FormattedMessage id="SIGHTED_TIMES" />
+  const includeEncounters = <FormattedMessage id="INCLUDE_ENCOUNTERS" />
   return (
     <div>
-      <h1>Identity Filter</h1>
+      <h3><FormattedMessage id="FILTER_IDENTIFY" /></h3>
+      <Description>
+        <FormattedMessage id="FILTER_IDENTIFY_DESC" />
+      </Description>
+      <h5><FormattedMessage id="FILTER_MINIMUM_TIMES_SIGHTED" /></h5>
+      <Description>
+        <FormattedMessage id="FILTER_MINIMUM_TIMES_SIGHTED_DESC" />
+      </Description>
+      <Form>
+        <Form.Check
+          type="checkbox"
+          id="custom-checkbox"
+          label={sightedTimes}
+        // checked={isChecked}
+        // onChange={handleOnChange}
+        />
+      </Form>
+      <Form>
+        <Form.Check
+          type="checkbox"
+          id="custom-checkbox"
+          label={includeEncounters}
+        // checked={isChecked}
+        // onChange={handleOnChange}
+        />
+      </Form>
+      <FormGroupText
+        label="FILTER_ALTERNATIVE_ID_CONTAINS"
+      />
+      <FormGroupText
+        label="FILTER_INDIVIDUAL_NAME_CONTAINS"
+      />
     </div>
   );
 }
