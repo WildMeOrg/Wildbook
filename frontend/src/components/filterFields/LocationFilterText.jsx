2d02d70df1bdf1c0b6535ba2079a5642f417976e
erinz2020 <erinz2020@gmail.com>
2024-07-12 02:24:25 +0000

    more details

--- frontend/src/components/filterFields/LocationFilterText.jsx
+++ frontend/src/components/filterFields/LocationFilterText.jsx
@@ -4,26 +4,23 @@ import { FormGroup, FormLabel, FormControl, FormSelect } from 'react-bootstrap';
 import Description from '../Form/Description';
 import useGetSiteSettings from '../../models/useGetSiteSettings';
 import MultiSelect from '../MultiSelect';
-import { set } from 'lodash-es';
+import FormGroupMultiSelect from '../Form/FormGroupMultiSelect';
 
 export default function LocationFilterText({
-    formFilters,
-    setFormFilters,
-    test,
     onChange,
     onClearFilter,
 }
 ) {
     const { data } = useGetSiteSettings();
-    const locations = data?.locationID || [{ value: 'VB', label: 'Country 1' },
-    { value: 'KB', label: 'Country 2' },
-    { value: 'KOB', label: 'Country   3' }];
-    const countries = data?.country || [
-        { value: '1', label: 'Country 1', realValue: 'VB'},
-        { value: '2', label: 'Country 2', realValue: 'KB' },
-        { value: '3', label: 'Country   3', realValue: 'KOB' }
-    ];
-
+    console.log("+++++++++++++++++data", data);
+    const locationNameOptions = data?.location;
+    const locationIDOptions = data?.location;
+    const countries = data?.country.map(data => {
+        return {
+            value: data,
+            label: data
+        }
+    }) || [];
 
     return (
         <div className="mt-3">
@@ -31,75 +28,36 @@ export default function LocationFilterText({
             <Description>
                 <FormattedMessage id="FILTER_LOCATION_DESC" />
             </Description>
+            <FormGroupMultiSelect
+                isMulti={true}
+                label="FILTER_LOCATION_NAME"
+                options={locationNameOptions}
+                onChange={onChange}
+                term="terms"
+                field="name"
 
-            <FormGroup style={{
-                marginRight: '10px',
-            }}>
-                <FormLabel><FormattedMessage id="FILTER_LOCATION_NAME" /></FormLabel>
-                <Description>
-                    <FormattedMessage id="FILTER_LOCATION_NAME_DESC" />
-                </Description>
-                <FormControl
-                    type="text"
-                    placeholder={"Type Here"}
-                    onChange={(e) => 
-                          onChange({                           
-                                
-                                    "match": {
-                                      ["locationName"]: e.target.value     
-                                    }
-                                          
-                          })
-                    }
-                   
-                />
-            </FormGroup>
-            <FormGroup style={{ marginRight: '10px' }}>
-                <FormLabel>
-                    <FormattedMessage id="FILTER_LOCATION_ID" defaultMessage="Location ID" />
-                </FormLabel>
-                <Description>
-                    <FormattedMessage id="FILTER_LOCATION_ID_DESC" defaultMessage="Enter the location ID." />
-                </Description>
+            />
 
-                <MultiSelect
-                    isMulti={true}
-                    options={locations}
-                    onChange={(e) => 
-                        onChange({                           
-                              
-                                  "terms": {
-                                    ["locationId"]: e.map((item) => item.value)    
-                                  }
-                                        
-                        })
-                  }
-                />
+            <FormGroupMultiSelect
+                isMulti={true}
+                label="FILTER_LOCATION_ID"
+                options={locationIDOptions}
+                onChange={onChange}
+                term="terms"
+                field="id"
 
-            </FormGroup>
+            />
 
-            <FormGroup style={{ marginRight: '10px' }}>
-                <FormLabel>
-                    <FormattedMessage id="FILTER_COUNTRY" defaultMessage="Country" />
-                </FormLabel>
-                <Description>
-                    <FormattedMessage id="FILTER_COUNTRY_DESC" defaultMessage="Enter the COUNTRY" />
-                </Description>
+            <FormGroupMultiSelect
+                isMulti={true}
+                label="FILTER_COUNTRY"
+                options={countries}
+                onChange={onChange}
+                term="terms"
+                field="country"
 
-                <MultiSelect
-                    isMulti={true}
-                    options={countries}
-                    onChange={(e) => 
-                        onChange({                           
-                              
-                                  "terms": {
-                                    ["country"]: e.map((item) => item.value)    
-                                  }
-                                        
-                        })
-                  }
-                />
-            </FormGroup>
+            />
+
         </div>
     );
 }
