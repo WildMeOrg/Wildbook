5b2bc628fe3c88e02a5afc161e7adbf7ef1657c3
erinz2020 <erinz2020@gmail.com>
2024-07-12 21:55:04 +0000

    sidebar and chip

---
+++ frontend/src/components/Chip.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Badge } from 'react-bootstrap';
+
+function Chip({ text, children }) {
+    function renderFilter(filter) {
+        console.log('filter', filter);
+        const entries = [];
+        const { filterId, query } = filter;
+
+        if (query?.range) {
+            Object.entries(query.range).forEach(([key, range]) => {
+                const parts = [];
+                if (range.gte) parts.push(`from "${range.gte}"`);
+                if (range.lte) parts.push(`to "${range.lte}"`);
+                entries.push(`${key} ${parts.join(' ')}`);
+            });
+        }
+        if (query?.match) {
+            Object.entries(query.match).forEach(([key, value]) => {
+                entries.push(`"${key}" matches "${value}"`);
+            });
+        }
+        if (query?.term) {
+            Object.entries(query.term).forEach(([key, value]) => {
+                entries.push(`${key} is "${value}"`);
+            });
+        }
+        if (query?.terms) {
+            Object.entries(query.terms).forEach(([key, values]) => {
+                if (Array.isArray(values)) {
+                    entries.push(`${key} is any of [${values.join(', ')}]`);
+                } else {
+                    entries.push(`${key} is "${values}"`);
+                }
+            });
+        }
+
+        return entries.length > 0 ? `${entries.join(', ')}` : `No filters set`;
+    }
+
+    return (
+        <div
+            style={{
+                backgroundColor: '#e5f6ff',
+                color: 'black',
+                borderRadius: '15px',
+                padding: '10px 20px',
+                boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.2)',
+                marginTop: '10px',
+            }}
+        >
+            {renderFilter(children)}
+        </div>
+    );
+}
+
+export default Chip;
---
+++ frontend/src/components/filterFields/SideBar.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import { Button, Offcanvas } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FormattedMessage } from 'react-intl';
+import Chip from '../Chip';
+
+function Sidebar({
+  formFilters,
+  setFilterPanel,
+  setFormFilters,
+}) {
+  const [show, setShow] = useState(false);
+  const sidebarWidth = 300;
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const num = formFilters.length;
+
+
+  return (
+    <>
+      <Button
+        onClick={handleShow}
+        style={{
+          borderRadius: '0 0 5px 5px',
+          width: '200px',
+          height: '45px',
+          backgroundColor: 'rgb(0, 117, 153)',
+          border: "none",
+          color: 'white',
+          position: 'fixed',
+          top: '50%',
+          right: show ? `${sidebarWidth}px` : '0px',
+          zIndex: '1030',
+          transform: 'translateY(-50%) translateX(42%) rotate(90deg)',
+          transition: 'right 0.3s ease-in-out',
+          padding: '8px 10px',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <FormattedMessage id="APPLIED_FILTERS" />
+        <div style={{
+          width: "20px",
+          height: "20px",
+          borderRadius: "50%",
+          backgroundColor: 'red',
+          color: 'white',
+          marginLeft: "10px",
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>{num}</div>
+      </Button>
+
+      <Offcanvas show={show} onHide={handleClose} placement="end" style={{ width: `${sidebarWidth}px`, borderRadius: '10px 0 0 10px' }}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Applied Filters</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between'
+        }}>
+          <div style={{ overflowY: 'auto' }}>
+            {formFilters.map((filter, index) => (
+              <Chip key={index}>
+                {filter}
+              </Chip>
+            ))}
+          </div>
+          <div className='d-flex justify-content-between align-items-center'
+            style={{
+              padding: '10px 0',
+            }}>
+            <button className="btn btn-primary" type="button"
+              onClick={() => {
+                setFilterPanel(true);
+                handleClose();
+              }}
+            >
+              Edit Filters
+            </button>
+            <button
+              className="btn btn-secondary"
+              type="button"
+              onClick={() => {
+                setFormFilters([]);
+                handleClose();
+                setFilterPanel(false);
+              }}
+            >
+              Reset Filters
+            </button>
+          </div>
+        </Offcanvas.Body>
+
+      </Offcanvas>
+    </>
+  );
+}
+
+export default Sidebar;
