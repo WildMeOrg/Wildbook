d35f1f53eac3512b23b1c4a514377cc3a48fbc09
Jon Van Oast <jon@wildme.org>
2024-07-17 11:52:25 -0600

    learning about brave new world of nested mappings

--- src/main/java/org/ecocean/Encounter.java
+++ src/main/java/org/ecocean/Encounter.java
@@ -4495,6 +4495,8 @@ public class Encounter extends Base implements java.io.Serializable {
         map.put("taxonomy", new org.json.JSONObject("{\"type\": \"keyword\"}"));
         map.put("state", new org.json.JSONObject("{\"type\": \"keyword\"}"));
         map.put("organizations", new org.json.JSONObject("{\"type\": \"keyword\"}"));
+        // https://stackoverflow.com/questions/68760699/matching-documents-where-multiple-fields-match-in-an-array-of-objects
+        map.put("measurements", new org.json.JSONObject("{\"type\": \"nested\"}"));
         map.put("locationGeoPoint", new org.json.JSONObject("{\"type\": \"geo_point\"}"));
         return map;
     }
