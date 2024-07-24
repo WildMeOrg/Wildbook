fa2aacfff0355b281084581b0b9ead2471e13bef
erinz2020 <erinz2020@gmail.com>
2024-07-24 15:42:00 +0000

    customize dropdown menu

--- frontend/src/components/MultiSelect.jsx
+++ frontend/src/components/MultiSelect.jsx
@@ -3,35 +3,41 @@ import Select from 'react-select';
 const colourStyles = {
     option: (styles) => ({
         ...styles,
-        color: 'black', 
+        color: 'black',
     }),
     control: (styles) => ({ ...styles, backgroundColor: 'white' }),
-    singleValue: (styles) => ({ ...styles, color: 'black' }), 
+    singleValue: (styles) => ({ ...styles, color: 'black' }),
+    menuPortal: base => ({ ...base, zIndex: 1050 }),
+    // menu: base => ({ ...base, maxHeight: '200px' }),
+    control: base => ({ ...base, zIndex: 1 }),
 };
 
 export default function MultiSelect({ isMulti, options, onChange, field, term }) {
     return (
         <Select
-            isMulti = {isMulti}
+            isMulti={isMulti}
             options={options}
             className="basic-multi-select"
             classNamePrefix="select"
             styles={colourStyles}
-            onChange={(e) =>{
-                if(e?.target?.value || e.length > 0){
+            menuPlacement="auto"
+            menuPortalTarget={document.body}
+
+            onChange={(e) => {
+                if (e?.target?.value || e.length > 0) {
                     onChange({
                         filterId: field,
                         clause: "filter",
-                        query:{
+                        query: {
                             [term]: {
-                                [field]: isMulti? e.map(item=> item.value) : e.value
+                                [field]: isMulti ? e.map(item => item.value) : e.value
                             }
-                        }                    
+                        }
                     })
-                }else {
+                } else {
                     onChange(null, field);
                 }
-                }
+            }
             }
         />
     );
