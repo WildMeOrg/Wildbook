38b8f776a8d3be3d236a251638f64b2394da763c
erinz2020 <erinz2020@gmail.com>
2024-07-26 15:00:01 +0000

    now this components is a mess, onChange function everywhere

--- frontend/src/components/filterFields/BiologicalSamplesAndAnalysesFilter.jsx
+++ frontend/src/components/filterFields/BiologicalSamplesAndAnalysesFilter.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FormattedMessage } from 'react-intl';
 import Form from 'react-bootstrap/Form';
 import Description from '../Form/Description';
@@ -8,6 +8,7 @@ import FormMeasurements from '../Form/FormMeasurements';
 import { FormGroup, FormLabel, FormControl } from 'react-bootstrap';
 import FormGroupMultiSelect from '../Form/FormGroupMultiSelect';
 import BioMeasurements from '../Form/BioMeasurements';
+import { all } from 'axios';
 
 export default function BiologicalSamplesAndAnalysesFilter({
     onChange,
@@ -17,9 +18,12 @@ export default function BiologicalSamplesAndAnalysesFilter({
     const [isChecked, setIsChecked] = React.useState(false);
     const bioMeasurementOptions = Object.entries(data?.bioMeasurement || {}).map(
         item => item[0]
-    ) || [];    
+    ) || [];
 
-    const microSatelliteMarkerLoci = data?.loci || [];
+    const microSatelliteMarkerLoci = ["X1", "Y2"];
+    // data?.loci || [];
+    // 
+
     const [checkedState, setCheckedState] = useState({});
     const [alleleLength, setAlleleLength] = React.useState(false);
     const [length, setLength] = React.useState(null);
@@ -38,6 +42,80 @@ export default function BiologicalSamplesAndAnalysesFilter({
         }
     }) || [];
 
+    const [currentValues, setCurrentValues] = useState({});
+
+
+    const buildQuery_range = (data, i, value) => {
+        onChange({
+            filterId: `microsatelliteMarkers.loci.${data}.allele${i}`,
+            clause: "filter",
+            query: {
+                "range": {
+                    [`microsatelliteMarkers.loci.${data}.allele${i}`]: {
+                        "gte": parseInt(value, 10) - parseInt(length),
+                        "lte": parseInt(value, 10) + parseInt(length)
+                    }
+                }
+            }
+        });
+    }
+    const buildQuery_match = (data, i, value) => {
+        onChange({
+            filterId: `microsatelliteMarkers.loci.${data}.allele${i}`,
+            clause: "filter",
+            query: {
+                "match": {
+                    [`microsatelliteMarkers.loci.${data}.allele${i}`]: value
+                }
+            }
+        });
+    }
+
+    useEffect(() => {
+        // Iterate through each marker and re-calculate the query if necessary        
+        Object.keys(currentValues).forEach(data => {
+            if (checkedState[data]) {
+                const value0 = currentValues[data]?.allele0;
+                const value1 = currentValues[data]?.allele1;
+                if (value0 !== undefined) {
+                    if (alleleLength && length) {
+                        buildQuery_range(data, 0, value0);
+                    } else {
+                        buildQuery_match(data, 0, value0);
+                    }
+                }
+                if (value1 !== undefined) {
+                    if (alleleLength && length) {
+                        buildQuery_range(data, 1, value1);
+                    } else {
+                        buildQuery_match(data, 1, value1);
+                    }
+                }
+            }
+        });
+
+    }, [alleleLength, length]);
+
+    const handleInputChange = (data, index, value) => {
+        setCurrentValues(prevState => ({
+            ...prevState,
+            [data]: {
+                ...prevState[data],
+                [`allele${index}`]: value
+            }
+        }));
+
+        if (checkedState[data]) {
+            if (value === "") {
+                onChange(null, `microsatelliteMarkers.loci.${data}.allele${index}`);
+            } else if (alleleLength && length) {
+                buildQuery_range(data, index, value);
+            } else {
+                buildQuery_match(data, index, value);
+            }
+        }
+    }
+
 
     const [bioChemicalValue, setBioChemicalValue] = React.useState(null);
     return (
@@ -54,22 +132,23 @@ export default function BiologicalSamplesAndAnalysesFilter({
                     checked={isChecked}
                     onChange={(e) => {
                         setIsChecked(!isChecked);
-                        if(!e.target.checked) {
+                        if (!e.target.checked) {
                             console.log(1);
                             onChange(null, `biologicalSampleId`);
                             return;
                         } else {
                             console.log(2);
-                        onChange({
-                            filterId: `biologicalSampleId`,
-                            clause: "filter",
-                            query: {
-                                "exists": {
-                                    "field": "tissueSampleIds"
+                            onChange({
+                                filterId: `biologicalSampleId`,
+                                clause: "filter",
+                                query: {
+                                    "exists": {
+                                        "field": "tissueSampleIds"
+                                    }
                                 }
-                            }
-                        })
-                    }}
+                            })
+                        }
+                    }
                     }
                 />
             </Form>
@@ -101,13 +180,6 @@ export default function BiologicalSamplesAndAnalysesFilter({
                 filterId={"geneticSex"}
             />
 
-            {/* <FormMeasurements
-                data={bioMeasurementOptions}
-                onChange={onChange}
-                filterId={"biologicalMeasurements"}
-                field={"biologicalMeasurements"}
-            /> */}
-
             <BioMeasurements
                 data={bioMeasurementOptions}
                 onChange={onChange}
@@ -117,7 +189,7 @@ export default function BiologicalSamplesAndAnalysesFilter({
 
             <div className='d-flex flex-row justify-content-between'>
                 <h5><FormattedMessage id="FILTER_MARKER_LOCI" /></h5>
-                <div className="d-flex flex-row">
+                <div className="d-flex flex-row align-items-center">
                     <Form.Check
                         label={<FormattedMessage id="FILTER_RELAX_ALLELE_LENGTH" />}
                         type="checkbox"
@@ -128,16 +200,22 @@ export default function BiologicalSamplesAndAnalysesFilter({
                         }
                         }
                     />
-                    
+                    <FormControl
+                        type="number"
+                        disabled={!alleleLength}
+                        placeholder="Type Here"
+                        style={{
+                            width: "70px",
+                            marginLeft: "10px",
+                        }}
+                        onChange={(e) => {
+                            setLength(e.target.value);                            
+                        }}
+                    />
                 </div>
-
             </div>
-
-
             {
                 microSatelliteMarkerLoci?.map((data) => {
-
-
                     const handleCheckboxChange = (item) => {
                         setCheckedState(prevState => ({
                             ...prevState,
@@ -145,101 +223,63 @@ export default function BiologicalSamplesAndAnalysesFilter({
                         }));
                     };
 
-                    const inputs = Object.keys(data.loci).map((item) => {
-                        return <FormGroup className="d-flex flex-column gap-2">
-                            <Form.Check
-                                type="checkbox"
-                                id="custom-checkbox"
-                                label={item}
-                                checked={checkedState[item] || false}
-                                onChange={() => {
-                                    handleCheckboxChange(item);
-                                }
-                                }
-                            />
-                            <div className="d-flex flex-row gap-3 ms-5">
-                                <div className="d-flex flex-column w-50">
-                                    <FormLabel ><FormattedMessage id={"ALLELE1"} defaultMessage={"ALLELE1"} /></FormLabel>
-                                    <FormControl className="mr-2"
-                                        type="text"
-                                        placeholder="Type Here"
-                                        onChange={(e) => {
-                                            if (checkedState[item]) {
-                                                if (alleleLength) {
-                                                    onChange({
-                                                        filterId: `${item}.allele1`,
-                                                        clause: "filter",
-                                                        query: {
-                                                            "range": {
-                                                                [item]: {
-                                                                    "gte": parseInt(e.target.value, 10) - parseInt(length),
-                                                                    "lte": parseInt(e.target.value, 10) + parseInt(length)
-                                                                }
-                                                            }
-                                                        }
-                                                    });
-                                                } else {
-                                                    onChange({
-                                                        filterId: `${item}.allele1`,
-                                                        clause: "filter",
-                                                        query: {
-                                                            "match": {
-                                                                [item]: e.target.value
-                                                            }
-                                                        }
-                                                    });
-                                                }
+                    return <FormGroup className="d-flex flex-column gap-2">
+                        <Form.Check
+                            type="checkbox"
+                            id="custom-checkbox"
+                            label={data}
+                            checked={checkedState[data] || false}
+                            onChange={() => {
+                                handleCheckboxChange(data);
+                                if (checkedState[data]) {
+                                    onChange(null, `microsatelliteMarkers.loci.${data}.allele0`);
+                                    onChange(null, `microsatelliteMarkers.loci.${data}.allele1`);
+                                } else {
+                                    if (currentValues[data]?.allele0) {
+                                        if(alleleLength && length){
+                                            console.log(1);
+                                            buildQuery_range(data, 0, currentValues[data]?.allele0);
+                                        }
+                                        else{
+                                            console.log(2);
+                                            buildQuery_match(data, 0, currentValues[data]?.allele0);
+                                        }
+                                    }
+                                    if (currentValues[data]?.allele1) {
+                                        if(alleleLength && length){
+                                            console.log(3);
+                                            buildQuery_range(data, 1, currentValues[data]?.allele1);
+                                        }else {
+                                            console.log(4);
+                                        buildQuery_match(data, 1, currentValues[data]?.allele1);
+                                        }
+                                    }
 
-                                            }
-                                        }}
-                                    />
-                                </div>
-                                <div className="d-flex flex-column w-50">
-                                    <FormLabel><FormattedMessage id={"ALLELE1"} defaultMessage={"ALLELE2"} /></FormLabel>
-                                    <FormControl
-                                        type="text"
-                                        placeholder="Type Here"
-                                        onChange={(e) => {
-                                            if (checkedState[item]) {
-                                                if (alleleLength) {
-                                                    onChange({
-                                                        filterId: `${item}.allele2`,
-                                                        clause: "filter",
-                                                        query: {
-                                                            "range": {
-                                                                [item]: {
-                                                                    "gte": parseInt(e.target.value, 10) - parseInt(length),
-                                                                    "lte": parseInt(e.target.value, 10) + parseInt(length)
-                                                                }
-                                                            }
-                                                        }
-                                                    });
-                                                } else {
-                                                    onChange({
-                                                        filterId: `${item}.allele2`,
-                                                        clause: "filter",
-                                                        query: {
-                                                            "match": {
-                                                                [item]: e.target.value
-                                                            }
-                                                        }
-                                                    });
-                                                }
-
-                                            }
-                                        }}
-                                    />
-                                </div>
+                            }
+                            }}
+                        />
+                        <div className="d-flex flex-row gap-3 ms-5">
+                            <div className="d-flex flex-column w-50">
+                                <FormLabel ><FormattedMessage id={"ALLELE1"} defaultMessage={"ALLELE1"} /></FormLabel>
+                                <FormControl className="mr-2"
+                                    type="text"
+                                    placeholder="Type Here"
+                                    onChange={(e) => handleInputChange(data, 0, e.target.value)}
+                                />
                             </div>
+                            <div className="d-flex flex-column w-50">
+                                <FormLabel><FormattedMessage id={"ALLELE1"} defaultMessage={"ALLELE2"} /></FormLabel>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Type Here"
+                                    onChange={(e) => handleInputChange(data, 1, e.target.value)}
+                                />
+                            </div>
+                        </div>
 
-                        </FormGroup>
-                    })
-
-                    return inputs;
-
-                })}
-
-
+                    </FormGroup>
+                })
+            }
         </div>
     );
 }
