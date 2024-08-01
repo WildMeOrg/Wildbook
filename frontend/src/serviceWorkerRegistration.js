f0cdb46814319cd03382b8fc87f00a226f1b6fca
erinz2020 <erinz2020@gmail.com>
2024-08-01 15:17:36 +0000

    only cancel service worker locally

--- frontend/src/serviceWorkerRegistration.js
+++ frontend/src/serviceWorkerRegistration.js
@@ -19,8 +19,6 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
-  console.log('Service worker registration cancelled');
-  return;
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
