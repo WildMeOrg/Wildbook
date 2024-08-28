daf66b30b652323a8d8b519c1f68aabbbd39e5f9
Jon Van Oast <jon@wildme.org>
2024-08-28 17:15:16 -0600

    src/test/

---
+++ src/test/java/SomeTest.java
@@ -0,0 +1,10 @@
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+ 
+
+class SomeTest {
+ 
+    @Test
+    void generalTest() {
+    }
+}
---
+++ src/test/java/org/ecocean/AnnotationTest.java
@@ -0,0 +1,10 @@
+package org.ecocean;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+ 
+class AnnotationTest {
+ 
+    @Test
+    void exampleTest() {
+    }
+}
