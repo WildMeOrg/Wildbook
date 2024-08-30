a2bc37063c072b2f14c92ae3d8d617dc7a37df7c
Jon Van Oast <jon@wildme.org>
2024-08-30 16:32:37 -0600

    simple example test

--- src/test/java/SomeTest.java
+++
@@ -1,7 +0,0 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
-
-class SomeTest {
-    @Test void generalTest() {
-    }
-}
--- src/test/java/org/ecocean/AnnotationTest.java
+++ src/test/java/org/ecocean/AnnotationTest.java
@@ -1,8 +1,25 @@
 package org.ecocean;
+
 // import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.ecocean.Annotation;
+import org.ecocean.media.*;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
 
 class AnnotationTest {
-    @Test void exampleTest() {
+/*
+    to create a (real) MediaAsset or a Feature we need to have a Shepherd!
+    TODO extend to this with mockito/Shepherd
+    https://semaphoreci.com/community/tutorials/stubbing-and-mocking-with-mockito-2-and-junit
+ */
+    @Test void createAnnotation() {
+        AssetStore store = null; // see note above
+        MediaAsset ma = new MediaAsset(store, null);
+        Annotation ann = new Annotation("species", ma);
+
+        assertNotNull(ann);
+        assertTrue(ann.isTrivial());
+        assertEquals(ann.getMediaAsset(), ma);
     }
 }
