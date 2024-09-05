f6fe9d657e6759327eb21f77f2c0fb86434d73ae
erinz2020 <erinz2020@gmail.com>
2024-09-05 19:49:27 +0000

    stricter syntax checking

--- frontend/src/components/filterFields/LocationFilter.jsx
+++ frontend/src/components/filterFields/LocationFilter.jsx
@@ -1,20 +1,12 @@
-import React from 'react';
-import LocationFilterMap from './LocationFilterMap';
-import LocationFilterText from './LocationFilterText';
+import React from "react";
+import LocationFilterMap from "./LocationFilterMap";
+import LocationFilterText from "./LocationFilterText";
 
-export default function LocationFilter({
-    onChange,
-    data,
-}) {
-    return (
-        <div>
-            <LocationFilterMap
-                onChange={onChange}
-                data={data}
-            />
-            <LocationFilterText
-                onChange={onChange}
-            />
-        </div>
-    )
+export default function LocationFilter({ onChange, data }) {
+  return (
+    <div>
+      <LocationFilterMap onChange={onChange} data={data} />
+      <LocationFilterText onChange={onChange} />
+    </div>
+  );
 }
