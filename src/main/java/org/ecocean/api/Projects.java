f0339e519eb40b832ef7679dff41e731429d76a0
veda-gogineni <vedagogineni98@gmail.com>
2024-09-05 14:26:55 -0700

    Api change /all on the end of the request

--- src/main/java/org/ecocean/api/Projects.java
+++ src/main/java/org/ecocean/api/Projects.java
@@ -1,10 +1,7 @@
 package org.ecocean.api;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
@@ -17,6 +14,7 @@ import org.ecocean.servlet.ServletUtilities;
 
 public class Projects extends ApiBase {
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         String context = ServletUtilities.getContext(request);
@@ -25,47 +23,61 @@ public class Projects extends ApiBase {
         myShepherd.setAction("api.Projects.GET");
         myShepherd.beginDBTransaction();
 
-        JSONObject result = new JSONObject();
         User currentUser = myShepherd.getUser(request);
         if (currentUser == null) {
             response.setStatus(401);
             response.setHeader("Content-Type", "application/json");
             response.getWriter().write("{\"success\": false}");
+            myShepherd.rollbackDBTransaction();
+            myShepherd.closeDBTransaction();
             return;
         }
 
-        List<Project> userProjects = currentUser.getProjects(myShepherd);
-        JSONArray userProjectsArr = new JSONArray();
-        for (Project proj : userProjects) {
-            JSONObject pj = new JSONObject();
-            pj.put("id", proj.getId());
-            pj.put("name", proj.getResearchProjectName());
-            pj.put("percentComplete", proj.getPercentWithIncrementalIds());
-            pj.put("numberEncounters", proj.getEncounters().size());
-            userProjectsArr.put(pj);
+        JSONObject result = new JSONObject();
+        String pathInfo = request.getPathInfo();
+
+        if ((pathInfo == null) || pathInfo.equals("/")) {
+            // No additional path, return projects accessible to the current user
+            List<Project> userProjects = currentUser.getProjects(myShepherd);
+            JSONArray userProjectsArr = new JSONArray();
+            for (Project proj : userProjects) {
+                JSONObject pj = new JSONObject();
+                pj.put("id", proj.getId());
+                pj.put("name", proj.getResearchProjectName());
+                pj.put("percentComplete", proj.getPercentWithIncrementalIds());
+                pj.put("numberEncounters", proj.getEncounters().size());
+                userProjectsArr.put(pj);
+            }
+            result.put("projects", userProjectsArr);
+
+            response.setStatus(200);
+            response.setHeader("Content-Type", "application/json");
+            response.getWriter().write(result.toString());
+        } else if (pathInfo.endsWith("/all")) {
+            // /all is present, return all projects in the system
+            List<Project> allSystemProjects = myShepherd.getAllProjects();
+            JSONArray allProjectsArr = new JSONArray();
+            for (Project proj : allSystemProjects) {
+                JSONObject pj = new JSONObject();
+                pj.put("id", proj.getId());
+                pj.put("name", proj.getResearchProjectName());
+                pj.put("percentComplete", proj.getPercentWithIncrementalIds());
+                pj.put("numberEncounters", proj.getEncounters().size());
+                allProjectsArr.put(pj);
+            }
+            result.put("projects", allProjectsArr);
+
+            response.setStatus(200);
+            response.setHeader("Content-Type", "application/json");
+            response.getWriter().write(result.toString());
+        } else {
+            // Invalid path
+            response.setStatus(404);
+            response.setHeader("Content-Type", "application/json");
+            response.getWriter().write("{\"success\": false, \"error\": \"Endpoint not found\"}");
         }
-        result.put("projects", userProjectsArr);
 
-        response.setStatus(200);
-        response.setHeader("Content-Type", "application/json");
-        response.getWriter().write(result.toString());
-    }
-
-    protected void AllProjects(HttpServletResponse response, Shepherd myShepherd, JSONObject result) throws Exception {
-        List<Project> allSystemProjects = myShepherd.getAllProjects();
-        JSONArray allProjectsArr = new JSONArray();
-        for (Project proj : allSystemProjects) {
-            JSONObject pj = new JSONObject();
-            pj.put("id", proj.getId());
-            pj.put("name", proj.getResearchProjectName());
-            pj.put("percentComplete", proj.getPercentWithIncrementalIds());
-            pj.put("numberEncounters", proj.getEncounters().size());
-            allProjectsArr.put(pj);
-        }
-        result.put("projects", allProjectsArr);
-
-        response.setStatus(200);
-        response.setHeader("Content-Type", "application/json");
-        response.getWriter().write(result.toString());
+        myShepherd.rollbackDBTransaction();
+        myShepherd.closeDBTransaction();
     }
 }
