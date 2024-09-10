01b8c79398699a959fcf8a70d850af68d2175dbc
erinz2020 <erinz2020@gmail.com>
2024-09-10 20:41:49 +0000

    jest configurations

---
+++ frontend/jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)", // Looks inside __tests__ folders
+    "**/?(*.)+(spec|test).[tj]s?(x)", // Looks for .spec.js/.test.js files
+  ],
+  transform: {
+    "^.+\\.[t|j]sx?$": "babel-jest",
+  },
+  // transformIgnorePatterns: [
+  //     '/node_modules/(?!(axios)/)', // Tell Jest to transform axios
+  // ],
+
+  // transform: {
+  //     "\\.tsx?$": "ts-jest",
+  //     "\\.jsx?$": "babel-jest",
+  // },
+  transformIgnorePatterns: ["/node_modules/(?!(axios)).+\\.js$"],
+  testEnvironment: "jsdom", // Set the environment for testing React components
+  moduleNameMapper: {
+    "^axios$": require.resolve("axios"),
+  },
+};
