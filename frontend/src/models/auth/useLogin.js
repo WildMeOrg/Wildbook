5d8db866e0ca9cc62e46b8e652646240db2e56d1
erinz2020 <erinz2020@gmail.com>
2024-09-24 23:31:48 +0000

    remove api call before login

--- frontend/src/models/auth/useLogin.js
+++ frontend/src/models/auth/useLogin.js
@@ -1,42 +1,46 @@
-import { useState } from 'react';
-import axios from 'axios';
-import { get } from 'lodash-es';
-import { useIntl } from 'react-intl';
+import { useState } from "react";
+import axios from "axios";
+import { get } from "lodash-es";
+import { useIntl } from "react-intl";
 
 export default function useLogin() {
   const intl = useIntl();
 
   const errorMessage = intl.formatMessage({
-    id: 'LOGIN_INVALID_EMAIL_OR_PASSWORD',
+    id: "LOGIN_INVALID_EMAIL_OR_PASSWORD",
   });
 
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const authenticate = async (username, password, nextLocation) => {
+  const authenticate = async (username, password) => {
     try {
       setLoading(true);
       const response = await axios.request({
         url: `/api/v3/login`,
-        method: 'post',
+        method: "post",
         data: {
           username,
           password,
         },
       });
 
-      const successful = get(response, 'data.success', false);
+      const nextLocation = get(response, "data.redirectUrl", null);
+
+      const successful = get(response, "data.success", false);
+
+      console.log("nextLocation", nextLocation);
 
       if (successful) {
         let url = `${process.env.PUBLIC_URL}/home`;
         if (nextLocation) {
-          url = nextLocation?.pathname;
-          url = nextLocation?.search
-            ? url + nextLocation.search
-            : url;
-          url = nextLocation?.hash ? url + nextLocation.hash : url;
-        }
-        window.location.href = url;
+          // url = nextLocation?.pathname;
+          // url = nextLocation?.search
+          //   ? url + nextLocation.search
+          //   : url;
+          // url = nextLocation?.hash ? url + nextLocation.hash : url;
+          window.location.href = nextLocation;
+        } else window.location.href = url;
         // Fun quirk - a reload is required if there is a hash in the URL.
         // https://stackoverflow.com/questions/10612438/javascript-reload-the-page-with-hash-value
         if (nextLocation?.hash) window.location.reload();
@@ -46,7 +50,7 @@ export default function useLogin() {
     } catch (loginError) {
       setLoading(false);
       setError(errorMessage);
-      console.error('Error logging in');
+      console.error("Error logging in");
       console.error(loginError);
     }
   };
