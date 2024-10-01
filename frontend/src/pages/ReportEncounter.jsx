ffed4ea74a3b1f1fd52df48592e4965b2b766e46
erinz2020 <erinz2020@gmail.com>
2024-10-01 22:56:23 +0000

    add catetories

--- frontend/src/pages/ReportEncounter.jsx
+++ frontend/src/pages/ReportEncounter.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import ThemeColorContext from "../ThemeColorProvider";
 import { Alert } from "react-bootstrap";
@@ -7,24 +7,25 @@ import MainButton from "../components/MainButton";
 export default function ReportEncounter() {
 
     const themeColor = useContext(ThemeColorContext);
-    const menu = [
-        { title: "Photos" },
-        { title: "Videos" },
-        { title: "Text" },
-        { title: "Location" },
-        { title: "Date" },
-        { title: "Time" },
-        { title: "Species" },
-    ]
+    const encounterCategories = ["photos", "date and time", "place", "species", "additional comments", "followup information"];
+    const menu = encounterCategories.map((category, index) => {
+        return { 
+            id: index,
+            title: category };
+    }
+    );
+
+    const [selectedCategory, setSelectedCategory ] = useState(0);
+
     return (
         <Container>
             <Row >
-                <h1>Report an Encounter</h1>
+                <h3 className="pt-4">Report an Encounter</h3>
                 <p>
                     Please use the online form below to record the details of your encounter. Be as accurate and specific as possible.                     </p>
                 <Alert
                     variant="warning"
-                    onClose={() => {}}
+                    onClose={() => { }}
                     dismissible
                 >
                     <i className="bi bi-info-circle-fill" style={{ marginRight: '8px', color: '#7b6a00' }}></i>
@@ -34,34 +35,49 @@ export default function ReportEncounter() {
                     </a>
                 </Alert>
             </Row>
+            <Row>
+                <Alert
+                    variant="light"
+                    className="d-inline-block p-2"
+                    style={{ backgroundColor: '#fff5f5', color: '#dc3545', width: "auto" }}>
+                    <strong>* required fields</strong>
+                </Alert>
+            </Row>
 
             <Row >
-                <Col className="col-lg-4 col-md-6 col-sm-12 col-12">
-                    <Alert variant="light" className="d-inline-block p-2" style={{ backgroundColor: '#fff5f5', color: '#dc3545' }}>
-                        <strong>*</strong> required fields
-                    </Alert>
+                <Col className="col-lg-5 col-md-6 col-sm-12 col-12 ps-0">
+
                     <div
                         style={{
-                            backgroundImage: `url(${process.env.PUBLIC_URL}/images/report_an_encounter.png)`,
+                            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(${process.env.PUBLIC_URL}/images/report_an_encounter.png)`,
                             backgroundSize: 'cover',
                             backgroundPosition: 'center',
                             borderRadius: '25px',
-                            padding: "10px",
-                            height: '500px',
+                            padding: "20px",
+                            height: '470px',
                             width: '350px',
                             color: 'white',
+                            marginBottom: '20px',
                         }}
                     >
                         {
                             menu.map(data => {
+                                console.log(data);
+                                console.log(selectedCategory);
                                 return <div
                                     className="d-flex justify-content-between"
                                     style={{
-                                        padding: '15px',
+                                        padding: '10px',
+                                        marginTop: '10px',
                                         fontSize: '20px',
                                         fontWeight: '500',
+                                        cursor: 'pointer',
+                                        borderRadius: '10px',
+                                        backgroundColor: selectedCategory === data.id ? "rgba(255,255,255,0.5)": 'transparent',
 
-                                    }}>
+                                    }}
+                                    onClick={() => setSelectedCategory(data.id)}
+                                    >
                                     {data.title}<i class="bi bi-chevron-right" style={{ fontSize: "14px", fontWeight: "500" }}></i>
                                 </div>
 
@@ -69,58 +85,94 @@ export default function ReportEncounter() {
 
                         }
 
-                       <MainButton 
-                        backgroundColor={themeColor.wildMeColors.cyan600}
-                        color={themeColor.defaultColors.white}
-                        shadowColor={themeColor.defaultColors.white}
-                        style={{
-                            width: "calc(100% - 20px)",
-                            fontSize: "20px",
-                        }}
-                        
-                       >
+                        <MainButton
+                            backgroundColor={themeColor.wildMeColors.cyan600}
+                            color={themeColor.defaultColors.white}
+                            shadowColor={themeColor.defaultColors.white}
+                            style={{
+                                width: "calc(100% - 20px)",
+                                fontSize: "20px",
+                                marginTop: "20px",
+                                marginBottom: "20px",
+                            }}
+
+                        >
                             Submit Encounter
-                        </MainButton> 
+                        </MainButton>
 
                     </div>
                 </Col>
-                <Col className="col-lg-8 col-md-6 col-sm-12 col-12">
+                <Col
+                    className="col-lg-7 col-md-6 col-sm-12 col-12 h-100"
+                    style={{
+                        // padding: '20px',
+                        // backgroundColor: '#f8f9fa',
+                        // borderRadius: '25px',
+                        maxHeight: '470px',
+                        overflow: 'auto',
+                    }}
+                >
                     <Form>
-                        <Form.Group controlId="formBasicEmail">
+                        <Form.Group controlId="0">
+                            <Form.Label>Location</Form.Label>
+                            <Form.Control type="text" placeholder="Enter location" />
+                            <Form.Label>Location</Form.Label>
+                            <Form.Control type="text" placeholder="Enter location" />
+                            <Form.Label>Location</Form.Label>
+                            <Form.Control type="text" placeholder="Enter location" />
                             <Form.Label>Location</Form.Label>
                             <Form.Control type="text" placeholder="Enter location" />
                         </Form.Group>
-                        <Form.Group controlId="formBasicPassword">
+                        <Form.Group controlId="1">
+                            <Form.Label>Date</Form.Label>
+                            <Form.Control type="date" placeholder="Enter date" />
+                            <Form.Label>Date</Form.Label>
+                            <Form.Control type="date" placeholder="Enter date" />
+                            <Form.Label>Date</Form.Label>
+                            <Form.Control type="date" placeholder="Enter date" />
                             <Form.Label>Date</Form.Label>
                             <Form.Control type="date" placeholder="Enter date" />
                         </Form.Group>
-                        <Form.Group controlId="formBasicPassword">
+                        <Form.Group controlId="2">
+                            <Form.Label>Time</Form.Label>
+                            <Form.Control type="time" placeholder="Enter time" />
+                            <Form.Label>Time</Form.Label>
+                            <Form.Control type="time" placeholder="Enter time" />
+                            <Form.Label>Time</Form.Label>
+                            <Form.Control type="time" placeholder="Enter time" />
                             <Form.Label>Time</Form.Label>
                             <Form.Control type="time" placeholder="Enter time" />
                         </Form.Group>
-                        <Form.Group controlId="formBasicPassword">
+                        <Form.Group controlId="3">
+                            <Form.Label>Species</Form.Label>
+                            <Form.Control type="text" placeholder="Enter species" />
+                            <Form.Label>Species</Form.Label>
+                            <Form.Control type="text" placeholder="Enter species" />
+                            <Form.Label>Species</Form.Label>
+                            <Form.Control type="text" placeholder="Enter species" />
                             <Form.Label>Species</Form.Label>
                             <Form.Control type="text" placeholder="Enter species" />
                         </Form.Group>
-                        <Form.Group controlId="formBasicPassword">
+                        <Form.Group controlId="4">
+                            <Form.Label>Number of animals</Form.Label>
+                            <Form.Control type="number" placeholder="Enter number of animals" />
+                            <Form.Label>Number of animals</Form.Label>
+                            <Form.Control type="number" placeholder="Enter number of animals" />
+                            <Form.Label>Number of animals</Form.Label>
+                            <Form.Control type="number" placeholder="Enter number of animals" />
                             <Form.Label>Number of animals</Form.Label>
                             <Form.Control type="number" placeholder="Enter number of animals" />
                         </Form.Group>
-                        <Form.Group controlId="formBasicPassword">
+                        <Form.Group controlId="5">
+                            <Form.Label>Behavior</Form.Label>
+                            <Form.Control type="text" placeholder="Enter behavior" />
+                            <Form.Label>Behavior</Form.Label>
+                            <Form.Control type="text" placeholder="Enter behavior" />
+                            <Form.Label>Behavior</Form.Label>
+                            <Form.Control type="text" placeholder="Enter behavior" />
                             <Form.Label>Behavior</Form.Label>
                             <Form.Control type="text" placeholder="Enter behavior" />
                         </Form.Group>
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Description</Form.Label>
-                            <Form.Control type="text" placeholder="Enter description" />
-                        </Form.Group>
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Photos</Form.Label>
-                            <Form.Control type="file" placeholder="Enter photos" />
-                        </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
                     </Form>
                 </Col>
 
