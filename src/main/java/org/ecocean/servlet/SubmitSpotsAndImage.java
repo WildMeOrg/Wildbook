0605c40cbe01046e196909134f40de5457eeba05
holmbergius <holmbergius@gmail.com>
2024-11-11 09:49:31 -0800

    LOCAL: preserving here for backward compatibility in transition phase

--- src/main/java/org/ecocean/servlet/SubmitSpotsAndImage.java
+++ src/main/java/org/ecocean/servlet/SubmitSpotsAndImage.java
@@ -129,7 +129,7 @@ public class SubmitSpotsAndImage extends HttpServlet {
         String speciesString = enc.getTaxonomyString();
         Annotation ann = new Annotation(speciesString, crMa);
         ann.setMatchAgainst(true);
-        String iaClass = "whalesharkCR"; // should we change this?
+        String iaClass = "shark"; // should we change this?
         ann.setIAClass(iaClass);
         if (rightSide) { ann.setViewpoint("right"); } else { ann.setViewpoint("left"); }
         enc.addAnnotation(ann);
