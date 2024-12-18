1af3784d687b8cf26bf11635d17a49f3f9a7b265
erinz2020 <erinz2020@gmail.com>
2024-12-18 20:28:43 +0000

    config webpack

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -24,20 +24,16 @@ const BundleAnalyzerPlugin =
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = override(
-  // 按需加载 antd 组件库
   fixBabelImports("import", {
     libraryName: "antd",
     libraryDirectory: "es",
     style: "css",
   }),
 
-  // 自定义配置生产模式
   (config, env) => {
     if (env === "production") {
-      // 开启 Source Map
       config.devtool = "source-map";
 
-      // 添加 TerserPlugin 压缩未使用代码
       config.optimization = {
         ...config.optimization,
         minimize: true,
@@ -45,37 +41,32 @@ module.exports = override(
           new TerserPlugin({
             terserOptions: {
               compress: {
-                drop_console: true, // 移除 console.log
-                drop_debugger: true, // 移除 debugger
-                unused: true, // 移除未使用代码 (Tree Shaking)
+                drop_console: true,
+                drop_debugger: true,
+                unused: true,
               },
-              output: {
-                comments: false, // 移除注释
-              },
+              output: {},
             },
-            extractComments: false, // 不生成额外的 LICENSE 文件
+            extractComments: false,
           }),
         ],
       };
 
-      // 添加 IgnorePlugin 移除 Moment.js 的语言包
       config.plugins.push(
         new webpack.IgnorePlugin({
-          resourceRegExp: /^\.\/locale$/, // 忽略所有 locale 文件
-          contextRegExp: /moment$/, // 只针对 moment 的 context
+          resourceRegExp: /^\.\/locale$/,
+          contextRegExp: /moment$/,
         }),
       );
     }
     return config;
   },
 
-  // 定义全局环境变量
   addWebpackPlugin(
     new webpack.DefinePlugin({
       "process.env.SITE_NAME": JSON.stringify(process.env.SITE_NAME),
     }),
   ),
 
-  // 添加 Bundle Analyzer 插件
   addWebpackPlugin(new BundleAnalyzerPlugin()),
 );
