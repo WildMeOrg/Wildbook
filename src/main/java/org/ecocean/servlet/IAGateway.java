b95bbd280b9d8dae93d91beec076db0b5aad50fe
sufwan khalid <sufwan@esketchers.com>
2025-01-08 02:11:16 +0500

    staging setup

--- src/main/java/org/ecocean/servlet/IAGateway.java
+++ src/main/java/org/ecocean/servlet/IAGateway.java
@@ -766,7 +766,7 @@ public class IAGateway extends HttpServlet {
     }
 
     public static boolean requeueJob(JSONObject jobj, final boolean increment) {
-        int MAX_RETRIES = 30;
+        int MAX_RETRIES = 3;
         long MAX_TIME_MILLIS = 2 * 24 * 60 * 60 * 1000;
         String context = jobj.optString("__context", "context0");
         String taskId = jobj.optString("taskId", "UNKNOWN_TASKID");
