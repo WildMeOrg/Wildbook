804b1b9c4a110bbb72add1abead4386af184930e
erinz2020 <erinz2020@gmail.com>
2025-01-24 21:06:01 +0000

    individual sex and taxonomy

--- frontend/src/components/filterFields/IndividualsObservationAttributeFilter.jsx
+++ frontend/src/components/filterFields/IndividualsObservationAttributeFilter.jsx
@@ -2,78 +2,57 @@ import React from "react";
 import Description from "../Form/Description";
 import { FormattedMessage } from "react-intl";
 import FormGroupMultiSelect from "../Form/FormGroupMultiSelect";
-import FormGroupText from "../Form/FormGroupText";
-import DynamicInputs from "../Form/DynamicInputs";
-import { FormLabel, FormGroup } from "react-bootstrap";
-import FormMeasurements from "../Form/FormMeasurements";
 
 export default function ObservationAttributeFilter({ onChange, data }) {
+  const sexOptions =
+    data?.sex?.map((item) => {
+      return {
+        value: item,
+        label: item,
+      };
+    }) || [];
 
-    const measurementsOptions = data?.measurement || [];
+  const genusAndSpeciesOptions =
+    data?.siteTaxonomies?.map((item) => {
+      return {
+        value: item?.scientificName,
+        label: item?.scientificName,
+      };
+    }) || [];
 
-    return (
-        <div
-            style={{
-                overflow: "visible",
-            }}
-        >
-            <h4>
-                <FormattedMessage id="FILTER_OBSERVATION_ATTRIBUTE" />
-            </h4>
-            <Description>
-                <FormattedMessage id="FILTER_OBSERVATION_ATTRIBUTE_DESC" />
-            </Description>
-
-            <FormGroupText
-                label="FILTER_SIGHTING_ID"
-                noDesc={true}
-                onChange={onChange}
-                term="match"
-                field="occurrenceId"
-                filterId={"occurrenceId"}
-                filterKey={"Sighting ID"}
-            />
-
-            <FormGroupText
-                label="FILTER_SIGHTING_COMMENTS"
-                noDesc={true}
-                onChange={onChange}
-                term="match"
-                field="sightingcomments"
-                filterId={"sightingcomments"}
-                filterKey={"Sighting Comments"}
-            />
-
-            <FormGroupText
-                label="FILTER_VISIBILIY_INDEX"
-                noDesc={true}
-                onChange={onChange}
-                term="match"
-                field="visibilityIndex"
-                filterId={"visibilityIndex"}
-                filterKey={"Visibility Index"}
-            />
-
-            <FormGroupText
-                label="FILTER_OBSERVATION_COMMENTS"
-                onChange={onChange}
-                term="match"
-                field="occurrenceRemarks"
-                filterId={"occurrenceRemarks"}
-                filterKey={"Observation Comments"}
-            />
-
-            <FormGroup className="mt-2">
-                <FormLabel>
-                    <FormattedMessage id="FILTER_MEASUREMENTS" />
-                </FormLabel>
-                <FormMeasurements
-                    data={measurementsOptions}
-                    onChange={onChange}
-                    filterId={"measurements"}
-                    field={"measurements"}
-                />
-            </FormGroup>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        overflow: "visible",
+      }}
+    >
+      <h4>
+        <FormattedMessage id="FILTER_OBSERVATION_ATTRIBUTE" />
+      </h4>
+      <Description>
+        <FormattedMessage id="FILTER_OBSERVATION_ATTRIBUTE_DESC" />
+      </Description>
+      <FormGroupMultiSelect
+        isMulti={true}
+        noDesc={true}
+        label="FILTER_INDIVIDUAL_SEX"
+        options={sexOptions}
+        onChange={onChange}
+        field="individualSex"
+        term="terms"
+        filterKey="individualSex"
+      />
+      <FormGroupMultiSelect
+        isMulti={true}
+        label="FILTER_INDIVIDUAL_TAXONOMY"
+        noDesc={true}
+        options={genusAndSpeciesOptions}
+        onChange={onChange}
+        field="individualtaxonomy"
+        term="terms"
+        filterId={"sightingsTaxonomy"}
+        filterKey={"Individual Taxonomy"}
+      />
+    </div>
+  );
 }
