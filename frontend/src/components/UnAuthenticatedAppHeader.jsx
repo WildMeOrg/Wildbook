0f55d80a6c0b7b7e3b2873ec51e6ad62e131b191
sufwan khalid <sufwan@esketchers.com>
2025-02-06 11:07:14 +0500

    test deployment

--- frontend/src/components/UnAuthenticatedAppHeader.jsx
+++ frontend/src/components/UnAuthenticatedAppHeader.jsx
@@ -54,7 +54,7 @@ export default function AuthenticatedAppHeader({ showAlert }) {
         style={{ marginLeft: "1rem" }}
       >
         <Logo />
-        {process.env.SITE_NAME}
+        {process.env.SITE_NAME}2
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse
