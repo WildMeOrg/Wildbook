b88fc4117c505b17e81f82ddcd0a16eda764b7d1
erinz2020 <erinz2020@gmail.com>
2025-02-06 23:03:31 +0000

    retrieve saved form values for text filters

--- frontend/src/components/Form/FormGroupText.jsx
+++ frontend/src/components/Form/FormGroupText.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FormGroup, FormLabel, FormControl } from "react-bootstrap";
 import Description from "./Description";
 import { FormattedMessage } from "react-intl";
 import { useIntl } from "react-intl";
+import { useSearchQueryParams } from "../../models/useSearchQueryParams";
+import { useStoredFormValue } from "../../models/useStoredFormValue";
 
 export default function FormGroupText({
   noLabel = false,
@@ -15,6 +17,16 @@ export default function FormGroupText({
   filterKey,
 }) {
   const intl = useIntl();
+  const [value, setValue] = React.useState("");
+  const paramsObject = useSearchQueryParams();
+  const resultValue = useStoredFormValue(filterId, term, field);
+
+  useEffect(() => {
+    if (paramsObject.searchQueryId && resultValue) {
+      setValue(resultValue);
+    }
+  }, [paramsObject, resultValue]);
+
   return (
     <FormGroup className="mt-2">
       {!noLabel && (
@@ -30,11 +42,13 @@ export default function FormGroupText({
       <FormControl
         type="text"
         placeholder={intl.formatMessage({ id: "TYPE_HERE" })}
+        value={value}
         onChange={(e) => {
           if (e.target.value === "") {
             onChange(null, field);
             return;
           }
+          setValue(e.target.value);
           onChange({
             filterId: filterId,
             clause: "filter",
