a693e5d044eb705ec0fbc60c3b1924d7bfa85c3d
erinz2020 <erinz2020@gmail.com>
2025-02-27 20:50:39 +0000

    test inputs, toggle password, links

---
+++ frontend/src/__tests__/pages/login/LoginPagePasswordToggle.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { fireEvent } from "@testing-library/react";
+import { renderWithProviders } from "../../../utils/utils";
+import LoginPage from "../../../pages/Login";
+import { screen, act } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+test("allows user to type username and password", async () => {
+  renderWithProviders(<LoginPage />);
+
+  const passwordInput = screen.getByPlaceholderText("Password");
+  const toggleButton = screen.getByTestId("password-toggle");
+
+  expect(passwordInput.type).toBe("password");
+
+  await act(async () => {
+    fireEvent.click(toggleButton);
+  });
+
+  expect(passwordInput.type).toBe("text");
+
+  await act(async () => {
+    fireEvent.click(toggleButton);
+  });
+  expect(passwordInput.type).toBe("password");
+});
