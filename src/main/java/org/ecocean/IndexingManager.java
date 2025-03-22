d48c9256e1d399a4970759021d2163410bfa137e
holmbergius <holmbergius@gmail.com>
2025-03-21 21:40:30 -0700

    Set num allowed threads for OpenSearch indexing

--- src/main/java/org/ecocean/IndexingManager.java
+++ src/main/java/org/ecocean/IndexingManager.java
@@ -4,16 +4,18 @@ package org.ecocean;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import org.ecocean.ShepherdProperties;
 
 
 
 public class IndexingManager {
 	
 	//The ExecutorService executes indexing jobs
-	private final ExecutorService executor = Executors.newFixedThreadPool(4);
-	
+	private final ExecutorService executor;
+		
 	//The indexingQueue is a List of Strings that represent the UUIDs of Base class-implementing objects 
 	//(Encounter, MarkedIndividual, Annotation, etc.) that need to be indexed or unindexed.
 	//The queue ensures that overzealous calls from the WildbookLifecycleListener do not cause
@@ -21,6 +23,21 @@ public class IndexingManager {
 	//from the queue once completed.
     private List<String> indexingQueue = Collections.synchronizedList(new ArrayList<String>());
   
+    public IndexingManager() {
+    	
+    	int numAllowedThreads = 4;
+    	Properties props = ShepherdProperties.getProperties("OpenSearch.properties", "", "context0");
+    	if(props!=null) {
+	    	String indexingNumAllowedThreads = props.getProperty("indexingNumAllowedThreads");
+	    	if(indexingNumAllowedThreads!=null) {
+	    		Integer allowThreads = Integer.getInteger(indexingNumAllowedThreads);
+	    		if(allowThreads!=null)numAllowedThreads = allowThreads.intValue();
+	    	}
+    	}
+    	executor = Executors.newFixedThreadPool(numAllowedThreads);
+    	
+    }
+    
     //Returns the indexing queue List of Strings
     public List<String> getIndexingQueue() { return indexingQueue; }
     
@@ -76,6 +93,10 @@ public class IndexingManager {
     public void resetIndexingQueuehWithInitialCapacity(int initialCapacity) {
     	indexingQueue = null;
     	indexingQueue = Collections.synchronizedList(new ArrayList<String>());
+    }
+    
+    public void shutdown() {
+    	if(executor!=null)executor.shutdown();
     }
 
 }
