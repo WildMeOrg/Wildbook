6727af7e1a90205922814665b0ff5d96dc020747
holmbergius <holmbergius@gmail.com>
2025-03-25 21:20:16 -0700

    Part 2: Integrate with bulk import

--- src/main/java/org/ecocean/servlet/importer/StandardImport.java
+++ src/main/java/org/ecocean/servlet/importer/StandardImport.java
@@ -2640,29 +2640,16 @@ public class StandardImport extends HttpServlet {
                 ArrayList<MediaAsset> assets = new ArrayList<MediaAsset>();
                 for (Encounter enc : allEncs) {
                     count++;
-
+                    itask.setStatus("Registering image assets for " + count + "/" + numEncs +
+                            " encounters.");
                     ArrayList<MediaAsset> theseAssets = enc.getMedia();
                     for (MediaAsset assy : theseAssets) {
                         if (!assy.hasAcmId()) {
-                            assets.add(assy);
+                        	IAGateway.addToAcmIdQueue(context, Integer.toString(assy.getId()));
                         }
                     }
-                    if (((assets.size() >= batchSize) && assets.size() > 0) || count == numEncs) {
-                        System.out.println("About to send " + assets.size() + " assets to IA! On " +
-                            count + "/" + numEncs);
-                        itask.setStatus("Registering image assets for " + count + "/" + numEncs +
-                            " encounters.");
-                        myShepherd.updateDBTransaction();
-                        IBEISIA.sendMediaAssetsNew(assets, context);
-                        assets = new ArrayList<MediaAsset>();
-                    }
                 }
-                if (assets.size() > 0) {
-                    itask.setStatus("Registering image assets for " + count + "/" + numEncs +
-                        " encounters.");
-                    myShepherd.updateDBTransaction();
-                    IBEISIA.sendMediaAssetsNew(assets, context);
-                }
+
             }
         } catch (Exception e) {
             myShepherd.rollbackDBTransaction();
