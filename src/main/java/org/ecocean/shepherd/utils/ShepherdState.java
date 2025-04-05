e735f8a3274f9ed303566ac39847e087b848dfd3
vkirkl <georgetownpolecat@gmail.com>
2025-04-05 10:36:20 -0700

    Some light commenting.

--- src/main/java/org/ecocean/shepherd/utils/ShepherdState.java
+++ src/main/java/org/ecocean/shepherd/utils/ShepherdState.java
@@ -2,8 +2,17 @@ package org.ecocean.shepherd.utils;
 
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * ShepherdState provides static utility methods to track the state and use of shepherd instances (database access).
+ *
+ * See, e.g., Shepherd transaction handlers, Shepherd.setAction(...), RestServlet.doGet(...), etc.
+ * A state dump can be accessed via dbconnections.jsp
+ *
+ * <p><strong>Note:</strong> This class is not intended to be instantiated.</p>
+ */
 public final class ShepherdState {
 
+    // Internal thread-safe static map to store the state.
     private static final ConcurrentHashMap<String, String> shepherds = new ConcurrentHashMap<>();
 
     private ShepherdState() {
