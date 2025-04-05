82936ebc3cc3cc7c6ea77951d64efca78ad06d43
vkirkl <georgetownpolecat@gmail.com>
2025-04-05 09:20:22 -0700

    Remove unnecessary null checks; prevent instantiation.

--- src/main/java/org/ecocean/shepherd/utils/ShepherdState.java
+++ src/main/java/org/ecocean/shepherd/utils/ShepherdState.java
@@ -2,28 +2,31 @@ package org.ecocean.shepherd.utils;
 
 import java.util.concurrent.ConcurrentHashMap;
 
-public class ShepherdState {
+public final class ShepherdState {
+    private ShepherdState() {
+        // Prevent instantiation
+    }
 
     private static ConcurrentHashMap<String, String> shepherds = new ConcurrentHashMap<String,
             String>();
 
     public static void setShepherdState(String shepherdID, String state) {
-        if (shepherds == null) shepherds = new ConcurrentHashMap<String, String>();
+        shepherds = new ConcurrentHashMap<String, String>();
         shepherds.put(shepherdID, state);
     }
 
     public static void removeShepherdState(String shepherdID) {
-        if (shepherds == null) shepherds = new ConcurrentHashMap<String, String>();
+        shepherds = new ConcurrentHashMap<String, String>();
         shepherds.remove(shepherdID);
     }
 
     public static String getShepherdState(String shepherdID) {
-        if (shepherds == null) shepherds = new ConcurrentHashMap<String, String>();
+        shepherds = new ConcurrentHashMap<String, String>();
         return shepherds.get(shepherdID);
     }
 
     public static ConcurrentHashMap<String, String> getAllShepherdStates() {
-        if (shepherds == null) shepherds = new ConcurrentHashMap<String, String>();
+        shepherds = new ConcurrentHashMap<String, String>();
         return shepherds;
     }
 }
