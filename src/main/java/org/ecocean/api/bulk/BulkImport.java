8015f48ad7b7520107c54cc7592677a677ac0838
Jon Van Oast <jon@wildme.org>
2025-04-17 15:40:37 -0600

    initial wip for backend/api

---
+++ src/main/java/org/ecocean/api/bulk/BulkImport.java
@@ -0,0 +1,6 @@
+// basic bulk import
+package org.ecocean.api.bulk;
+
+public class BulkImport {
+}
+
---
+++ src/main/java/org/ecocean/api/bulk/BulkImportUtil.java
@@ -0,0 +1,6 @@
+// basic bulk import utilities
+package org.ecocean.api.bulk;
+
+public class BulkImportUtil {
+}
+
