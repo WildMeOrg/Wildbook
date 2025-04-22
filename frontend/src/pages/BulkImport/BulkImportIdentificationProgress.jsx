c22984c1fffdc1866b89a79addcfdcf7cfa77056
erinz2020 <erinz2020@gmail.com>
2025-04-22 22:52:29 +0000

    basic structure part 3

---
+++ frontend/src/pages/BulkImport/BulkImportIdentificationProgress.jsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+import { observer } from "mobx-react-lite";
+
+export const BulkImportIdentificationProgress = observer(({ store }) => {
+
+    return (
+        <div>
+            <h2>Identification Progress</h2>
+        </div>
+    );
+})  
