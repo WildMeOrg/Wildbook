0ad93de50100fecf85205061664791221dce3e6b
Jon Van Oast <jon@wildme.org>
2025-04-22 17:10:54 -0600

    some basic utils; "master list" of fields (take 1 of N)

--- src/main/java/org/ecocean/api/bulk/BulkImportUtil.java
+++ src/main/java/org/ecocean/api/bulk/BulkImportUtil.java
@@ -1,6 +1,23 @@
 // basic bulk import utilities
 package org.ecocean.api.bulk;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.json.JSONObject;
+
 public class BulkImportUtil {
+
+	public static Map<String, BulkValidatorException> validateRow(JSONObject row) {
+		Map<String, BulkValidatorException> rtn = new HashMap<String, BulkValidatorException>();
+		if (row == null) return rtn;
+		for (String fieldName : row.keySet()) {
+			try {
+				new BulkValidator(fieldName, row.optString(fieldName, null));
+			} catch (BulkValidatorException bve) {
+				rtn.put(fieldName, bve);
+			}
+		}
+		return rtn;
+	}
 }
 
