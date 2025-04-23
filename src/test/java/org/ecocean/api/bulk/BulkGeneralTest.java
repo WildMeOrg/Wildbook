54be98dfda892c873c999eec89d73ca813ed7ff3
Jon Van Oast <jon@wildme.org>
2025-04-23 11:33:33 -0600

    initial basic tests for bulk validation

---
+++ src/test/java/org/ecocean/api/bulk/BulkGeneralTest.java
@@ -0,0 +1,46 @@
+package org.ecocean;
+
+import org.ecocean.api.bulk.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.Assert.*;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.when;
+import org.mockito.MockedConstruction;
+import org.mockito.MockedStatic;
+
+
+class BulkGeneralTest {
+
+    static String fieldNameValidEncounterYear = "Encounter.year";
+    static String fieldNameInvalid = "Fail.fubar";
+
+    @Test void basicValidation() throws BulkValidatorException {
+        BulkValidator bv = new BulkValidator(fieldNameValidEncounterYear, 2000);
+        assertNotNull(bv);
+        assertTrue(BulkValidator.isValidFieldName(fieldNameValidEncounterYear));
+        assertFalse(BulkValidator.isValidFieldName(fieldNameInvalid));
+
+        // some indexable fieldName tests
+        assertTrue(BulkValidator.isValidFieldName("Encounter.mediaAsset123"));
+        assertEquals(BulkValidator.indexPrefixValue("Encounter.mediaAsset123"), "Encounter.mediaAsset");
+        assertEquals(BulkValidator.indexIntValue("Encounter.mediaAsset123"), 123);
+        assertNull(BulkValidator.indexPrefixValue(fieldNameValidEncounterYear));
+        assertEquals(BulkValidator.indexIntValue(fieldNameValidEncounterYear), -1);
+        Exception ex = assertThrows(BulkValidatorException.class, () -> {
+            BulkValidator.indexIntValue(fieldNameInvalid);
+        });
+        assertTrue(ex.getMessage().contains("invalid fieldName"));
+        ex = assertThrows(BulkValidatorException.class, () -> {
+            BulkValidator.indexPrefixValue(fieldNameInvalid);
+        });
+        assertTrue(ex.getMessage().contains("invalid fieldName"));
+    }
+
+}
