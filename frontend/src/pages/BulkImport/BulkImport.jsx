d434fff848947436d20cf2bacc94d18b35259005
erinz2020 <erinz2020@gmail.com>
2025-05-14 21:15:43 +0000

    restore data from local storage

--- frontend/src/pages/BulkImport/BulkImport.jsx
+++ frontend/src/pages/BulkImport/BulkImport.jsx
@@ -1,5 +1,5 @@
 import { observer, useLocalObservable } from "mobx-react-lite";
-import React from "react";
+import React, { useEffect } from "react";
 import BulkImportStore from "./BulkImportStore";
 import { BulkImportImageUpload } from "./BulkImportImageUpload";
 import { Container } from "react-bootstrap";
@@ -10,22 +10,46 @@ import { BulkImportTableReview } from "./BulkImportTableReview";
 import { BulkImportIdentification } from "./BulkImportIdentification";
 import { BulkImportTask } from "./BulkImportTask";
 import { BulkImportSetLocation } from "./BulkImportSetLocation";
-import { v4 as uuidv4 } from "uuid";
-
+import { BulkImportContinueModal } from "./BulkImportContinueModal";
 const BulkImport = observer(() => {
   const store = useLocalObservable(() => new BulkImportStore());
+  const [savedSubmissionId, setSavedSubmissionId] = React.useState(null);
+
+  useEffect(() => {
+    const savedStore = JSON.parse(localStorage.getItem("BulkImportStore"));
+    const submissionId = savedStore?.submissionId;
+    console.log("submissionId", submissionId);
+    if (submissionId) {
+      setSavedSubmissionId(submissionId);
+      store.hydrate(savedStore);
+      store.setActiveStep(0);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (store.submissionId && (store.imagePreview.length > 0 || store.spreadsheetData.length > 0)) {
+      console.log("Saving store to localStorage", store.submissionId);
+      store.saveState();
+    }
+  }, [JSON.stringify(store.imagePreview), JSON.stringify(store.spreadsheetData)])
+
   return (
     <Container>
       <h1 className="mt-3">
         <FormattedMessage id="BULK_IMPORT" />
       </h1>
-      {!store._uploadFinished && <BulkImportUploadProgress store={store} />}
-      {store._activeStep === 0 && <BulkImportImageUpload store={store} />}
-      {store._activeStep === 1 && <BulkImportSpreadsheet store={store} />}
-      {store._activeStep === 2 && <BulkImportTableReview store={store} />}
-      {store._activeStep === 3 && (
+      {<BulkImportUploadProgress store={store} />}
+      {store.activeStep === 0 && <BulkImportImageUpload store={store} />}
+      {store.activeStep === 1 && <BulkImportSpreadsheet store={store} />}
+      {store.activeStep === 2 && <BulkImportTableReview store={store} />}
+      {store.activeStep === 3 && (
         <BulkImportSetLocation store={store} />
       )}
+      {
+        savedSubmissionId && (
+          <BulkImportContinueModal store={store} />
+        )
+      }
     </Container>
   );
 });
