5bde4cc128bd6aa18a48d14d316736a80c8493b7
holmbergius <holmbergius@gmail.com>
2025-06-04 14:42:22 -0700

    LOCAL: post-merge namespace fix

--- src/main/java/org/ecocean/servlet/export/ExportExcelFile.java
+++ src/main/java/org/ecocean/servlet/export/ExportExcelFile.java
@@ -14,6 +14,7 @@ import java.lang.reflect.Method;
 import jxl.write.*;
 import jxl.Workbook;
 import org.ecocean.shepherd.core.Shepherd;
+import org.ecocean.Util;
 
 public class ExportExcelFile extends HttpServlet {
     private static final int BYTES_DOWNLOAD = 1024;
