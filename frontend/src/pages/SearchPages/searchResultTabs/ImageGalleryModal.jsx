135f610998c40402dea8cb20d0596d1de1ca225f
erinz2020 <erinz2020@gmail.com>
2025-10-23 23:14:15 +0000

    ui updates

--- frontend/src/pages/SearchPages/searchResultTabs/ImageGalleryModal.jsx
+++ frontend/src/pages/SearchPages/searchResultTabs/ImageGalleryModal.jsx
@@ -200,9 +200,6 @@ export const ImageGalleryModal = observer(
                       <div
                         key={index}
                         className="position-absolute"
-                        // onClick={() => {
-                        //     imageStore.setSelectedAnnotationId(rect.annotationId);
-                        // }}
                         style={{
                           left: rect.x / scaleX,
                           top: rect.y / scaleY,
@@ -226,13 +223,32 @@ export const ImageGalleryModal = observer(
                             cursor: "pointer",
                             backgroundColor: "rgba(240, 11, 11, 0.5)",
                             padding: 0,
+                            width: "16px",
+                            height: "16px",
                           }}
                           onClick={() => {
-                            const url = `/rect/encounter?number=${rect.encounterId}`;
+                            if (!rect.encounterId) return;
+                            console.log(
+                              `Opening encounter ${JSON.stringify(rect.encounterId)}`,
+                            );
+                            const url = `/react/encounter?number=${rect.encounterId}`;
                             window.open(url, "_blank");
                           }}
                         >
-                          <i className="bi bi-box-arrow-right"></i>
+                          {rect.encounterId && (
+                            <svg
+                              width="16"
+                              height="16"
+                              viewBox="0 0 16 16"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                d="M13.8333 13.8333H2.16667V2.16667H8V0.5H2.16667C1.24167 0.5 0.5 1.25 0.5 2.16667V13.8333C0.5 14.75 1.24167 15.5 2.16667 15.5H13.8333C14.75 15.5 15.5 14.75 15.5 13.8333V8H13.8333V13.8333ZM9.66667 0.5V2.16667H12.6583L4.46667 10.3583L5.64167 11.5333L13.8333 3.34167V6.33333H15.5V0.5H9.66667Z"
+                                fill="white"
+                              />
+                            </svg>
+                          )}
                         </div>
                       </div>
                     ))}
