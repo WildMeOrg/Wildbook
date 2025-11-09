316d97dec8ef0c0ca6e61dbcb74892c932130235
holmbergius <holmbergius@gmail.com>
2025-11-08 17:16:21 -0800

    fix first pass name special character mangling in User account creation

--- src/main/java/org/ecocean/servlet/UserCreate.java
+++ src/main/java/org/ecocean/servlet/UserCreate.java
@@ -26,6 +26,7 @@ public class UserCreate extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
         doPost(request, response);
     }
 
