b2f2308b7869b04705d44af7e68453274bf9b71d
Jon Van Oast <jon@wildme.org>
2025-11-20 11:41:35 -0700

    bugfix for possible NPE

--- src/main/java/org/ecocean/security/Collaboration.java
+++ src/main/java/org/ecocean/security/Collaboration.java
@@ -279,6 +279,7 @@ public class Collaboration implements java.io.Serializable {
     }
 
     public static boolean canCollaborate(User u1, User u2, String context) {
+        if (u1 == null) return false; // seems right choice?
         if (u1.equals(u2)) return true;
         Collaboration c = collaborationBetweenUsers(u1, u2, context);
         if ((c == null) || (c.getState() == null)) return false;
@@ -288,6 +289,7 @@ public class Collaboration implements java.io.Serializable {
     }
 
     public static boolean canCollaborate(String context, String u1, String u2) {
+        // note: User.isUsernameAnonymous(null) returns true, which seems... sketchy here?
         if (User.isUsernameAnonymous(u1) || User.isUsernameAnonymous(u2)) return true;
         if (u1.equals(u2)) return true;
         Collaboration c = collaborationBetweenUsers(u1, u2, context);
