c65f8ab379baabef5648957f944b58c2e8bab1ee
erinz2020 <erinz2020@gmail.com>
2025-12-11 16:10:01 +0000

    functions to collect prospects

---
+++ frontend/src/pages/MatchResultsPage/helperFunctions.js
@@ -0,0 +1,54 @@
+const collectProspects = (node, type, result = []) => {
+  const hasMethod = !!node.method;
+  const methodName = node.method?.name ?? null;
+  const methodDescription = node.method?.description ?? null;
+
+  const prospects = node.matchResults?.prospects?.[type];
+
+  const hasResults = Array.isArray(prospects) && prospects.length > 0;
+
+  if (hasResults && hasMethod) {
+    const common = {
+      algorithm: methodName,
+      date: node.matchResults.created,
+      numberCandidates: node.matchResults.numberCandidates || 0,
+      queryEncounterId:
+        node.matchResults.queryAnnotation?.encounter?.id || null,
+      queryIndividualId:
+        node.matchResults.queryAnnotation?.individual?.id || null,
+      queryIndividualDisplayName:
+        node.matchResults.queryAnnotation?.individual?.displayName || null,
+      queryEncounterImageAsset:
+        node.matchResults.queryAnnotation?.asset || null,
+      queryEncounterImageUrl:
+        node.matchResults.queryAnnotation?.asset?.url || null,
+
+      methodName,
+      methodDescription,
+      method: node.method || null,
+      taskId: node.id ?? null,
+      taskStatus: node.status ?? null,
+      taskStatusOverall: node.statusOverall ?? null,
+
+      hasResults: true,
+    };
+
+    result.push(
+      ...prospects.map((item) => ({
+        ...item,
+        ...common,
+      })),
+    );
+  }
+
+  if (Array.isArray(node.children)) {
+    node.children.forEach((child) => collectProspects(child, type, result));
+  }
+
+  return result;
+};
+
+export const getAllIndiv = (node, result) =>
+  collectProspects(node, "indiv", result);
+export const getAllAnnot = (node, result) =>
+  collectProspects(node, "annot", result);
