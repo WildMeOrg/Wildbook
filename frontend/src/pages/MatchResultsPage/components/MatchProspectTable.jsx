8ded839fa0db1af42b46c79b870498d0d09ca4c0
erinz2020 <erinz2020@gmail.com>
2026-01-22 22:51:27 +0000

    handle empty filter

--- frontend/src/pages/MatchResultsPage/components/MatchProspectTable.jsx
+++ frontend/src/pages/MatchResultsPage/components/MatchProspectTable.jsx
@@ -371,6 +371,7 @@ const MatchProspectTable = ({
                             rowKey,
                             candidateEncounterId,
                             candidateIndividualId,
+                            candidateIndividualDisplayName,
                           )
                         }
                       />
